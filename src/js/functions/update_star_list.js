8c658097f20ba398d7e7690182f5ceba4c7567bc
Perlerin Vincent <vperlerin@gmail.com>
2019-05-30 09:42:09 +0200

    Tmp push for testing purpose

--- src/js/functions/update_star_list.js
+++ src/js/functions/update_star_list.js
@@ -1,4 +1,97 @@
-function update_cat_stars() {
+function update_stars_on_canvas_and_table(json_resp) {
+    var cat_stars = json_resp['close_stars'];     
+           
+    // Draw New Box
+    canvas.add(
+        new fabric.Rect({
+            fill: 'rgba(0,0,0,0)', 
+            strokeWidth: 1, 
+            stroke: 'rgba(230,230,230,.2)',  
+            left: json_resp['crop_box'][0] / 2, 
+            top: json_resp['crop_box'][1] / 2,
+            width: (json_resp['crop_box'][2] - json_resp['crop_box'][0]) / 2,
+            height: (json_resp['crop_box'][3] - json_resp['crop_box'][1]) / 2,
+            selectable: false
+         })
+    );
+
+    // Updating star table info 
+    // Residual Error
+    total_res_deg = (Math.round(json_resp['total_res_deg'] * 100) / 100);
+    total_res_px = (Math.round(json_resp['total_res_px'] *100) / 100);
+    $('#star_res_p').remove();
+    $('<p id="star_res_p" class="mt-2"><b>Residual Error:</b> '+  total_res_deg + '&deg; / ' + total_res_px + 'px.</p>').insertBefore('#stars-tab table');
+
+    // Add same text to image 
+    res_desc = "Res. Star Error: " + total_res_deg + " degrees / " + total_res_px + " px \nTotal stars: " + cat_stars.length;
+    canvas.add(new fabric.Text(res_desc , {
+        fontFamily: 'Arial',
+        fontSize: 12,
+        left: 5 ,
+        top: 5,
+        fill: 'rgba(255,255,255,.75)',
+        selectable: false
+    }));
+
+
+    var table_tbody_html = '';
+
+    // Table - tbody (in #stars-tab) & draw on canvas
+    
+    $.each(cat_stars,function(i,v) {
+
+        // Add to circle canvas
+        canvas.add(
+            new fabric.Circle({
+                radius: 5, fill: 'rgba(0,0,0,0)', strokeWidth: 1, stroke: 'rgba(100,200,200,.5)', 
+                left: (v[13] - 11)/2, 
+                top: (v[14] - 11)/2,
+                selectable: false
+        }));
+
+        // Add "+" on canvas
+        canvas.add(
+            new fabric.Text("+", {
+                fontFamily: 'Arial', 
+                fontSize: 12, 
+                left: ((v[7] - 11)/2)+4,   // +4 = shift text
+                top: ((v[8] - 11)/2) -4,    // -4 = shift text
+                fill:'rgba(255,0,0,.75)',
+                selectable: false
+        }));
+
+        // Add Star Name on canvas
+        canvas.add(new fabric.Text(v[0], {
+                fontFamily: 'Arial', 
+                fontSize: 12, 
+                left: (v[11] - 11)/2+5,
+                top: (v[12] - 11)/2+8,
+                fill:'rgba(255,255,255,.45)',
+                selectable: false
+        }));
+
+        // Add Rectangle
+        canvas.add(new fabric.Rect({
+            fill: 'rgba(0,0,0,0)', strokeWidth: 1, stroke: 'rgba(230,100,200,.5)', 
+            left:  (v[11] - 11)/2, 
+            top:(v[12] - 11)/2,
+            width: 10,
+            height: 10 ,
+            selectable: false
+         }));
+
+        // Add the corresponding row
+        // Name	mag	Cat RA/Dec	Res °	Res. Pixels
+        table_tbody_html+= '<tr><td>'+v[0]+'</td><td>'+v[1]+'</td><td>'+v[2]+'/'+v[3]+'</td><td>'+v[6]+'</td><td>'+v[15]+'</td></tr>';
+
+    });
+
+    // Replace current table content
+    $('#stars-tab tbody').html(table_tbody_html);
+}
+
+
+function update_star_list() {
     var cmd_data = {
         video_file:          main_vid,          // Defined on the page
         hd_stack_file:       hd_stack_file,     // Defined on the page
@@ -29,96 +122,7 @@ function update_cat_stars() {
         data: cmd_data,
         success: function(data) {
         
-            var json_resp = $.parseJSON(data);
-            var cat_stars = json_resp['close_stars'];     
-           
-            // Draw New Box
-            canvas.add(
-                new fabric.Rect({
-                    fill: 'rgba(0,0,0,0)', 
-                    strokeWidth: 1, 
-                    stroke: 'rgba(230,230,230,.2)',  
-                    left: json_resp['crop_box'][0] / 2, 
-                    top: json_resp['crop_box'][1] / 2,
-                    width: (json_resp['crop_box'][2] - json_resp['crop_box'][0]) / 2,
-                    height: (json_resp['crop_box'][3] - json_resp['crop_box'][1]) / 2,
-                    selectable: false
-                 })
-            );
-
-            // Updating star table info 
-            // Residual Error
-            total_res_deg = (Math.round(total_res_deg * 100) / 100);
-            total_res_px = (Math.round(total_res_px *100) / 100);
-            $('#star_res_p').remove();
-            $('<p id="star_res_p" class="mt-2"><b>Residual Error:</b> '+  total_res_deg + '&deg; / ' + total_res_px + 'px.</p>').insertBefore('#stars-tab table');
-
-            // Add same text to image 
-            res_desc = "Res. Star Error: " + total_res_deg + " degrees / " + total_res_px + " px \nTotal stars: " + cat_stars.length;
-            canvas.add(new fabric.Text(res_desc , {
-                fontFamily: 'Arial',
-                fontSize: 12,
-                left: 5 ,
-                top: 5,
-                fill: 'rgba(255,255,255,.75)',
-                selectable: false
-            }));
-
-
-            var table_tbody_html = '';
-
-            // Table - tbody (in #stars-tab) & draw on canvas
-            
-            $.each(cat_stars,function(i,v) {
-
-                // Add to circle canvas
-                canvas.add(
-                    new fabric.Circle({
-                        radius: 5, fill: 'rgba(0,0,0,0)', strokeWidth: 1, stroke: 'rgba(100,200,200,.5)', 
-                        left: (v[13] - 11)/2, 
-                        top: (v[14] - 11)/2,
-                        selectable: false
-                }));
-
-                // Add "+" on canvas
-                canvas.add(
-                    new fabric.Text("+", {
-                        fontFamily: 'Arial', 
-                        fontSize: 12, 
-                        left: ((v[7] - 11)/2)+4,   // +4 = shift text
-                        top: ((v[8] - 11)/2) -4,    // -4 = shift text
-                        fill:'rgba(255,0,0,.75)',
-                        selectable: false
-                }));
-
-                // Add Star Name on canvas
-                canvas.add(new fabric.Text(v[0], {
-                        fontFamily: 'Arial', 
-                        fontSize: 12, 
-                        left: (v[11] - 11)/2+5,
-                        top: (v[12] - 11)/2+8,
-                        fill:'rgba(255,255,255,.45)',
-                        selectable: false
-                }));
-
-                // Add Rectangle
-                canvas.add(new fabric.Rect({
-                    fill: 'rgba(0,0,0,0)', strokeWidth: 1, stroke: 'rgba(230,100,200,.5)', 
-                    left:  (v[11] - 11)/2, 
-                    top:(v[12] - 11)/2,
-                    width: 10,
-                    height: 10 ,
-                    selectable: false
-                 }));
-
-                // Add the corresponding row
-                // Name	mag	Cat RA/Dec	Res °	Res. Pixels
-                table_tbody_html+= '<tr><td>'+v[0]+'</td><td>'+v[1]+'</td><td>'+v[2]+'/'+v[3]+'</td><td>'+v[6]+'</td><td>'+v[15]+'</td></tr>';
-
-            });
-
-            // Replace current table content
-            $('#stars-tab tbody').html(table_tbody_html);
+            update_stars_on_canvas_and_table($.parseJSON(data));
 
             // Open proper tab
             $('#stars-tab-l').click();
@@ -135,7 +139,7 @@ function update_cat_stars() {
 $(function () {
 
     $('#update_stars').click(function() {
-        update_cat_stars();
+        update_star_list();
     });
 
 });
