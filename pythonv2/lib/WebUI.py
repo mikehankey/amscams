a1793c6b23f0f55c8776053b9f1633b187d45ca8
Perlerin Vincent <vperlerin@gmail.com>
2019-05-31 14:56:03 +0200

    Main Page

--- pythonv2/lib/WebUI.py
+++ pythonv2/lib/WebUI.py
@@ -155,10 +155,17 @@ def make_day_preview(day_dir, stats_data, json_conf):
          meteor_stack = "/mnt/ams2/blank.jpg"
 
       day=day.replace("_","")
-      html_out = html_out + "<figure><a href=\"webUI.py?cmd=browse_day&day=" + day_str + "&cams_id=" + cams_id \
-         + "\" onmouseover=\"document.getElementById(" + day + cams_id + ").src='" + meteor_stack \
-         + "'\" onmouseout=\"document.getElementById(" + day + cams_id + ").src='" + obj_stack + "'\">"
-      html_out = html_out + "<img style=\"border: 1px solid #ff9900; padding: 1px; margin: 5px\" id=\"" + day+ cams_id + "\" width='200' height='163' src='" + obj_stack + "'></a><br><figcaption>" + str(min_total) + " Minutes</figcaption></figure>"
+
+
+      html_out = "<div class='col-lg-2 col-md-3 preview'>"
+      html_out = html_out + "<a class='mtt' href='webUI.py?cmd=browse_day&day=" + day_file + "&cams_id="+cam_id+"' data-obj='"+obj_stack+"' title='Browse all day'>"
+      html_out = html_out + "<img alt='"+day_str+"' class='img-fluid ns lz' src='" + meteor_stack + "'>"
+      html_out = html_out + "<span>" + str(min_total) + " minutes</span></a></div>"     
+     
+      #html_out = html_out + "<figure><a href=\"webUI.py?cmd=browse_day&day=" + day_str + "&cams_id=" + cams_id \
+      #   + "\" onmouseover=\"document.getElementById(" + day + cams_id + ").src='" + meteor_stack \
+      #   + "'\" onmouseout=\"document.getElementById(" + day + cams_id + ").src='" + obj_stack + "'\">"
+      #html_out = html_out + "<img style=\"border: 1px solid #ff9900; padding: 1px; margin: 5px\" id=\"" + day+ cams_id + "\" width='200' height='163' src='" + obj_stack + "'></a><br><figcaption>" + str(min_total) + " Minutes</figcaption></figure>"
    return(html_out, day_str)
 
 def parse_jsid(jsid):
