7432d7770f51387f88dee4de146e945ced201fd9
Perlerin Vincent <vperlerin@gmail.com>
2019-06-03 18:38:54 +0200

    Gallery

--- src/js/functions/gallery_missing_images.js
+++ src/js/functions/gallery_missing_images.js
@@ -4,11 +4,12 @@ $(function() {
     if($('.gal-resize').length!=0) {
 
         $('.preview img').each(function() {
-            var url = $(this).attr('src');
+            var $img = $(this);
+            var url = $img.attr('src');
             $.get(url)
             .fail(function() { 
                 console.log('NOT FOUND ', url)
-                $(this).attr('src','./dist/img/proccessing.png')
+                $img.attr('src','./dist/img/proccessing.png')
             })
 
         });
