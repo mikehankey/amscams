97f3bbd9d0b42832d9cd6bbf32714a47868615af
Perlerin Vincent <vperlerin@gmail.com>
2019-06-04 10:20:02 +0200

    Media Gallery

--- src/js/functions/media_popup.js
+++ src/js/functions/media_popup.js
@@ -18,14 +18,16 @@ $(function() {
 
       $('.vid_link_gal').magnificPopup({
         type: 'iframe',
+        preloader: true,
         gallery:{
-          enabled:true,
-          preload: [0,2],     
-          navigateByImgClick: true,
-          arrowMarkup: '<button title="%title%" type="button" class="mfp-arrow mfp-arrow-%dir%"></button>', // markup of an arrow button
-          tPrev: 'Previous (Left arrow key)', // title for left button
-          tNext: 'Next (Right arrow key)', // title for right button
-          tCounter: '<span class="mfp-counter">%curr% of %total%</span>' // markup of counter
+          enabled:true
+        },
+        iframe: {
+          markup: '<div class="mfp-iframe-scaler">'+
+            '<div class="mfp-close"></div>'+
+            '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>TOTOTO'+
+          '</div>',
+           
         }
       });
 });
