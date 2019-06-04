e43c5e802b24bf14b15118126a0dd9210327079f
Perlerin Vincent <vperlerin@gmail.com>
2019-06-04 09:58:24 +0200

    Gal test

--- src/js/functions/media_popup.js
+++ src/js/functions/media_popup.js
@@ -17,7 +17,16 @@ $(function() {
       });
 
       $('.vid_link_gal').magnificPopup({
-        type: 'iframe' 
+        type: 'iframe',
+        gallery:{
+          enabled:true,
+          preload: [0,2],     
+          navigateByImgClick: true,
+          arrowMarkup: '<button title="%title%" type="button" class="mfp-arrow mfp-arrow-%dir%"></button>', // markup of an arrow button
+          tPrev: 'Previous (Left arrow key)', // title for left button
+          tNext: 'Next (Right arrow key)', // title for right button
+          tCounter: '<span class="mfp-counter">%curr% of %total%</span>' // markup of counter
+        }
       });
 });
 
