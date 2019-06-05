ee2c2ff159a7528bd38eb1a0cd8f20f05b26a572
Perlerin Vincent <vperlerin@gmail.com>
2019-06-05 15:57:27 +0200

    Frame Anim Fix

--- pythonv2/lib/WebUI.py
+++ pythonv2/lib/WebUI.py
@@ -903,6 +903,8 @@ def get_meteors(meteor_dir,meteors):
  
 def meteors_new(json_conf,form):  
 
+   cgitb.enable()
+
    limit_day = form.getvalue('limit_day')
    cur_page  = form.getvalue('p')
 
@@ -930,9 +932,6 @@ def meteors_new(json_conf,form):
          meteors = get_meteors(meteor_dir, meteors)
       elif limit_day == this_date:
          meteors = get_meteors(meteor_dir, meteors)
-
-         print(meteors)
-
          if(len(meteors)>=1):
             header_out = header_out + "<h1><span class='h'><span id='meteor_count'>"+format(len(meteors))+"</span> meteors</span> captured on"
             header_out = header_out + "<div class='input-group date datepicker' data-display-format='YYYY/MM/DD' data-action='reload' data-url-param='limit_day' data-send-format='YYYY_MM_DD'>"
