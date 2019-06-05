a149d63f08c1b7eb51bbb41c1dc526858d31d7e2
Perlerin Vincent <vperlerin@gmail.com>
2019-06-05 17:06:09 +0200

    Examing

--- src/js/functions/delete_frame.js
+++ src/js/functions/delete_frame.js
@@ -10,7 +10,7 @@
         var d = id.split('_');
  
         $.ajax({ 
-            url:  "/pycgi/webUI.py?cmd=del_frame&meteor_json_file=" + meteor_json_file + "&fn=" + d[],
+            url:  "/pycgi/webUI.py?cmd=del_frame&meteor_json_file=" + meteor_json_file + "&fn=" + d[1],
             success: function(response) {
                 $row.fadeOut(150, function() {$row.remove();})
             } 
