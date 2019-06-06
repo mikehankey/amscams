d1afe0fafed0d133e85fd159585d511b5b32d08f
Perlerin Vincent <vperlerin@gmail.com>
2019-06-06 13:05:18 +0200

    Up

--- src/js/functions/select_meteor.js
+++ src/js/functions/select_meteor.js
@@ -3,35 +3,29 @@ function meteor_select(dir,all_frames_ids) {
     var next_id;
     var cur_id = parseInt($('#sel_frame_id').text());
     var cur_index = all_frames_ids.indexOf(cur_id);
+     
 
-    console.log('CUR ID' + cur_id);
-    console.log('DIR ' +  dir)
-    console.log('ALL FRAMES ' + all_frames_ids)
-    console.log('CUR INDEX ' + cur_index)
- 
     if(dir=="prev") {
-        if(all_frames_ids.indexOf(cur_id)==0) {
-            next_id = all_frames_ids[all_frames_ids.length-1];
+        if(cur_index==0) {
+            next_id = all_frames_ids.length-1;
         } else {
-            next_id = all_frames_ids.indexOf(cur_id)-1;
+            next_id = cur_index - 1;
         }
     } else {
-         if(all_frames_ids.indexOf(cur_id)==all_frames_ids.length) {
-            next_id = all_frames_ids[0];
+        if(cur_index==all_frames_ids.length-1) {
+            next_id = 0;
         } else {
-            next_id = all_frames_ids.indexOf(cur_id)+1;
+            next_id = cur_index + 1;
         }
-    }
-
-    console.log('NEXT ID ' + next_id);
-    $('#reduc-tab table tbody tr#fr_' + next_id + " .select_meteor").click();
+    } 
+    $('#reduc-tab table tbody tr#fr_' + all_frames_ids[next_id] + " .select_meteor").click();
 }
 
 
 // Modal for selector
 function addModalTemplate() {
     if($('#select_meteor_modal').length==0) {
-        $('<div id="select_meteor_modal" class="modal fade" tabindex="-1"><input type="hidden" name="thumb_w"/><input type="hidden" name="thumb_h"/><div class="modal-dialog  modal-lg modal-dialog-centered" role="document"><div class="modal-content"><div class="modal-body"><button id="met-sel-next" title="Next" type="button" class="mfp-arrow mfp-arrow-right mfp-prevent-close"></button><button id="met-sel-prev" title="Prev" type="button" class="mfp-arrow mfp-arrow-left mfp-prevent-close"></button><p><strong>FRAME #<span id="sel_frame_id"></span> - Click the center of the meteor.</strong> <span id="meteor_org_pos" class="float-right pl-3"><b>Org:</b></span> <span id="meteor_pos" class="float-right"></span></p><div class="meteor_chooser"><div id="org_lh"></div><div id="org_lv"></div><div id="lh"></div><div id="lv"></div></div></div><div class="modal-footer p-0 pb-2 pr-2"><button type="button" class="btn btn-primary" id="Save Meteor Center">Save</button><button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button></div></div></div></div>').appendTo('body');
+        $('<div id="select_meteor_modal" class="modal fade" tabindex="-1"><input type="hidden" name="thumb_w"/><input type="hidden" name="thumb_h"/><div class="modal-dialog  modal-lg modal-dialog-centered" role="document"><div class="modal-content"><div class="modal-body"><button id="met-sel-next" title="Next" type="button" class="mfp-arrow mfp-arrow-right mfp-prevent-close"></button><button id="met-sel-prev" title="Prev" type="button" class="mfp-arrow mfp-arrow-left mfp-prevent-close"></button><p><strong>FRAME #<span id="sel_frame_id"></span> - Click the center of the meteor.</strong> <span id="meteor_org_pos" class="float-right pl-3"><b>Org:</b></span> <span id="meteor_pos" class="float-right"></span></p><div style="box-shadow: 0 0px 8px rgba(0,0,0,.6);" class="meteor_chooser"><div id="org_lh"></div><div id="org_lv"></div><div id="lh"></div><div id="lv"></div></div></div><div class="modal-footer p-0 pb-2 pr-2"><button type="button" class="btn btn-primary" id="Save Meteor Center">Save</button><button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button></div></div></div></div>').appendTo('body');
     }
 }
 
