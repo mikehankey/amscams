c1f04c6be653762ebdd559030681584fb9c19abe
Perlerin Vincent <vperlerin@gmail.com>
2019-06-06 16:59:13 +0200

    Up

---
+++ src/js/functions/find_stars.js
@@ -0,0 +1,56 @@
+
+// FROM hd_stack_file 
+function find_stars() {
+	var cmd_data = {
+		cmd: 'find_stars_ajax',
+		stack_file: hd_stack_file // Defined on the page
+	}
+
+	loading({text: "Automatically Finding Stars",overlay:true});
+
+	$.ajax({ 
+        url:  "/pycgi/webUI.py",
+        data: cmd_data,
+        success: function(data) {
+        	try {
+		  		var json_resp = $.parseJSON(data);
+         		var auto_stars = json_resp['stars'];
+
+         		for (let s in auto_stars) {
+		            x = auto_stars[s][0] / 2 
+		            y = auto_stars[s][1] / 2
+		            var circle = new fabric.Circle({
+		               radius: 5, fill: 'rgba(0,0,0,0)', strokeWidth: 1, stroke: 'rgba(255,255,255,.5)', left: x-5, top: y-5,
+		               selectable: false
+		            });
+		            canvas.add(circle);
+		         }
+			}
+			catch(err) {
+		  		bootbox.alert({
+	                message: "The process returned an error - " + err,
+	                className: 'rubberBand animated error',
+	                centerVertical: true
+            	});
+			}
+
+            
+            loading_done();
+        }, 
+        error:function() {
+            bootbox.alert({
+                message: "The process returned an error",
+                className: 'rubberBand animated error',
+                centerVertical: true
+            });
+            loading_done();
+        }
+    });
+}
+
+
+$(function() {
+	$('.auto_detect_stars').click(function() {
+		find_stars();
+	})
+})
