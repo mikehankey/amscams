19be8174b2720e3b7bc81287780cf5eb6b3e877f
Perlerin Vincent <vperlerin@gmail.com>
2019-06-11 10:36:43 +0200

    Up

--- pythonv2/lib/WebUI.py
+++ pythonv2/lib/WebUI.py
@@ -769,7 +769,7 @@ def hd_cal_index(json_conf, form):
                   color = ""
                   
                print('<div class="preview p-2"><a href="'+detail_link+'" class="mttt">')
-               print('<img data-src="'+tn+'" class="ns lz" width="200" height="112"/>');
+               print('<img data-src="'+tn+'" class="ns lz-shown" width="200" height="112"/>');
                print('</a><span class="det" '+color+'><b>' + str(ts) + "</b> stars - <b>" + str(trp)[0:5] + "</b>Rpx - <b>" +  str(trd)[0:5] + "</b>Rd</span></div>")
 
              
--- src/js/ui/lazy_load.js
+++ src/js/ui/lazy_load.js
@@ -4,6 +4,9 @@ $(function() {
         visibleOnly: true,
         onError: function(element) {
             console.log('error loading ' + element.data('src'));
-        }
+        },
+        afterLoad: function(element) {
+            console.log('LOADED: ' + element.data('src'));
+        },
     });
 });
