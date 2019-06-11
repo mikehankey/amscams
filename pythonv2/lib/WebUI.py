d2ab1b777076b34c5568188b74f330eddc8ae3aa
Perlerin Vincent <vperlerin@gmail.com>
2019-06-11 14:45:03 +0200

    Up

--- pythonv2/lib/WebUI.py
+++ pythonv2/lib/WebUI.py
@@ -1141,10 +1141,15 @@ def live_view(json_conf):
 
       img = "/mnt/ams2/latest/" + cams_id + ".jpg?" + str(rand)
 
-      print('<div class="preview col-lg-2"><a class="mtt" href="'+img+'" title="Live View">')
-      print('<img alt="'+cam_id+'" class="img-fluid ns lz img-link-gal" src="'+img+'"><span><b>Cam '+cams_id+'</span></b></a></div>')
+      print('<div class="preview col-lg-2 col-md-3 "><a class="mtt" href="'+img+'" title="Live View">')
+      print('<img alt="'+cams_id+'" class="img-fluid ns lz img-link-gal" src="'+img+'"><span><b>Cam '+cams_id+'</span></b></a></div>')
 
-    print("</div></div></div>") 
+      #print ("<div style=\"padding: 5px; border: 1px ffffff solid; float:left\" ><figure><img src=/mnt/ams2/latest/" + cams_id + ".jpg?" + str(rand) + " width=640 height=360><figcaption>" + cams_id + "</figcaption></figure></div>")
+   print("</div></div></div>")
+   #print ("</div>")
+   #print ("</div>")
+   #print ("</div>")
+   #print("<div style=\"clear: both\"></div>")
 
 def as6_config(json_conf):
    print("AS6 Config")
