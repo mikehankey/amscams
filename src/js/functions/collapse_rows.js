d27d6484800dc9195c17cdcf8a63a571e9f7fb6c
Perlerin Vincent <vperlerin@gmail.com>
2019-06-11 11:59:37 +0200

    M

--- src/js/functions/collapse_rows.js
+++ src/js/functions/collapse_rows.js
@@ -12,8 +12,7 @@ $(function() {
             // Build Gallery
             $.each(all_cal_details[id.substr(1)], function(i,v){
 
-                $('<div class="preview p-2"><a href="'+v.lk+'" class="mttt"><img src="'+v.src+'" class="ns" width="200" height="112"/> \\
-                </a><span class="det" '+v.col+'><b>' + v.st + "</b> stars - <b>" + v.trp + "</b>Rpx - <b>" + .trd + "</b>Rd</span></div>").appendTo($gal);
+                $('<div class="preview p-2"><a href="'+v.lk+'" class="mttt"><img src="'+v.src+'" class="lz ns" width="200" height="112"/></a><span class="det" '+v.col+'><b>' + v.st + "</b> stars - <b>" + v.trp + "</b>Rpx - <b>" + v.trd + "</b>Rd</span></div>").appendTo($gal);
 
 
             });
