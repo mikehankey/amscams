e76efd0fd4a13b81e370df42861d98793d976355
Perlerin Vincent <vperlerin@gmail.com>
2019-06-11 18:00:00 +0200

    L

--- src/js/functions/upscale_to_hd.js
+++ src/js/functions/upscale_to_hd.js
@@ -1,4 +1,7 @@
-function upscale_HD(img_url) {
+
+function upscale_to_HD() {
+    loading({text:"Upscaling to HD...", overlay:true});
+
     var point_str = ""
     for (i in user_stars) {
        point_str = point_str + user_stars[i].toString()  + "|"
@@ -12,17 +15,29 @@ function upscale_HD(img_url) {
        if (objects[i].get('type') == "circle") {
        point_str = point_str + x.toString() + "," + y.toString() + "|"
        }
-    }
+    } 
 
-    ajax_url = "/pycgi/webUI.py?cmd=upscale_2HD&hd_stack_file=" + hd_stack_file + "&points=" + point_str
-    console.log(ajax_url)
-    $.get(ajax_url, function(data) {
-       $(".result").html(data);
-       var json_resp = $.parseJSON(data);
-       var new_img = json_resp['hd_stack_file'] 
-       var new_url = "webUI.py?cmd=free_cal&input_file=" + new_img
-       alert("Upscale Complete!")
-       window.location.replace(new_url);
-
+    $.ajax({ 
+          url:  "webUI.py?cmd=upscale_2HD",
+          data: {
+            hd_stack_file: hd_stack_file,
+            points:point_str
+          },
+          success: function(data) {
+                loading({text:"Upscaling COMPLETE", overlay:true});
+                setTimeout(function() {
+                    var json_resp = $.parseJSON(data);
+                    var new_img = json_resp['hd_stack_file'] 
+                    var new_url = "webUI.py?cmd=free_cal&input_file=" + new_img 
+                    window.location.replace(new_url);
+                    loading_done();
+                    },1000)
+          }, 
+          error: function() {
+                alert('Impossible to upscale. Please try again later')
+                loading_done();
+          }
     });
- }
+}
+
+ 
