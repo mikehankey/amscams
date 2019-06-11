9768f32a01160c6cef76c8dda0b444062dfaeaac
Perlerin Vincent <vperlerin@gmail.com>
2019-06-11 13:36:49 +0200

    L

--- pythonv2/lib/WebCalib.py
+++ pythonv2/lib/WebCalib.py
@@ -3874,18 +3874,18 @@ def sd_pic_stars(json_conf,form):
 
 
    js_html = """
-
    <script>
       var my_image = '""" + half_stack_file + """'
       var hd_stack_file = '""" + stack_file + """'
       var stars = []
    </script>
-
-
    """.format(stack_file)
+
+   canvas_html = '<div id="main_container" class="container-fluid d-flex h-100 mt-4 position-relative">'
+   canvas_html = canvas_html + '<div class="alert alert-info">An HD source file was not found for this time period. No worries, we can still calibrate from an SD image, but first we need to pick the stars so we can upscale the image.<br/><b>Select as many stars as possible from the image below and then click the "Upscale To HD" button.</b></div><div class="h-100 flex-fixed-canvas">'
+
    canvas_html = """
-      <p>An HD source file was not found for this time period. No worries, we can still calibrate from an SD image, but first we need to pick the stars so we can upscale the image. Select as many stars as possible from the image below and then click the "Upscale To HD" button.</p>
-      <div style="float:left"><canvas id="c" width="960" height="540" style="border:2px solid #000000;"></canvas></div>
+      <div class="canvas-container"><canvas id="c" width="960" height="540"></canvas></div>
       <div style="float:left"><div style="position: relative; height: 50px; width: 50px" id="myresult" class="img-zoom-result"> </div></div>
       <div style="clear: both"></div>
    """
