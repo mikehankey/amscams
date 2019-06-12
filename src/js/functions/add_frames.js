d4020f823f314737118224c8894f145cbf481dd8
Perlerin Vincent <vperlerin@gmail.com>
2019-06-12 16:14:46 +0200

    M

--- src/js/functions/add_frames.js
+++ src/js/functions/add_frames.js
@@ -62,3 +62,44 @@ $(function() {
         add_frames();
     });
 })
+
+/********************************************** */
+
+
+
+function add_a_frame(cur_fn) {
+    loading({text: "Generating Frame #"+ cur_fn,overlay:true});
+    var cmd_data = {
+		cmd: 'add_frame',
+        sd_video_file: sd_video_file, // Defined on the page
+        fn: cur_fn
+    };
+
+    $.ajax({ 
+        url:  "/pycgi/WebUI.py",
+        data: cmd_data, 
+        success: function(data) { 
+            loading_done();
+            
+            bootbox.alert({
+                message: "FRAME CREATED " + data,
+                className: 'rubberBand animated',
+                centerVertical: true 
+            });
+            
+        }, 
+        error:function() {
+            bootbox.alert({
+                message: "The process returned an error",
+                className: 'rubberBand animated error',
+                centerVertical: true 
+            });
+        }
+    });
+}
+
+$(function() {
+    $('.btn-mm .btn-pp').click(function() {
+        add_a_frame($(this).attr('data-rel'));
+    }); 
+})
