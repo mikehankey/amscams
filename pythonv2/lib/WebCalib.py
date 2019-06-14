813c5da77c5337d388be0f6e1889ddb1a492523f
Perlerin Vincent <vperlerin@gmail.com>
2019-06-14 14:24:44 +0200

    M

--- pythonv2/lib/WebCalib.py
+++ pythonv2/lib/WebCalib.py
@@ -4224,15 +4224,17 @@ def free_cal(json_conf,form):
    #print(stack_file)
 
 
-   list_of_buttons =  """
-         <input type=button class="btn btn-primary" value="Show Image" onclick="javascript:show_image('""" + half_stack_file + """',1,1)">
-         <input type=button class="btn btn-primary" value="Find Stars" onclick="javascript:find_stars('""" + stack_file + """')">
-         <input type=button class="btn btn-primary" value="Make Plate" onclick="javascript:make_plate('""" + stack_file + """')">
-         <input type=button class="btn btn-primary" value="Solve Field" onclick="javascript:solve_field('""" + stack_file + """')">
-         <input type=button class="btn btn-primary" value="Show Catalog Stars" onclick="javascript:show_cat_stars('""" + stack_file + "','" + "" + """', 'pick')">
-         <input type=button class="btn btn-primary" value="Fit Field" onclick="javascript:fit_field('""" + stack_file + """')">
-         <input type=button class="btn btn-primary" value="AZ Grid" onclick="javascript:az_grid('""" + az_grid_blend + """')">
-         <input type=button class="btn btn-primary" value="Delete Calibration" onclick="javascript:delete_cal('""" + stack_file + """')">"""
+   list_of_buttons =   '<input type=button class="btn btn-primary" value="Show Image" onclick="javascript:show_image("' + half_stack_file + '",1,1)">'
+   list_of_buttons +=  '<input type=button class="btn btn-primary" value="Find Stars" onclick="javascript:find_stars("' + stack_file + '")">'
+   list_of_buttons +=  '<input type=button class="btn btn-primary" value="Make Plate" onclick="javascript:make_plate("' + stack_file + '")">'
+   list_of_buttons +=  '<input type=button class="btn btn-primary" value="Solve Field" onclick="javascript:solve_field("' + stack_file + '")">'
+   list_of_buttons +=  '<input type=button class="btn btn-primary" value="Show Catalog Stars" onclick="javascript:show_cat_stars("' + stack_file + '")">'
+
+         
+         <input type=button class="btn btn-primary" value="Show Catalog Stars" onclick="javascript:show_cat_stars(' + stack_file + "','" + "" + ', 'pick')">
+         <input type=button class="btn btn-primary" value="Fit Field" onclick="javascript:fit_field(' + stack_file + ')">
+         <input type=button class="btn btn-primary" value="AZ Grid" onclick="javascript:az_grid(' + az_grid_blend + ')">
+         <input type=button class="btn btn-primary" value="Delete Calibration" onclick="javascript:delete_cal(' + stack_file + ')">
    template.replace("{%BUTTONS%}", list_of_buttons)
    print(template)
    print(canvas_html)
