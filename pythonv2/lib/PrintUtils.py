04fc4c0fb79d76aaa6aa9efda5f8346a4c48344e
Perlerin Vincent <vperlerin@gmail.com>
2019-06-24 11:45:12 +0200

    Select Frame row

--- pythonv2/lib/PrintUtils.py
+++ pythonv2/lib/PrintUtils.py
@@ -5,7 +5,7 @@ def get_meteor_date(file):
 	return fn[0] + "/" + fn[1] + "/" + fn[2] + " " + fn[3] + ":" + fn[4] + ":" + fn[5]
 
 
-def get_date_from_file(file)
+def get_date_from_file(file):
 	fn = file.split("/")[-1] 
 	fn = fn.split('_',3)
 	return fn[0] + "/" + fn[1] + "/" + fn[2]
