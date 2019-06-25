63f833af7be2f5a28a1317e373664164cb395644
Perlerin Vincent <vperlerin@gmail.com>
2019-06-25 17:14:30 +0200

    Cok

--- pythonv2/lib/PwdProtect.py
+++ pythonv2/lib/PwdProtect.py
@@ -18,7 +18,8 @@ def encode(key, string):
     encoded_chars = []
     for i in range(len(string)):
         key_c = key[i % len(key)]
-        encoded_c = chr(ord(string[i]) + ord(key_c) % 256)
+        if(string[i] is not None):
+            encoded_c = chr(ord(string[i]) + ord(key_c) % 256)
         encoded_chars.append(encoded_c)
     encoded_string = ''.join(encoded_chars)
     return encoded_string
