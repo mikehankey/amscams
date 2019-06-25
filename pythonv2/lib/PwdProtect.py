c477ff6b0e244865977e62e8c18266a3d23e4355
Perlerin Vincent <vperlerin@gmail.com>
2019-06-25 14:36:03 +0200

    Cok

--- pythonv2/lib/PwdProtect.py
+++ pythonv2/lib/PwdProtect.py
@@ -5,14 +5,14 @@ import cgitb
 def setup_pwd(domain,user,pwd):
     cgitb.enable()
     expiration = datetime.datetime.now() + datetime.timedelta(days=0.5)
+    
+    print ("Content-type:text/html\r\n")
     print ("Set-Cookie:User = "+user+";\r\n")
     print ("Set-Cookie:Password = "+pwd+"\r\n")
     print ("Set-Cookie:Expires = "+expiration.strftime("%a, %d-%b-%Y %H:%M:%S GMT")+";\r\n")
     print ("Set-Cookie:Domain = lovable-falcon-4326.dataplicity.io;\r\n")
     print ("Set-Cookie:Path = /;\n")
-    print ("Content-type:text/html\r\n\r\n")
-
-  
+ 
     
     #print "Set-Cookie:UserID = XYZ;\r\n"
     #print "Set-Cookie:Password = XYZ123;\r\n"
