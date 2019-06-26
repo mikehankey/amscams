b3ba0f19cb06f0a7ed8b26727deb675452a28079
Perlerin Vincent <vperlerin@gmail.com>
2019-06-26 11:32:33 +0200

    Co

--- pythonv2/lib/WebUI.py
+++ pythonv2/lib/WebUI.py
@@ -612,11 +612,12 @@ def hd_cal_detail(json_conf, form):
    return(js_html)
 
 def meteor_index(json_conf, form):
-   print("<h1>Meteor Index</h1>")
+
    cam_id = form.getvalue("cam_id")
    day_limit = form.getvalue("day")
    mi = load_json_file("/mnt/ams2/cal/hd_images/meteor_index.json")
-    
+
+   print("<h1>Meteor Index</h1>")
    print("<table class='table table-dark table-striped table-hover td-al-m m-auto table-fit'>")
    print("<thead><tr><th>&nbsp;</th><th>Meteor</th><th>Reduced</th><th>Multi-Station</th><th>AZ/EL FOV</th><th>Pos Ang</th><th>Pixscale</th><th>Stars</th><th>Res Px</th><th>Res Deg</th><th>Dur</th><th>Ang Sep</th><th>Mag</th><th>Seg Res</td><th>Missing Frames</th></tr></thead>")
    print("<tbody>")
@@ -628,10 +629,10 @@ def meteor_index(json_conf, form):
    meteor_end = meteor_start + NUMBER_OF_METEOR_PER_PAGE
    all_meteors = smi
 
-   smi = smi[meteor_start:meteor_end]
+   dsmi = smi[meteor_start:meteor_end]
 
-   for day in smi:
-      for meteor_file in mi[day]:
+   for day in dsmi:
+      for meteor_file in dsmi[day]:
          hd_datetime, hd_cam, hd_date, hd_y, hd_m, hd_d, hd_h, hd_M, hd_s = convert_filename_to_date_cam(meteor_file)
          if cam_id is None:
             show = 1
