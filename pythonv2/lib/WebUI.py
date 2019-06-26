bca17e0833e2747a2dbec52cbbb124008a77c0df
Perlerin Vincent <vperlerin@gmail.com>
2019-06-26 16:04:58 +0200

    Co

--- pythonv2/lib/WebUI.py
+++ pythonv2/lib/WebUI.py
@@ -629,6 +629,9 @@ def meteor_index(json_conf, form):
    meteors = {}
    day_defined = 0
 
+
+   day_limit = "2019_05_26"
+
    #Remove not needed
    if(day_limit is not None):
       day_defined = 1   
