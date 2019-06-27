0cf7cc9e40aaf401daa207b51c7c618c5b53e5ee
Perlerin Vincent <vperlerin@gmail.com>
2019-06-27 16:38:36 +0200

    Co

--- src/js/functions/delete_detection_gallery.js
+++ src/js/functions/delete_detection_gallery.js
@@ -1,9 +1,25 @@
-$('.sel-box input[type=checkbox]').change(function() {
-    var $t = $(this), f = $t.attr('for'), id = f.substr(5,f.length);
-    if($t.is(':checked')) {
-        $('#'+id).addClass('selected');
-    } else {
-        $('#'+id).removeClass('selected');
-    }
- 
- });
+// Update counter
+function update_selected_counter() {
+    $('#sel-ctn').text($('.preview.selected').length);
+}
+
+
+$(function() {
+
+    // Select one
+    $('.sel-box input[type=checkbox]').change(function() {
+        var $t = $(this), f = $t.attr('id'), id = f.substr(5,f.length);
+        if($t.is(':checked')) {
+            $('#'+id).addClass('selected');
+        } else {
+            $('#'+id).removeClass('selected');
+        }
+         update_selected_counter();
+     });
+    
+     // Select All
+     $('#sel-all').click(function() {
+        $('.sel-box input[type=checkbox]').click();
+        update_selected_counter();
+     })
+})
