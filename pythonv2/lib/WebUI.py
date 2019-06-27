7e97e1cc4a5d24fe35121d859f3b060ad3be7266
Perlerin Vincent <vperlerin@gmail.com>
2019-06-27 15:06:06 +0200

    Co

--- pythonv2/lib/WebUI.py
+++ pythonv2/lib/WebUI.py
@@ -1152,7 +1152,7 @@ def meteors_new(json_conf,form):
             html_out = html_out + "<a class='mtt' href='webUI.py?cmd=reduce&video_file=" + video_file + "' data-obj='"+stack_obj_img+"' title='Go to Info Page'>"
             html_out = html_out + "<img alt='"+desc+"' class='img-fluid ns lz' src='" + stack_file_tn + "'>"
             html_out = html_out + "<span>" + desc + "</span></a>"  
-            html_out = html_out + "<div class='details'><div class='list-onl'><span>" + desc + "</span>"
+            html_out = html_out + "<div class='details'><div class='list-onl'><span>" + desc + "</span></div>"
             html_out = html_out + "<div class='btn-toolbar'><div class='btn-group'>"
             html_out = html_out + "<a class='vid_link_gal col btn btn-primary btn-sm' title='Play Video' href='./video_player.html?video=" + video_file + "&vid_id="+del_id+"'><i class='icon-play'></i></a>"
             html_out = html_out + "<a class='delete_meteor_gallery col btn btn-danger btn-sm' title='Delete Detection' data-meteor='" + del_id + "'><i class='icon-delete'></i></a>"
