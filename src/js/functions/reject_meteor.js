e3b59bc7a70d2200b0b908c1aebede98927e8998
Perlerin Vincent <vperlerin@gmail.com>
2019-06-28 09:56:53 +0200

    Del

--- src/js/functions/reject_meteor.js
+++ src/js/functions/reject_meteor.js
@@ -1,16 +1,22 @@
+function meteor_is_deleted(id) {
+      $('#'+id).css('opacity',.5).removeClass('norm meteor reduced').addClass('del').find('.btn-toolbar').remove().end().find('a').removeAttr('href').removeAttr('title').attr('title','DELETED').unbind('mouseover').unbind('mouseout').end().find(".custom-checkbox").remove();                  
+
+}
+
+
 function reject_meteor(id) {
-      loading({text:"Deleting"});
+      loading({text:"Deleting", container:$("#"+id)});
       $.ajax({ 
             url:  "webUI.py?cmd=override_detect",
             data: {jsid: id},
             success: function(data) {
                   loading_done();
-                  $('#'+id).css('opacity',.5).removeClass('norm meteor reduced').addClass('del').find('.btn-toolbar').remove().end().find('a').removeAttr('src').removeAttr('title').attr('title','DELETED').unbind('mouseover').unbind('mouseout');                  
+                  meteor_is_deleted(id);
                   // Debug
-                  console.log(data);
+                  //console.log(data);
             }, 
             error: function() {
-                  alert('Impossible to reject. Please try again later')
+                  alert('Impossible to reject. Please, reload the page and try again later.')
                   loading_done();
             }
       });
