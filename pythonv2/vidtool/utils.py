2f00c6551986775d8fdc3e63b5da28dcf072f947
Perlerin Vincent <vperlerin@gmail.com>
2019-07-01 10:40:35 +0200

    M

--- pythonv2/vidtool/utils.py
+++ pythonv2/vidtool/utils.py
@@ -9,9 +9,9 @@ SD_PATH='/mnt/ams2/SD/proc2/'
 #Ouput: list of sd frames found for this date
 def get_sd_frames(camID,date):
     #ex:camID:010035, date:2019_05_26
-    cur_path = SD_PATH + date + "/" + images
+    cur_path = SD_PATH + date + "/images/"
     onlyfiles = [f for f in listdir(cur_path) if camID in f and isfile(join(cur_path, f))]
-    print onlyfiles
-
+    print(onlyfiles)
+ 
 
 get_sd_frames("010035","2019_05_26")
