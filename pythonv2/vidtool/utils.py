37f114b686a5e940cd9afeca1953be6a50a93ea3
Perlerin Vincent <vperlerin@gmail.com>
2019-07-01 10:56:20 +0200

    Del

--- pythonv2/vidtool/utils.py
+++ pythonv2/vidtool/utils.py
@@ -11,7 +11,22 @@ def get_sd_frames(camID,date):
     #ex:camID:010035, date:2019_05_26
     cur_path = SD_PATH + date + "/images/"
     onlyfiles = [f for f in listdir(cur_path) if camID in f and "-tn" not in f and isfile(join(cur_path, f))]
-    print(sorted(onlyfiles))
+    return(sorted(onlyfiles), cur_path)
  
+#Input list of SD files, path of the current image
+#Output Video of resized frames
+def create_sd_vid(frames, path): 
 
-get_sd_frames("010035","2019_05_26")
+    #Create temporary folder to store the frames for the video
+    newpath = r''+path+'/tmp/'
+    if not os.path.exists(newpath):
+        os.makedirs(newpath)
+
+    for f in frames: 
+        #Resize 
+        #ffmpeg -i input.jpg -vf scale=320:240 output_320x240.png
+        print(f)
+
+
+files, path = get_sd_frames("010035","2019_05_26")
+create_sd_vid(files,path)
