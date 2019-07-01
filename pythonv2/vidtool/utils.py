6e533e82518957b951882b1e16a117732ead06be
Perlerin Vincent <vperlerin@gmail.com>
2019-07-01 14:32:31 +0200

    Del

--- pythonv2/vidtool/utils.py
+++ pythonv2/vidtool/utils.py
@@ -12,8 +12,10 @@ def get_sd_frames(camID,date):
     cur_path = SD_PATH + date + "/images/"
     onlyfiles = [f for f in listdir(cur_path) if camID in f and "-tn" not in f and "-night" not in f and "trim" not in f and isfile(join(cur_path, f))]
     return(sorted(onlyfiles), cur_path, date, camID)
- 
-#Input list of SD files, path of the current image
+
+
+
+#Input list of SD files, path of the current image, date, camID
 #Output Video of resized frames
 def create_sd_vid(frames, path, date, camID): 
 
@@ -36,11 +38,17 @@ def create_sd_vid(frames, path, date, camID):
    
     #Draw text on video
     text = "AMS Cams #"+camID+ " " +  str(date.replace("_", "/")) 
-    cmd = 'ffmpeg -i '+ tmp_file_path +' -vf drawtext="text='+text+': fontcolor=white: fontsize=24: box=1: boxcolor=black@0.5: boxborderw=5: x=10: y=h-10" -codec:a copy ' + def_file_path
+    cmd = 'ffmpeg -i '+ tmp_file_path +' -vf drawtext="text='+text+': fontcolor=white: fontsize=24: box=1: boxcolor=black@0.5: boxborderw=5: x=10: y=h-10-text_h" -codec:a copy ' + def_file_path
     output = subprocess.check_output(cmd, shell=True).decode("utf-8")
 
+    #DELETING RESIZE FRAMES
+    os.unlink(path+"/tmp/*.png")
+
+    #DELETING TMP VIDEOS
+    os.unlink(tmp_file_path)
+
     #print(output)
-    print('VIDEO READ AT '+newpath+'/'+date + '_' + camID + '.mp4' )
+    print('VIDEO READY AT '+newpath+'/'+date + '_' + camID + '.mp4' )
 
 files, path, date, camID = get_sd_frames("010034","2019_06_23")
 create_sd_vid(files,path, date, camID)
