3e6a5eabea68a95f07df8adfe5be6f7c6f61ea16
Perlerin Vincent <vperlerin@gmail.com>
2019-07-01 12:12:37 +0200

    M

--- pythonv2/vidtool/utils.py
+++ pythonv2/vidtool/utils.py
@@ -12,12 +12,12 @@ SD_PATH='/mnt/ams2/SD/proc2/'
 def get_sd_frames(camID,date):
     #ex:camID:010034, date:2019_06_23
     cur_path = SD_PATH + date + "/images/"
-    onlyfiles = [f for f in listdir(cur_path) if camID in f and "-tn" not in f and isfile(join(cur_path, f))]
+    onlyfiles = [f for f in listdir(cur_path) if camID in f and "-tn" not in f and "-night" not in f "trim" not in f and isfile(join(cur_path, f))]
     return(sorted(onlyfiles), cur_path)
  
 #Input list of SD files, path of the current image
 #Output Video of resized frames
-def create_sd_vid(frames, path): 
+def create_sd_vid(frames, path, date): 
 
     #Create temporary folder to store the frames for the video
     newpath = r''+path+'/tmp/'
@@ -26,14 +26,15 @@ def create_sd_vid(frames, path):
 
     for idx,f in enumerate(frames): 
         #Resize the frames in /tmp
-        cmd = 'ffmpeg -i ' + path+'/'+ f + ' -vf scale=1920:1080 ' + newpath + '/' + str(idx) + '.png'
+        cmd = 'ffmpeg -hide_banner -loglevel panic -i ' + path+'/'+ f + ' -vf scale=1920:1080 ' + newpath + '/' + str(idx) + '.png'
         output = subprocess.check_output(cmd, shell=True).decode("utf-8")
         #print(output)
 
     #Create Video based on all newly create frames
-    cmd = 'ffmpeg -r 60 -f image2 -s 1920x1080 -i ' + newpath+ '/%d.png -vcodec libx264 -crf 25 -pix_fmt yuv420p '+newpath+'/test.mp4'
+    cmd = 'ffmpeg -hide_banner -loglevel panic -r 45 -f image2 -s 1920x1080 -i ' + newpath+ '/%d.png -vcodec libx264 -crf 25 -pix_fmt yuv420p '+newpath+'/'+date +'.mp4'
     output = subprocess.check_output(cmd, shell=True).decode("utf-8")
-    print(output)
+    #print(output)
+    print('VIDEO READ AT '+newpath+'/'+date +'.mp4' )
 
-files, path = get_sd_frames("010034","2019_06_23")
-create_sd_vid(files,path)
+files, path, date = get_sd_frames("010034","2019_06_23")
+create_sd_vid(files,path, date)
