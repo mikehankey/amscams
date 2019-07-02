828919b108d87c7c2ec6812abdec4e32f8e85b89
Perlerin Vincent <vperlerin@gmail.com>
2019-07-02 09:52:25 +0200

    J

--- pythonv2/vidtool/utils.py
+++ pythonv2/vidtool/utils.py
@@ -9,7 +9,7 @@ SD_PATH='/mnt/ams2/SD/proc2/'
 def get_meteor_date(file):
 	fn = file.split("/")[-1] 
 	fn = fn.split('_',6)
-	return fn[0] + "/" + fn[1] + "/" + fn[2] + " " + fn[3] + ":" + fn[4] + ":" + fn[5]
+	return fn[0] + "-" + fn[1] + "-" + fn[2] + " " + fn[3] + "-" + fn[4] + "-" + fn[5]
 
 #Input: camID, date
 #Ouput: list of sd frames found for this date
