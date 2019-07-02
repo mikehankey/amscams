ee6c784bac1802adb1571127dca7b8a6120dcd8c
Perlerin Vincent <vperlerin@gmail.com>
2019-07-02 15:09:54 +0200

    M

--- src/js/functions/generate_timelapse.js
+++ src/js/functions/generate_timelapse.js
@@ -18,8 +18,12 @@ function add_timelapse_modal() {
                         <div class="col-sm-4"> \
                             <input type="text" readonly class="form-control-plaintext" id="tot_f" value=""> \
                         </div> \
-                        <div class="col-sm-4"><span id="tld"></span>\
-                        </div>\
+                    </div> \
+                    <div class="form-group row mb-1"> \
+                        <label class="col-sm-2 col-form-label"><b>Duration</b></label> \
+                        <div class="col-sm-4"> \
+                            <input type="text" readonly class="form-control-plaintext" id="tld" value=""> \
+                        </div> \
                     </div> \
                     <div class="form-group row mb-1"> \
                         <label for="fps" class="col-sm-2 col-form-label"><b>FPS</b></label> \
@@ -27,7 +31,7 @@ function add_timelapse_modal() {
                             <select id="fps" class="form-control"> \
                                 <option value="1">1 fps</option> \
                                 <option value="5">5 fps</option> \
-                                <option value="10">10 fps</option> \
+                                <option value="10" selected>10 fps</option> \
                                 <option value="15">15 fps</option> \
                                 <option value="24.975">24.975 fps</option> \
                                 <option value="29.97">29.97 fps</option> \
@@ -40,7 +44,7 @@ function add_timelapse_modal() {
                         <div class="col-sm-4"> \
                             <select id="dim" class="form-control"> \
                                 <option value="dim1">1920x1080</option> \
-                                <option value="dim2">1280x720</option> \
+                                <option value="dim2" selected>1280x720</option> \
                                 <option value="dim3">640x320</option> \
                             </select> \
                         </div> \
@@ -59,9 +63,11 @@ function add_timelapse_modal() {
     hmf = $('img.lz').not('.process').length;
     $('#tot_f').val(hmf);
 
+    // Init duration
+
     // Update duration 
     $('#fps').unbind('change').bind('change',function() {
-        $('#tld').val(parseFloat($(this).val())/parseFloat($('#tot_f').val()).toFixed(2) + 'seconds');
+        $('#tld').text(parseFloat($('#tot_f').val()/parseFloat($(this).val())).toFixed(2) + ' seconds');
     });
 }
 
