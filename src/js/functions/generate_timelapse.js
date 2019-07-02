a03bb569bcea0077bbb0f4e93a481266e6c358d4
Perlerin Vincent <vperlerin@gmail.com>
2019-07-02 15:14:08 +0200

    M

--- src/js/functions/generate_timelapse.js
+++ src/js/functions/generate_timelapse.js
@@ -13,42 +13,45 @@ function add_timelapse_modal() {
             </div> \
             <div class="modal-body"> \
                 <form> \
-                    <div class="form-group row mb-1"> \
-                        <label class="col-sm-2 col-form-label"><b>Frame Count</b></label> \
-                        <div class="col-sm-4"> \
-                            <input type="text" readonly class="form-control-plaintext" id="tot_f" value=""> \
+                    <div class="row"> \
+                        <div class="col-sm-6"> \
+                            <div class="form-group row mb-1"> \
+                                <label class="col-sm-4 col-form-label"><b>Frame Count</b></label> \
+                                <div class="col-sm-8"> \
+                                    <input type="text" readonly class="form-control-plaintext" id="tot_f" value=""> \
+                                </div> \
+                            </div> \
+                            <div class="form-group row mb-1"> \
+                                <label class="col-sm-4 col-form-label"><b>Duration</b></label> \
+                                <div class="col-sm-8"> \
+                                    <input type="text" readonly class="form-control-plaintext" id="tld" value=""> \
+                                </div> \
+                            </div> \
+                            <div class="form-group row mb-1"> \
+                                <label for="fps" class="col-sm-4 col-form-label"><b>FPS</b></label> \
+                                <div class="col-sm-8"> \
+                                    <select id="fps" class="form-control"> \
+                                        <option value="1">1 fps</option> \
+                                        <option value="5">5 fps</option> \
+                                        <option value="10" selected>10 fps</option> \
+                                        <option value="15">15 fps</option> \
+                                        <option value="24.975">24.975 fps</option> \
+                                        <option value="29.97">29.97 fps</option> \
+                                        <option value="54.94">54.94 fps</option> \
+                                    </select> \
+                                </div> \
+                            </div> \
+                            <div class="form-group row mb-1"> \
+                                <label for="dim" class="col-sm-4 col-form-label"><b>Dimension</b></label> \
+                                <div class="col-sm-8"> \
+                                    <select id="dim" class="form-control"> \
+                                        <option value="dim1">1920x1080</option> \
+                                        <option value="dim2" selected>1280x720</option> \
+                                        <option value="dim3">640x320</option> \
+                                    </select> \
+                                </div> \
+                            </div> \
                         </div> \
-                    </div> \
-                    <div class="form-group row mb-1"> \
-                        <label class="col-sm-2 col-form-label"><b>Duration</b></label> \
-                        <div class="col-sm-4"> \
-                            <input type="text" readonly class="form-control-plaintext" id="tld" value=""> \
-                        </div> \
-                    </div> \
-                    <div class="form-group row mb-1"> \
-                        <label for="fps" class="col-sm-2 col-form-label"><b>FPS</b></label> \
-                        <div class="col-sm-4"> \
-                            <select id="fps" class="form-control"> \
-                                <option value="1">1 fps</option> \
-                                <option value="5">5 fps</option> \
-                                <option value="10" selected>10 fps</option> \
-                                <option value="15">15 fps</option> \
-                                <option value="24.975">24.975 fps</option> \
-                                <option value="29.97">29.97 fps</option> \
-                                <option value="54.94">54.94 fps</option> \
-                            </select> \
-                        </div> \
-                    </div> \
-                    <div class="form-group row mb-1"> \
-                        <label for="dim" class="col-sm-2 col-form-label"><b>Dimension</b></label> \
-                        <div class="col-sm-4"> \
-                            <select id="dim" class="form-control"> \
-                                <option value="dim1">1920x1080</option> \
-                                <option value="dim2" selected>1280x720</option> \
-                                <option value="dim3">640x320</option> \
-                            </select> \
-                        </div> \
-                    </div> \
                 </form> \
             </div> \
             <div class="modal-footer"> \
@@ -64,10 +67,11 @@ function add_timelapse_modal() {
     $('#tot_f').val(hmf);
 
     // Init duration
+    $('#tld').val(parseFloat($('#tot_f').val()/parseFloat($('#fps').val())).toFixed(2) + ' seconds');
 
     // Update duration 
     $('#fps').unbind('change').bind('change',function() {
-        $('#tld').text(parseFloat($('#tot_f').val()/parseFloat($(this).val())).toFixed(2) + ' seconds');
+        $('#tld').val(parseFloat($('#tot_f').val()/parseFloat($(this).val())).toFixed(2) + ' seconds');
     });
 }
 
