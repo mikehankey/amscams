c6da2a1a8b3c28ede59b4b0a348b122945b674db
Perlerin Vincent <vperlerin@gmail.com>
2019-07-05 15:11:03 +0200

    Vid

--- src/js/functions/cam_picker.js
+++ src/js/functions/cam_picker.js
@@ -1,10 +1,14 @@
 $(function() {
 
     $('.cam_picker').each(function() {
-        if($(this).hasAttr('data-url-param')) {
+     
+        if(typeof $(this).attr('data-url-param') != undefined) {
             $(this).change(function() {
+                var $t = $(this);
+                var cur_params = getQueryParameters();
                 var param_to_update = $t.attr('data-url-param');
-                setQueryParameters($(this).val());
+                cur_params[param_to_update] = $(this).val();
+                setQueryParameters(cur_params); // Defined in date_picker.js
             });
         }
     }) 
