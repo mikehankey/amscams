5da3abce16ac1ba7bd65eaa725caa019250e1286
Perlerin Vincent <vperlerin@gmail.com>
2019-07-05 11:08:08 +0200

    M

--- pythonv2/Cleanup_Json_Conf.py
+++ pythonv2/Cleanup_Json_Conf.py
@@ -12,11 +12,18 @@ PATH_TO_CLEAN_CONF_JSON = "/home/ams/amscams/conf/testconf-clean.json"
 
 
 # Test if the clean versio already exist
-js_file = Path(PATH_TO_CLEAN_CONF_JSON)
+file_org == Path(PATH_TO_CONF_JSON)
+file_to_clean = Path(PATH_TO_CLEAN_CONF_JSON)
 
-if js_file.is_file():
+
+if file_to_clean.is_file():
     #ADD POSSIBILITY TO Erase?
     print("Clean version already exists") 
+    exit()
+
+
+elif file_org.is_file() == False: 
+    print("Conf file is missing") 
     exit()
 
 else:
@@ -26,7 +33,7 @@ else:
         try:
             data = json.load(jsonFile)
         except:
-            print("Impossible to open the conf file ("+PATH_TO_CONF_JSON+")") 
+            print("Impossible to read or parse the conf file ("+PATH_TO_CONF_JSON+")") 
             exit()
 
     #Create new (clean) array to create clean conf file
