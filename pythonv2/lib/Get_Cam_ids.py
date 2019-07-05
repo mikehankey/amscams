9e53817e0e0f6962095eb02585d9a37a7b9c4f94
Perlerin Vincent <vperlerin@gmail.com>
2019-07-05 14:34:17 +0200

    Vid

--- pythonv2/lib/Get_Cam_ids.py
+++ pythonv2/lib/Get_Cam_ids.py
@@ -3,14 +3,9 @@ from lib.Cleanup_Json_Conf import cleanup_json
 
 def get_the_cam_ids():
     json_path = cleanup_json()
+    toReturn = []
     with open(json_path, "r+") as jsonFile:
         data = json.load(jsonFile)
-        toReturn = []
         for cam in data['cameras']:
-            toReturn.append(data['cameras'][cam]['id'])
-        return toReturn
-
-
-
-
- 
+            toReturn.append(cam['id']) 
+    return toReturn
