178050589d261036f76b8b6d3f1dac7f1a71595b
Perlerin Vincent <vperlerin@gmail.com>
2019-07-09 11:14:23 +0200

    New Video jobs management

--- src/js/ui/canvas-interactions.js
+++ src/js/ui/canvas-interactions.js
@@ -140,7 +140,7 @@ if ($('canvas#c').length!=0) {
             var wh = $(window).height();
             var cw = $('#c').width();
             var ch = $('#c').height();
-            e.stopProgation();
+            e.stopPropagation();
             $('#c').css({ 
               'width': wh*cw/ch,
               'height': wh
@@ -153,7 +153,7 @@ if ($('canvas#c').length!=0) {
         // Remove zoom
         if($('#c').hasClass('r-zoomed')) {
           $('#c').removeClass('r-zoomed');
-          e.stopProgation();
+          e.stopPropagation();
           return false;
         }
 
