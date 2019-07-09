aaf48d97b575fadcf32edbdf18adea8daea693fc
Perlerin Vincent <vperlerin@gmail.com>
2019-07-09 11:51:36 +0200

    New Video jobs management

--- src/js/ui/canvas-zoom.js
+++ src/js/ui/canvas-zoom.js
@@ -3,9 +3,11 @@ $(function() {
     // Full zoom on canvas
     $('#zoom_canvas').click(function() {
         if($('#c').hasClass('r-zoomed')) {
+            $('#zc').remove();
             $('#c').removeClass('r-zoomed');
             $(this).removeClass('zoom-btn-f').find('i').removeClass('icon-zoom-out').addClass('icon-zoom-in');
         } else {
+            $('<div id="zc" class="modal-backdrop fade show"></div>').appendTo($(body));
             $('#c').addClass('r-zoomed');
             $(this).addClass('zoom-btn-f').find('i').removeClass('icon-zoom-in').addClass('icon-zoom-out');
         }
