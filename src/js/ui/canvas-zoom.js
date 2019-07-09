e37ce29e56591c3f5c92f758ec9b8dacac403290
Perlerin Vincent <vperlerin@gmail.com>
2019-07-09 11:44:25 +0200

    Canvas Zoom

--- src/js/ui/canvas-zoom.js
+++ src/js/ui/canvas-zoom.js
@@ -1,10 +1,13 @@
 $(function() {
 
+    // Full zoom on canvas
     $('#zoom_canvas').click(function() {
         if($('#c').hasClass('r-zoomed')) {
             $('#c').removeClass('r-zoomed');
+            $(this).removeClass('zoom-btn-f').find('i').removeClass('icon-zoom-out').addClass('icon-zoom-in');
         } else {
             $('#c').addClass('r-zoomed');
+            $(this).addClass('zoom-btn-f').find('i').removeClass('icon-zoom-in').addClass('icon-zoom-out');
         }
     });
 })
