702fc78f19109ba109a1e696c0c7d3bcc42b62d7
Perlerin Vincent <vperlerin@gmail.com>
2019-07-10 15:42:44 +0200

    Js

--- pythonv2/video_debugger.py
+++ pythonv2/video_debugger.py
@@ -6,42 +6,32 @@ from lib.Video_Tools import *
 from os import listdir, remove
 from os.path import isfile, join, exists
 
-
-#TEST HD_FRAMES
-#print("GET HD FRAMES FOR '010040','2019_07_10'")
-#frames, path, date, camID = get_hd_frames('010040','2019_07_10')
-#print("PATH " + str(path))
-#print("date " + str(date))
-#print("camID " + str(camID))
-#print("files " + str(files))
-#/mnt/ams2/HD/tmp/2019_07_10_08_53_53_000_010040.mp4.png
-#new_path = add_info_to_frames(frames, path, date, camID,  "1920:1080",  'bl',  'tr',  0)
-#s = create_vid_from_frames(frames, new_path, date, camID, fps="15")
-#print('THE VID SHOULD BE THERE ' + s)
+ 
 
 
 #TEST SD FRAMES
-print("GET SD FRAMES FOR '010042','2019_06_020'")
+#print("GET SD FRAMES FOR '010042','2019_06_20'")
+#path, date, camID
+#date = '2019_06_02'
+#camID = '010042'
+#frames, path = get_sd_frames(camID,date)
+#if(frames is None):
+ #   print('NO FRAME FOUND')
+#else:
+#    where_path = add_info_to_frames(frames, path, date, camID,  "1920:1080",  'bl',  'tr',  0)
+#    s = create_vid_from_frames(frames, where_path, date, camID, fps="60")
+#    print('THE VID SHOULD BE THERE ' + s)
+
+ 
+ #TEST HD FRAMES
+print("GET SD FRAMES FOR '010038','2019_07_09'")
 #path, date, camID
 date = '2019_06_02'
 camID = '010042'
-frames, path = get_sd_frames(camID,date)
+frames, path = get_hd_frames(camID,date)
 if(frames is None):
     print('NO FRAME FOUND')
 else:
     where_path = add_info_to_frames(frames, path, date, camID,  "1920:1080",  'bl',  'tr',  0)
-    s = create_vid_from_frames(frames, where_path, date, camID, fps="60")
+    s = create_vid_from_frames(frames, where_path, date, camID, fps="120")
     print('THE VID SHOULD BE THERE ' + s)
-
-
-#print(str(frames))
-#print("PATH " + str(path))
-#print("date " + str(date))
-#print("camID " + str(camID))5
-#print("files " + str(files))
-#if(frames is None):
-#    print('NO FRAME FOUND')
-#else:
-    #new_path = add_info_to_frames(frames, path, date, camID,  "1920:1080",  'bl',  'tr',  0)
-    #s = create_vid_from_frames(frames, new_path, date, camID, fps="60")
-#    print('THE VID SHOULD BE THERE ' + s)
