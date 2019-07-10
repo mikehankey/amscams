7ccd3d829433846eca3a9d4ae0752f140e7eaf93
Perlerin Vincent <vperlerin@gmail.com>
2019-07-10 11:56:46 +0200

    R

--- pythonv2/video_debugger.py
+++ pythonv2/video_debugger.py
@@ -8,12 +8,19 @@ from os.path import isfile, join, exists
 
 
 #TEST HD_FRAMES
-files, path, date, camID = get_hd_frames('010040','2019_07_10')
+#print("GET HD FRAMES FOR '010040','2019_07_10'")
+#files, path, date, camID = get_hd_frames('010040','2019_07_10')
+#print("PATH " + str(path))
+#print("date " + str(date))
+#print("camID " + str(camID))
+#print("files " + str(files))
+#/mnt/ams2/HD/tmp/2019_07_10_08_53_53_000_010040.mp4.png
+
+
+#TEST SD FRAMES
+print("GET HD FRAMES FOR '010040','2019_07_08'")
+files, path, date, camID = get_sd_frames('010040','2019_07_07')
 print("PATH " + str(path))
 print("date " + str(date))
 print("camID " + str(camID))
 print("files " + str(files))
-
-
-#TEST SD FRAMES
-#get_sd_frames('010040','2019_07_07')
