75b16db340d1ba9bc340ee3bb8ad30f464c1c836
Perlerin Vincent <vperlerin@gmail.com>
2019-07-10 12:34:05 +0200

    V

--- pythonv2/video_debugger.py
+++ pythonv2/video_debugger.py
@@ -9,20 +9,20 @@ from os.path import isfile, join, exists
 
 #TEST HD_FRAMES
 #print("GET HD FRAMES FOR '010040','2019_07_10'")
-frames, path, date, camID = get_hd_frames('010040','2019_07_10')
+#frames, path, date, camID = get_hd_frames('010040','2019_07_10')
 #print("PATH " + str(path))
 #print("date " + str(date))
 #print("camID " + str(camID))
 #print("files " + str(files))
 #/mnt/ams2/HD/tmp/2019_07_10_08_53_53_000_010040.mp4.png
-add_info_to_frames(frames, path, date, camID,  "1920:1080",  'bl',  'tr',  0)
+#add_info_to_frames(frames, path, date, camID,  "1920:1080",  'bl',  'tr',  0)
 
 
 #TEST SD FRAMES
-#print("GET HD FRAMES FOR '010040','2019_07_08'")
-#frames, path, date, camID = get_sd_frames('010040','2019_07_07')
+print("GET HD FRAMES FOR '010040','2019_07_08'")
+frames, path, date, camID = get_sd_frames('010040','2019_07_07')
 #print("PATH " + str(path))
 #print("date " + str(date))
 #print("camID " + str(camID))
 #print("files " + str(files))
-#add_info_to_frames(frames, path, date, camID,  "1920:1080",  'bl',  'tr',  0)
+add_info_to_frames(frames, path, date, camID,  "1920:1080",  'bl',  'tr',  0)
