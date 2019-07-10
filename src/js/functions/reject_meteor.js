6d8a008295750dcd1507f451fad2d45bd88bd319
Perlerin Vincent <vperlerin@gmail.com>
2019-07-10 10:33:28 +0200

    M

--- src/js/functions/reject_meteor.js
+++ src/js/functions/reject_meteor.js
@@ -1,6 +1,7 @@
 function meteor_is_deleted(id) {
       $('#'+id).css('opacity',.5).removeClass('norm meteor reduced').addClass('del').find('.btn-toolbar').remove().end().find('a').removeAttr('href').removeAttr('title').attr('title','DELETED').unbind('mouseover').unbind('mouseout').end().find(".custom-checkbox").remove();                  
-
+      $('#'+id).find('.overlay_loader').remove(); 
+      update_selected_counter();
 }
 
 
@@ -10,11 +11,7 @@ function reject_meteor(id) {
             url:  "webUI.py?cmd=override_detect",
             data: {jsid: id},
             success: function(data) {
-                  
-                  meteor_is_deleted(id);
-                  // Debug
-                  //console.log(data);
-                  loading_done();
+                  meteor_is_deleted(id); 
             }, 
             error: function() {
                   alert('Impossible to reject. Please, reload the page and try again later.')
@@ -22,6 +19,7 @@ function reject_meteor(id) {
             }
       });
 }
+ 
 
 
 function reject_multiple_meteor(array_of_jsid, ids) {
@@ -30,7 +28,7 @@ function reject_multiple_meteor(array_of_jsid, ids) {
             loading({text:"Deleting", container:$("#"+v), overlay:true, standalone:true});
       });
 
-      console.log("array_of_jsid ", array_of_jsid)
+      //console.log("array_of_jsid ", array_of_jsid)
 
       $.ajax({ 
             url:  "webUI.py?cmd=delete_multiple_detection",
@@ -45,11 +43,9 @@ function reject_multiple_meteor(array_of_jsid, ids) {
                   $.each(ids, function(i,v){
                         console.log("IS DELETED " + v);
                         console.log("I ", i);
-                        meteor_is_deleted(v);
-                        loading_done();
+                        meteor_is_deleted(v); 
                   });
-                  loading_done();
-                  loading_done();
+                  
                  
             }, 
             error: function() {
