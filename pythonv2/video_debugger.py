481f4295b7e56852e8253f6cf242012e6baf108c
Perlerin Vincent <vperlerin@gmail.com>
2019-07-10 12:41:01 +0200

    Js

--- pythonv2/video_debugger.py
+++ pythonv2/video_debugger.py
@@ -15,16 +15,18 @@ from os.path import isfile, join, exists
 #print("camID " + str(camID))
 #print("files " + str(files))
 #/mnt/ams2/HD/tmp/2019_07_10_08_53_53_000_010040.mp4.png
-#add_info_to_frames(frames, path, date, camID,  "1920:1080",  'bl',  'tr',  0)
+new_path = add_info_to_frames(frames, path, date, camID,  "1920:1080",  'bl',  'tr',  0)
+s = create_vid_from_frames(frames, new_path, date, camID, fps="15")
+print('THE VID SHOULD BE THERE ' + s)
 
 
 #TEST SD FRAMES
-print("GET HD FRAMES FOR '010040','2019_07_08'")
-frames, path, date, camID = get_sd_frames('010040','2019_07_07')
+#print("GET HD FRAMES FOR '010040','2019_07_08'")
+#frames, path, date, camID = get_sd_frames('010040','2019_07_07')
 #print("PATH " + str(path))
 #print("date " + str(date))
 #print("camID " + str(camID))
 #print("files " + str(files))
-new_path = add_info_to_frames(frames, path, date, camID,  "1920:1080",  'bl',  'tr',  0)
-s = create_vid_from_frames(frames, new_path, date, camID, fps="15")
+#new_path = add_info_to_frames(frames, path, date, camID,  "1920:1080",  'bl',  'tr',  0)
+#s = create_vid_from_frames(frames, new_path, date, camID, fps="15")
 print('THE VID SHOULD BE THERE ' + s)
