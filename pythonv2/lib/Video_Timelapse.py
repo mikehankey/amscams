38b18e5954df179894c70c304aebdc0b57f795bf
Perlerin Vincent <vperlerin@gmail.com>
2019-07-10 16:43:29 +0200

    Js

--- pythonv2/lib/Video_Timelapse.py
+++ pythonv2/lib/Video_Timelapse.py
@@ -3,7 +3,7 @@ from lib.Video_Tools import *
 #Generate Timelapse
 def generate_timelapse(cam_id,date,fps,dim,text_pos,wat_pos, enhancement=0): 
     #frames are SD if HD are not found
-    frames, path = get_hd_frames(camID,date)
+    frames, path = get_hd_frames(cam_id,date)
     if(frames is None):
         print('NO FRAME FOUND')
         return ''
