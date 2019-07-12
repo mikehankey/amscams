e37b50c3eb6ac774ca471a4d9ca8bcbfd5f29bbf
Perlerin Vincent <vperlerin@gmail.com>
2019-07-12 10:56:40 +0200

    M

--- pythonv2/lib/Video_Tools.py
+++ pythonv2/lib/Video_Tools.py
@@ -147,9 +147,12 @@ def add_info_to_frames(frames, path, date, camID, extra_text, dimensions="1920:1
     watermark_position = get_watermark_pos(watermark_pos)
 
     # Do we have extra text?
-    with_extra_text = !(extra_text is None or extra_text.strip()=='')
-    if(!with_extra_text):
+    if(extra_text is not None or extra_text.strip()!='') {
+        with_extra_text = True
+    else:
+        with_extra_text = False
         extra_text=''
+ 
 
     # Info position based on options
     text_position, extra_text_position = get_text_pos(text_pos, (extra_text!=''))
