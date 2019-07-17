4afbdf595f3e2c49e284dcd49155c2b0a4f6b881
Perlerin Vincent <vperlerin@gmail.com>
2019-07-17 17:56:46 +0200

    M

--- src/js/functions/generate_timelapse_full.js
+++ src/js/functions/generate_timelapse_full.js
@@ -39,7 +39,7 @@ function add_timelapse_full_modal() {
                                 </div> \
                             </div> \
                             <div class="form-group row mb-1"> \
-                                <label class="col-sm-4 col-form-label"><b>Cam Id</b></label> \
+                                <label class="col-sm-4 col-form-label"><b>Camera</b></label> \
                                 <div class="col-sm-8">'+cam_select+'</div> \
                             </div> \ 
                         </div> \
