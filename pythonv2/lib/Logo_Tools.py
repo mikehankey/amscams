520458cb0119663f72f3713446a302d4615cb3f9
Perlerin Vincent <vperlerin@gmail.com>
2019-07-18 16:20:49 +0200

    M

--- pythonv2/lib/Logo_Tools.py
+++ pythonv2/lib/Logo_Tools.py
@@ -4,5 +4,18 @@ import shutil
 from lib.LOGOS_VARS import LOGOS_PATH 
 
 def upload_logo(form):
-    logo = form.getvalue("logo").filename 
-    print("TEST " + logo)
+    logo = form.getvalue("logo")
+ 
+    fileitem = logo
+    if fileitem.file:
+        # It's an uploaded file; count lines
+        linecount = 0
+        while 1:
+            line = fileitem.file.readline()
+            if not line: break
+            linecount = linecount + 1
+        
+        print("linecount " + linecount)
+        print(fileitem.file)
+    else:
+        print("NO FILE FOUND")
