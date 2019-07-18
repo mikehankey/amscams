b9eda1493499749d0987b76b25449fbde51bb868
Perlerin Vincent <vperlerin@gmail.com>
2019-07-18 13:45:29 +0200

    M

--- pythonv2/Video_Jobs_Cron.py
+++ pythonv2/Video_Jobs_Cron.py
@@ -58,7 +58,7 @@ def get_job_to_process():
 
         #When the file is empty at first
         if(len(data)==0):
-            data['jobs'] = []
+            data['jobs'] = {}
 
         toReturn['status'] = "processing"
         data['jobs'].append(toReturn)
