9b11f027d85176a063640790faa481fe1d53375b
Perlerin Vincent <vperlerin@gmail.com>
2019-07-18 16:45:53 +0200

    U

--- pythonv2/lib/Logo_Tools.py
+++ pythonv2/lib/Logo_Tools.py
@@ -5,23 +5,28 @@ import cgitb
 from lib.LOGOS_VARS import LOGOS_PATH 
 
 def upload_logo(form):
-    cgitb.enable()
+    #cgitb.enable()
 
-    logo = form.getvalue("logo")
+    try:
+        logo = form.getvalue("logo")
+        
+        #Is the LOGOS_PATH folder exists? 
+        if not os.path.exists(LOGOS_PATH):
+            os.makedirs(LOGOS_PATH)
+
+        #Count the Existing Logos 
+        cur = len([name for name in os.listdir(LOGOS_PATH) if os.path.isfile(os.path.join(LOGOS_PATH, name))])
+        cur += 1
+
+        #print("WE CURRENTLY HAVE " + str(cur) + " LOGOS")
+
+        #Create PNG in LOGOS_PATH
+        f= open(LOGOS_PATH+str(cur)+'.png',"wb+")
+        f.write(bytes(logo))
+        f.close()
     
-    #Is the LOGOS_PATH folder exists? 
-    if not os.path.exists(LOGOS_PATH):
-        os.makedirs(LOGOS_PATH)
+        print("{'msg':'Logo Uploaded'}")
+    except:
+        print("{'error':'Something went wrong, please try again'}")
 
-    #Count the Existing Logos 
-    cur = len([name for name in os.listdir(LOGOS_PATH) if os.path.isfile(os.path.join(LOGOS_PATH, name))])
-    cur += 1
-
-    print("WE CURRENTLY HAVE " + str(cur) + " LOGOS")
-
-    #Create PNG in LOGOS_PATH
-    f= open(LOGOS_PATH+str(cur)+'.png',"wb+")
-    f.write(bytes(logo))
-    f.close()
-
-    print("LOGO SHOULD BE HERE: " + LOGOS_PATH+str(cur)+'.png')
+    #print("LOGO SHOULD BE HERE: " + LOGOS_PATH+str(cur)+'.png')
--- src/js/functions/upload_logo.js
+++ src/js/functions/upload_logo.js
@@ -2,3 +2,21 @@
 $('#logo_file_upload').change(function() {
     $('#upload_logo').submit();
 })
+
+
+$("form#upload_logo").submit(function(e) {
+    e.preventDefault();    
+    var formData = new FormData(this);
+
+    $.ajax({
+        url: '/pycgi/webUI.py?cmd=upload_logo',
+        type: 'POST',
+        data: formData,
+        success: function (data) {
+            alert(data)
+        },
+        cache: false,
+        contentType: false,
+        processData: false
+    });
+});
