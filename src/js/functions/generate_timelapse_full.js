ad48ce321524f4f3b00bc944353a50ecef9f111b
Perlerin Vincent <vperlerin@gmail.com>
2019-07-18 09:51:13 +0200

    M

--- src/js/functions/generate_timelapse_full.js
+++ src/js/functions/generate_timelapse_full.js
@@ -11,7 +11,7 @@ function add_timelapse_full_modal() {
     var cam_select = "<select name='cam' class='form-control'  multiple='multiple'>";
     $.each(cam_ids,function(i,v){
         if($.trim(v)!=='') {
-            cam_select = cam_select + "<option value='"+v+"'>" + v + "</option>";
+            cam_select = cam_select + "<option value='"+v+"'>Camera #" + v + "</option>";
         }
     });
     cam_select = cam_select + "</select>";
@@ -30,80 +30,64 @@ function add_timelapse_full_modal() {
             <div class="modal-body"> \
                 <form id="timelapse_full_form"> \
                     <div class="row p-3"> \
-                        <div class="col-sm-6"> \
-                            <div class="form-group row mb-1"> \
-                                <label class="col-sm-4 col-form-label"><b>Date</b></label> \
-                                <div class="col-sm-8"> \
-                                    <input name="video_date" value="'+curD+'" type="text" data-display-format="YYYY/MM/DD" class="datepicker form-control"> \
-                                </div> \
+                        <div class="col-sm-12 mb-1"> \
+                            <div class="form-group mb-1"> \
+                                <label class="col-form-label"><b>Date</b></label> \
+                                <input name="video_date" value="'+curD+'" type="text" data-display-format="YYYY/MM/DD" class="datepicker form-control"> \
+                            </div> \
+                            <div class="form-group mb-1"> \
+                                <label class="col-form-label"><b>Camera</b></label> \
+                                <div>'+cam_select+'</div> \
+                            </div> \
+                            <div class="form-group  mb-1"> \
+                                <label for="fps" class=" col-form-label"><b>FPS</b></label> \
+                                <select name="fps" class="form-control"> \
+                                    <option value="1">1 fps</option> \
+                                    <option value="5">5 fps</option> \
+                                    <option value="10">10 fps</option> \
+                                    <option value="15">15 fps</option> \
+                                    <option value="23.976">23.976 fps</option> \
+                                    <option value="24">24 fps</option> \
+                                    <option value="25">25 fps</option> \
+                                    <option value="29.97" >29.97 fps</option> \
+                                    <option value="30" selected>30 fps</option> \
+                                    <option value="50">50 fps</option> \
+                                    <option value="59.94">59.94 fps</option> \
+                                    <option value="60">60 fps</option> \
+                                </select> \
                             </div> \
                             <div class="form-group row mb-1"> \
-                                <label class="col-sm-4 col-form-label"><b>Camera</b></label> \
-                                <div class="col-sm-8">'+cam_select+'</div> \
+                                <label for="dim" class="col-form-label"><b>Dimension</b></label> \
+                                <select name="dim" class="form-control"> \
+                                    <option value="1920:1080">1920x1080</option> \
+                                    <option value="1280:720" selected>1280x720</option> \
+                                    <option value="640:320">640x320</option> \
+                                </select> \
                             </div> \
-                        </div> \
-                        <div class="col-sm-6"> \
-                            <div class="form-group row mb-1"> \
-                                <label for="fps" class="col-sm-4 col-form-label"><b>FPS</b></label> \
-                                <div class="col-sm-8"> \
-                                    <select name="fps" class="form-control"> \
-                                        <option value="1">1 fps</option> \
-                                        <option value="5">5 fps</option> \
-                                        <option value="10">10 fps</option> \
-                                        <option value="15">15 fps</option> \
-                                        <option value="23.976">23.976 fps</option> \
-                                        <option value="24">24 fps</option> \
-                                        <option value="25">25 fps</option> \
-                                        <option value="29.97" >29.97 fps</option> \
-                                        <option value="30" selected>30 fps</option> \
-                                        <option value="50">50 fps</option> \
-                                        <option value="59.94">59.94 fps</option> \
-                                        <option value="60">60 fps</option> \
-                                    </select> \
-                                </div> \
+                            <div class="form-group">\
+                                <label for="wat_pos" class="col-form-label"><b>Position of the AMS Watermark</b></label> \
+                                <select name="wat_pos" class="form-control"> \
+                                    <option value="tr" selected>Top right</option> \
+                                    <option value="tl" >Top Left</option> \
+                                    <option value="br" >Bottom Right</option> \
+                                    <option value="bl" >Bottom Left</option> \
+                                </select> \
                             </div> \
-                            <div class="form-group row mb-1"> \
-                                <label for="dim" class="col-sm-4 col-form-label"><b>Dimension</b></label> \
-                                <div class="col-sm-8"> \
-                                    <select name="dim" class="form-control"> \
-                                        <option value="1920:1080">1920x1080</option> \
-                                        <option value="1280:720" selected>1280x720</option> \
-                                        <option value="640:320">640x320</option> \
-                                    </select> \
-                                </div> \
+                            <div class="form-group"> \
+                                <label for="text_pos" class="col-form-label"><b>Position of the Camera Info</b></label> \
+                                <select name="text_pos" class="form-control"> \
+                                    <option value="tr">Top right</option> \
+                                    <option value="tl" >Top Left</option> \
+                                    <option value="br" >Bottom Right</option> \
+                                    <option value="bl" selected>Bottom Left</option> \
+                                </select> \
                             </div> \
-                        </div> \
-                    </div> \
-                    <div class="row p-3"> \
-                            <div class="col-sm-12 mb-1"> \
-                                <div class="form-group">\
-                                    <label for="wat_pos" class="col-form-label"><b>Position of the AMS Watermark</b></label> \
-                                    <select name="wat_pos" class="form-control"> \
-                                        <option value="tr" selected>Top right</option> \
-                                        <option value="tl" >Top Left</option> \
-                                        <option value="br" >Bottom Right</option> \
-                                        <option value="bl" >Bottom Left</option> \
-                                    </select> \
-                                </div> \
-                            </div> \
-                            <div class="col-sm-12 mb-1"> \
-                                <div class="form-group"> \
-                                    <label for="text_pos" class="col-form-label"><b>Position of the Camera Info</b></label> \
-                                    <select name="text_pos" class="form-control"> \
-                                        <option value="tr">Top right</option> \
-                                        <option value="tl" >Top Left</option> \
-                                        <option value="br" >Bottom Right</option> \
-                                        <option value="bl" selected>Bottom Left</option> \
-                                    </select> \
-                                </div> \
-                            </div> \
-                            <div class="col-sm-12"> \
-                                <div class="form-group">\
-                                    <label for="extra_text" class="col-form-label"><b>Extra info (added above the Camera Info)</b></label> \
-                                    <input type="text" name="extra_text" class="form-control" value=""/> \
-                                </div>\
+                            <div class="form-group">\
+                                <label for="extra_text" class="col-form-label"><b>Extra info (added above the Camera Info)</b></label> \
+                                <input type="text" name="extra_text" class="form-control" value=""/> \
                             </div>\
-                    </div> \
+                        </div> \
+                     </div> \
                 </form> \
             </div> \
             </div> \
