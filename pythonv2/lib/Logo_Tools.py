47455a5608ab056fea695eaad5ce9d4476536f5e
Perlerin Vincent <vperlerin@gmail.com>
2019-07-18 16:22:44 +0200

    M

--- pythonv2/lib/Logo_Tools.py
+++ pythonv2/lib/Logo_Tools.py
@@ -1,12 +1,15 @@
 import os
 import uuid
 import shutil
+import cgitb
 from lib.LOGOS_VARS import LOGOS_PATH 
 
 def upload_logo(form):
+    cgitb.enable()
     logo = form.getvalue("logo")
- 
     fileitem = logo
+    print('FILE ITEM' + fileitem)
+    print('FILE ITEM FILE' + fileitem.file)
     if fileitem.file:
         # It's an uploaded file; count lines
         linecount = 0
