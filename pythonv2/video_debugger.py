894c1459b6abeb899b728a400fbafb5b5c928854
Perlerin Vincent <vperlerin@gmail.com>
2019-07-19 10:51:40 +0200

    U

--- pythonv2/video_debugger.py
+++ pythonv2/video_debugger.py
@@ -39,7 +39,7 @@ from os.path import isfile, join, exists
 #Test text & logo
 text_position, extra_text_position = get_text_pos('br',True)
 watermark_position = get_watermark_pos('tr')
-logo_pos  = logo_position = get_watermark_pos(logo_pos)
+logo_pos  = logo_position = get_watermark_pos('br')
 add_info_to_frame('/mnt/ams2/CUSTOM_VIDEOS/2019_07_19_010037.png',"CAM TEXT",'EXTRA TEXT',text_position,extra_text_position,watermark_position,'/mnt/ams2//CUSTOM_LOGOS/2.png',logo_pos,'/mnt/ams2/CUSTOM_VIDEOS/to_test_BR','1920:1080', 0)
 
 #text_position, extra_text_position = get_text_pos('bl',True)
