af6786d464d18adf02046f6fd68a9af70f44c70b
Perlerin Vincent <vperlerin@gmail.com>
2019-07-23 11:33:20 +0200

    M

--- src/js/functions/select_meteor.js
+++ src/js/functions/select_meteor.js
@@ -85,6 +85,7 @@ function addModalTemplate(neighbor) {
         <input type="hidden" name="thumb_w"/><input type="hidden" name="thumb_h"/>\
         <div class="modal-dialog  modal-lg modal-dialog-centered" role="document">\
         <div class="modal-content">\
+        <div class="modal-body">\
         <div class="d-flex justify-content-center" id="nav_prev">';
         
         // Get the previews here
@@ -94,7 +95,6 @@ function addModalTemplate(neighbor) {
 
         
         c +='</div>\
-        <div class="modal-body">\
         <button id="met-sel-next" title="Next" type="button" class="mfp-arrow mfp-arrow-right mfp-prevent-close"></button>\
         <button id="met-sel-prev" title="Prev" type="button" class="mfp-arrow mfp-arrow-left mfp-prevent-close"></button>\
         <p><strong>FRAME #<span id="sel_frame_id"></span> - Click the center of the meteor.</strong> \
