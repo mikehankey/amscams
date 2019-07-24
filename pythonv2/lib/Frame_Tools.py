85f936591cf94fda6080652bfdec19861a1de0fe
Perlerin Vincent <vperlerin@gmail.com>
2019-07-24 16:37:50 +0200

    M

--- pythonv2/lib/Frame_Tools.py
+++ pythonv2/lib/Frame_Tools.py
@@ -2,6 +2,7 @@ import json
 import os 
 import subprocess 
 import cgitb
+import glob
 from os.path import isfile, join, exists
 
 TMP_FRAME_FOLDER = '/mnt/ams2/TMP'
@@ -24,6 +25,15 @@ def get_frame(fr_id,sd_vid):
         return {'full_fr': name, 'id': fr_id}
     else:
         #We need to generate all the frames in TMP_FRAME_FOLDER
+
+        #First we delete all from TMP_FRAME_FOLDER
+        filelist = glob.glob(os.path.join(TMP_FRAME_FOLDER))
+        for f in filelist:
+            os.remove(f)
+
+        #We generate all the frames
         cmd =  'ffmpeg -y -hide_banner -loglevel panic -i '+ sd_vid + '  ' + TMP_FRAME_FOLDER + '/' + cur_name + '_%d.png ' 
         output = subprocess.check_output(cmd, shell=True).decode("utf-8")    
         return {'full_fr':TMP_FRAME_FOLDER + '/' + cur_name + '_' + fr_id + '.png', 'id': fr_id}
+
+ 
