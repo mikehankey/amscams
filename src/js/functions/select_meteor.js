1b3a598db0d0fdb2c09424c63e0a1b89d4e8d6dd
Perlerin Vincent <vperlerin@gmail.com>
2019-07-30 14:49:08 +0200

    M

--- src/js/functions/select_meteor.js
+++ src/js/functions/select_meteor.js
@@ -122,7 +122,7 @@ function addModalTemplate(meteor_id,neighbor) {
         if(v.id==0) {
             // We add a +
             $('<div class="position-relative ">\
-                <a title="Add a frame" class="create_frame_fs btn btn-primary btn-mm add_f position-absolute" data-rel="'+v.id+'" data-fr="'+v.id+'"><i class="icon-plus"></i> #'+v.vid+'</a>\
+                <a title="Add a frame" class="create_frame_fs btn btn-primary btn-mm add_f position-absolute" data-rel="'+v.id+'" data-fr="'+v.vid+'"><i class="icon-plus"></i> #'+v.vid+'</a>\
                 <img src="'+v.img+'" id="'+v.id+'" style="border-color:'+v.color+';" class="'+_class+'" >\
                </div>').appendTo($('#nav_prev'));
 
@@ -143,7 +143,7 @@ function addModalTemplate(meteor_id,neighbor) {
     });
 
     // Click on "+"
-    $('.create_frame_fs').click(function() {
+    $('.create_frame_fs').click(function() { 
         get_frame($(this).attr('data-fr'));
     });
 }
