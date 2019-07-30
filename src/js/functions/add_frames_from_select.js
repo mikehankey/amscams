48879826b3bde4add56960b53dd2e44665b810e5
Perlerin Vincent <vperlerin@gmail.com>
2019-07-30 14:23:16 +0200

    M

--- src/js/functions/add_frames_from_select.js
+++ src/js/functions/add_frames_from_select.js
@@ -195,8 +195,8 @@ function get_frame(cur_fn) {
         success: function(data) { 
           
             loading_done(); 
-            console.log("DATA",data);
-            data = JSON.parse([data]);
+            data = JSON.stringify(d);
+            data = JSON.parse(data);
             // Data = {'id': '44', 'full_fr': '/mnt/ams2/TMP/2019_03_16_07_19_39_000_010038-trim1245_44.png'}
             console.log("DATA",data);
             console.log("FRAME PATH ",data.full_fr)
