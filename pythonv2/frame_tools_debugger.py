e9cf2185b0cbd7dacea94c2efe33b95dc12b57d3
Perlerin Vincent <vperlerin@gmail.com>
2019-07-31 11:18:50 +0200

    M

--- pythonv2/frame_tools_debugger.py
+++ pythonv2/frame_tools_debugger.py
@@ -8,5 +8,5 @@ meteor_json_file = '/mnt/ams2/meteors/2019_03_06/2019_03_06_06_47_25_000_010038-
 sd_video_file = '/mnt/ams2/meteors/2019_03_06/2019_03_06_06_47_25_000_010038-trim0072.mp4'
 
 #def add_frame(json_conf, sd_video_file, fr_id, hd_x=-1, hd_y=-1): 
-add_frame(meteor_json_file,sd_video_file,22,251,123) 
-add_frame(meteor_json_file,sd_video_file,24,251,123) 
+add_frame(meteor_json_file,sd_video_file,str(22),251,123) 
+add_frame(meteor_json_file,sd_video_file,str(24),251,123) 
