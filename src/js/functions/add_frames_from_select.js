31352000f9de8a4c042451e84ff3b8ac5ca58c31
Perlerin Vincent <vperlerin@gmail.com>
2019-08-01 15:07:59 +0200

    M

--- src/js/functions/add_frames_from_select.js
+++ src/js/functions/add_frames_from_select.js
@@ -187,9 +187,6 @@ function create_meteor_selector_from_frame(frame_id, image_src) {
     // Create frame
     $('#create_frame').click(function() {
 
-        // Hide the modal below (it will be reopened anyway)
-        $('#select_meteor_modal').modal('hide');
-
         loading({'text':'Creating the frame','overlay':true}); 
 
         // Create cropped frame
@@ -251,6 +248,10 @@ function get_frame(cur_fn) {
         success: function(data) { 
             loading_done();  
             data = JSON.parse(data); 
+            
+            // Hide the modal below (it will be reopened anyway)
+            $('#select_meteor_modal').modal('hide');
+            
             create_meteor_selector_from_frame(data.id,data.full_fr); 
         }, 
         error:function() { 
