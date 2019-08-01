140e4ce92ce580e2450e6a6d876febc41a92b69b
Perlerin Vincent <vperlerin@gmail.com>
2019-08-01 16:09:14 +0200

    M

--- src/js/functions/add_frames_from_select.js
+++ src/js/functions/add_frames_from_select.js
@@ -61,10 +61,10 @@ function create_meteor_selector_from_frame(frame_id, image_src) {
             <div id="main_view" style="background-color:#000;background-image:url('+image_src+'); width:'+prev_W+'px; height:'+prev_H+'px; margin: 0 auto; position:relative">\
                 <div id="dl"></div><div id="dt"></div><div id="dr"></div><div id="db"></div>\
                 <div id="selector" style="width:'+cursor_dim+'px; height:'+cursor_dim+'px; border:'+cursor_border_width+'px solid #fff;"></div>\
-                <div id="select_f_tools">\
+                 <div id="select_f_tools">\
                     <div class="drag-h d-flex justify-content-between">\
                         <div><small>Preview</small></div>\
-                        <div><small>X:<span id="pos_x"></span> / Y:<span id="pos_y"></span></small>\
+                        <div><small>X:<span id="pos_x"></span> / Y:<span id="pos_y"></span></small></div>\
                     </div>\
                     <div class="p-1">\
                     <div id="select_preview" style="width:'+preview_dim+'px; height:'+preview_dim+'px"></div>\
