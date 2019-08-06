de1ba0f6a7d11b510955243264f8684fd994c78b
Perlerin Vincent <vperlerin@gmail.com>
2019-08-06 14:14:16 +0200

    M

--- pythonv2/lib/Video_HD_Images_Cron.py
+++ pythonv2/lib/Video_HD_Images_Cron.py
@@ -40,10 +40,10 @@ def get_stack(org_image):
 
     print("WE SEARCH IN " + STACK_FOLDER+date)
 
-    print(str(listdir(STACK_FOLDER+date)))
+    #print(str(listdir(STACK_FOLDER+date)))
  
     #find in STACK_FOLDER/date/ all the files that starts with date and have same cam id
-    stacks = [f for f in listdir(STACK_FOLDER+date) if date in f and cam_id in f and "-tn" not in f and "-night" not in f and "trim" not in f]
+    stacks = [f for f in listdir(STACK_FOLDER+date) if date in f and cam_id in f and "-tn" not in f and "-night" not in f ]
 
     print('STACKS FOUND')
     print(stacks)
