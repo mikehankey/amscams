31dd0fa8c23306bac10b973d2872c94a2cb0d462
Perlerin Vincent <vperlerin@gmail.com>
2019-08-06 10:57:22 +0200

    M

--- pythonv2/lib/Video_Parameters.py
+++ pythonv2/lib/Video_Parameters.py
@@ -6,7 +6,7 @@ from lib.FileIO import load_json_file, save_json_file
 from lib.Get_Operator_info import get_operator_info
  
 
-def get_video_job_default_parameters():
+def create_param_file_if_necessary():
     #Create JSON file if it doesn't exist yet 
     js_file = Path(DEFAULT_VIDEO_PARAM)
     if js_file.is_file()== False: 
@@ -18,4 +18,15 @@ def get_video_job_default_parameters():
 
         f.write('{"param": {"fps":'+ D_FPS+', "extra_logo": "'+D_EXTRA_LOGO+'", "wat_pos":"'+ D_AMS_LOGO_POS +'", "text_pos":"'+D_CAM_INFO_POS+'", "logo_pos":'+D_CUS_LOGO_POS+', "extra_text":"'+  operator_info + '"}}')
         f.close()
+    
+
+def get_video_job_default_parameters():
+
+    create_param_file_if_necessary()
+    
+    #Read the param files and return them
+    mr = load_json_file(DEFAULT_VIDEO_PARAM)
+
+    print(mr)
+    
     return False
