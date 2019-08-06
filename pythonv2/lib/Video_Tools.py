f357a5d95670988598fe34c3cfc9d0c1c4a54553
Perlerin Vincent <vperlerin@gmail.com>
2019-08-06 15:58:09 +0200

    M

--- pythonv2/lib/Video_Tools.py
+++ pythonv2/lib/Video_Tools.py
@@ -14,7 +14,7 @@ from shutil import copyfile
 # blend(org,stack,40,'/mnt/ams2/TMP/test.png')
 def blend(image1, image2, perc_trans_image1, output_file):
     other_perc =  int(perc_trans_image1)/100
-    cmd = 'ffmpeg -y  -i '+image2+' -i '+image1+' -filter_complex "[0:v]scale='+HD_DIM+'[scaled];[scaled]blend=all_mode=\'overlay\':all_opacity='+str(other_perc)+'[out]" -map "[out]" '+ output_file
+    cmd = 'ffmpeg -y -hide_banner -loglevel panic -i '+image2+' -i '+image1+' -filter_complex "[0:v]scale='+HD_DIM+'[scaled];[scaled]blend=all_mode=\'overlay\':all_opacity='+str(other_perc)+'[out]" -map "[out]" '+ output_file
     output = subprocess.check_output(cmd, shell=True).decode("utf-8")    
     return output_file
  
