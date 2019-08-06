d99ab876656cf8cff5211e266642d7f326008a8b
Perlerin Vincent <vperlerin@gmail.com>
2019-08-06 11:04:12 +0200

    M

--- pythonv2/lib/Video_Parameters.py
+++ pythonv2/lib/Video_Parameters.py
@@ -16,17 +16,23 @@ def create_param_file_if_necessary():
         operator = get_operator_info()
         operator_info = operator['name'] + ', ' + operator['obs_name']+ ', ' + operator['city'] + ', ' + operator['state']+ ', ' + operator['country']
 
-        f.write('{"param": {"fps":'+ D_FPS+', "extra_logo": "'+D_EXTRA_LOGO+'", "wat_pos":"'+ D_AMS_LOGO_POS +'", "text_pos":"'+D_CAM_INFO_POS+'", "logo_pos":"'+D_CUS_LOGO_POS+'", "extra_text":"'+  operator_info + '"}}')
+        f.write('{"param": {"fps":'+ D_FPS+', "dim":"'+D_DIM+', "extra_logo": "'+D_EXTRA_LOGO+'", "wat_pos":"'+ D_AMS_LOGO_POS +'", "text_pos":"'+D_CAM_INFO_POS+'", "logo_pos":"'+D_CUS_LOGO_POS+'", "extra_text":"'+  operator_info + '"}}')
         f.close()
     
 
 def get_video_job_default_parameters():
-
     create_param_file_if_necessary()
-    
     #Read the param files and return them
-    mr = load_json_file(DEFAULT_VIDEO_PARAM)
+    return load_json_file(DEFAULT_VIDEO_PARAM)
 
-    print(mr)
-    
-    return False
+
+def save_video_job_parameters(fps,dim,extra_logo,wat_pos,text_pos,logo_pos,extra_text):
+    params = load_json_file(DEFAULT_VIDEO_PARAM)
+    params['fps'] = fps
+    params['dim'] = dim  
+    params['extra_logo'] = extra_logo    
+    params['wat_pos'] = wat_pos    
+    params['text_pos'] = text_pos    
+    params['logo_pos'] = logo_pos    
+    params['extra_text'] = extra_text      
+    save_json_file(params, DEFAULT_VIDEO_PARAM)
