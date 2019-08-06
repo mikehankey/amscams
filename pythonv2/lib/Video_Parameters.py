6b22d1fb1ec8cc30047b9444f9e9daf2b6ab6245
Perlerin Vincent <vperlerin@gmail.com>
2019-08-06 10:49:21 +0200

    M

--- pythonv2/lib/Video_Parameters.py
+++ pythonv2/lib/Video_Parameters.py
@@ -16,6 +16,6 @@ def get_video_job_default_parameters():
         operator = get_operator_info()
         operator_info = operator['name'] + ', ' + operator['obs_name']+ ', ' + operator['city'] + ', ' + operator['state']+ ', ' + operator['country']
 
-        f.write('{"param": {"fps":'+ D_FPS+', "extra_logo": '+D_EXTRA_LOGO+', "wat_pos":'+ D_AMS_LOGO_POS +', "text_pos":'+D_CAM_INFO_POS+', "logo_pos":'+D_CUS_LOGO_POS+', "extra_text":'+  operator_info + '}}')
+        f.write('{"param": {"fps":'+ D_FPS+', "extra_logo": "'+D_EXTRA_LOGO+'", "wat_pos":'+ D_AMS_LOGO_POS +', "text_pos":'+D_CAM_INFO_POS+', "logo_pos":'+D_CUS_LOGO_POS+', "extra_text":"'+  operator_info + '"}}')
         f.close()
     return False
