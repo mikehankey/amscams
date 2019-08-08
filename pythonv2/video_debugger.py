10cd286b415a40b957eb6ec2ba4ca3c58c9a1837
Perlerin Vincent <vperlerin@gmail.com>
2019-08-08 13:47:00 +0200

    D

--- pythonv2/video_debugger.py
+++ pythonv2/video_debugger.py
@@ -75,7 +75,7 @@ from os.path import isfile, join, exists
 
 
  
-array_of_frames, path  = get_hd_frames_from_HD_repo("10038","2019/08/07","2019/08/07 06:40","2019/08/07 08:40")
+array_of_frames, path  = get_hd_frames_from_HD_repo("10038","2019/08/07","2019/08/07 06:40","2019/08/07 11:40")
 print("FRAMES")
 print(array_of_frames) 
 if(array_of_frames is None):
