4e2251bcf6ff8bf4d38a9fdd1e45ed306a040627
Perlerin Vincent <vperlerin@gmail.com>
2019-08-08 15:41:00 +0200

    D

--- pythonv2/lib/Video_Tools.py
+++ pythonv2/lib/Video_Tools.py
@@ -199,7 +199,7 @@ def get_sd_frames_from_HD_video(hd_video_file, camID):
     else: 
         print('CAM ID ' + camID)
         print('date_and_time' + date_and_time)
-        print('NOT FOUND IN ' + +sd_path+'/images')
+        print('NOT FOUND IN ' +  sd_path+'/images')
 
     potential_frames = [f for f in listdir(sd_path+'/passed') if camID in f and date_and_time in f and isfile(join(sd_path, f))]
     if(potential_frames is not None and len(potential_frames)!=0):
@@ -210,7 +210,7 @@ def get_sd_frames_from_HD_video(hd_video_file, camID):
     else: 
         print('CAM ID ' + camID)
         print('date_and_time' + date_and_time)
-        print('NOT FOUND IN ' + +sd_path+'/passed')
+        print('NOT FOUND IN ' +  sd_path+'/passed')
 
     potential_frames = [f for f in listdir(sd_path+'/failed') if camID in f and date_and_time in f and isfile(join(sd_path, f))]
     if(potential_frames is not None and len(potential_frames)!=0):
@@ -221,7 +221,7 @@ def get_sd_frames_from_HD_video(hd_video_file, camID):
     else: 
         print('CAM ID ' + camID)
         print('date_and_time' + date_and_time)
-        print('NOT FOUND IN ' + +sd_path+'/failed')
+        print('NOT FOUND IN ' +  sd_path+'/failed')
 
     potential_videos = [f for f in listdir(sd_path) if camID in f and date_and_time in f and isfile(join(sd_path, f))]
     if(potential_videos is not None and len(potential_videos)!=0):
