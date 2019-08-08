60660e237eff0cab76d1a1a772b0fd5607c185fd
Perlerin Vincent <vperlerin@gmail.com>
2019-08-08 14:07:41 +0200

    D

--- pythonv2/lib/Video_Tools.py
+++ pythonv2/lib/Video_Tools.py
@@ -238,9 +238,9 @@ def get_hd_frames_from_HD_repo(camID,date,start_date,end_date,limit_frame=False)
                 # and blend it with the HD frame
                 print('BEFORE FRAME TO BLEND')
                 frame_to_blend = get_sd_frames_from_HD_video(f, camID)
-                frame_to_blend = TMP_IMG_HD_SRC_PATH + frame_to_blend
                 print('FRAME TO BLEND ' +  frame_to_blend)
                 if(frame_to_blend is not False):
+                frame_to_blend = TMP_IMG_HD_SRC_PATH + frame_to_blend
                     f2 = blend(cur_path + '/' + f,frame_to_blend,40,cur_path + '/' + f)
                     shutil.copy2(f2, tmppath + '/' + f)
                 else:
