d809a6e4b713161a40b1bf96f5134435d96104bf
AMS11 <mike.hankey@gmail.com>
2019-08-12 16:38:00 +0000

    upda

--- pythonv2/cat-videos.py
+++ pythonv2/cat-videos.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-from lib.UtilLib import check_running, get_sun_info, convert_filename_to_date_cam
+#from lib.UtilLib import check_running, get_sun_info, convert_filename_to_date_cam
 import time
 import os
 import glob
@@ -13,6 +13,7 @@ cat_tmp2 = glob_dir + "/mnt/ams2/tmp/tmp2.mp4"
 cat_line = ""
 count = 0
 meteor_files = sorted(glob.glob(glob_dir + "*archiveHD.mp4"))
+meteor_files = sorted(glob.glob(glob_dir + "*HD-meteor.mp4"))
 
 data = ""
 for i in range(0,len(meteor_files)):
