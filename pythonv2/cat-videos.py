2c63652e1b67d585d0112563b1320910519e455c
mikehankey <mike.hankey@gmail.com>
2019-08-12 12:59:05 -0400

    updates

--- pythonv2/cat-videos.py
+++ pythonv2/cat-videos.py
@@ -7,13 +7,14 @@ import sys
 #ffmpeg -i "concat:/mnt/ams2/HD/2018-03-23_02-21-02-cam2-trim-466.mp4|/mnt/ams2/HD/2018-03-23_02-21-02-cam2-stacked.mp4" -c copy /mnt/ams2/HD/2018-03-23_02-21-02-cam2-final.mp4
 
 glob_dir = sys.argv[1]
-cat_out = glob_dir + "/allmeteors.mp4"
+cam_num = "010006"
+cat_out = glob_dir + "/allmeteors" + cam_num + ".mp4"
 cat_tmp1 = glob_dir + "/mnt/ams2/tmp/tmp1.mp4"
 cat_tmp2 = glob_dir + "/mnt/ams2/tmp/tmp2.mp4"
 cat_line = ""
 count = 0
 meteor_files = sorted(glob.glob(glob_dir + "*archiveHD.mp4"))
-meteor_files = sorted(glob.glob(glob_dir + "*HD-meteor.mp4"))
+meteor_files = sorted(glob.glob(glob_dir + "*" + cam_num + "*HD-meteor.mp4"))
 
 data = ""
 for i in range(0,len(meteor_files)):
