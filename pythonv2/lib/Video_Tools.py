1488401e4bdb39fa7013064edc5d91cfc5f2ca9e
Perlerin Vincent <vperlerin@gmail.com>
2019-08-22 12:17:02 +0200

    M

--- pythonv2/lib/Video_Tools.py
+++ pythonv2/lib/Video_Tools.py
@@ -581,5 +581,5 @@ def get_all_meteor_detections(date,start_date,end_date,cam_id):
         cur_date = get_meteor_date_and_time_object(str(detection))
         
         if(cur_date>=start_date_obj and end_date_obj>=cur_date):
-            print("FOUND " + cur_date)
+            print("FOUND " + str(cur_date))
             print(detection)
