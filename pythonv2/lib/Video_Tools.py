970340eadbd47042e2320471b71f847cf3559fe6
Perlerin Vincent <vperlerin@gmail.com>
2019-08-22 15:18:44 +0200

    M

--- pythonv2/lib/Video_Tools.py
+++ pythonv2/lib/Video_Tools.py
@@ -604,6 +604,8 @@ def get_all_detection_frames(path,vid):
     #Extract all frames
     vid_name = vid.split('.') 
     vid_name = vid_name
+    print("VID NAME ")
+    print(vid_name)
     cmd = 'ffmpeg -y -hide_banner -loglevel panic -i ' + path + '/' + vid + ' ' +  TMP_IMG_HD_SRC_PATH + '%04d' + '-' + vid_name + '.png' 
     print(cmd)
     output = subprocess.check_output(cmd, shell=True).decode("utf-8")
