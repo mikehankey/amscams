0b957ccee59d8fa739e8c668dc0101503b1e5518
Perlerin Vincent <vperlerin@gmail.com>
2019-08-23 18:03:26 +0200

    Test

--- pythonv2/test_overlay2.py
+++ pythonv2/test_overlay2.py
@@ -5,4 +5,4 @@ overlay = cv2.imread('./dist/img/ams_logo_vid_anim/1920x1080/AMS30.png')
 
 added_image = cv2.addWeighted(background,0.4,overlay,0.1,0)
 
-cv2.imwrite('mnt/ams2/test4.png', added_image)
+cv2.imwrite('/mnt/ams2/test4.png', added_image)
