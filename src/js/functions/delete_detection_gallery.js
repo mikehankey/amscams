d6b81dfd6b2f4a90286978889974fde29533f691
Perlerin Vincent <vperlerin@gmail.com>
2019-08-23 15:07:27 +0200

    M

--- src/js/functions/delete_detection_gallery.js
+++ src/js/functions/delete_detection_gallery.js
@@ -24,7 +24,7 @@ $(function() {
      })
 
      // Delete All
-     $('#del-all').click(function() {
+     $('.del-all').click(function() {
          // Get all id
      
          var detections = [];
