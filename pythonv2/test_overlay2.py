7759c43ae0e60b3828f6794aaf93bfe81ada91ad
Perlerin Vincent <vperlerin@gmail.com>
2019-08-23 18:06:23 +0200

    Test

--- pythonv2/test_overlay2.py
+++ pythonv2/test_overlay2.py
@@ -1,8 +1,43 @@
 import cv2
+import numpy as np
+
+def overlay_transparent(background, overlay, x, y):
+
+    background_width = background.shape[1]
+    background_height = background.shape[0]
+
+    if x >= background_width or y >= background_height:
+        return background
+
+    h, w = overlay.shape[0], overlay.shape[1]
+
+    if x + w > background_width:
+        w = background_width - x
+        overlay = overlay[:, :w]
+
+    if y + h > background_height:
+        h = background_height - y
+        overlay = overlay[:h]
+
+    if overlay.shape[2] < 4:
+        overlay = np.concatenate(
+            [
+                overlay,
+                np.ones((overlay.shape[0], overlay.shape[1], 1), dtype = overlay.dtype) * 255
+            ],
+            axis = 2,
+        )
+
+    overlay_image = overlay[..., :3]
+    mask = overlay[..., 3:] / 255.0
+
+    background[y:y+h, x:x+w] = (1.0 - mask) * background[y:y+h, x:x+w] + mask * overlay_image
+
+    return background
 
 background = cv2.imread('/mnt/ams2/meteors/2019_08_23/2019_08_23_00_03_23_000_010040-trim-1-HD-meteor-stacked.png')
 overlay = cv2.imread('./dist/img/ams_logo_vid_anim/1920x1080/AMS30.png')
 
-added_image = cv2.addWeighted(background,0.4,overlay,0.1,0)
+added_image = overlay_transparent(background,overlay,0,0)
 
 cv2.imwrite('/mnt/ams2/test4.png', added_image)
