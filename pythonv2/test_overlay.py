65ce636a656d70b20430d3edee1dc792e72a56aa
Perlerin Vincent <vperlerin@gmail.com>
2019-08-23 16:37:08 +0200

    Test

--- pythonv2/test_overlay.py
+++ pythonv2/test_overlay.py
@@ -5,24 +5,28 @@ import cv2
 
 
 def add_info_to_frame_cv(hd_img, date_text, extra_text, text_position, extra_text_position, watermark, watermark_position, logo, logo_pos, enhancement=0):
-   (wH, wW) = watermark.shape[:2]
+   # Get org img & Watermark dimensions
    (h, w) = hd_img.shape[:2]
- 
+   (wH, wW) = watermark.shape[:2]
+   
+   #Get the 4 channels to handle watermark transparency
    (B, G, R, A) = cv2.split(watermark)
    B = cv2.bitwise_and(B, B, mask=A)
    G = cv2.bitwise_and(G, G, mask=A)
    R = cv2.bitwise_and(R, R, mask=A)
    watermark_image = cv2.merge([B, G, R, A])
-
-   print("WI:", watermark_image.shape)
-
+ 
+   #Add 4th dimension to image to deal with watermark transparency
    image = np.dstack([hd_img, np.ones((h, w), dtype="uint8") * 255])
 
+   #Construct overlay for watermark
    overlay = np.zeros((h, w, 4), dtype="uint8")
    print("OVERLAY:", h,  wH, wW, w )
-   overlay[h - wH - 580:h - 580, w - wW - 10:w - 10] = watermark_image
-   overlay[h - wH - 580:h - 580, 10:wW + 10] = watermark_image
-
+   overlay[h - wH - 10:h - 10, w - wW - 10:w - 10] = watermark
+   #overlay[h - wH - 580:h - 580, w - wW - 10:w - 10] = watermark_image
+   #overlay[h - wH - 580:h - 580, 10:wW + 10] = watermark_image
+   
+    
 
    # blend the two images together using transparent overlays
    output = image.copy()
@@ -41,7 +45,7 @@ watermark = cv2.imread("./dist/img/ams_logo_vid_anim/1920x1080/AMS30.png", cv2.I
 
 print(watermark.shape)
 
-logo  = cv2.imread("./dist/img/ams_logo_vid_anim/1920x1080/AMS30.png")
+logo  = ""
 date_text = "test"
 extra_text = "test"
 text_position = 0
