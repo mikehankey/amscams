51c599dc44fd2ccc42f23d22f1b797e0d6e8bab4
Perlerin Vincent <vperlerin@gmail.com>
2019-08-23 18:08:16 +0200

    Test

--- pythonv2/test_overlay2.py
+++ pythonv2/test_overlay2.py
@@ -38,6 +38,6 @@ def add_overlay(background, overlay, x, y):
 background = cv2.imread('/mnt/ams2/meteors/2019_08_23/2019_08_23_00_03_23_000_010040-trim-1-HD-meteor-stacked.png')
 overlay = cv2.imread('./dist/img/ams_logo_vid_anim/1920x1080/AMS30.png', cv2.IMREAD_UNCHANGED)
 
-added_image = add_overlay(background,overlay,0,0)
+added_image = add_overlay(background,overlay,10,10)
 
 cv2.imwrite('/mnt/ams2/test4.png', added_image)
