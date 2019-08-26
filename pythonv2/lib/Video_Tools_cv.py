f8cbc39af9f46433d08cd97e8d6beb3f625762ad
Perlerin Vincent <vperlerin@gmail.com>
2019-08-26 14:22:11 +0200

    V

--- pythonv2/lib/Video_Tools_cv.py
+++ pythonv2/lib/Video_Tools_cv.py
@@ -1,8 +1,18 @@
 import cv2
 import numpy as numpy
 from lib.VIDEO_VARS import *   
-from lib.Video_Tools_cv_lib import *
+from lib.Video_Tools_cv_pos import *
 from PIL import ImageFont, ImageDraw, Image  
+
+
+# Add text to x,y to a PIL image
+def add_text(pil_background,text,x,y,font):
+    # Draw the text
+    pil_background.text((x, y), text, font=font)  
+
+    # Return an OpenCV 
+    return cv2.cvtColor(np.array(pil_background), cv2.COLOR_RGB2BGR)  
+
   
 # Add text over background
 # WARNING: bg is NOT a cv image but a full path (for PIL)
@@ -11,13 +21,14 @@ from PIL import ImageFont, ImageDraw, Image
 # ex: if line_number = 1 => first line at this position
 #                    = 2 => second line at this position
 # return updated cv matrix
-def add_text(background,text,position,line_number=1):
+def add_text_to_pos(background,text,position,line_number=1):
     # Convert background to RGB (OpenCV uses BGR)  
     cv2_background_rgb = cv2.cvtColor(background,cv2.COLOR_BGR2RGB)  
     
     # Pass the image to PIL  
     pil_im = Image.fromarray(cv2_background_rgb)  
     draw = ImageDraw.Draw(pil_im)  
+
 
     # use DEFAULT truetype font  
     if(line_number==1):
@@ -30,10 +41,7 @@ def add_text(background,text,position,line_number=1):
     y,x = get_text_position_cv(background,text,position,line_number,font)
 
     # Draw the text
-    draw.text((x, y), text, font=font)  
-
-    # Get back the image to OpenCV  
-    cv2_im_processed = cv2.cvtColor(np.array(pil_im), cv2.COLOR_RGB2BGR)  
+    cv2_im_processed = add_text(pil_im,x,y,font)
     
     return cv2_im_processed
   
@@ -76,3 +84,11 @@ def add_overlay_cv(background, overlay, position):
     background[y:y+h, x:x+w] = (1.0 - mask) * background[y:y+h, x:x+w] + mask * overlay_image
 
     return background
+
+
+# Add radiant to a frame
+def add_radiant_cv(background,x,y,text):
+    cv2.circle(background,(x,y), 25 , (128,128,128), 1)
+    background = add_text(background,text,)
+    cv2.putText(image, "Perseid Radiant",  (new_cat_x, new_cat_y), cv2.FONT_HERSHEY_SIMPLEX, .5, (145, 145, 145), 1)
+
