1f25ec99690c192259c3dbd6bc61abc9f4fe437a
Perlerin Vincent <vperlerin@gmail.com>
2019-08-26 12:55:34 +0200

    V

--- pythonv2/lib/Video_Tools_cv_lib.py
+++ pythonv2/lib/Video_Tools_cv_lib.py
@@ -28,7 +28,7 @@ def get_text_position_cv(background,text,position,line_number,font,margins=VIDEO
         if(line_number==1):
             return VIDEO_MARGINS,VIDEO_MARGINS    
         else:
-            return VIDEO_MARGINS+text_h*line_number,VIDEO_MARGINS    
+            return VIDEO_LINE_HEIGHT+text_h*line_number,VIDEO_MARGINS    
  
 
      
