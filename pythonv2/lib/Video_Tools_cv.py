660149ff5e41ebe302aec9f9290541eb67f712cf
Perlerin Vincent <vperlerin@gmail.com>
2019-08-26 15:10:58 +0200

    V

--- pythonv2/lib/Video_Tools_cv.py
+++ pythonv2/lib/Video_Tools_cv.py
@@ -5,7 +5,12 @@ from lib.Video_Tools_cv_pos import *
 from PIL import ImageFont, ImageDraw, Image  
 
 
-# Add text on x,y with default small font
+# Add Radiant symbol + text to a given possition
+def add_radiant(background,text,x,y):
+    
+
+
+# Add text on x,y with default small font (for radiant or other info)
 def add_text(background,text,x,y):
     # Convert background to RGB (OpenCV uses BGR)  
     cv2_background_rgb = cv2.cvtColor(background,cv2.COLOR_BGR2RGB)  
@@ -50,18 +55,16 @@ def add_text_to_pos(background,text,position,line_number=1):
     cv2_im_processed = cv2.cvtColor(np.array(pil_im), cv2.COLOR_RGB2BGR)  
     
     return cv2_im_processed
-  
 
-# Add semi-transparent overlay over background
-# Position = br, bl, tr, tl (ex: br = bottom right)
+
+
+# Add semi-transparent overlay over background on x,y
 # return updated cv matrix
-def add_overlay_cv(background, overlay, position):
+def add_overlay_x_y_cv(background, overlay, x, y):
 
     background_width,background_height  = background.shape[1], background.shape[0]
 
-    # Get overlay position - see lib.Video_Tools_cv_lib
-    x,y = get_overlay_position_cv(background,overlay,position) 
-    
+     
     if x >= background_width or y >= background_height:
         return background
 
@@ -90,6 +93,17 @@ def add_overlay_cv(background, overlay, position):
     background[y:y+h, x:x+w] = (1.0 - mask) * background[y:y+h, x:x+w] + mask * overlay_image
 
     return background
+
+# Add semi-transparent overlay over background
+# Position = br, bl, tr, tl (ex: br = bottom right)
+# return updated cv matrix
+def add_overlay_cv(background, overlay, position):
+
+    background_width,background_height  = background.shape[1], background.shape[0]
+
+    # Get overlay position - see lib.Video_Tools_cv_lib
+    x,y = get_overlay_position_cv(background,overlay,position) 
+    return add_overlay_x_y_cv(background, overlay, x, y)
 
 
 # Add radiant to a frame
