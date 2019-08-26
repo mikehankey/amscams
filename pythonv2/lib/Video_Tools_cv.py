3207b24d644b7d28d25323a73bdb43e179424c65
Perlerin Vincent <vperlerin@gmail.com>
2019-08-26 11:32:49 +0200

    V

--- pythonv2/lib/Video_Tools_cv.py
+++ pythonv2/lib/Video_Tools_cv.py
@@ -30,6 +30,9 @@ def add_overlay_cv(background, overlay, position):
     background_width,background_height  = background.shape[1], background.shape[0]
 
     x,y = get_overlay_position_cv(background,overlay,position)
+    print("X " + str(x))
+    print("Y " + str(y))
+    
     
     if x >= background_width or y >= background_height:
         return background
