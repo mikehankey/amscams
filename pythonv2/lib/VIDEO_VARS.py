28172530e6ee9d1a0edb1d2d420409144bb79e5d
Perlerin Vincent <vperlerin@gmail.com>
2019-08-26 12:56:17 +0200

    V

--- pythonv2/lib/VIDEO_VARS.py
+++ pythonv2/lib/VIDEO_VARS.py
@@ -71,4 +71,4 @@ VIDEO_MARGINS = 10
 #DEFAULT FONTS
 VIDEO_FONT = "/home/ams/amscams/dist/fonts/Roboto_Condensed/RobotoCondensed-Regular.ttf"
 VIDEO_FONT_SIZE=20
-VIDEO_LINE_HEIGHT = int(VIDEO_FONT_SIZE/2)
+VIDEO_LINE_HEIGHT = VIDEO_FONT_SIZE/3
