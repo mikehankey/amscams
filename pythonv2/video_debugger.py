66d772bab0b6ebcb4d118f7afbeec2bf92672485
Perlerin Vincent <vperlerin@gmail.com>
2019-08-26 11:28:45 +0200

    V

--- pythonv2/video_debugger.py
+++ pythonv2/video_debugger.py
@@ -107,6 +107,8 @@ from os.path import isfile, join, exists
 background = cv2.imread('/mnt/ams2/meteors/2019_08_23/2019_08_23_00_03_23_000_010040-trim-1-HD-meteor-stacked.png')
 overlay = cv2.imread('../dist/img/ams_logo_vid_anim/1920x1080/AMS30.png', cv2.IMREAD_UNCHANGED)
 
+print(overlay)
+
 added_image = add_overlay_cv(background,overlay,'tl')
 cv2.imwrite('/mnt/ams2/test_tl.png', added_image)
 
