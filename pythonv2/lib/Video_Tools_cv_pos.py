4dce6ad5ea17e65c5ab41d9ec99a728230f06caf
Perlerin Vincent <vperlerin@gmail.com>
2019-08-27 10:49:07 +0200

    V

--- pythonv2/lib/Video_Tools_cv_pos.py
+++ pythonv2/lib/Video_Tools_cv_pos.py
@@ -13,7 +13,7 @@ from lib.VIDEO_VARS import *
 def get_text_position_cv(background,text,position,line_number,font,margins=VIDEO_MARGINS):
     
     # Get font.getsize(txt)
-    text_w, text_h = font.getsize(text) 
+    text_w, text_h = font.getsize(text.lower()+',') 
 
     print('***********')
     print(text)
