884db940d0113f3d935c67cf0d22bb7266ad2916
Perlerin Vincent <vperlerin@gmail.com>
2019-08-27 11:46:23 +0200

    V

--- pythonv2/video_debugger.py
+++ pythonv2/video_debugger.py
@@ -140,14 +140,7 @@ from os.path import isfile, join, exists
 
 
 ################### TEST NEW REMASTER
-# OVERLAP WITH LOGO TR
-data = {
-    'json_conf' : '/mnt/ams2/meteor_archive/2019_08_12/2019_08_12_05_38_08_880_010037_AMS7_HD.json',
-    'video_file' :'/mnt/ams2/meteor_archive/2019_08_12/2019_08_12_05_38_08_880_010037_AMS7_HD.mp4',
-    'rad_x': 500,
-    'rad_y': 500,
-    'rad_name': 'Perseids'
-}
+
 
 
 # DOESNT OVERLAP  
@@ -159,7 +152,14 @@ data = {
     'rad_name': 'Perseids'
 }
 
-
+# OVERLAP WITH LOGO TR
+data = {
+    'json_conf' : '/mnt/ams2/meteor_archive/2019_08_12/2019_08_12_05_38_08_880_010037_AMS7_HD.json',
+    'video_file' :'/mnt/ams2/meteor_archive/2019_08_12/2019_08_12_05_38_08_880_010037_AMS7_HD.mp4',
+    'rad_x': 500,
+    'rad_y': 500,
+    'rad_name': 'Perseids'
+}
  
 
 remaster(data)
