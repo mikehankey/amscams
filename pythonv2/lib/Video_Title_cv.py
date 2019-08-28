e24a31a34372e06f1a64e8faf8995a3887b36315
Perlerin Vincent <vperlerin@gmail.com>
2019-08-28 16:16:32 +0200

    V Title

--- pythonv2/lib/Video_Title_cv.py
+++ pythonv2/lib/Video_Title_cv.py
@@ -10,6 +10,25 @@ TITLE_1280x720 = "/home/ams/amscams/dist/vids/ams_intro/1280x720.mp4"
 DEFAULT_TITLE = TITLE_1280x720
 
 
+
+# Add big text centered 
+def add_text(background,text,y):
+
+    # Convert background to RGB (OpenCV uses BGR)  
+    cv2_background_rgb = cv2.cvtColor(background,cv2.COLOR_BGR2RGB)  
+    
+    # Pass the image to PIL to use ttf fonts
+    pil_im  = Image.fromarray(cv2_background_rgb)  
+    draw    = ImageDraw.Draw(pil_im)  
+    font    = ImageFont.truetype(VIDEO_FONT_BOLD, 50)  
+
+    x = cv2_background_rgb.shape[1]/2 - font.getsize(text)[0]/2
+    
+    
+    draw.text((x, y), text, font=font, fill=VIDEO_FONT_SMALL_COLOR)  
+    return  cv2.cvtColor(np.array(pil_im), cv2.COLOR_RGB2BGR)  
+
+
 # This function create a quick video (lenght of DEFAULT_TITLE ~ 3sec )
 # with the animated AMS logo and a custom text (ONE LINE TEXT ONLY)
 def create_title_video(text,output):
@@ -30,11 +49,7 @@ def create_title_video(text,output):
             go = 0 
     
     cap.release()
-  
-    print(str(len(frames)) + " found")
-
-
-    #frames = load_video_frames(DEFAULT_TITLE,"")
+   
     new_frames = []
 
     for frame in frames:
@@ -42,8 +57,10 @@ def create_title_video(text,output):
         #Convert to proper colors
         n_frame = frame
 
-        #Add Text
-        n_frame = add_text(n_frame,text,0,0,True)
+        #Add Text 
+
+         n_frame = add_text(n_frame,text,x,y,centered=False, 50)
+
         new_frames.append(n_frame)
 
     make_movie_from_frames(new_frames, [0,len(new_frames) - 1], output, 1)
