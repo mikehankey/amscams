59d7b5521e99b84f141c5e73efa3a6b2ed603c6f
Perlerin Vincent <vperlerin@gmail.com>
2019-08-28 12:31:37 +0200

    Title Videos

--- pythonv2/lib/Video_Title_cv.py
+++ pythonv2/lib/Video_Title_cv.py
@@ -9,8 +9,7 @@ from lib.VideoLib import load_video_frames, make_movie_from_frames
 TITLE_1280x720 = "/home/ams/amscams/dist/vids/ams_intro/1280x720.mp4"
 DEFAULT_TITLE = TITLE_1280x720
 
- 
- 
+
 # This function create a quick video (lenght of DEFAULT_TITLE ~ 3sec )
 # with the animated AMS logo and a custom text (ONE LINE TEXT ONLY)
 def create_title_video(text,output):
