f98e613228e762dae8c2725af8c01ba9599f98a0
Perlerin Vincent <vperlerin@gmail.com>
2019-08-28 12:09:51 +0200

    Title Videos

--- pythonv2/lib/Video_Title_cv.py
+++ pythonv2/lib/Video_Title_cv.py
@@ -3,28 +3,20 @@ import numpy as numpy
 from lib.VIDEO_VARS import * 
 from lib.Video_Tools_cv import add_text
 
+# From Mike
+from lib.VideoLib import load_video_frames, make_movie_from_frames
+
 TITLE_1280x720 = "/home/ams/amscams/dist/vids/ams_intro/1280x720.mp4"
 DEFAULT_TITLE = TITLE_1280x720
 
-
-# Return All frames from video
-def get_video_frames(_input):
-    frames = []
-    vidcap = cv2.VideoCapture(_input)
-    success,frame = vidcap.read() 
-    while success:
-        frames.append(frame)
-        success,frame = vidcap.read()
-    vidcap.release()
-    return frames
-
+ 
  
 # This function create a quick video (lenght of DEFAULT_TITLE ~ 3sec )
 # with the animated AMS logo and a custom text (ONE LINE TEXT ONLY)
 def create_title_video(text,output):
 
     # Get the original frames
-    frames = get_video_frames(DEFAULT_TITLE)
+    frames = load_video_frames(DEFAULT_TITLE)
     new_frames = []
 
     for frame in frames:
