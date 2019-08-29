303028541e18c97bffa60edf5065ff0231a1c3aa
Perlerin Vincent <vperlerin@gmail.com>
2019-08-29 16:39:33 +0200

    M

--- pythonv2/lib/Video_Title_cv.py
+++ pythonv2/lib/Video_Title_cv.py
@@ -108,8 +108,13 @@ def create_title_video(text,text2,output,title_color=(255,255,255,255), rect= Fa
         #rect_x = int(fc*((rect_min_x-rect_x_init)/rect_anim_duration)+rect_x_init)
         #rect_w = int(fc*(max_rect_half_width/rect_anim_duration))
         if(rect is True):
-            rect_x = int(rect_x - fc*(rect_x-rect_min_x)/rect_anim_duration)
-            rect_w = int(rect_w + fc*(rect_max_w/2/rect_anim_duration))
+
+            if(rect_x > rect_min_x):
+                rect_x = int(rect_x - fc*(rect_x-rect_min_x)/rect_anim_duration)
+
+            if(rect_w < rect_max_w):
+                rect_w = int(rect_w + fc*(rect_max_w/2)/rect_anim_duration)
+
             print("REC " + str(rect_x) + " => " +  str(rect_w))
 
             cv2.rectangle(n_frame, (rect_x, rect_y), (rect_x+rect_w, rect_y+rect_h),title_color, 1)
