a47df35802b6ffb558617616109c4ab7629b4ec1
Perlerin Vincent <vperlerin@gmail.com>
2019-08-29 11:46:30 +0200

    M

--- pythonv2/lib/Video_Title_cv.py
+++ pythonv2/lib/Video_Title_cv.py
@@ -53,7 +53,7 @@ def create_title_video(text,text2,output):
 
     #AMS LOGO
     anim_duration  = 85
-    end_fade_in_ams_logo = 35
+    end_fade_in_ams_logo = 25
 
     # TITLE OF THE VIDEO
     title_duration = 95
