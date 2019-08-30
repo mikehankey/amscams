086aa403ab0bc6383c559b4740ce136d3d5f80ec
Perlerin Vincent <vperlerin@gmail.com>
2019-08-30 18:29:24 +0200

    M

--- pythonv2/lib/Video_Title_cv.py
+++ pythonv2/lib/Video_Title_cv.py
@@ -243,8 +243,8 @@ def concat_videos_fade(video1,video2,output):
             -vcodec libx264 -map [outv] '+output
 
     cmd = ' ffmpeg -i '+video1+' -i '+video2+' -f lavfi -i color=black -filter_complex \
-            "[0:v]format=pix_fmts=yuva420p,fade=t=out:st=4:d=1:alpha=1,setpts=PTS-STARTPTS[va0];\
-             [1:v]format=pix_fmts=yuva420p,fade=t=in:st=0:d=1:alpha=1,setpts=PTS-STARTPTS+4/TB[va1];\
+            "[0:v]scale=1280x720,format=pix_fmts=yuva420p,fade=t=out:st=4:d=1:alpha=1,setpts=PTS-STARTPTS[va0];\
+             [1:v]scale=1280x720,format=pix_fmts=yuva420p,fade=t=in:st=0:d=1:alpha=1,setpts=PTS-STARTPTS+4/TB[va1];\
              [2:v]scale=1280x720,trim=duration=9[over];\
              [over][va0]overlay[over1];\
              [over1][va1]overlay=format=yuv420[outv]" \
