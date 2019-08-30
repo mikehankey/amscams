3838afb5fef47604cccb488d5a5bf32a761d3cd4
Perlerin Vincent <vperlerin@gmail.com>
2019-08-30 15:14:24 +0200

    M

--- pythonv2/lib/MeteorReducePage.py
+++ pythonv2/lib/MeteorReducePage.py
@@ -4,9 +4,10 @@ import cgitb
 # PATTERN FOR THE FILE NAMES
 # YYYY_MM_DD_HH_MM_SS_MSS_CAM_STATION[_HD].EXTENSION
 FILE_NAMES_REGEX = r"(\d{4})_(\d{2})_(\d{2})_(\d{2})_(\d{2})_(\d{2})_(\d{3})_(\d{6})_([^_^.]+)(_HD)?(\.)?(\.[0-9a-z]+$)"
-FILE_NAMES_REGEX_GROUP = ["full","year","month","day","hour","min","sec","ms","cam_id","station_id","HD","ext"]
+FILE_NAMES_REGEX_GROUP = ["name","year","month","day","hour","min","sec","ms","cam_id","station_id","HD","ext"]
 
-
+# Parses a regexp (FILE_NAMES_REGEX) a file name
+# and returns all the info defined in FILE_NAMES_REGEX_GROUP
 def name_analyser(file_names):
    matches = re.finditer(FILE_NAMES_REGEX, file_names, re.MULTILINE)
    res = {}
@@ -24,7 +25,7 @@ def name_analyser(file_names):
 # GENERATES THE REDUCE PAGE METEOR
 # from a URL 
 # cmd=reduce2
-# &video_file=[VIDEO_FILE].mp4
+# &video_file=[PATH]/[VIDEO_FILE].mp4
 def reduce_meteor2(json_conf,form):
    
    # Debug
@@ -34,19 +35,22 @@ def reduce_meteor2(json_conf,form):
    video_file    = form.getvalue("video_file")
    analysed_name = name_analyser(video_file)
 
+   # Test if the name is ok
    if(len(analysed_name)==0):
       print("FILE NAME " + video_file + " is not valid")
       exit
    else:
       print(analysed_name)
 
-   # Is it HD?
+   # Is it HD? & retrieve the related JSON file that contains the reduced data
    if("HD" in analysed_name):
       HD = True
       meteor_json_file = video_file.replace("_HD.mp4", ".json") 
    else:
       HD = False
-      meteor_json_file = video_file.replace(".mp4", ".json") 
+      meteor_json_file = video_file.replace(".mp4", ".json")
+
+   
 
     
 
