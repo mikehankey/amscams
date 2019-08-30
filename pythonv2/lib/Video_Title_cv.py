6518ea42e4db5afa85775b4c4b8c0ede634916b5
Perlerin Vincent <vperlerin@gmail.com>
2019-08-30 16:34:27 +0200

    M

--- pythonv2/lib/Video_Title_cv.py
+++ pythonv2/lib/Video_Title_cv.py
@@ -164,7 +164,7 @@ def create_thank_operator_video(operators,duration,output,_with_line_animation,l
             rect_w = 1280-rect_x*2     
             print('RECT X' + str(rect_x))
             print('RECT W ' + str(rect_w))
-            new_frame = cv2.rectangle(frame, (rect_x, rect_y), (rect_x+rect_w, rect_y+rect_h),(255,255,255,255), 1)
+            new_frame = cv2.rectangle(new_frame, (rect_x, rect_y), (rect_x+rect_w, rect_y+rect_h),(255,255,255,255), 1)
 
         all_frames.append(new_frame)        
 
