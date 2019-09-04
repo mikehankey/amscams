9f367f154a3311f4dcbd6c6ed4503744a61d898f
Perlerin Vincent <vperlerin@gmail.com>
2019-09-04 15:32:39 +0200

    M

--- pythonv2/video_debugger.py
+++ pythonv2/video_debugger.py
@@ -264,11 +264,13 @@ thumb_y = 0
 
 if(x<=0):
 
+   # Destation in thumb (img)
    thumb_dest_x = int(thumb_w/2 + x)
    thumb_dest_y = 0
    thumb_dest_w = int(thumb_w-thumb_dest_x)
    thumb_dest_h = int(thumb_h-thumb_dest_y)
 
+   # Part of original image
    org_x = 0
    org_y = y
    org_w = int(x + (org_select_x/2))
@@ -289,11 +291,14 @@ print("H : " + str(org_h))
 
 #   x = 0
 #   y = int(y - org_select_y/2)
+ 
 
-cropped_org = img[org_x:org_w,org_y:org_h]
-print("CROPPED ORG " + str(cropped_org.shape[0]) + " " + str(cropped_org.shape[1]))
 
-crop_img[thumb_dest_x:thumb_dest_w,thumb_dest_y:thumb_dest_h] = cropped_org
+#cropped_org = img[org_x:org_w,org_y:org_h] 
+#crop_img[thumb_dest_x:thumb_dest_w,thumb_dest_y:thumb_dest_h] = cropped_org
+
+
+crop_img[25:50,0:50] = img[0:25,300:350]
 cv2.imwrite('/mnt/ams2/test.png',img[org_x:org_w,org_y:org_h])
 
 
