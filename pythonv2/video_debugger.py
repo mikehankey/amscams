477b17c264566d08cb5439e51b250ab84df5a6dc
Perlerin Vincent <vperlerin@gmail.com>
2019-09-04 17:12:45 +0200

    M

--- pythonv2/video_debugger.py
+++ pythonv2/video_debugger.py
@@ -306,7 +306,7 @@ def create_crop(file,x,y,dest):
       thumb_dest_y = int(thumb_h+diff_y_bottom)  
 
       # Part of the original image    
-      org_h =  thumb_dest_h - thumb_dest_y
+      org_h =  org_y + (thumb_dest_h - thumb_dest_y)
 
    print(" ")
    print("IN THE CROPPED ")
