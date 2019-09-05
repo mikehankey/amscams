a316821bc49b5080e49945c83dd4bf8b7238eaed
Perlerin Vincent <vperlerin@gmail.com>
2019-09-05 10:41:13 +0200

    M

--- pythonv2/lib/MeteorReducePage.py
+++ pythonv2/lib/MeteorReducePage.py
@@ -237,8 +237,9 @@ def generate_cropped_frames(video_full_path,analysed_name,meteor_json_file,HD_fr
          start_event_time = datetime.datetime.strptime(meteor_json_file['event_start_time'], "%Y-%m-%d %H:%M:%S.%f")
          print ("start_event_time {:%Y-%m-%d %H:%M:%S.%f}".format(start_event_time)  + "<br>" )
          diff_in_sec  =  start_event_time-start_video_time
-         print(diff_in_sec)
-         print(diff_in_sec.seconds)
+         print(diff_in_sec + "<br>")
+         print(diff_in_sec.seconds + "sec<br>")
+         print(diff_in_sec.milliseconds + "sec<br>")
 
    for frame in meteor_frame_data: 
 
