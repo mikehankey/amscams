9a71f73eb0f00545f679ff415fba133441c8a4b7
Perlerin Vincent <vperlerin@gmail.com>
2019-09-06 13:26:33 +0200

    M

--- pythonv2/lib/MeteorReducePage.py
+++ pythonv2/lib/MeteorReducePage.py
@@ -39,15 +39,10 @@ def reduce_meteor2(json_conf,form):
       print_error(video_full_path + " <b>is not valid video file name.</b>")
    elif(os.path.isfile(video_full_path) is False):
       print_error(video_full_path + " <b>not found.</b>")
-  
-   # Is it HD? & retrieve the related JSON file that contains the reduced data
-   if("HD" in analysed_name):
-      HD = True
-      meteor_json_file = video_full_path.replace("_HD.mp4", ".json") 
-   else:
-      HD = False
-      meteor_json_file = video_full_path.replace(".mp4", ".json")
 
+   # Get JSON File
+   meteor_json_file = video_full_path.replace(".mp4", ".json") 
+   
    # Does the JSON file exists?
    if(os.path.isfile(meteor_json_file) is False):
       print_error(meteor_json_file + " <b>not found.</b><br>This detection may had not been reduced yet or the reduction failed.")
