3f8151426a2610363433594c77f81bffcd95ce5d
Perlerin Vincent <vperlerin@gmail.com>
2019-09-09 11:31:51 +0200

    M

--- src/js/ui/canvas-interactions.js
+++ src/js/ui/canvas-interactions.js
@@ -216,7 +216,9 @@ if ($('canvas#c').length!=0) {
 
       // End Loading Animation
       loading_done(); 
-  
+      
+
+      console.log("CANVAS DIM " + canvas.width + " " + canvas.height)
     },
     {
       width: canvas.width,
