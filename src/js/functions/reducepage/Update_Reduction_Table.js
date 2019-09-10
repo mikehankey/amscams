1e44110541b34f47dcdf945035cc045a01ee6c50
Perlerin Vincent <vperlerin@gmail.com>
2019-09-10 10:57:22 +0200

    M

--- src/js/functions/reducepage/Update_Reduction_Table.js
+++ src/js/functions/reducepage/Update_Reduction_Table.js
@@ -29,8 +29,7 @@ function update_reduction_on_canvas_and_table(json_resp) {
      
     
     $.each(smf, function(i,v){
- 
-        
+  
         // Get thumb path
         var frame_id = parseInt(v[1]);
         var thumb_path = my_image.substring(0,my_image.indexOf('-half')) + '-frm' + frame_id + '.png';
