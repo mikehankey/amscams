d50ba6d5f406f070aae5089f770c643015f78150
Perlerin Vincent <vperlerin@gmail.com>
2019-09-10 15:04:53 +0200

    M

--- pythonv2/lib/MeteorReduce_Ajax_Tools.py
+++ pythonv2/lib/MeteorReduce_Ajax_Tools.py
@@ -2,6 +2,7 @@ import sys
 import cgitb
 import json
 import sys
+import os
 
 from lib.FileIO import cfe, load_json_file, save_json_file
 from lib.MeteorReduce_Tools import get_cache_path, name_analyser, EXT_CROPPED_FRAMES, new_crop_thumb, get_HD_frame, get_thumb
