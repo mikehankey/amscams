cad24313dfa0a9eb6d6771495b9699fc050737e0
Perlerin Vincent <vperlerin@gmail.com>
2019-09-12 14:43:15 +0200

    M

--- pythonv2/frame_debugger.py
+++ pythonv2/frame_debugger.py
@@ -6,5 +6,5 @@ from lib.MeteorReduce_Tools import *
 
 json_file = "/mnt/ams2/meteor_archive/AMS7/METEOR/2019/08/14/2019_08_14_09_32_02_520_010040_AMS7_HD.json"
 meteor_json_file = load_json_file(json_file) 
-get_frame_time(meteor_json_file,29)
+print(get_frame_time(meteor_json_file,29))
 
