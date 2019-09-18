dbf122bb73fb18431709d2cdfd0b8e2c9878c267
Perlerin Vincent <vperlerin@gmail.com>
2019-09-18 16:23:36 +0200

    HD Frames for Timelapses

--- pythonv2/video_debugger.py
+++ pythonv2/video_debugger.py
@@ -181,34 +181,34 @@ from lib.Video_Title_cv import *
 #import subprocess 
 
 #CREATE TITLE VIDEO
-_main_title = "BEST OF PERSEIDS 2019"
-_producer =  ""
-_output_path = '/mnt/ams2/opening_title.mp4'
+#_main_title = "BEST OF PERSEIDS 2019"
+#_producer =  ""
+#_output_path = '/mnt/ams2/opening_title.mp4'
 #create_title_video(_main_title,_producer,_output_path,(255,255,255,255),True)
 
 
 #CREATE OPERATOR VIDEOS
-_operators = ["Mike Hankey","Elizabeth Warner - UMD", "Ed Abel", "Peter Deterline", "Vishnu Reddy", "Bob Lunsford", "Mel Helm - SRO"]
-_duration = 75 # In frames at 25fps
-_output_path =  '/mnt/ams2/operator_credits.mp4'
-_with_line_animation = True # Optional - it's True by default
-_line_height = 55 # Optional - it's 45 by default, it works well with <=12 operators (one per line)
-_operator_font_size = 40 # Optional - it's 30 by default, it works well with <=12 operators (one per line)
+#_operators = ["Mike Hankey","Elizabeth Warner - UMD", "Ed Abel", "Peter Deterline", "Vishnu Reddy", "Bob Lunsford", "Mel Helm - SRO"]
+#_duration = 75 # In frames at 25fps
+#_output_path =  '/mnt/ams2/operator_credits.mp4'
+#_with_line_animation = True # Optional - it's True by default
+#_line_height = 55 # Optional - it's 45 by default, it works well with <=12 operators (one per line)
+#_operator_font_size = 40 # Optional - it's 30 by default, it works well with <=12 operators (one per line)
 #create_thank_operator_video(_operators, _duration, _output_path,_with_line_animation,_line_height,_operator_font_size) 
 
 #CREATE ALL SKY CAMS VIDEOS
-_text1= "Allskycams.com"
-_text2= ""
-_duration = 100 # In frames at 25fps
-_output = '/mnt/ams2/allskycams.mp4'
+#_text1= "Allskycams.com"
+#_text2= ""
+#_duration = 100 # In frames at 25fps
+#_output = '/mnt/ams2/allskycams.mp4'
 #create_allskycams_video(_text1,_text2,_duration,_output)
 
 #CREATE MUSIC CREDIT VIDEO
-_text1= "Music by"
-_text2= "NAKED JUNGLE"
-_text3= "nakedjungle.bandcamp.com"
-_duration = 50 # In frames at 25fps
-_output = '/mnt/ams2/music_credits.mp4'
+#_text1= "Music by"
+#_text2= "NAKED JUNGLE"
+#_text3= "nakedjungle.bandcamp.com"
+#_duration = 50 # In frames at 25fps
+#_output = '/mnt/ams2/music_credits.mp4'
 #create_credit_video(_text1,_text2,_text3,_duration,_output)
 # PRODUCE CREEDIT
 #create_credit_video("Produced by","Mike Hankey","",_duration,'/mnt/ams2/producedby.mp4')
@@ -233,12 +233,31 @@ _total_duration = 12 # in seconds
 import cv2
 import sys
 import numpy as np
-from lib.MeteorReduce_Tools import new_crop_thumb
+from lib.MeteorReduce_Tools import *
 from lib.VIDEO_VARS import *
-
+from lib.Video_Timelapse import *
+from lib.Video_Tools import *
  
+#new_crop_thumb("/mnt/ams2/CACHE/AMS7/2019/08/14/2019_08_14_09_32_02_520_010040_AMS7_HD/FRAMES/2019_08_14_09_32_02_520_010040_AMS7_HD_HDfr_0032.png",500,1060,"/mnt/ams2/test1.png",True)  
+#print("/mnt/ams2/test1.png") 
 
 
 
-new_crop_thumb("/mnt/ams2/CACHE/AMS7/2019/08/14/2019_08_14_09_32_02_520_010040_AMS7_HD/FRAMES/2019_08_14_09_32_02_520_010040_AMS7_HD_HDfr_0032.png",500,1060,"/mnt/ams2/test1.png",True)  
-print("/mnt/ams2/test1.png") 
+
+############# TEST TIMELAPSES FROM JPEGs
+job = {
+   "cam_id": "010042",
+   "date": "2019/09/17",
+   "stard_date": "2019/09/17 00:00:00",
+   "end_date": "2019/09/17 23:59:59",
+   "fps": 25,
+   "dim":"1920x1080",
+   "text_pos": "bl",
+   "wat_pos": "tl",
+   "extra_text": "br",
+   "logo": "",
+   "logo_pos": "",
+   "blend_sd": 0
+}
+video_path =  generate_timelapse(job['cam_id'],job['date'],job['start_date'],job['end_date'],job['fps'],job['dim'],job['text_pos'],job['wat_pos'],job['extra_text'],job['logo'],job['logo_pos'],job['blend_sd'],0) 
+print(video_path)       
