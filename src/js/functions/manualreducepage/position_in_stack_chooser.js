f0546b7b41ac6fd6a6bc0f231fc217ca6160ba0d
Perlerin Vincent <vperlerin@gmail.com>
2019-09-19 14:02:26 +0200

    Manual Reduction Page

--- src/js/functions/manualreducepage/position_in_stack_chooser.js
+++ src/js/functions/manualreducepage/position_in_stack_chooser.js
@@ -18,7 +18,7 @@ function create_meteor_selector_from_frame(image_src) {
    var W_factor = real_W/prev_W;
    var H_factor = real_H/prev_H; 
 
-   $('<h1>Manual Reduction Step 1</h1><p>Select the first and last point of the visible path of the meteor.</p></p><div id="main_view" style="background-color:#000;background-image:url('+image_src+'); width:'+prev_W+'px; height:'+prev_H+'px; margin: 0 auto; position:relative; background-size: contain;">\
+   $('<h2>Manual Reduction Step 1</h2><p>Select the first and last point of the visible path of the meteor.</p></p><div id="main_view" style="background-color:#000;background-image:url('+image_src+'); width:'+prev_W+'px; height:'+prev_H+'px; margin: 0 auto; position:relative; background-size: contain;">\
                 <div id="dl"></div><div id="dt"></div><div id="dr"></div><div id="db"></div>\
                 <div id="selector" style="width:'+cursor_dim+'px; height:'+cursor_dim+'px; border:'+cursor_border_width+'px solid #fff;"></div>\
                  <div id="select_f_tools">\
@@ -26,10 +26,6 @@ function create_meteor_selector_from_frame(image_src) {
                         <div><small>Preview</small></div>\
                         <div class="pr-2"><small>X:<span id="pos_x"></span> / Y:<span id="pos_y"></span></small></div>\
                     </div>\
-                    <div class="p-1">\
-                    <div id="select_preview" style="width:'+preview_dim+'px; height:'+preview_dim+'px"></div>\
-                    <div><input type="range" value="'+transp_val+'" id="transp" min="0"  max="60" ></div>\
-                    </div>\
                 </div>\
-            </div>').appendTo($('#step1'));
+   </div>').appendTo($('#step1'));
 }
