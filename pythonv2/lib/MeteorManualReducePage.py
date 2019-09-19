0b398a176d9d68667c108d6ba60c6cb9a08f8216
Perlerin Vincent <vperlerin@gmail.com>
2019-09-19 13:16:56 +0200

    Manual Reduction Page

--- pythonv2/lib/MeteorManualReducePage.py
+++ pythonv2/lib/MeteorManualReducePage.py
@@ -22,7 +22,7 @@ def fix_old_file_name(filename):
          # Remove first "_"
          tmp_fixed_video_full_path = tmp_fixed_video_full_path[1:]
          # Add an extension
-         tmp_fixed_video_full_path += ".json"
+         tmp_fixed_video_full_path += "_HD.json"
 
          return tmp_fixed_video_full_path
    else:
