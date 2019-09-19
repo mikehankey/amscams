c44938968db7de2977d2472ee3d96fed105f144b
Perlerin Vincent <vperlerin@gmail.com>
2019-09-19 12:57:12 +0200

    Manual Reduction Page

--- pythonv2/lib/MeteorManualReducePage.py
+++ pythonv2/lib/MeteorManualReducePage.py
@@ -17,13 +17,15 @@ def manual_reduction(form):
    # Get Video File & Analyse the Name to get quick access to all info
    video_full_path = form.getvalue("video_file")
 
-   # Get the stacks
-   stack = get_stacks(analysed_name,clear_cache)
-   #print(get_cache_path(analysed_name,"stacks") +"<br>")
-
    if(video_full_path is not None):
       analysed_name = name_analyser(video_full_path)
    else:
       print_error("<b>You need to add a video file in the URL.</b>")
+
+   # Get the stacks
+   stack = get_stacks(analysed_name,clear_cache)
+   #print(get_cache_path(analysed_name,"stacks") +"<br>")
+
+  
 
    print("STACK " + stack)
