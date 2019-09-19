945b096a73dd46ba86897952664b48ad4a0dd1a6
Perlerin Vincent <vperlerin@gmail.com>
2019-09-19 17:38:39 +0200

    Manual Reduction Page

--- pythonv2/lib/MeteorReduce_Calib_Ajax_Tools.py
+++ pythonv2/lib/MeteorReduce_Calib_Ajax_Tools.py
@@ -21,25 +21,25 @@ def getRADEC(form):
    if "reduced_stack" in json:
       
       # It's an old we need to create the right calib json object
-      new_json_content = { "calib": \
-        { "device": {\
-            "alt":  float(json['cal_params']['device_alt']),\
-            "lat":  float(json['cal_params']['device_lat']),\
-            "lng":  float(json['cal_params']['device_lng']),\
-            "scale_px":  float(json['cal_params']['pixscale']),\
-            "poly": {\
-                "y_fwd": json['cal_params']['y_poly_fwd'],\
-                "x_fwd": json['cal_params']['x_poly_fwd']\
-            },\
-            "center": {\
-                "az": float(json['cal_params']['orig_az_center']),  \
-                "ra": float(json['cal_params']['orig_ra_center']), \
-                "el": float(json['cal_params']['orig_el_center']),\
-                "dec": float(json['cal_params']['orig_dec_center']) \
-            },\
-            "angle":  float(json['cal_params']['orig_pos_ang'])\
-         }      \
-      }
+      new_json_content = { "calib":  
+        { "device": {
+            "alt":  float(json['cal_params']['device_alt']),
+            "lat":  float(json['cal_params']['device_lat']),
+            "lng":  float(json['cal_params']['device_lng']),
+            "scale_px":  float(json['cal_params']['pixscale']),
+            "poly": {
+                "y_fwd": json['cal_params']['y_poly_fwd'],
+                "x_fwd": json['cal_params']['x_poly_fwd']
+            },
+            "center": {
+                "az": float(json['cal_params']['orig_az_center']),  
+                "ra": float(json['cal_params']['orig_ra_center']), 
+                "el": float(json['cal_params']['orig_el_center']),
+                "dec": float(json['cal_params']['orig_dec_center']) 
+            },
+            "angle":  float(json['cal_params']['orig_pos_ang'])
+         }      
+      }}
       print(new_json_content)
       #x_poly_fwd  = json_file['calib']['device']['poly']['x_fwd']
       #y_poly_fwd  = json_file['calib']['device']['poly']['y_fwd']
