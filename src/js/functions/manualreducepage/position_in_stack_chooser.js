d5e0bc4a16d597c8648fa6e0dcbe9c657ca5cbea
Perlerin Vincent <vperlerin@gmail.com>
2019-09-19 15:24:55 +0200

    Manual Reduction Page

--- src/js/functions/manualreducepage/position_in_stack_chooser.js
+++ src/js/functions/manualreducepage/position_in_stack_chooser.js
@@ -12,9 +12,9 @@ function update_select_preview(top,left,margins,W_factor,H_factor,cursor_dim, cu
 
    if(cur_step_start) {
       // Update START X/Y
-      $('#res_start').text('x:' + Math.floor(sel_x*W_factor)+ 'px ' + 'y:'+  Math.floor(sel_y*H_factor) +'px');
+      $('#res .start').text('x:' + Math.floor(sel_x*W_factor)+ 'px ' + 'y:'+  Math.floor(sel_y*H_factor) +'px');
    } else {
-      $('#res_end').text('x:' + Math.floor(sel_x*W_factor)+ 'px ' + 'y:'+  Math.floor(sel_y*H_factor) +'px');
+      $('#res .end').text('x:' + Math.floor(sel_x*W_factor)+ 'px ' + 'y:'+  Math.floor(sel_y*H_factor) +'px');
    }
 
    return !cur_step_start
@@ -48,9 +48,9 @@ function create_meteor_selector_from_stack(image_src) {
  
    $('<h1>Manual Reduction Step 1</h1>\
      <div class="box">\
-     <div class="modal-header p-0" style="border:none:important">\
+     <div class="modal-header p-0" style="border:none!important">\
       <div class="alert alert-info mb-3 p-1 pr-1 pl-2">Select the STARTING point of the meteor path.</div>\
-      <div id="res"><span class="res_start"></span> <span class="res_end"></span></div>\
+      <div id="res"><span class="start"></span> <span class="end"></span></div>\
      </div>\
      <div id="draggable_area" style="width:'+(prev_W+margins*2) + 'px; height:' +( prev_H+margins*2) + 'px;margin:0 auto;">\
      <div id="main_view" style="background-color:#000;background-image:url('+image_src+'); width:'+prev_W+'px; height:'+prev_H+'px; margin: 0 auto; position:relative; background-size: contain;">\
