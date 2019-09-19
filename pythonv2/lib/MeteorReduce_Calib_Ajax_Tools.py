4d440aab2c97ba36d0c9a40a5250d2e72ef88340
Perlerin Vincent <vperlerin@gmail.com>
2019-09-19 17:21:12 +0200

    Manual Reduction Page

--- pythonv2/lib/MeteorReduce_Calib_Ajax_Tools.py
+++ pythonv2/lib/MeteorReduce_Calib_Ajax_Tools.py
@@ -19,9 +19,10 @@ def getRADEC(form):
 
    # Test if we have an old or a new JSON
    if "reduced_stack" in json:
+      print("YES")
       # It's an old we need to create the right calib json object
-      new_json_content = {'calib'  : {  'device': { 'scale_px': json['pixscale'] }    }      }
-      print(new_json_content)
+      #new_json_content = {'calib'  : {  'device': { 'scale_px': json['pixscale'] }    }      }
+      #print(new_json_content)
       #x_poly_fwd  = json_file['calib']['device']['poly']['x_fwd']
       #y_poly_fwd  = json_file['calib']['device']['poly']['y_fwd']
       #lat         = float(json_file['calib']['device']['lat'])
