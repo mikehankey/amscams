3f110d61b8877769b4267c6b8de1fe9a1f0c7d73
Perlerin Vincent <vperlerin@gmail.com>
2019-09-20 10:48:27 +0200

    RA/Dec Mode

--- src/js/functions/reducepage/Radec_Mode.js
+++ src/js/functions/reducepage/Radec_Mode.js
@@ -1,5 +1,6 @@
 var RADEC_MODE = false; // Shared with canvas-interactions
 
+var star_info_object = [];
 
 
 // Remove All stars info from canvas
@@ -7,8 +8,16 @@ function remove_stars_info_from_canvas() {
    var objects = canvas.getObjects()
    $.each(objects,function(i,v){
        if(v.type=='star_info') {
-           canvas.remove(objects[i]);
+         star_info_object.push(objects[i]);
+         canvas.remove(objects[i]);
        }
+   });
+}
+
+// Put All Starts info on canvas
+function add_stars_info_on_canvas() { 
+   $.each(star_info_object,function(i,v){
+      canvas.add(star_info_object[i]);
    });
 }
 
@@ -21,5 +30,8 @@ $('#radec_mode').click(function() {
    if(RADEC_MODE) {
       // We hide the stars
       remove_stars_info_from_canvas();
+   } else {
+      // We put back the stars
+      add_stars_info_on_canvas();
    }
 })
