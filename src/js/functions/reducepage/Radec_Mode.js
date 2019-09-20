fe9a6568a1ad7252ccb7979c01ccf6bca5049605
Perlerin Vincent <vperlerin@gmail.com>
2019-09-20 12:10:07 +0200

    RA/Dec Mode

--- src/js/functions/reducepage/Radec_Mode.js
+++ src/js/functions/reducepage/Radec_Mode.js
@@ -81,25 +81,37 @@ function add_radec_info(info) {
 
 
 $('#radec_mode').click(function() {
-   RADEC_MODE = !RADEC_MODE;
-    
 
-   if(RADEC_MODE) {
-      // We hide the stars
-      remove_stars_info_from_canvas();
 
-      // We add the radec panel
-      build_radecpanel();
-      
-
+   if(typeof json_reduced  == "undefined") {
+      loading_done();
+      bootbox.alert({
+          message: "JSON file is missing. A JSON files with calibration parameters is required.<br/>",
+          className: 'rubberBand animated error',
+          centerVertical: true 
+      });
    } else {
-      // We put back the stars
-      add_stars_info_on_canvas();
+      RADEC_MODE = !RADEC_MODE;
+    
+      if(RADEC_MODE) {
+         // We hide the stars
+         remove_stars_info_from_canvas();
 
-      // Remove the radec info from canvas
-      remove_radec_info_from_canvas();
+         // We add the radec panel
+         build_radecpanel();
+         
 
-      // We empty rad_dec_object
-      rad_dec_object = [];
+      } else {
+         // We put back the stars
+         add_stars_info_on_canvas();
+
+         // Remove the radec info from canvas
+         remove_radec_info_from_canvas();
+
+         // We empty rad_dec_object
+         rad_dec_object = [];
+      }
    }
+
+   
 })
