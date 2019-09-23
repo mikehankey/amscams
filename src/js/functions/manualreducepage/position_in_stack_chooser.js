2169cf6c2c4e2e7d6420fffa5ff40ac9600d5caf
Perlerin Vincent <vperlerin@gmail.com>
2019-09-23 10:31:51 +0200

    Old Json => New Json - Calib tools

--- src/js/functions/manualreducepage/position_in_stack_chooser.js
+++ src/js/functions/manualreducepage/position_in_stack_chooser.js
@@ -72,8 +72,9 @@ function update_select_preview(top,left,margins,W_factor,H_factor,cursor_dim, cu
       }
 
       // We draw a rectangle 
+      var h = parseInt($('input[name=x_start]').val())  - parseInt($('input[name=x_end]').val());
       var w = parseInt($('input[name=x_start]').val())  - parseInt($('input[name=x_end]').val());
-      
+
       if(w<=0) {
          console.log("w<0");
          $('#sel_rectangle_static').css({
