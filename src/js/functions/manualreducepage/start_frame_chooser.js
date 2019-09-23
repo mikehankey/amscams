38844da3dc9df21d2cd5a0bbdc960371eb60c839
Perlerin Vincent <vperlerin@gmail.com>
2019-09-23 14:11:48 +0200

    Manual Reduction

--- src/js/functions/manualreducepage/start_frame_chooser.js
+++ src/js/functions/manualreducepage/start_frame_chooser.js
@@ -4,9 +4,4 @@ function select_start_frame() {
       window.location = './webUI.py?cmd=manual_reduction_meteor_pos_selector&video_file='+video_file+'&x='+x+'&y='+y+'&w='+w+'&h='+h
    })
 }
-
-var video_file = "{VIDEO}";
-var x = "{X}";
-var y = "{Y}";
-var w = "{W}";
-var h = "{H}";
+ 
