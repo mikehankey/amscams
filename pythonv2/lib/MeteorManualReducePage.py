536490d07db368919e266dc7ca5c69695ba1b1a6
Perlerin Vincent <vperlerin@gmail.com>
2019-09-26 13:50:59 +0200

    Manual Red

--- pythonv2/lib/MeteorManualReducePage.py
+++ pythonv2/lib/MeteorManualReducePage.py
@@ -294,7 +294,7 @@ def manual_reduction_create_final_json(form):
          print( '    <meta http-equiv="refresh" content="0;url='+redirectURL+'" />' )
          print( '    <title>You are going to be redirected</title>')
          print( '  </head>') 
-         print( '  <body style="width:100vw; height:100vh; background:#000>')
+         print( '  <body style="width:100vw; height:100vh; background:#000">')
          print('<div style="color:#000; background:#000">')
          print( '    Redirecting... <a href="'+redirectURL+'" style="color:#000">Click here if you are not redirected</a>')
          print('</div'); 
