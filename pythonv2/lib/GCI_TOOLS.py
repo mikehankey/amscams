8361b78d5e09a7edcc739c9183e40ec0bbcf83d3
Perlerin Vincent <vperlerin@gmail.com>
2019-09-26 16:55:57 +0200

    R

--- pythonv2/lib/GCI_TOOLS.py
+++ pythonv2/lib/GCI_TOOLS.py
@@ -2,12 +2,12 @@ import sys
 
 # CGI redirect
 def redirect_to(redirectURL):
-   print( '<html  style="width:100vw; height:100vh; background:#000">')
+   print( '<html  style="width:100vw; height:100vh; background:#061329">')
    print( '  <head>')
    print( '    <meta http-equiv="refresh" content="0;url='+redirectURL+'" />' )
    print( '    <title>You are going to be redirected</title><link rel="stylesheet" href="./dist/css/main.css"/>')
    print( '  </head>') 
-   print( '  <body style="width:100vw; height:100vh; background:#000">')
+   print( '  <body style="width:100vw; height:100vh; background:#061329">')
    print( '  <div id="overlay" class="animated"><div class="row h-100 text-center"><div class="col-sm-12 my-auto"><div class="card card-block" style="background:transparent"><iframe style="border:0;margin: 0 auto;" src="./dist/img/anim_logo.svg" width="140" height="90"></iframe><h4>please wait while you\'re redirected</h4></div></div></div></div>')
    print( '  </body>')
    print( '</html>')
