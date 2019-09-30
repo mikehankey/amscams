165f1e2e30d191e7ba8885c3c0ba982c0c038cbc
Perlerin Vincent <vperlerin@gmail.com>
2019-09-30 12:18:29 +0200

    Converstion

--- pythonv2/lib/Get_Station_Id.py
+++ pythonv2/lib/Get_Station_Id.py
@@ -4,6 +4,5 @@ from lib.Cleanup_Json_Conf import cleanup_json
 def get_station_id():
     json_path = cleanup_json() 
     with open(json_path, "r+") as jsonFile:
-        data = json.load(jsonFile) 
-        print(data)
-        return data['site']['ams_id'] 
+        data = json.load(jsonFile)  
+        return data['operator']['ams_id'] 
