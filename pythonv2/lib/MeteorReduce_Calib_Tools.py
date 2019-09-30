d0c1734ad2fabedf752a80b46707b147ca4a164d
Perlerin Vincent <vperlerin@gmail.com>
2019-09-30 10:39:47 +0200

    Converstion

--- pythonv2/lib/MeteorReduce_Calib_Tools.py
+++ pythonv2/lib/MeteorReduce_Calib_Tools.py
@@ -7,6 +7,11 @@ from lib.MeteorReduce_Tools import name_analyser, get_datetime_from_analysedname
 from lib.VIDEO_VARS import HD_W, HD_H
 from lib.REDUCE_VARS import *
 
+# TO DELETE 
+from lib.UtilLib import convert_filename_to_date_cam, bound_cnt, check_running,date_to_jd, angularSeparation , calc_dist, better_parse_file_date
+
+
+
 # Convert Y, M, d into JD Date
 def date_to_jd(year,month,day):
     """
@@ -66,10 +71,165 @@ def date_to_jd(year,month,day):
 
 
 # TEST
+def XYtoRADec(img_x,img_y,analysed_name,json_file):
 
+   #def XYtoRADec(img_x,img_y,cal_file,cal_params,json_conf):
+   #hd_datetime, hd_cam, hd_date, hd_y, hd_m, hd_d, hd_h, hd_M, hd_s = convert_filename_to_date_cam(cal_file)
+   hd_datetime, hd_cam, hd_date, hd_y, hd_m, hd_d, hd_h, hd_M, hd_s = convert_filename_to_date_cam(json_file)
+   
+   print('Year    ' +  str(hd_y) +"<br>")
+   print('Mpnth   ' +  str(hd_m)+"<br>")
+   print('Day     ' +  str(hd_d)+"<br>")
+   print('Hour    ' +  str(hd_h)+"<br>")
+   print('Minute     ' +  str(hd_M)+"<br>") 
+
+
+   
+   F_scale = 3600/float(cal_params['pixscale'])
+   #F_scale = 24
+
+   total_min = (int(hd_h) * 60) + int(hd_M)
+   day_frac = total_min / 1440 
+   hd_d = int(hd_d) + day_frac
+   jd = date_to_jd(int(hd_y),int(hd_m),float(hd_d))
+
+   lat = float(json_conf['site']['device_lat'])
+   lon = float(json_conf['site']['device_lng'])
+
+   # Calculate the reference hour angle
+   T = (jd - 2451545.0)/36525.0
+   Ho = (280.46061837 + 360.98564736629*(jd - 2451545.0) + 0.000387933*T**2 \
+      - (T**3)/38710000.0)%360
+
+   x_poly_fwd = cal_params['x_poly_fwd']
+   y_poly_fwd = cal_params['y_poly_fwd']
+   
+   dec_d = float(cal_params['dec_center']) 
+   RA_d = float(cal_params['ra_center']) 
+
+   dec_d = dec_d + (x_poly_fwd[13] * 100)
+   dec_d = dec_d + (y_poly_fwd[13] * 100)
+
+   RA_d = RA_d + (x_poly_fwd[14] * 100)
+   RA_d = RA_d + (y_poly_fwd[14] * 100)
+
+   pos_angle_ref = float(cal_params['position_angle']) + (1000*x_poly_fwd[12]) + (1000*y_poly_fwd[12])
+
+   # Convert declination to radians
+   dec_rad = math.radians(dec_d)
+
+   # Precalculate some parameters
+   sl = math.sin(math.radians(lat))
+   cl = math.cos(math.radians(lat))
+
+
+   x_det = img_x - int(cal_params['imagew'])/2
+   y_det = img_y - int(cal_params['imageh'])/2
+
+   dx = (x_poly_fwd[0]
+      + x_poly_fwd[1]*x_det
+      + x_poly_fwd[2]*y_det
+      + x_poly_fwd[3]*x_det**2
+      + x_poly_fwd[4]*x_det*y_det
+      + x_poly_fwd[5]*y_det**2
+      + x_poly_fwd[6]*x_det**3
+      + x_poly_fwd[7]*x_det**2*y_det
+      + x_poly_fwd[8]*x_det*y_det**2
+      + x_poly_fwd[9]*y_det**3
+      + x_poly_fwd[10]*x_det*math.sqrt(x_det**2 + y_det**2)
+      + x_poly_fwd[11]*y_det*math.sqrt(x_det**2 + y_det**2))
+
+   # Add the distortion correction
+   x_pix = x_det + dx 
+
+   #print("ORIG X:", img_x)
+   #print("X DET:", x_det)
+   #print("DX :", dx)
+   #print("NEWX :", x_pix)
+
+   dy = (y_poly_fwd[0]
+      + y_poly_fwd[1]*x_det
+      + y_poly_fwd[2]*y_det
+      + y_poly_fwd[3]*x_det**2
+      + y_poly_fwd[4]*x_det*y_det
+      + y_poly_fwd[5]*y_det**2
+      + y_poly_fwd[6]*x_det**3
+      + y_poly_fwd[7]*x_det**2*y_det
+      + y_poly_fwd[8]*x_det*y_det**2
+      + y_poly_fwd[9]*y_det**3
+      + y_poly_fwd[10]*y_det*math.sqrt(x_det**2 + y_det**2)
+      + y_poly_fwd[11]*x_det*math.sqrt(x_det**2 + y_det**2))
+
+   # Add the distortion correction
+   y_pix = y_det + dy 
+
+   x_pix = x_pix / F_scale
+   y_pix = y_pix / F_scale
+
+   ### Convert gnomonic X, Y to alt, az ###
+
+   # Caulucate the needed parameters
+   radius = math.radians(math.sqrt(x_pix**2 + y_pix**2))
+   theta = math.radians((90 - pos_angle_ref + math.degrees(math.atan2(y_pix, x_pix)))%360)
+
+   sin_t = math.sin(dec_rad)*math.cos(radius) + math.cos(dec_rad)*math.sin(radius)*math.cos(theta)
+   Dec0det = math.atan2(sin_t, math.sqrt(1 - sin_t**2))
+
+   sin_t = math.sin(theta)*math.sin(radius)/math.cos(Dec0det)
+   cos_t = (math.cos(radius) - math.sin(Dec0det)*math.sin(dec_rad))/(math.cos(Dec0det)*math.cos(dec_rad))
+   RA0det = (RA_d - math.degrees(math.atan2(sin_t, cos_t)))%360
+
+   h = math.radians(Ho + lon - RA0det)
+   sh = math.sin(h)
+   sd = math.sin(Dec0det)
+   ch = math.cos(h)
+   cd = math.cos(Dec0det)
+
+   x = -ch*cd*sl + sd*cl
+   y = -sh*cd
+   z = ch*cd*cl + sd*sl
+
+   r = math.sqrt(x**2 + y**2)
+
+   # Calculate azimuth and altitude
+   azimuth = math.degrees(math.atan2(y, x))%360
+   altitude = math.degrees(math.atan2(z, r))
+
+
+
+   ### Convert alt, az to RA, Dec ###
+
+   # Never allow the altitude to be exactly 90 deg due to numerical issues
+   if altitude == 90:
+      altitude = 89.9999
+
+   # Convert altitude and azimuth to radians
+   az_rad = math.radians(azimuth)
+   alt_rad = math.radians(altitude)
+
+   saz = math.sin(az_rad)
+   salt = math.sin(alt_rad)
+   caz = math.cos(az_rad)
+   calt = math.cos(alt_rad)
+
+   x = -saz*calt
+   y = -caz*sl*calt + salt*cl
+   HA = math.degrees(math.atan2(x, y))
+
+   # Calculate the hour angle
+   T = (jd - 2451545.0)/36525.0
+   hour_angle = (280.46061837 + 360.98564736629*(jd - 2451545.0) + 0.000387933*T**2 - T**3/38710000.0)%360
+
+   RA = (hour_angle + lon - HA)%360
+   dec = math.degrees(math.asin(sl*salt + cl*calt*caz))
+
+   ### ###
+ 
+
+   return(x_pix+img_x,y_pix+img_y,RA,dec,azimuth,altitude)
 
 # Return Ra/Dec based on X,Y for a given frame
-def XYtoRADec(x,y,analysed_name,json_file):
+def XYtoRADec___(x,y,analysed_name,json_file):
  
    # Get Data from analysed_name
    hd_y = analysed_name['year']
@@ -89,19 +249,17 @@ def XYtoRADec(x,y,analysed_name,json_file):
    angle       = float(json_file['calib']['device']['angle']) 
 
 
-   print('Year' +  str(hd_y) +"<br>")
-   print('Mpnth' +  str(hd_m)+"<br>")
-   print('Day' +  str(hd_d)+"<br>")
-   print('Hour' +  str(hd_h)+"<br>")
-   print('Minute' +  str(hd_M)+"<br>") 
-
-
-   print('F_scale' +  str(F_scale) +"<br>")
-   print('lat' +  str(lat)+"<br>")
-   print('lon' +  str(lon)+"<br>")
-   print('dec_d' +  str(dec_d)+"<br>")
-   print('RA_d' +  str(RA_d)+"<br>")
-   print('angle' +  str(angle)+"<br>")
+   #print('Year' +  str(hd_y) +"<br>")
+   #print('Mpnth' +  str(hd_m)+"<br>")
+   #print('Day' +  str(hd_d)+"<br>")
+   #print('Hour' +  str(hd_h)+"<br>")
+   #print('Minute' +  str(hd_M)+"<br>") 
+   #print('F_scale' +  str(F_scale) +"<br>")
+   #print('lat' +  str(lat)+"<br>")
+   #print('lon' +  str(lon)+"<br>")
+   #print('dec_d' +  str(dec_d)+"<br>")
+   #print('RA_d' +  str(RA_d)+"<br>")
+   #print('angle' +  str(angle)+"<br>")
 
    print(json_file)
    
