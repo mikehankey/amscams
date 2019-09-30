27f9ac0876e8b588179bd67e9aaa3cae978ea227
Perlerin Vincent <vperlerin@gmail.com>
2019-09-30 12:30:18 +0200

    Converstion

--- pythonv2/lib/Old_JSON_converter.py
+++ pythonv2/lib/Old_JSON_converter.py
@@ -167,6 +167,9 @@ def convert(json_file_path):
 # from a old -reduced.json file
 def move_old_reduced_to_archive(json_file_path, display=False):
 
+   # Remove the eventual "-reduced" from name
+   json_file_path  = replace('-reduced','')
+
    # We fix the old name to get the proper info
    fixed_json_file_path = fix_old_file_name(json_file_path)
    analysed_name = name_analyser(fixed_json_file_path) 
