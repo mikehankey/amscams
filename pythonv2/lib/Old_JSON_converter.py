c6cdd6bd514898d89ba88dda5c6af5bfa9ffad5a
Perlerin Vincent <vperlerin@gmail.com>
2019-09-30 16:14:18 +0200

    Converstion

--- pythonv2/lib/Old_JSON_converter.py
+++ pythonv2/lib/Old_JSON_converter.py
@@ -138,6 +138,10 @@ def get_new_info(json_f):
    else:
       station_name = json_f['station_name']
 
+   print("IN GET NEW INFO <br/>")
+   print(json_f)
+   sys.exit(0)
+
    return  {
       "info": {
          "station": station_name,
@@ -197,19 +201,13 @@ def convert(json_file_path):
 # with a proper name, and in the proper folder
 # from a old -reduced.json file
 def move_old_reduced_to_archive(json_file_path, display=False):
-
-   print("<br>IN move_old_reduced_to_archive<br>")
-   print("<br>json_file_path: ", json_file_path)
-
+ 
    # Remove the eventual "-reduced" from name
    json_file_path  = json_file_path.replace('-reduced','')
 
    # We fix the old name to get the proper info
    fixed_json_file_path = fix_old_file_name(json_file_path)
-
-
-
-
+ 
    analysed_name = name_analyser(fixed_json_file_path) 
 
    # Determine the folder where to put the files
