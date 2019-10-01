31389b750912ace7b1f89045479ae6ea016e3039
Perlerin Vincent <vperlerin@gmail.com>
2019-10-01 10:09:22 +0200

    Converstion

--- pythonv2/lib/Old_JSON_converter.py
+++ pythonv2/lib/Old_JSON_converter.py
@@ -317,10 +317,10 @@ def move_old_detection_to_archive(json_file_path, display=False):
    json_content['info'] = new_info['info']
    json_content['frames'] = []
    
-   print( tan['name'])
+   print( "<br/><br/><br/>"+ new_folder + tan['name'])
 
    # Save the new JSON file
-   save_json_file( tan['name'], json_content)
+   save_json_file(new_folder + tan['name'], json_content)
    if(display is True):
       print("JSON SAVED TO " + json_file)
 
