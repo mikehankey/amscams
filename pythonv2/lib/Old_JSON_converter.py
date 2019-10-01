9e6142710e5e19b44aa0f7f6f3c430698556f138
Perlerin Vincent <vperlerin@gmail.com>
2019-10-01 11:38:08 +0200

    Converstion

--- pythonv2/lib/Old_JSON_converter.py
+++ pythonv2/lib/Old_JSON_converter.py
@@ -63,13 +63,9 @@ def fix_old_file_name(filename):
       matches =  re.finditer(OLD_FILE_NAME_REGEX, filename, re.MULTILINE)
       res = {}
  
-  
       for matchNum, match in enumerate(matches, start=1):
          for groupNum in range(0, len(match.groups())):
-            if(match.group(groupNum) is not None):
-               print("groupNum " + str(groupNum) +"<br>")
-               print("match groupNum " + str( match.group(groupNum)) +"<br>")
-               print(OLD_FILE_NAME_REGEX_GROUP[groupNum]+"<br>")
+            if(match.group(groupNum) is not None): 
                res[OLD_FILE_NAME_REGEX_GROUP[groupNum]] = match.group(groupNum)
             groupNum = groupNum + 1
    
