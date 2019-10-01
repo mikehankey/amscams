40253b2ea3ec8f3e4d773b7ffa689b47e1f6f134
Perlerin Vincent <vperlerin@gmail.com>
2019-10-01 10:58:32 +0200

    Converstion

--- pythonv2/lib/MeteorManualReducePage.py
+++ pythonv2/lib/MeteorManualReducePage.py
@@ -249,20 +249,7 @@ def manual_reduction_create_final_json(form):
       # based on the diff between the time of the last frame and the time of the first frame
       dt_start = datetime.strptime(mr['frames'][0]['dt'], "%Y-%m-%d %H:%M:%S.%f")
       dt_end   = datetime.strptime(mr['frames'][len(mr['frames'])-1]['dt'] , "%Y-%m-%d %H:%M:%S.%f")
-
-      diff = dt_end - dt_start 
-      print(diff )
-      print(  "<br/>")
-      print(diff.seconds)
-      print(" seconds<br/>")
-      print(diff.microseconds)
-      print(" microseconds<br/>")
-      print(timedelta.total_seconds(diff))
-      print(" seconds<br/>")
-      
-      sys.exit(0)
-
-      mr['info']['dur'] = abs((dt_end - dt_start).microseconds) / 1000 # /1000 to have seconds instead of milliseconds
+      mr['info']['dur'] = timedelta.total_seconds( dt_end - dt_start )  
 
       # We update the JSON with the new frames
       save_json_file(meteor_red_file, mr) 
