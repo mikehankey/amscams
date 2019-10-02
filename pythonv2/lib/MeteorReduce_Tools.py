a33d8eecacb34763748bcb86242fd32cb1aa4f83
Perlerin Vincent <vperlerin@gmail.com>
2019-10-02 15:02:37 +0200

    Tmp.

--- pythonv2/lib/MeteorReduce_Tools.py
+++ pythonv2/lib/MeteorReduce_Tools.py
@@ -220,7 +220,7 @@ def new_crop_thumb(frame,x,y,dest,HD = True):
       print("org_w " + str(org_w))
       print("org_Y " + str(org_y) )
       print("org_h " + str(org_h))
-      sys.exit(0)
+      
  
      
    # ON TOP (VERIFIED)
