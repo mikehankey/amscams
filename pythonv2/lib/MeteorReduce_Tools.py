c78ffb9de55485d1b1c6d9bab254e909e375a1d8
Perlerin Vincent <vperlerin@gmail.com>
2019-10-02 14:26:27 +0200

    Tmp

--- pythonv2/lib/MeteorReduce_Tools.py
+++ pythonv2/lib/MeteorReduce_Tools.py
@@ -212,15 +212,15 @@ def new_crop_thumb(frame,x,y,dest,HD = True):
       # Destination in thumb (img) 
       thumb_dest_w =  HD_W - org_x
 
-      print("THUMB_X" + thumb_dest_x )
-      print("THUMB_W" + thumb_dest_w)
-      print("THUMB_Y" + thumb_dest_y )
-      print("THUMB_H" + thumb_dest_h)
+      print("THUMB_X" + str(thumb_dest_x ))
+      print("THUMB_W" + str(thumb_dest_w))
+      print("THUMB_Y" + str(thumb_dest_y ))
+      print("THUMB_H" + str(thumb_dest_h))
       print("----------------------")
-      print("org_x" + org_x )
-      print("org_w" + org_w)
-      print("org_Y" + org_Y )
-      print("org_h" + org_h)
+      print("org_x" + str(org_x) )
+      print("org_w" + str(org_w))
+      print("org_Y" + str(org_Y) )
+      print("org_h" + str(org_h)
       sys.exit(0)
    # ON TOP
    if(org_y<=0):
