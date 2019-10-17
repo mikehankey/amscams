ccb23b3e4c39d79aec48b9b05d2237270f703aeb
Perlerin Vincent <vperlerin@gmail.com>
2019-10-17 15:28:24 +0200

    M

--- pythonv2/lib/WebUI.py
+++ pythonv2/lib/WebUI.py
@@ -47,7 +47,7 @@ from lib.MeteorManualReducePage import manual_reduction, manual_reduction_croppe
 from lib.MeteorReduce_Calib_Ajax_Tools import getRADEC
 
 NUMBER_OF_METEOR_PER_PAGE = 60
-POSSIBLE_PER_PAGE = [20,40,60,80,100,150,200]
+POSSIBLE_PER_PAGE = [20,40,60,80,100,150,200,500,1000,10000]
 NUMBER_OF_DAYS_PER_PAGE = 7
 
  
