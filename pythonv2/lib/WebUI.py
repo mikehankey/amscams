e6a01d1772b7c9ce75f3fe9c020e2f62a4b96415
Perlerin Vincent <vperlerin@gmail.com>
2019-10-17 15:14:39 +0200

    M

--- pythonv2/lib/WebUI.py
+++ pythonv2/lib/WebUI.py
@@ -1475,7 +1475,7 @@ def meteors_new(json_conf,form):
       # Build num per page selector
       ppp_select = ''
       for ppp in POSSIBLE_PER_PAGE:
-         ppp_select+= '<option value="'+ppp+'">'+ppp+'/page</option>'
+         ppp_select+= '<option value="'+str(ppp)+'">'+str(ppp)+'/page</option>'
       
       header_out += "<div class='mr-2'><select name='rpp' id='rpp' class='btn btn-primary'>"+ppp_select+"</select></div>"
       header_out += "<div class='d-flex'><div class='btn-group mr-3'><button id='show_gal' class='btn btn-primary act'><i class='icon-list'></i></button></div>"
