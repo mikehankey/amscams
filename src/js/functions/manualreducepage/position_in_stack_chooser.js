6911fffc382125dd8b24cb5928ff1be682fa7cf1
Perlerin Vincent <vperlerin@gmail.com>
2019-10-22 12:21:36 +0200

    M

--- src/js/functions/manualreducepage/position_in_stack_chooser.js
+++ src/js/functions/manualreducepage/position_in_stack_chooser.js
@@ -150,7 +150,7 @@ function create_meteor_selector_from_stack(image_src) {
    $('<div id="draggable_area" style="width:'+(prev_W+margins*2) + 'px; height:' +( prev_H+margins*2) + 'px;margin:0 auto;">\
      <div id="main_view" style="background-color:#000;background-image:url('+image_src+'); width:'+prev_W+'px; height:'+prev_H+'px; margin: 0 auto; position:relative; background-size: contain;">\
       <div id="selector" class="ng pa" style="; border-radius:50%; top:-9999px; left:-9999px;width:'+cursor_dim+'px; height:'+cursor_dim+'px; border:'+cursor_border_width+'px solid green;"></div>\
-   </div></div><div class="text-right"><button id="step1_btn" class="btn btn-lg btn-primary disabled" disabled>Continue</button></div>').appendTo($('#step1'));
+     </div></div></div><div class="text-right"><button id="step1_btn" class="btn btn-lg btn-primary disabled" disabled>Continue</button></div>').appendTo($('#step1'));
    
    
    offset = $('#main_view').offset();
