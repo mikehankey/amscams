34c49d48cd841201996266a39809f11bbd8a9e13
Perlerin Vincent <vperlerin@gmail.com>
2019-10-22 15:26:12 +0200

    M

--- pythonv2/lib/Old_JSON_converter.py
+++ pythonv2/lib/Old_JSON_converter.py
@@ -71,6 +71,10 @@ def fix_old_file_name(filename):
                res[OLD_FILE_NAME_REGEX_GROUP[groupNum]] = match.group(groupNum)
             groupNum = groupNum + 1
     
+
+      print("DEBUG<br/>")
+      print(res)
+
       # Get original Date & Time 
       org_dt = datetime.strptime(res['year']+'-'+res['month']+'-'+res['day']+' '+res['hour']+':'+res['min']+':'+res['sec']+'.'+res['ms'], '%Y-%m-%d %H:%M:%S.%f')
 
