4aaec63d58c0986453e4491af3d5f58982b23f72
Perlerin Vincent <vperlerin@gmail.com>
2019-10-23 15:19:28 +0200

    M

--- pythonv2/lib/MeteorReduce_Tools.py
+++ pythonv2/lib/MeteorReduce_Tools.py
@@ -84,6 +84,8 @@ def does_cache_exist(analysed_file_name,cache_type):
    # Get Cache Path
    cache_path = get_cache_path(analysed_file_name,cache_type)
  
+   print("CACHE PATH " + cache_path)
+
    if(os.path.isdir(cache_path)):
       # We return the glob of the folder with all the images
       return sorted(glob.glob(cache_path+"/*.png"))
@@ -356,10 +358,6 @@ def get_HD_frames(analysed_name,clear_cache):
 
    # Test if folder exists / Create it if not
    HD_frames = does_cache_exist(analysed_name,"frames")
-
-   print("HD FRAMES " +  len(HD_Frames))
-   print("CACHE " + get_cache_path(analysed_name,"frames"))
-   sys.exit(0)
 
    if(len(HD_frames)==0 or clear_cache is True):
       # We need to generate the HD Frame
