7b36510946ae4151d3bc677076fe9009088d678c
Perlerin Vincent <vperlerin@gmail.com>
2019-10-23 09:50:15 +0200

    Stars

--- src/js/ui/canvas-interactions.js
+++ src/js/ui/canvas-interactions.js
@@ -167,6 +167,7 @@ if ($('canvas#c').length!=0) {
           
         
         for (var key in all_added_stars) {
+           console.log("*******************************************")
            console.log(all_added_stars[key]);
            for (var i = 0; i < all_added_stars[key].length; i++) {
                console.log("CANVAS OBJECT " + all_added_stars[key][i]);
