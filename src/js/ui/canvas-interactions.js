2cf3f05c991de26a46358189c9612ba2dc5a4016
Perlerin Vincent <vperlerin@gmail.com>
2019-10-23 10:04:14 +0200

    Stars

--- src/js/ui/canvas-interactions.js
+++ src/js/ui/canvas-interactions.js
@@ -169,7 +169,7 @@ if ($('canvas#c').length!=0) {
         for (var key in all_added_stars) {
            for (var i = 0; i < all_added_stars[key].length; i++) {
                if(objFound == false && all_added_stars[key][i].containsPoint(clickPoint)) {
-                  alert(all_added_stars[key][i]);
+                  console.log(all_added_stars[key][i]);
                   objFound = true;
                   break;
                }
