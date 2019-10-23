9d52e758f3d3171c92992e42fd8e6191e0318771
Perlerin Vincent <vperlerin@gmail.com>
2019-10-23 10:46:21 +0200

    M

--- pythonv2/old_json_inline_converter.py
+++ pythonv2/old_json_inline_converter.py
@@ -7,4 +7,5 @@ from lib.Old_JSON_converter import convert, move_old_detection_to_archive
 
 # Convert an old detection by updating the JSON and moving the file to /meteor_archive/
 json_file = sys.argv[1]
-move_old_detection_to_archive(json_file,True)
+video_file = sys.argv[2]
+move_old_detection_to_archive(json_file,video_file,True)
