f82776054fc722783956b4575e4d1a2bb8d2a49d
Perlerin Vincent <vperlerin@gmail.com>
2019-10-23 11:51:58 +0200

    M

--- pythonv2/lib/Old_JSON_converter.py
+++ pythonv2/lib/Old_JSON_converter.py
@@ -212,7 +212,10 @@ def convert(json_file_path, sd_video_file_path, hd_video_file_path):
    # Add the original name with trim in case there's an issue
    info['info']['org_file_name'] = json_file_path
 
-   calib = get_new_calib(json_f)
+   calib = get_new_calib(reduced_info)
+
+   sys.exit(0);
+
    stars = get_new_stars(json_f)
 
    # The stars belong to calib
