2429b62c0daf4410ece4df73ef33b16bd17801f6
Perlerin Vincent <vperlerin@gmail.com>
2019-10-24 12:10:02 +0200

    M

--- src/js/functions/reducepage/Update_Star_List.js
+++ src/js/functions/reducepage/Update_Star_List.js
@@ -54,13 +54,13 @@ function update_stars_on_canvas_and_table(json_resp) {
         // Add Rectangle
         canvas.add(new fabric.Rect({
             fill: 'rgba(0,0,0,0)', strokeWidth: 1, stroke: 'rgba(230,100,200,.5)', 
-            left: ((v["cat_dist_pos"][POS_X] - 11)/2)+4,   // +4 = shift text
-            top: ((v["cat_dist_pos"][POS_Y] - 11)/2) -4,    // -4 = shift text
+            left:  (v['i_pos'][POS_X] - 11)/2, 
+            top: (v['i_pos'][POS_Y]  - 11)/2,
             width: 10,
             height: 10 ,
             selectable: false,
             gp_id: v[0],
-            type: 'star_info',
+            type: 'star_info', 
          }));
 
         // Add "+" on canvas
@@ -68,8 +68,8 @@ function update_stars_on_canvas_and_table(json_resp) {
             new fabric.Text("+", {
                 fontFamily: 'Arial', 
                 fontSize: 12, 
-                left:  (v['i_pos'][POS_X] - 11)/2, 
-                top: (v['i_pos'][POS_Y]  - 11)/2,
+                left: ((v["cat_dist_pos"][POS_X] - 11)/2)+4,   // +4 = shift text
+                top: ((v["cat_dist_pos"][POS_Y] - 11)/2) -4,    // -4 = shift text
                 fill:'rgba(255,0,0,.75)',
                 selectable: false ,
                 gp_id: v[0],
