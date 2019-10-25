8eb44ec22d2e7deae355aa9f2387199eb9311fe7
Perlerin Vincent <vperlerin@gmail.com>
2019-10-25 12:23:51 +0200

    M

--- pythonv2/old_json_inline_converter.py
+++ pythonv2/old_json_inline_converter.py
@@ -11,5 +11,7 @@ from lib.Old_JSON_converter import move_old_detection_to_archive
 #move_old_detection_to_archive(json_file,video_file,True)
 
 
+
 # CONVERT JSON ONLY
-move_old_detection_to_archive('/mnt/ams2/meteors/2019_10_23/2019_10_23_04_33_17_000_010041-trim0594.json','/mnt/ams2/meteors/2019_10_23/2019_10_23_04_33_17_000_010041-trim0594.mp4','/mnt/ams2/meteors/2019_10_23/2019_10_23_04_33_14_000_010041-trim-594-HD-meteor.mp4')
+#move_old_detection_to_archive('/mnt/ams2/meteors/2019_10_23/2019_10_23_04_33_17_000_010041-trim0594.json','/mnt/ams2/meteors/2019_10_23/2019_10_23_04_33_17_000_010041-trim0594.mp4','/mnt/ams2/meteors/2019_10_23/2019_10_23_04_33_14_000_010041-trim-594-HD-meteor.mp4')
+move_old_detection_to_archive(sys.argv[1],sys.argv[2],sys.argv[3])
