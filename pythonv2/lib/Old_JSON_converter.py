dc9311cf369ab38c326aa3b69231fe302c182613
Perlerin Vincent <vperlerin@gmail.com>
2019-10-28 16:58:46 +0100

    Move To Archive

--- pythonv2/lib/Old_JSON_converter.py
+++ pythonv2/lib/Old_JSON_converter.py
@@ -328,11 +328,11 @@ def move_old_detection_to_archive(json_file_path, sd_video_file_path, hd_video_f
 
 
 
-
-   def move_to_archive(form):
-      hd_video = form.getValue("video_file")
-      sd_video = form.getValue("sd_video")
-      json_file = form.getValue("json_file")
-      print("HD VIDEO" + hd_video )
-      print("HD VIDEO" + sd_video)
-      print("JSON FILE " + json_file)
+# Move detection to Archives
+def move_to_archive(form):
+   hd_video = form.getValue("video_file")
+   sd_video = form.getValue("sd_video")
+   json_file = form.getValue("json_file")
+   print("HD VIDEO" + hd_video )
+   print("HD VIDEO" + sd_video)
+   print("JSON FILE " + json_file)
