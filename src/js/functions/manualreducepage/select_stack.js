4339427e0a42e07efbf0795ae567587bf92966b5
Perlerin Vincent <vperlerin@gmail.com>
2019-10-29 13:22:54 +0100

    Reduce2

--- src/js/functions/manualreducepage/select_stack.js
+++ src/js/functions/manualreducepage/select_stack.js
@@ -1,23 +1,26 @@
+function select_one_stack(type) {
+   var next_step_url = $('input[name=next_step_url]').val();
+
+   if(type=="SD") {
+      window.location = next_step_url + "&type=SD&stack=" + sd_stack + "&video=" + sd_video_file + "&json=" + json_file;
+   } else {
+      window.location = next_step_url + "&type=HD&stack=" + hd_stack + "&video=" + hd_video_file + "&json=" + json_file;
+   }
+}
+
 function select_stack() {
    // Select a stack
    $('.select_stack').click(function() {
-
-
-      var type = $(this).attr('data-rel');
-      var next_step_url = $('input[name=next_step_url]').val();
-
-      if(type=="SD") {
-         window.location = next_step_url + "&type=SD&stack=" + sd_stack + "&video=" + sd_video_file + "&json=" + json_file;
-      } else {
-         window.location = next_step_url + "&type=HD&stack=" + hd_stack + "&video=" + hd_video_file + "&json=" + json_file;
-      }
+      
+      select_one_stack(type);
+ 
    })
 
 
    // If the sd doesn't exist, we automatically click the HD
    if(sd_stack.indexOf('{') > -1) {
-      window.location = next_step_url + "&type=HD&stack=" + hd_stack + "&video=" + hd_video_file + "&json=" + json_file;
+      select_one_stack("HD");
    } else if(hd_stack.indexOf('{') > -1) {
-      window.location = next_step_url + "&type=SD&stack=" + sd_stack + "&video=" + sd_video_file + "&json=" + json_file;
+      select_one_stack("SD");
    }
 } 
