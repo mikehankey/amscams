332d4545bf2df853aefdccbd0d217832c0859434
Perlerin Vincent <vperlerin@gmail.com>
2019-10-30 16:00:17 +0100

    M

--- pythonv2/lib/MeteorReduce_Ajax_Tools.py
+++ pythonv2/lib/MeteorReduce_Ajax_Tools.py
@@ -382,8 +382,7 @@ def convert_encoding(data, new_coding = 'UTF-8'):
 
    print("<br/>IN convert_encoding<br/>")
    print("<br/>DATA " + str(data))
-   print("<br/>ENCODING " + str(encoding))
-   data = str(data)
+   print("<br/>ENCODING " + str(encoding)) 
    toto =  data.decode(encoding)
    print("TOTO " + str(toto))
 
