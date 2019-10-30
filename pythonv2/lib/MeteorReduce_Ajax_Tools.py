1287c17b7f96109a8f30ebe3d5aa162af880f647
Perlerin Vincent <vperlerin@gmail.com>
2019-10-30 16:39:56 +0100

    M

--- pythonv2/lib/MeteorReduce_Ajax_Tools.py
+++ pythonv2/lib/MeteorReduce_Ajax_Tools.py
@@ -406,7 +406,7 @@ def get_catalog_stars(cal_params):
     
       if(not cname):
          print("<br/>dbname<br/>")
-         name =  bname
+         name =  bname.encode('UTF-8')
       else:
          print("<br/>dcname<br/>")
          name = cname.decode('UTF-8')  
