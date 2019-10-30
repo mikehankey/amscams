b137f23838c1ef0817967753353f44859fddce94
Perlerin Vincent <vperlerin@gmail.com>
2019-10-30 15:10:32 +0100

    M

--- pythonv2/lib/MeteorReduce_Ajax_Tools.py
+++ pythonv2/lib/MeteorReduce_Ajax_Tools.py
@@ -415,7 +415,7 @@ def get_catalog_stars(cal_params):
       #else:
       #   name = cname
 
-      print("<br/>name " + str(name))
+      print("<br/>name ****" + str(name) + "*******")
 
       ang_sep = angularSeparation(ra,dec,RA_center,dec_center)
       if ang_sep < fov_radius and float(mag) < 5.5:
