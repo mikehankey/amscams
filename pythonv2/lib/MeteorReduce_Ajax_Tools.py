fffb980344c0bdb5528e4bc5005f459f33ceed38
Perlerin Vincent <vperlerin@gmail.com>
2019-10-30 15:44:51 +0100

    M

--- pythonv2/lib/MeteorReduce_Ajax_Tools.py
+++ pythonv2/lib/MeteorReduce_Ajax_Tools.py
@@ -378,7 +378,7 @@ def distort_xy_new(sx,sy,ra,dec,RA_center, dec_center, x_poly, y_poly, x_res, y_
 # Encode string to UTF8
 def convert_encoding(data, new_coding = 'UTF-8'):
 
-
+  data = data.encode(new_coding) 
   encoding = chardet.detect(data)['encoding']
 
   print("IN convert_encoding<br/>")
