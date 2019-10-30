6f9e0be7556cea3f75e80ff5885d905567bac0dd
Perlerin Vincent <vperlerin@gmail.com>
2019-10-30 14:27:42 +0100

    M

--- pythonv2/lib/MeteorReduce_Ajax_Tools.py
+++ pythonv2/lib/MeteorReduce_Ajax_Tools.py
@@ -461,7 +461,7 @@ def update_cat_stars(form):
 
    for name,mag,ra,dec,new_cat_x,new_cat_y in cat_stars :
       #dcname = str(name.decode("utf-8"))
-      dcname = str(name.encode("utf-8"))
+      dcname = "TOTO"  + str(name.encode("utf-8"))
       #dbname = dcname.encode("utf-8")
       my_cat_stars.append((dcname,mag,ra,dec,new_cat_x,new_cat_y)) 
  
