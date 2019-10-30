54878562358b1e98b9513d9198018b74627463fb
Perlerin Vincent <vperlerin@gmail.com>
2019-10-30 15:17:27 +0100

    M

--- pythonv2/lib/MeteorReduce_Ajax_Tools.py
+++ pythonv2/lib/MeteorReduce_Ajax_Tools.py
@@ -401,9 +401,9 @@ def get_catalog_stars(cal_params):
    for bname, cname, ra, dec, mag in bright_stars_sorted:
       print("IN get_catalog_stars<br/>")
 
-      if not bname and cname:
+      if(not bname and cname):
          name = cname.decode("utf-8")
-      elif not cname and bname:
+      elif(not cname and bname):
          name = bname.decode("utf-8") 
       elif:
          name = "?"
