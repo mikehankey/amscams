e99f46e0413bbb82fa393490050761dda1200a9d
Perlerin Vincent <vperlerin@gmail.com>
2019-10-31 14:22:19 +0100

    Decoding

--- pythonv2/lib/MeteorReduce_Ajax_Tools.py
+++ pythonv2/lib/MeteorReduce_Ajax_Tools.py
@@ -403,7 +403,7 @@ def get_catalog_stars(cal_params):
    for name, cons, ra, dec, mag in bright_stars_sorted:
       print(name)
       print("<br>")
-      bname = name.decode('UTF-8').encode('UTF-8')
+      bname = name.decode('iso-8859-1').encode('UTF-8')
       print(bname)
       print("<br>**********<br>")
       #   #print (name.encode().decode())
