50bb5775e9f90104bf3ae1454ab60a4331e74009
Perlerin Vincent <vperlerin@gmail.com>
2019-10-31 14:14:19 +0100

    Decoding

--- pythonv2/lib/MeteorReduce_Ajax_Tools.py
+++ pythonv2/lib/MeteorReduce_Ajax_Tools.py
@@ -399,15 +399,17 @@ def get_catalog_stars(cal_params):
 
    pos_angle_ref = cal_params['device']['angle']  
 
-    
+   bright_stars_sorted = sorted(bright_stars, key=lambda x: x[4], reverse=False)
+   for bname, cname, ra, dec, mag in bright_stars_sorted:
+      dcname = cname.decode("utf-8")
+      dbname = bname.decode("utf-8")
+      if dcname == "":
+         name = dbname
+      else:
+         name = dcname
+      print(name)
+      print("<br/>")
 
-   my_cat_stars = []
-   for name,mag,ra,dec,new_cat_x,new_cat_y in bright_stars_sorted :
-      dcname = str(name.decode("utf-8"))
-      dbname = dcname.encode("utf-8")
-      my_cat_stars.append((dcname,mag,ra,dec,new_cat_x,new_cat_y))
-
-   print(my_cat_stars)
 
    sys.exit(0)
 
