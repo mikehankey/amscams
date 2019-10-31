a143e7fbb3d52a0d0b260bb03a42fab3a0b9ff8c
Perlerin Vincent <vperlerin@gmail.com>
2019-10-31 14:20:25 +0100

    Decoding

--- pythonv2/lib/MeteorReduce_Ajax_Tools.py
+++ pythonv2/lib/MeteorReduce_Ajax_Tools.py
@@ -403,7 +403,7 @@ def get_catalog_stars(cal_params):
    for name, cons, ra, dec, mag in bright_stars_sorted:
       print(name)
       print("<br>")
-      bname = name.encode().decode()
+      bname = name.decode().encode()
       print(bname)
       print("<br>**********<br>")
       #   #print (name.encode().decode())
