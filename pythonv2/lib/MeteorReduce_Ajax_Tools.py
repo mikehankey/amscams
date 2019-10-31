8ec69e31eaafc22132d01418f256577bf83b95de
Perlerin Vincent <vperlerin@gmail.com>
2019-10-31 17:11:14 +0100

    M

--- pythonv2/lib/MeteorReduce_Ajax_Tools.py
+++ pythonv2/lib/MeteorReduce_Ajax_Tools.py
@@ -403,9 +403,7 @@ def get_catalog_stars(cal_params):
 
 
    for bname, cname, ra, dec, mag in bright_stars_sorted:
-      print(bname)
-      print("<br/>")
-      print(bname.decode('utf-8'))
+      print(bname) 
 
    sys.exit(0)
 
