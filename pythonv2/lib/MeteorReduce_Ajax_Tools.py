55f926866da046ad6cd1ed61bc4064d21dd4516e
Perlerin Vincent <vperlerin@gmail.com>
2019-11-01 15:08:32 +0100

    M

--- pythonv2/lib/MeteorReduce_Ajax_Tools.py
+++ pythonv2/lib/MeteorReduce_Ajax_Tools.py
@@ -418,7 +418,7 @@ def get_catalog_stars(cal_params):
             #name = bname.decode('utf-8').encode('utf-8', errors='replace')  
             #name = name[1:]
             #yy = str(name)
-            print("NAME XXX - " + dbname.decode("utf-8") )
+            
             v = dbname.decode("utf-8")
             #print("<br/>TYPE")
             #print(str(type(yy))) 
