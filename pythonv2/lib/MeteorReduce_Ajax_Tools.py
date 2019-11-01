afdef9ef83a4863cc7d1e5e7fe2d26335a03735c
Perlerin Vincent <vperlerin@gmail.com>
2019-11-01 13:03:49 +0100

    M

--- pythonv2/lib/MeteorReduce_Ajax_Tools.py
+++ pythonv2/lib/MeteorReduce_Ajax_Tools.py
@@ -404,11 +404,11 @@ def get_catalog_stars(cal_params):
    for bname, cname, ra, dec, mag in bright_stars_sorted:
       print("<br/>***********<br/>")
       try:
-         dbname = bname.encode('utf-8')
-         dcname = cname.encode('utf-8')
-         print("DBNAME "+  dbname.decode('utf8'))
+         dbname = bname.decode("utf-8")
+         dcname = cname.decode('utf-8')
+         print("DBNAME "+  dbname)
          print("<br/>")
-         print("DCNAME "+  dcname.decode('utf8'))
+         print("DCNAME "+  dcname)
          print("<br/>")
       except Exception as e:
          print(str(e))
