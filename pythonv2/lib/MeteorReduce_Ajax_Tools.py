baef91640a78f3edf816a465c67999fb1a38dffb
Perlerin Vincent <vperlerin@gmail.com>
2019-11-01 12:18:11 +0100

    M

--- pythonv2/lib/MeteorReduce_Ajax_Tools.py
+++ pythonv2/lib/MeteorReduce_Ajax_Tools.py
@@ -412,7 +412,7 @@ def get_catalog_stars(cal_params):
       #   print("<br/>NO DIRECT")
 
       try: 
-         v = b" ".join(bname)
+         v = join(bname)
          print(str(v))
          sys.exit(0)
       except Exception as e:
