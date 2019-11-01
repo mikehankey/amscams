11481466e7877d1d3fd713610cda6d727a5e76f2
Perlerin Vincent <vperlerin@gmail.com>
2019-11-01 12:21:21 +0100

    M

--- pythonv2/lib/MeteorReduce_Ajax_Tools.py
+++ pythonv2/lib/MeteorReduce_Ajax_Tools.py
@@ -1,4 +1,4 @@
-import sys as sys
+import sys  
 import cgitb
 import json
 import os
@@ -400,9 +400,7 @@ def get_catalog_stars(cal_params):
    pos_angle_ref = cal_params['device']['angle']  
 
    bright_stars_sorted = sorted(bright_stars, key=lambda x: x[4], reverse=False)
- 
-    
-   sys.setdefaultencoding('utf8')
+  
 
    for bname, cname, ra, dec, mag in bright_stars_sorted:
       
