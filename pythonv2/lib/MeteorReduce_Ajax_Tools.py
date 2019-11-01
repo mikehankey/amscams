7f0f34463aad4203810c7d088856c626a7b282fa
Perlerin Vincent <vperlerin@gmail.com>
2019-11-01 12:33:17 +0100

    M

--- pythonv2/lib/MeteorReduce_Ajax_Tools.py
+++ pythonv2/lib/MeteorReduce_Ajax_Tools.py
@@ -408,7 +408,7 @@ def get_catalog_stars(cal_params):
        
 
       try:  
-         print(bname.decode('utf8'))
+         print(bname.encode('utf8'))
          
          print("<br/>")
       except Exception as e:
