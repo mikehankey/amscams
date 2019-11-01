c7878fca094aacb37763f2b98dbf4a80fb214888
Perlerin Vincent <vperlerin@gmail.com>
2019-11-01 12:42:02 +0100

    M

--- pythonv2/lib/MeteorReduce_Ajax_Tools.py
+++ pythonv2/lib/MeteorReduce_Ajax_Tools.py
@@ -400,14 +400,14 @@ def get_catalog_stars(cal_params):
    pos_angle_ref = cal_params['device']['angle']  
 
    bright_stars_sorted = sorted(bright_stars, key=lambda x: x[4], reverse=False)
-  
    
-   bright_stars_sorted = sorted(bright_stars, key=lambda x: x[4], reverse=False)
    for bname, cname, ra, dec, mag in bright_stars_sorted:
       try:
          dbname = bname.decode("utf-8")
          dcname = cname.decode("utf-8")
-         print(dbname)
+         print("DBNAME "+ dbname)
+         print("<br/>")
+         print("DCNAME "+ dcname)
          print("<br/>")
       except Exception as e:
          print(str(e))
