dbeedd9af2bd56865991ef8e9ec5eb9c21bc3f67
Perlerin Vincent <vperlerin@gmail.com>
2019-11-01 14:43:32 +0100

    M

--- pythonv2/lib/MeteorReduce_Ajax_Tools.py
+++ pythonv2/lib/MeteorReduce_Ajax_Tools.py
@@ -421,6 +421,7 @@ def get_catalog_stars(cal_params):
             print("NAME XXX - " + str(dbname))
             #print("<br/>TYPE")
             #print(str(type(yy)))
+            name = str(dbname)
          except Exception as e:
             print("ERROR " + str(e) +  "<br/>")
 
@@ -434,7 +435,7 @@ def get_catalog_stars(cal_params):
          possible_stars = possible_stars + 1
          catalog_stars.append((name,mag,ra,dec,new_cat_x,new_cat_y))
 
-   sys.exit(0)
+    
 
    return(catalog_stars)
 
