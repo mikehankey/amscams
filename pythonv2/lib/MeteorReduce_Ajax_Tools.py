f91be505115847b1285c1e588f7dad357cc39a0d
Perlerin Vincent <vperlerin@gmail.com>
2019-11-01 12:14:22 +0100

    M

--- pythonv2/lib/MeteorReduce_Ajax_Tools.py
+++ pythonv2/lib/MeteorReduce_Ajax_Tools.py
@@ -402,7 +402,7 @@ def get_catalog_stars(cal_params):
    bright_stars_sorted = sorted(bright_stars, key=lambda x: x[4], reverse=False)
  
 
-   print(bright_stars_sorted)
+   print(str(bright_stars_sorted))
    sys.exit(0)
 
    for bname, cname, ra, dec, mag in bright_stars_sorted:
