f57e67fd79469fb6c8d46fcd8c0acb02ef4be63a
Perlerin Vincent <vperlerin@gmail.com>
2019-11-04 13:06:21 +0100

    Enc UTF

--- src/js/functions/reducepage/ui/Canvas-Interactions.js
+++ src/js/functions/reducepage/ui/Canvas-Interactions.js
@@ -140,7 +140,7 @@ if ($('canvas#c').length!=0) {
       }); 
    
 
-      // Move over over canvas
+      // Move up over canvas
       canvas.on('mouse:over', function(e) { 
          var pointer = canvas.getPointer(event.e);
          var x_val = pointer.x | 0;
@@ -152,33 +152,13 @@ if ($('canvas#c').length!=0) {
          // Remove an existing star
          for (let i in objects) {
             if (objects[i].containsPoint(clickPoint) && objects[i].type != "reduc_rect" && typeof(objects[i].ui_type)!='undefined') {
-               e.target.set('fontSize', 20); 
-               e.target.set('fill', 'red');
-               console.log("CHANGE");
-               console.log(objects[i]);
+               
+               objects[i].set('fontSize', '20'); 
+               console.log(objects[i].ui_type);
             }
          }
       });
-
-      // Move up over canvas
-      canvas.on('mouse:out', function(e) { 
-         var pointer = canvas.getPointer(event.e);
-         var x_val = pointer.x | 0;
-         var y_val = pointer.y | 0;
-         var clickPoint = new fabric.Point(x_val,y_val);
-         var objects = canvas.getObjects();
-         var id; 
       
-         // Remove an existing star
-         for (let i in objects) {
-            if (objects[i].containsPoint(clickPoint) && objects[i].type != "reduc_rect" && typeof(objects[i].ui_type)!='undefined') {
-               e.target.set('fontSize', 12); 
-               e.target.set('fill', 'rgba(255,255,255,.55)');
-               console.log("CHANGE");
-               console.log(objects[i]);
-            }
-         }
-      });
 
       // Move the zoom when moving over canvas
       canvas.on('mouse:move', function(e) { 
