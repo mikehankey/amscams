39112ad3dd295ad796c79e53cdac703ea8d3c6ed
Perlerin Vincent <vperlerin@gmail.com>
2019-11-04 12:09:39 +0100

    Enc UTF

--- src/js/functions/reducepage/Update_Star_List.js
+++ src/js/functions/reducepage/Update_Star_List.js
@@ -99,7 +99,7 @@ function update_stars_on_canvas_and_table(json_resp) {
 
         if(name_pos.length!=0) {
             $.each(name_pos,function(i,v){
-               if(v.containsPoint(new fabric.Point(name_pos_x,name_pos_y)) {
+               if(v.containsPoint(new fabric.Point(name_pos_x,name_pos_y))) {
                   console.log("MOVE " + v['name']);
                }
          })
