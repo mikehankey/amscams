48a10a0f7e978cbbe078e1865fd5dc1dcf8d157f
Perlerin Vincent <vperlerin@gmail.com>
2019-11-04 11:03:47 +0100

    Enc UTF

--- pythonv2/lib/MeteorReduce_Ajax_Tools.py
+++ pythonv2/lib/MeteorReduce_Ajax_Tools.py
@@ -411,9 +411,8 @@ def get_catalog_stars(cal_params):
       else:
          name = bname.encode('utf-8')
          name = str(name.decode('utf-8'))     
-         
-       
-      #ang_sep = angularSeparation(ra,dec,RA_center,dec_center)
+          
+      ang_sep = angularSeparation(ra,dec,RA_center,dec_center)
       if ang_sep < fov_radius and float(mag) < 5.5:
          new_cat_x, new_cat_y = distort_xy_new (0,0,ra,dec,RA_center, dec_center, x_poly, y_poly, img_w, img_h, pos_angle_ref,F_scale)
 
