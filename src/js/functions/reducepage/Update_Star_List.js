dcc9c7e9f7b90847f73c2da91c3e23dfbada507b
Perlerin Vincent <vperlerin@gmail.com>
2019-11-04 12:05:22 +0100

    Enc UTF

--- src/js/functions/reducepage/Update_Star_List.js
+++ src/js/functions/reducepage/Update_Star_List.js
@@ -97,7 +97,7 @@ function update_stars_on_canvas_and_table(json_resp) {
            type: 'star_info',
         });
 
-        if(length(name_pos)!=0) {
+        if(name_pos.length!=0) {
             $.each(name_pos,function(i,v){
                if(name_pos[i].containsPoint(name_pos_x,name_pos_y)) {
                   console.log("MOVE " + v['name']);
