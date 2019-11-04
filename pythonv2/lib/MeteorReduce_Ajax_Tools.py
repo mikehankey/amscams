e456cb5be9ac06318897124576b7347ce070de15
Perlerin Vincent <vperlerin@gmail.com>
2019-11-04 10:34:27 +0100

    Enc UTF

--- pythonv2/lib/MeteorReduce_Ajax_Tools.py
+++ pythonv2/lib/MeteorReduce_Ajax_Tools.py
@@ -415,7 +415,9 @@ def get_catalog_stars(cal_params):
             #dcname = str(bname.decode("utf-8"))
             #dbname = dcname.encode("utf-8")
             test = str(bname).encode("utf-8")
-            print(test.decode("utf-8") )
+            v = test.decode("utf-8")
+            print(v)
+            print("<br/>")
          except Exception as e:
             print("ERROR " + str(e) +  "<br/>")
 
