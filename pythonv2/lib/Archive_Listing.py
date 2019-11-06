0e8a4e942f84cb0a43cb863d57fed6be4ab187ba
Perlerin Vincent <vperlerin@gmail.com>
2019-11-06 14:43:34 +0100

    M

--- pythonv2/lib/Archive_Listing.py
+++ pythonv2/lib/Archive_Listing.py
@@ -37,7 +37,8 @@ def archive_listing(form):
    for i,month in enumerate(all_months): 
       print(month + "<br>")
       all_[month] = {int(os.path.basename(x)) for x in sorted(glob.glob(month+'/*'))}
-      
+      for day in all_[month]:
+         day = {os.path.basename(x) for x in sorted(glob.glob(day+'/*.json'))}
       
       
 
