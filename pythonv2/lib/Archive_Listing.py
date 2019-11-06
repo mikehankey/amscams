a75f6bb884fcb40a840c310e8463cd89ab13281e
Perlerin Vincent <vperlerin@gmail.com>
2019-11-06 16:54:20 +0100

    M

--- pythonv2/lib/Archive_Listing.py
+++ pythonv2/lib/Archive_Listing.py
@@ -27,7 +27,7 @@ def folder_analyser(folder):
     
     for groupNum in range(0, len(matches.groups())):
         groupNum = groupNum + 1
-        if(match.group(groupNum) is not None):
+        if(matches.group(groupNum) is not None):
            res[ARCHIVE_SUB_FOLDER_GROUP[groupNum]] = match.group(groupNum)
         #print ("Group {groupNum} found at {start}-{end}: {group}".format(groupNum = groupNum, start = matches.start(groupNum), end = matches.end(groupNum), group = matches.group(groupNum)))
    
