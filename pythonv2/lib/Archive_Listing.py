beae455b5d273c5523eadb8aec48b910397267f4
Perlerin Vincent <vperlerin@gmail.com>
2019-11-06 17:09:09 +0100

    M

--- pythonv2/lib/Archive_Listing.py
+++ pythonv2/lib/Archive_Listing.py
@@ -6,63 +6,15 @@ import re
 import glob
 import json
 
-from os import *
-from lib.Get_Station_Id import get_station_id
-from lib.REDUCE_VARS import *
-from collections import defaultdict 
-
-
-ARCHIVE_SUB_FOLDER_REGEX = r"/([0-9]{4})*?/([0-9]{2})*?/([0-9]{2})*?/"
-ARCHIVE_SUB_FOLDER_GROUP = ['all','year','month','day','totot']
-
-
-# PARSE ARCHIVE FOLDER TO RETRIEVE YEAR, MONTH & DAY
-def folder_analyser(folder):
-   n_folder = folder + "/"
-   matches = re.search(ARCHIVE_SUB_FOLDER_REGEX, n_folder)
-   res = {}
-   
-   if matches: 
-    for groupNum in range(0, len(matches.groups())):
-        groupNum = groupNum + 1
-        if(matches.group(groupNum) is not None):
-           res[ARCHIVE_SUB_FOLDER_GROUP[groupNum]] = int(matches.group(groupNum))
-
-   
-   return res
-   
-
-# GET ALL ARCHIVES FOR A GIVEN YEAR
-def get_archive_for_year(year):
-   # DEBUG
-   cgitb.enable()
-
-   main_dir = METEOR_ARCHIVE + get_station_id() + '/' + METEOR + str(year)
-   d = defaultdict(list)
-
-   for file in glob.iglob(path.join(main_dir, '**/*.json'), recursive=True):
-      analysed_folder = folder_analyser(path.dirname(file))
-      print(analysed_folder)
-      print("<br/>")
-      print("=> " +  file)
-      print("<br/>")
-
-      if(d[analysed_folder['month']] is None):
-         d[analysed_folder['month']] = []
-      
-      if(analysed_folder['day'] not in d[analysed_folder['month']] ):
-         d[analysed_folder['month']][analysed_folder['day']] = []
-
-      d[int(analysed_folder['month'])][int(analysed_folder['day'])].append(path.basename(file))
-   
-   return d
-
 
 # MAIN FUNCTION FOR THE ARCHIVE LISTING PAGE
 def archive_listing(form):
    limit_day = form.getvalue('limit_day')
    cur_page  = form.getvalue('p')
    meteor_per_page = form.getvalue('meteor_per_page')
+
+   print("NOTHING")
+   sys.exit(0)
 
    if (cur_page is None) or (cur_page==0):
       cur_page = 1
