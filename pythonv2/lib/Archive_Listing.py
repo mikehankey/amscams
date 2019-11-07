0a656c5597d715a8ea48b1528293a909de16377f
Perlerin Vincent <vperlerin@gmail.com>
2019-11-07 11:39:35 +0100

    Arch List

--- pythonv2/lib/Archive_Listing.py
+++ pythonv2/lib/Archive_Listing.py
@@ -74,7 +74,7 @@ def get_results_from_date(date,json_index,max_res):
          for day in month['days']:
             if(int(day['day'])>=date.day and res_cnt<=max_res):
                for dec in day['det']:
-                  if(res_cnt<=max_res):
+                  if(res_cnt<max_res):
                      res.append(dec)
                      res_cnt+=1
                #print(day['day'])
