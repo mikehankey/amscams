fd53c7704db233846a255d9a20605cf5a527e7db
Perlerin Vincent <vperlerin@gmail.com>
2019-11-07 10:18:13 +0100

    Arch List

--- pythonv2/lib/Archive_Listing.py
+++ pythonv2/lib/Archive_Listing.py
@@ -1,38 +1,44 @@
 # coding: utf-8
-import sys  
-import cgitb
-import datetime
-import re
 import glob
-import json
+import os
+
+from lib.REDUCE_VARS import *
+from lib.FileIO import save_json_file
+
+path = "/mnt/ams2/meteor_archive/AMS7/METEOR/2019/"
+index_year = {'year':2019,'months':[]}
+
+# Create index for a given year
+def create_json_index_year(year):
+   main_dir = METEOR_ARCHIVE + get_station_id() + '/' + METEOR + str(year)
+   index_year = {'year':year,'months':[]}
+
+   for month in sorted(glob.iglob(main_dir + '*' + os.sep, recursive=False)):	
+      cur_month = os.path.basename(os.path.normpath(month))
+     
+      cur_month_data = {'month':cur_month,'days':[]}
+      
+      for day in sorted(glob.iglob(month + '*' + os.sep, recursive=False)):	
+         cur_day = os.path.basename(os.path.normpath(day))		
+         cur_day_data = {'day':cur_day,'detections':[]}
+         
+         for detection in sorted(glob.iglob(day +  '*' + '.json', recursive=False)):
+            
+            cur_day_data['detections'].append(os.path.basename(detection))
+         cur_month_data['days'].append(cur_day_data)
+      index_year['months'].append(cur_month_data)
+
+   return json.dumps(index_year)
 
 
-# MAIN FUNCTION FOR THE ARCHIVE LISTING PAGE
-def archive_listing(form):
-   limit_day = form.getvalue('limit_day')
-   cur_page  = form.getvalue('p')
-   meteor_per_page = form.getvalue('meteor_per_page')
+# Write index for a given year
+def write_index(year):
+   json_data = create_json_index_year(year)
 
-   print("NOTHING")
-   sys.exit(0)
-
-   if (cur_page is None) or (cur_page==0):
-      cur_page = 1
-   else:
-      cur_page = int(cur_page)
-
-   if (limit_day is None):
-      now = datetime.datetime.now()
-      year = now.year
-      #TODO: else we get the date!
-  
-   # MAIN DIR:METEOR
-   #/mnt/ams2/meteor_archive/[STATION_ID]/METEOR/[YEAR]
-   #main_dir = METEOR_ARCHIVE + get_station_id() + '/' + METEOR + str(year)
-   achr = get_archive_for_year(year)
-   all = {year:achr}
-
-   print(json.dumps(all))
+   # Index PATH
+   if(json_data is not None):
+      main_dir = METEOR_ARCHIVE + get_station_id() + '/' + METEOR + str(year)
+      save_json_file(json_data, main_dir + os.sep + year + ".json")
 
 
- 
+write_index('2019')
