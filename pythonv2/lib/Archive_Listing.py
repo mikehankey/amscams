3a56ed459a3f1f88153fbee2729cb561c8f91e85
Perlerin Vincent <vperlerin@gmail.com>
2019-11-11 15:04:54 +0100

    Arch

--- pythonv2/lib/Archive_Listing.py
+++ pythonv2/lib/Archive_Listing.py
@@ -113,7 +113,7 @@ def get_html_detections(res,clear_cache):
  
       if(prev_date is None):
          prev_date = cur_date
-         res_html += '<div class="h2_holder  d-flex justify-content-between"><h2>'+cur_date.format("%Y-%m-%d")'</h2></div>'
+         res_html += '<div class="h2_holder  d-flex justify-content-between"><h2>'+cur_date.strftime("%Y-%m-%d")+'</h2></div>'
 
       if(cur_date.month != prev.date.month or cur_date.day != prev_date.day):
          prev_date = cur_date
