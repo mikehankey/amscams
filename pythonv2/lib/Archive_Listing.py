77a6afbf6e88970d197ea4136f56e592d1989637
Perlerin Vincent <vperlerin@gmail.com>
2019-11-11 16:49:33 +0100

    M

--- pythonv2/lib/Archive_Listing.py
+++ pythonv2/lib/Archive_Listing.py
@@ -151,7 +151,7 @@ def get_html_detections(res,clear_cache):
       res_html += '<div class="d-flex justify-content-between">'
       res_html += '<div class="pre-b">Cam #'+det['cam_id']+' - <b>'+det['hour']+':'+det['min']+'</b></div>'
       res_html += '<div class="btn-toolbar pr-0 pb-0"><div class="btn-group"><a class="vid_link_gal col btn btn-primary btn-sm" title="Play Video" href="./video_player.html?video='+path_to_vid+'"><i class="icon-play"></i></a>'
-      res_html += '<a class="delete_meteor_gallery col btn btn-danger btn-sm" title="Delete Detection" data-meteor=""><i class="icon-delete"></i></a></div></div>'
+      res_html += '<a class="delete_meteor_archive_gallery col btn btn-danger btn-sm" title="Delete Detection" data-meteor="'+det['full_path']+'"><i class="icon-delete"></i></a></div></div>'
       res_html += '</div></div>'
 
    
@@ -162,9 +162,7 @@ def get_html_detections(res,clear_cache):
  
 
 # MAIN FUNCTION FOR THE ARCHIVE LISTING PAGE
-def archive_listing(form):
-
-   write_index(2019)
+def archive_listing(form): 
 
    limit_day = form.getvalue('limit_day')
    cur_page  = form.getvalue('p')
