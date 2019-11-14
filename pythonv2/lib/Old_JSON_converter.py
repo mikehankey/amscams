c43a1bebeabb149a2aa50df6128e6ab914170dd2
Perlerin Vincent <vperlerin@gmail.com>
2019-11-14 14:23:43 +0100

    Detection without reduction

--- pythonv2/lib/Old_JSON_converter.py
+++ pythonv2/lib/Old_JSON_converter.py
@@ -376,6 +376,11 @@ def move_old_detection_to_archive(json_file_path, sd_video_file_path, hd_video_f
       if('org_file' in json_content['calib']):
          if(cfe(json_content['calib']['org_file'])):
             calibration_param = load_json_file(json_content['calib']['org_file'])
+            
+
+            print(calibration_param)
+
+
             json_content['calib']['device'] = {}
             
             json_content['calib']['device']['scale_px'] = float(calibration_param['pixscale'])
