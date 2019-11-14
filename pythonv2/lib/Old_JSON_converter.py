798808c013b17a453d05384060d3080a6112e0a5
Perlerin Vincent <vperlerin@gmail.com>
2019-11-14 14:12:36 +0100

    M

--- pythonv2/lib/Old_JSON_converter.py
+++ pythonv2/lib/Old_JSON_converter.py
@@ -377,10 +377,10 @@ def move_old_detection_to_archive(json_file_path, sd_video_file_path, hd_video_f
          if(cfe(json_content['calib']['org_file'])):
             calibration_param = load_json_file(json_content['calib']['org_file'])
             json_content['calib']['device'] = {}
+            json_content['calib']['device']['scale_px'] = calibration_param['pixscale']
             json_content['calib']['device']['poly'] = {}
             json_content['calib']['device']['poly']['x_fwd'] = calibration_param['x_poly_fwd']
             json_content['calib']['device']['poly']['y_fwd'] = calibration_param['y_poly_fwd']
-            json_content['calib']['device']['scale_px'] = calibration_param['pixscale']
              
    # Add the sync SD/HD if we have them
    if(sync_res != False): 
