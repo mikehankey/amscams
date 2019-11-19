3261c5da4d904152c5e22a06129f94607b350c29
Perlerin Vincent <vperlerin@gmail.com>
2019-11-19 14:43:45 +0100

    M

--- pythonv2/lib/Archive_Listing.py
+++ pythonv2/lib/Archive_Listing.py
@@ -130,7 +130,7 @@ def create_json_index_year(year):
 
 # Write index for a given month
 def write_month_index(month, year):
-   json_data = create_json_index_month(year) 
+   json_data = create_json_index_month(month, year) 
 
    # Write Index if we have data
    if('days' in json_data):
