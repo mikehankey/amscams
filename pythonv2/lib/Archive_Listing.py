9f59e7b3151058ccdb6f85c0ff0e1fb966b0c360
Perlerin Vincent <vperlerin@gmail.com>
2019-11-19 16:36:23 +0100

    M

--- pythonv2/lib/Archive_Listing.py
+++ pythonv2/lib/Archive_Listing.py
@@ -151,7 +151,7 @@ def create_json_index_month(month,year):
             index_month['days'][int(cur_day)].append({'p':det[11:],'mag':mag,'dur':dur,'red':red})
 
     
-   index_month['days'] = collections.reversed(collections.OrderedDict(sorted(index_month['days'].items())))
+   index_month['days'] =  reversed(collections.OrderedDict(sorted(index_month['days'].items())))
 
    return index_month             
 
