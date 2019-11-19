8dd2e1bd9960291252bde68f67051a963cbff69a
Perlerin Vincent <vperlerin@gmail.com>
2019-11-19 12:43:58 +0100

    M

--- pythonv2/lib/Archive_Listing.py
+++ pythonv2/lib/Archive_Listing.py
@@ -54,7 +54,7 @@ def create_json_index_month(month,year):
    station_id = get_station_id()
    main_dir = METEOR_ARCHIVE +  station_id + os.sep + METEOR + str(year) + os.sep + str(month)
 
-   index_month = {'station_id':station_id,'year':int(year),'month':int(month),'days':{}}}
+   index_month = {'station_id':station_id,'year':int(year),'month':int(month),'days':{}}
    
    for day in sorted(glob.iglob(main_dir + '*' + os.sep + '*', recursive=True), reverse=True):	
       cur_day = os.path.basename(os.path.normpath(day))
