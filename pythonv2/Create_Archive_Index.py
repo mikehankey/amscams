0a1f47f5166a08d25e8e6788401f8500fc548d34
Perlerin Vincent <vperlerin@gmail.com>
2019-11-19 16:33:42 +0100

    M

--- pythonv2/Create_Archive_Index.py
+++ pythonv2/Create_Archive_Index.py
@@ -7,7 +7,7 @@ write_month_index(11,2019)
 #v = add_to_month_index('/mnt/ams2/meteor_archive/AMS7/METEOR/2019/11/16/2019_11_16_07_51_52_000_010037-trim0670.json')
 #print(v)
 #v = add_to_month_index('/mnt/ams2/meteor_archive/AMS7/METEOR/2019/11/16/2019_11_16_22_36_24_000_010042-trim0519.json')
-"print(v)
+#print(v)
 
 
 
