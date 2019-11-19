d348ea1450a6bc5b883f9b8b38e4bd196d033220
Perlerin Vincent <vperlerin@gmail.com>
2019-11-19 14:59:55 +0100

    M

--- pythonv2/Create_Archive_Index.py
+++ pythonv2/Create_Archive_Index.py
@@ -4,9 +4,10 @@ from lib.Archive_Listing import *
 #write_month_index(11,2019)
 
 
-search_month_index('2019_11_16_07_51_52_000_010037-trim0670.json')
-search_month_index('2019_11_16_07_51_52_000_010037-trim1212.json')
-
+v = search_month_index('2019_11_16_07_51_52_000_010037-trim0670.json')
+print(v)
+v = search_month_index('2019_11_16_07_51_52_000_010037-trim1212.json')
+print(v)
 
 
 
