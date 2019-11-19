93799c3f5dfb6c26ea4398eed5150daa3e3f4b2b
Perlerin Vincent <vperlerin@gmail.com>
2019-11-19 11:26:36 +0100

    M

--- pythonv2/lib/Archive_Listing.py
+++ pythonv2/lib/Archive_Listing.py
@@ -34,6 +34,9 @@ def get_diag_fields(detection):
 
       for up_diag_field in ARCHIVE_LISTING_DIAGNOSTIC_FIELDS:
          print("NAME OF THE FIELD "  + str(up_diag_field)) 
+         print("SUB KEYS")
+         print(ARCHIVE_LISTING_DIAGNOSTIC_FIELDS[up_diag_field].keys())
+
 
          i=0
          # We get all the included values to get the proper JSON field
