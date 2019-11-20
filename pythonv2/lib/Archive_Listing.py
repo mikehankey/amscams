67ca07a28eacd03fdfd0b3c66c7a946f8b4c46ba
Perlerin Vincent <vperlerin@gmail.com>
2019-11-20 11:46:55 +0100

    ARchive listings

--- pythonv2/lib/Archive_Listing.py
+++ pythonv2/lib/Archive_Listing.py
@@ -282,6 +282,14 @@ def get_results_from_date_from_monthly_index(date,json_index,max_res):
          if( (cur_month_test and int(day)<=int(date.day) and res_cnt<=max_res) or (not cur_month_test and int(cur_month)<int(date.month))and res_cnt<=max_res):
             for detection in detections:
                if(res_cnt<=max_res):
+
+                  # We complete the detection['p'] to get the full path (as the index only has compressed name)
+                  # ex: 'p': '22_36_24_000_010042-trim0519'
+                  #      => '/mnt/ams2/meteor_archive/AMS7/METEOR/2019/11/16/2019_11_16_22_36_24_000_010042-trim0519.json'
+                  detection['p'] = METEOR_ARCHIVE + get_station_id()  + os.sep + METEOR + str(date.year) + os.sep + str(date.month.zfill(2)) + os.sep + str(day.zfill(2)) + os.sep + str(date.year) + '_' + str(date.month.zfill(2))+ '_' + str(date.day.zfill(2)) + detection['p'] + ".json"
+                  print("<br/>NEW P")
+                  print(detection['p'])
+                  print("<hr>")
                   res.append(detection)
                   res_cnt+=1 
   
@@ -306,15 +314,14 @@ def get_video(_file):
       return _file
 
 
+
+
 # Get HTML version of each detection
 def get_html_detections(res,clear_cache):
 
    res_html = ''
    prev_date = None
    
-   print(res)
-   sys.exit()
-
    for detection in res:
 
       # We add the missing info to detection['p']
