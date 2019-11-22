d85dd00904079ca93abf04fcd75bc79e4fbae72a
Perlerin Vincent <vperlerin@gmail.com>
2019-11-22 14:00:55 +0100

    M

--- pythonv2/lib/Archive_Listing.py
+++ pythonv2/lib/Archive_Listing.py
@@ -399,18 +399,20 @@ def get_html_detections(res,clear_cache):
       # so the name analyser will work
       det = name_analyser(detection['p'])
       cur_date = get_datetime_from_analysedname(det)
- 
-      if(prev_date is None): 
-         prev_date = cur_date
-         cur_title  += '<div class="h2_holder d-flex justify-content-between"><h2>'+cur_date.strftime("%Y/%m/%d")+" - %TOTAL"+str(cur_counter)+"%</h2></div>"
-         cur_title  += '<div class="gallery gal-resize row text-center text-lg-left mb-5 mr-5 ml-5">' 
-      elif(cur_date.month != prev_date.month or cur_date.day != prev_date.day or cur_date.year != prev_date.year):
-         first = False
-         prev_date = cur_date
-         cur_title  +=  '</div><div class="h2_holder d-flex justify-content-between"><h2>'+cur_date.strftime("%Y/%m/%d")+" -  %TOTAL"+str(cur_counter)+"</h2></div>"
-         cur_title  += '<div class="gallery gal-resize row text-center text-lg-left mb-5 mr-5 ml-5">'
-      else:
-         cur_counter+=1
+
+      res_html += cur_date.strftime("%Y/%m/%d")
+
+      #if(prev_date is None): 
+      #   prev_date = cur_date
+      #   cur_title  += '<div class="h2_holder d-flex justify-content-between"><h2>'+cur_date.strftime("%Y/%m/%d")+" - %TOTAL"+str(cur_counter)+"%</h2></div>"
+      #   cur_title  += '<div class="gallery gal-resize row text-center text-lg-left mb-5 mr-5 ml-5">' 
+      #elif(cur_date.month != prev_date.month or cur_date.day != prev_date.day or cur_date.year != prev_date.year):
+      #   first = False
+      #   prev_date = cur_date
+      #   cur_title  +=  '</div><div class="h2_holder d-flex justify-content-between"><h2>'+cur_date.strftime("%Y/%m/%d")+" -  %TOTAL"+str(cur_counter)+"</h2></div>"
+      #   cur_title  += '<div class="gallery gal-resize row text-center text-lg-left mb-5 mr-5 ml-5">'
+      #else:
+      #   cur_counter+=1
 
       if(cur_title!='' and first==False):
          res_html    += cur_title.replace('%TOTAL%',str(cur_counter) +  ' detections')  
