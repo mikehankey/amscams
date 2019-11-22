22996cf446e525bb9937de6a8f8cc6c54361c802
Perlerin Vincent <vperlerin@gmail.com>
2019-11-22 13:21:25 +0100

    M

--- pythonv2/lib/Archive_Listing.py
+++ pythonv2/lib/Archive_Listing.py
@@ -401,12 +401,12 @@ def get_html_detections(res,clear_cache):
  
       if(prev_date is None):
          prev_date = cur_date
-         cur_title  += '<div class="h2_holder d-flex justify-content-between"><h2>'+cur_date.strftime("%Y/%m/%d")+'</h2></div>'
+         cur_title  += '<div class="h2_holder d-flex justify-content-between"><h2>'+cur_date.strftime("%Y/%m/%d")+' - {TOTAL}</h2></div>'
          cur_title  += '<div class="gallery gal-resize row text-center text-lg-left mb-5 mr-5 ml-5">'
          cur_counter = 0
       elif(cur_date.month != prev_date.month or cur_date.day != prev_date.day or cur_date.year != prev_date.year):
          prev_date = cur_date
-         cur_title  +=  '</div><div class="h2_holder d-flex justify-content-between"><h2>'+cur_date.strftime("%Y/%m/%d")+'</h2></div>'
+         cur_title  +=  '</div><div class="h2_holder d-flex justify-content-between"><h2>'+cur_date.strftime("%Y/%m/%d")+'' - {TOTAL}</h2></div>'
          cur_title  += '<div class="gallery gal-resize row text-center text-lg-left mb-5 mr-5 ml-5">'
          cur_counter = 0
       else:
@@ -422,6 +422,11 @@ def get_html_detections(res,clear_cache):
 
       # Get Video for preview
       path_to_vid = get_video(det['full_path'])       
+
+
+      if(cur_title!=''):
+         res_html += cur_title.replace('{TOTAL}',str(cur_counter) +  ' detections')
+
       
       # Otherwise preview = preview (:)
       res_html += '<div class="preview col-lg-3 col-md-3 select-to mb-3'
