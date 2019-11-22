7953a6ac9d38e2d7b9e080130929085fa3dfd8d2
Perlerin Vincent <vperlerin@gmail.com>
2019-11-22 14:17:03 +0100

    M

--- pythonv2/lib/Archive_Listing.py
+++ pythonv2/lib/Archive_Listing.py
@@ -456,8 +456,7 @@ def get_html_detections(res,clear_cache):
       # so the name analyser will work
       det = name_analyser(detection['p'])
       cur_date = get_datetime_from_analysedname(det)
-
-
+ 
       if(prev_date is None): 
          prev_date = cur_date
          cur_title  += '<div class="h2_holder d-flex justify-content-between"><h2>'+cur_date.strftime("%Y/%m/%d")+" - %TOTAL%</h2></div>"
