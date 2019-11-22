5ddd789bb36166f20cb3e4797cedbd50db061799
Perlerin Vincent <vperlerin@gmail.com>
2019-11-22 13:28:09 +0100

    M

--- pythonv2/lib/Archive_Listing.py
+++ pythonv2/lib/Archive_Listing.py
@@ -399,7 +399,19 @@ def get_html_detections(res,clear_cache):
       det = name_analyser(detection['p'])
       cur_date = get_datetime_from_analysedname(det)
  
-      
+      if(prev_date is None):
+         res_html += "AAAAAAAAAAAAAAAAAAAAAAA"
+         prev_date = cur_date
+         cur_title  += '<div class="h2_holder d-flex justify-content-between"><h2>'+cur_date.strftime("%Y/%m/%d")+" - %TOTAL%</h2></div>"
+         cur_title  += '<div class="gallery gal-resize row text-center text-lg-left mb-5 mr-5 ml-5">'
+      elif(cur_date.month != prev_date.month or cur_date.day != prev_date.day or cur_date.year != prev_date.year):
+         res_html += "BBBBBBBBBBBBBBBBBBBBBBB"
+         prev_date = cur_date
+         cur_title  +=  '</div><div class="h2_holder d-flex justify-content-between"><h2>'+cur_date.strftime("%Y/%m/%d")+" - %TOTAL%</h2></div>"
+         cur_title  += '<div class="gallery gal-resize row text-center text-lg-left mb-5 mr-5 ml-5">'
+      else:
+         res_html += "CCCCCCCCCCCCCCCCCCCCCC"
+         cur_counter+=1
  
       # Do we have a thumb stack preview for this detection?
       preview = does_cache_exist(det,"preview","/*.jpg")
@@ -417,16 +429,6 @@ def get_html_detections(res,clear_cache):
          cur_title  = ''
          cur_counter = 1
 
-      if(prev_date is None):
-         prev_date = cur_date
-         cur_title  += '<div class="h2_holder d-flex justify-content-between"><h2>'+cur_date.strftime("%Y/%m/%d")+" - %TOTAL%</h2></div>"
-         cur_title  += '<div class="gallery gal-resize row text-center text-lg-left mb-5 mr-5 ml-5">'
-      elif(cur_date.month != prev_date.month or cur_date.day != prev_date.day or cur_date.year != prev_date.year):
-         prev_date = cur_date
-         cur_title  +=  '</div><div class="h2_holder d-flex justify-content-between"><h2>'+cur_date.strftime("%Y/%m/%d")+" - %TOTAL%</h2></div>"
-         cur_title  += '<div class="gallery gal-resize row text-center text-lg-left mb-5 mr-5 ml-5">'
-      else:
-         cur_counter+=1
       
       # Otherwise preview = preview (:)
       res_html += '<div class="preview col-lg-3 col-md-3 select-to mb-3'
