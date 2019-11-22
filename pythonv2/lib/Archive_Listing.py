bbe5e66c0592f5d654d393f25a577989341f3d6b
Perlerin Vincent <vperlerin@gmail.com>
2019-11-22 13:22:55 +0100

    M

--- pythonv2/lib/Archive_Listing.py
+++ pythonv2/lib/Archive_Listing.py
@@ -401,16 +401,15 @@ def get_html_detections(res,clear_cache):
  
       if(prev_date is None):
          prev_date = cur_date
-         cur_title  += '<div class="h2_holder d-flex justify-content-between"><h2>'+cur_date.strftime("%Y/%m/%d")+' - {TOTAL}</h2></div>'
+         cur_title  += '<div class="h2_holder d-flex justify-content-between"><h2>'+cur_date.strftime("%Y/%m/%d")+" - %TOTAL%</h2></div>"
          cur_title  += '<div class="gallery gal-resize row text-center text-lg-left mb-5 mr-5 ml-5">'
          cur_counter = 0
       elif(cur_date.month != prev_date.month or cur_date.day != prev_date.day or cur_date.year != prev_date.year):
          prev_date = cur_date
-         cur_title  +=  '</div><div class="h2_holder d-flex justify-content-between"><h2>'+cur_date.strftime("%Y/%m/%d")+'' - {TOTAL}</h2></div>'
+         cur_title  +=  '</div><div class="h2_holder d-flex justify-content-between"><h2>'+cur_date.strftime("%Y/%m/%d")+" - %TOTAL%</h2></div>"
          cur_title  += '<div class="gallery gal-resize row text-center text-lg-left mb-5 mr-5 ml-5">'
          cur_counter = 0
       else:
-         cur_title  = ''
          cur_counter+=1
  
       # Do we have a thumb stack preview for this detection?
@@ -425,7 +424,8 @@ def get_html_detections(res,clear_cache):
 
 
       if(cur_title!=''):
-         res_html += cur_title.replace('{TOTAL}',str(cur_counter) +  ' detections')
+         res_html += cur_title.replace('%TOTAL%',str(cur_counter) +  ' detections')
+         cur_title  = ''
 
       
       # Otherwise preview = preview (:)
