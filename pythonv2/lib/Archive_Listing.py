6ac5057063db32149386933bf8b7a968a0f7c054
Perlerin Vincent <vperlerin@gmail.com>
2019-11-22 13:30:58 +0100

    M

--- pythonv2/lib/Archive_Listing.py
+++ pythonv2/lib/Archive_Listing.py
@@ -391,6 +391,7 @@ def get_html_detections(res,clear_cache):
    cur_title = ''
    prev_date = None
    cur_counter = 1
+   first = True
    
    for detection in res:
 
@@ -404,6 +405,7 @@ def get_html_detections(res,clear_cache):
          cur_title  += '<div class="h2_holder d-flex justify-content-between"><h2>'+cur_date.strftime("%Y/%m/%d")+" - %TOTAL%</h2></div>"
          cur_title  += '<div class="gallery gal-resize row text-center text-lg-left mb-5 mr-5 ml-5">'
       elif(cur_date.month != prev_date.month or cur_date.day != prev_date.day or cur_date.year != prev_date.year):
+         first = False
          prev_date = cur_date
          cur_title  +=  '</div><div class="h2_holder d-flex justify-content-between"><h2>'+cur_date.strftime("%Y/%m/%d")+" - %TOTAL%</h2></div>"
          cur_title  += '<div class="gallery gal-resize row text-center text-lg-left mb-5 mr-5 ml-5">'
@@ -421,7 +423,7 @@ def get_html_detections(res,clear_cache):
       path_to_vid = get_video(det['full_path'])       
 
 
-      if(cur_title!=''):
+      if(cur_title!='' and first==False):
          res_html += cur_title.replace('%TOTAL%',str(cur_counter) +  ' detections')
          cur_title  = ''
          cur_counter = 1
