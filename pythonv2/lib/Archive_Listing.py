aa9bc3babf4afba42504c883d3ed26675f3a4203
Perlerin Vincent <vperlerin@gmail.com>
2019-11-27 15:13:42 +0100

    M

--- pythonv2/lib/Archive_Listing.py
+++ pythonv2/lib/Archive_Listing.py
@@ -525,11 +525,11 @@ def create_criteria_selector(selected, criteria, all_msg, sign, unit=''):
    for mag in POSSIBLE_MAGNITUDES:
       if(one_selected==True):
          if(float(mag)==float(selected)):
-            mag_select+= '<option selected value="'+str(mag)+'">'+sign+str(mag)+ unit'</option>'
+            mag_select+= '<option selected value="'+str(mag)+'">'+sign+str(mag)+ unit+'/option>'
          else:
-            mag_select+= '<option value="'+str(mag)+'">'+sign + str(mag)+ unit'</option>'  
+            mag_select+= '<option value="'+str(mag)+'">'+sign + str(mag)+ unit+'</option>'  
       else:
-         mag_select+= '<option value="'+str(mag)+'">'+sign + str(mag)+ unit'</option>'  
+         mag_select+= '<option value="'+str(mag)+'">'+sign + str(mag)+ unit+'</option>'  
    
    return mag_select, criteria
    
