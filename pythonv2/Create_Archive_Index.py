7eecbe0809833adc2ef4efc7144176d39ff34b0a
Perlerin Vincent <vperlerin@gmail.com>
2019-11-27 12:11:43 +0100

    M

--- pythonv2/Create_Archive_Index.py
+++ pythonv2/Create_Archive_Index.py
@@ -1,21 +1,26 @@
+import sys
+
 from lib.Archive_Listing import *
 
-#write_year_index(2019)
-write_month_index(8,2019)
-write_month_index(9,2019)
-write_month_index(10,2019)
-write_month_index(11,2019) 
 
+# Allows to update an index of detections in the archive
+# for a given year 
+# or a given year & month
 
-#v = add_to_month_index('/mnt/ams2/meteor_archive/AMS7/METEOR/2019/11/16/2019_11_16_07_51_52_000_010037-trim0670.json')
-#print(v)
-#v = add_to_month_index('/mnt/ams2/meteor_archive/AMS7/METEOR/2019/11/16/2019_11_16_22_36_24_000_010042-trim0519.json')
-#print(v)
+# usage
+# Create_Archive_Index YEAR
+# Create_Archive_Index YEAR MONTH
 
+year = sys.argv[1]
+month = sys.argv[2]
 
+if(month):
+   write_month_index(int(month),int(year))
+   print("INDEX FOR " + int(month) + "/" + int(year) +  " updated")
+else:
+   write_year_index(int(year))
 
-
-
+ 
 
 
 
