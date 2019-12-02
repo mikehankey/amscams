9b3e68c5e69df8338d59e1a9718469f02ca35093
Perlerin Vincent <vperlerin@gmail.com>
2019-12-02 16:24:32 +0100

    M

--- pythonv2/lib/Archive_Listing.py
+++ pythonv2/lib/Archive_Listing.py
@@ -692,9 +692,9 @@ def archive_listing(form):
       template = template.replace("{PAGINATION}", "")
       template = template.replace("{FOUND}", "")   
    elif(len(res)+1!=total):
-      template = template.replace("{FOUND}", "<div class='page_h mr-2'><small>Displaying " + str(len(res)+1) + " out of " +  str(total))    + '</small></div>'
+      template = template.replace("{FOUND}", "<div class='page_h mr-2'><small>Displaying " + str(len(res)+1) + " out of " +  str(total))    + "</small></div>")
    else:
-       template = template.replace("{FOUND}", "")  
+      template = template.replace("{FOUND}", "")  
 
    # Display Template
    return template
