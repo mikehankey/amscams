19b470158d1bb7b9631c0001141ee1ca164ad29b
Perlerin Vincent <vperlerin@gmail.com>
2019-12-02 15:14:40 +0100

    M

--- pythonv2/lib/MeteorReduce_Tools.py
+++ pythonv2/lib/MeteorReduce_Tools.py
@@ -380,6 +380,13 @@ def get_stacks(analysed_name,clear_cache,toHD):
 
 # Generate the Stacks for a meteor detection
 def generate_stacks(video_full_path, destination, toHD):
+   
+   print("IN GENERATE STACKS<br/>")
+   print("path " + video_full_path +"<br/>")
+   
+   print("destination " + destination +"<br/>")
+   
+   print("toHD " + str(toHD) +"<br/>")
  
    # Debug
    cgitb.enable() 
