3bce2dc595418c0b30c3456da1c0f4e278ad16f0
Perlerin Vincent <vperlerin@gmail.com>
2019-12-02 15:32:56 +0100

    M

--- pythonv2/lib/Archive_Listing.py
+++ pythonv2/lib/Archive_Listing.py
@@ -405,12 +405,12 @@ def get_results_from_date_from_monthly_index(criteria,date,max_res_per_page,cur_
          cur_month = 12
          cur_year =  cur_year - 1
          json_index =  get_monthly_index(cur_month,cur_year)
-         date = date.replace(year=cur_year,month=cur_month)
+         date = date.replace(year=cur_year, month=cur_month)
          print("a) WE CHANGE THE INDEX TO " + str(cur_month) + '/' +  str(cur_year) + "<br/>")
       else:
          cur_month = cur_month -1
-         json_index =  get_monthly_index(cur_month,cur_year)0
-         date = date.replace(year=cur_year,month=cur_month)
+         json_index =  get_monthly_index(cur_month,cur_year)
+         date = date.replace(year=cur_year, month=cur_month)
          print("b) WE CHANGE THE INDEX TO " + str(cur_month) + '/' +  str(cur_year) + "<br/>")
    
       print("res_counter " + str(res_counter) + "<br/>")
