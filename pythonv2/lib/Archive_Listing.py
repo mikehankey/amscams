0ca492d1a22fc0ae58a149d57f0dc9ccfa523f84
Perlerin Vincent <vperlerin@gmail.com>
2019-12-02 16:13:47 +0100

    M

--- pythonv2/lib/Archive_Listing.py
+++ pythonv2/lib/Archive_Listing.py
@@ -690,7 +690,9 @@ def archive_listing(form):
       template = template.replace("{RESULTS}", "<div class='alert alert-danger mx-auto'>No detection found in your the archive for your criteria.</div>")
       template = template.replace("{PAGINATION_DET}", "")    
       template = template.replace("{PAGINATION}", "")
-
+       template = template.replace("{FOUND}", "")   
+   else:
+      template = template.replace("{FOUND}", str(len(res)) + "/" +  str(total))   
 
    # Display Template
    return template
