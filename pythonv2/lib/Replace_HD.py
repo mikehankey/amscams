0497ab08f7bc173562bbc8eb850999bdc52e3e6a
Perlerin Vincent <vperlerin@gmail.com>
2019-12-05 12:36:05 +0100

    M

--- pythonv2/lib/Replace_HD.py
+++ pythonv2/lib/Replace_HD.py
@@ -4,6 +4,7 @@ import os
 
 from lib.FileIO import cfe
 from lib.VIDEO_VARS import HD_W, HD_H
+from lib.CGI_Tools import redirect_to
 
 
 # Replace HD video by the resized SD video
@@ -36,4 +37,10 @@ def replace_HD(form):
    # Resize SD and replace HD 
    cmd = "ffmpeg -y -i " + video_sd_full_path + " -vf scale="+str(HD_W)+":"+str(HD_H)+" " + video_hd_full_path
    os.system(cmd)
+ 
+   # We assume everything went fine
+   print("{'status':1}")
+
+   # We recreate the eventual media after we get rid of the HD video
+   redirect_to("/pycgi/webUI.py?cmd=reduce2&video_file=" + json_file + "&clear_cache=1&c=" + str(random.randint(0,100000000)), "reduction")
  
