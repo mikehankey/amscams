ae85bc0b3c10a490e61ca179593a596eb99b6db5
Perlerin Vincent <vperlerin@gmail.com>
2019-12-05 11:48:04 +0100

    M

--- src/js/functions/reducepage/HD_fix.js
+++ src/js/functions/reducepage/HD_fix.js
@@ -1,7 +1,7 @@
 function HD_fix() {
 
    bootbox.confirm({
-      message: "Use this function if the meteor doesnt appear in the HD video (and the HD Stack). The HD video will be replaced by a resized version of the SD video.",
+      message: "Use this function if the meteor doesnt appear in the HD video. The HD video will be permanently replaced by a resized version of the SD video.",
       className: 'rubberBand animated info',
       centerVertical: true,
       buttons: {
