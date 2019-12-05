1bbf891d980c36a338e432dc8b25ba0260640ae9
Perlerin Vincent <vperlerin@gmail.com>
2019-12-05 12:25:21 +0100

    M

--- pythonv2/lib/Replace_HD.py
+++ pythonv2/lib/Replace_HD.py
@@ -34,4 +34,6 @@ def replace_HD(form):
       sys.exit(0)
 
    # Resize SD and replace HD 
-   os.system("ffmpeg -i " + video_sd_full_path + " -vf scale="+HD_W+":"+HD_H+" " + video_hd_full_path)
+   cmd = "ffmpeg -y -i " + video_sd_full_path + " -vf scale="+HD_W+":"+HD_H+" " + video_hd_full_path
+   os.system(cmd)
+ 
