0806c01d8cfcc6963fb8120ab15234e4334d545c
Perlerin Vincent <vperlerin@gmail.com>
2019-12-11 12:39:34 +0100

    Test YOUTUBE LIVE

--- pythonv2/liveyoutube.sh
+++ pythonv2/liveyoutube.sh
@@ -5,8 +5,8 @@
 
 # Configurer youtube avec une résolution 720p. La vidéo n'est pas scalée.
 
-VBR="4000k"                               # Bitrate de la vidéo en sortie
-FPS="25"                                  # FPS de la vidéo en sortie
+VBR="2000k"                               # Bitrate de la vidéo en sortie
+FPS="20"                                  # FPS de la vidéo en sortie
 QUAL="fast"                               # Preset de qualité FFMPEG
  
 
@@ -20,6 +20,7 @@ YOUTUBE_URL="rtmp://a.rtmp.youtube.com/live2/$KEY"
 ffmpeg \
     -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero  \
     -i "$SOURCE" -deinterlace -vf scale=1280:720\
+    -vf  drawtext="text='GEMINIDES': fontcolor=white: fontsize=24: box=1: boxcolor=black@0.5:boxborderw=5: x=(w-text_w)/2: y=(h-text_h)/2"
     -vcodec libx264 -pix_fmt yuv420p -preset $QUAL -r $FPS -g $(($FPS * 2)) -b:v $VBR \
     -acodec libmp3lame -ar 44100 -threads 6 -qscale 3 -b:a 712000 -bufsize 512k \
     -f flv "$YOUTUBE_URL"
