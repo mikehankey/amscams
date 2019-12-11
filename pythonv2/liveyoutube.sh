328de2f2d38a286c85c0052c77c91504a3aa5b9e
Perlerin Vincent <vperlerin@gmail.com>
2019-12-11 14:05:56 +0100

    Test YOUTUBE LIVE

--- pythonv2/liveyoutube.sh
+++ pythonv2/liveyoutube.sh
@@ -15,15 +15,15 @@ QUAL="fast"                               # Preset de qualité FFMPEG
 #YOUTUBE_URL="rtmps://live-api-s.facebook.com:443/rtmp/10157538776313530?s_bl=1&s_ps=1&s_sml=0&s_sw=0&s_vt=api-s&a=AbxaYxpWnYB_oXzm"
 SOURCE="rtsp://192.168.76.72/user=admin&password=&channel=1&stream=0.sdp"              # Source UDP (voir les annonces SAP)
 KEY="2e88-ec97-t36t-ec68"                                     # Clé à récupérer sur l'event youtube
-YOUTUBE_URL="rtmp://a.rtmp.youtube.com/live2/$KEY" 
 AMSIMAGE = "/home/ams/amscams/dist/img/ams_logo_vid_anim/1280x720/AMS30.png"
+YOUTUBE_URL="rtmp://a.rtmp.youtube.com/live2/$KEY" 
 #-vf drawtext="text='GEMINIDES': fontcolor=white: fontsize=24: box=1: boxcolor=black@0.5:boxborderw=5: x=(w-text_w)/2: y=(h-text_h)/2" \
 
 
 
 
 echo ffmpeg \
-      -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero -i "$SOURCE" -i "$AMSIMAGE"\
+      -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero -i "$SOURCE" -i "$AMSIMAGE" \
       -filter_complex "[1:v]scale=1280x720[scaled];[scaled]yadif[m];[m][1]overlay=25:25,realtime" \
       -af arealtime  \
       -deinterlace \
