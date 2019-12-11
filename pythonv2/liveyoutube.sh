4b23b98b3f7b11d4db4e482264d476f3f4d2dfa2
Perlerin Vincent <vperlerin@gmail.com>
2019-12-11 14:03:20 +0100

    Test YOUTUBE LIVE

--- pythonv2/liveyoutube.sh
+++ pythonv2/liveyoutube.sh
@@ -20,12 +20,27 @@ AMS_IMAGE = "/home/ams/amscams/dist/img/ams_logo_vid_anim/1280x720/AMS30.png"
 #-vf drawtext="text='GEMINIDES': fontcolor=white: fontsize=24: box=1: boxcolor=black@0.5:boxborderw=5: x=(w-text_w)/2: y=(h-text_h)/2" \
 
 
+
+
 ffmpeg \
-    -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero  \
-    -i "$SOURCE" -i "$AMS_IMAGE"  \
-     -vf scale=1280:720\
-    -filter_complex "[0]yadif[m];[m][1]overlay=25:25,realtime" -af arealtime  \
-    -deinterlace \
-    -vcodec libx264 -pix_fmt yuv420p -preset $QUAL -r $FPS -g $(($FPS * 2)) -b:v $VBR \
-    -acodec libmp3lame -ar 44100 -threads 6 -qscale 3 -b:a 712000 -bufsize 512k \
-    -f flv "$YOUTUBE_URL"
+      -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero -i "$SOURCE"   -i "$AMS_IMAGE"\
+      -filter_complex "[1:v]scale=1280x720[scaled];[scaled]yadif[m];[m][1]overlay=25:25,realtime" \
+      -af arealtime  \
+      -deinterlace \
+      -vcodec libx264 -pix_fmt yuv420p -preset $QUAL -r $FPS -g $(($FPS * 2)) -b:v $VBR \
+      -acodec libmp3lame -ar 44100 -threads 6 -qscale 3 -b:a 712000 -bufsize 512k \
+      -f flv "$YOUTUBE_URL"
+
+
+
+ 
+#ffmpeg \
+#    -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero  \
+#    -i "$SOURCE" 
+#    -vf scale=1280:720 \
+#    -i "$AMS_IMAGE"  \
+#    -filter_complex "[0]yadif[m];[m][1]overlay=25:25,realtime" -af arealtime  \
+#    -deinterlace \
+#   -vcodec libx264 -pix_fmt yuv420p -preset $QUAL -r $FPS -g $(($FPS * 2)) -b:v $VBR \
+#    -acodec libmp3lame -ar 44100 -threads 6 -qscale 3 -b:a 712000 -bufsize 512k \
+#    -f flv "$YOUTUBE_URL"
