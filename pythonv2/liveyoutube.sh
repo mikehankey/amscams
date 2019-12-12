363d5c17ab6c85c6a9c7340969f458fab12371a1
Perlerin Vincent <vperlerin@gmail.com>
2019-12-12 10:10:54 +0100

    M

--- pythonv2/liveyoutube.sh
+++ pythonv2/liveyoutube.sh
@@ -26,7 +26,7 @@ YOUTUBE_URL="rtmp://a.rtmp.youtube.com/live2/${KEY}"
 echo ffmpeg \
       -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero -i "${SOURCE}" -i "${AMSIMAGE}" \
       -filter_complex "[1:v]scale=1280x720[scaled];\
-      [scaled]drawtext=:text="${TEXT}":fontfile='/usr/share/fonts/truetypeTEXT}/dejavu/DejaVuSans-Bold.ttf':fontcolor=white@0.45:fontsize=14:x=20:y=20[texted]; \
+      [scaled]drawtext=:text="${TEXT}":fontfile='/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf':fontcolor=white@0.45:fontsize=14:x=20:y=20[texted]; \
       [texted]yadif[m];[m][2]overlay=25:25" \
       -vcodec libx264 -pix_fmt yuv420p -preset ${QUAL} -r ${FPS} -g $((${FPS} * 2)) -b:v ${VBR} \
       -acodec libmp3lame -ar 44100 -threads 6 -qscale 3 -b:a 712000 -bufsize 512k \
