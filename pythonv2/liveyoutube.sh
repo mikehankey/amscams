e3a8a27b6f19d31eee06851846d1b0f4c2f5b5fb
Perlerin Vincent <vperlerin@gmail.com>
2019-12-12 10:00:29 +0100

    Youtube Live

--- pythonv2/liveyoutube.sh
+++ pythonv2/liveyoutube.sh
@@ -17,16 +17,16 @@ CAM_IP= "192.168.76.71"
 KEY="2e88-ec97-t36t-ec68"                                                              # Clé à récupérer sur l'event youtube
 AMSIMAGE="/home/ams/amscams/dist/img/1280x720/AMS_UA.png"
 
-TEST = "GEMINIDS 2019 - Live from West Virginia"
+TEXT = "GEMINIDS 2019 - Live from West Virginia"
 
 
-SOURCE="rtsp://$CAM_IP/user=admin&password=&channel=1&stream=0.sdp"              # Source UDP (voir les annonces SAP)
-YOUTUBE_URL="rtmp://a.rtmp.youtube.com/live2/$KEY" 
+SOURCE="rtsp://${CAM_IP}/user=admin&password=&channel=1&stream=0.sdp"              # Source UDP (voir les annonces SAP)
+YOUTUBE_URL="rtmp://a.rtmp.youtube.com/live2/${KEY}" 
 
 ffmpeg \
-      -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero -i "$SOURCE" -i "$AMSIMAGE" \
+      -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero -i "${SOURCE}" -i "${AMSIMAGE}" \
       -filter_complex "[1:v]scale=1280x720[scaled];\
-      [scaled]drawtext=:text="GEMINIDS 2019 - Live from West Virginia":fontfile='/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf':fontcolor=white@0.45:fontsize=14:x=20:y=20[texted]; \
+      [scaled]drawtext=:text="${TEXT}":fontfile='/usr/share/fonts/truetypeTEXT}/dejavu/DejaVuSans-Bold.ttf':fontcolor=white@0.45:fontsize=14:x=20:y=20[texted]; \
       [texted]yadif[m];[m][2]overlay=25:25" \
       -vcodec libx264 -pix_fmt yuv420p -preset $QUAL -r $FPS -g $(($FPS * 2)) -b:v $VBR \
       -acodec libmp3lame -ar 44100 -threads 6 -qscale 3 -b:a 712000 -bufsize 512k \
