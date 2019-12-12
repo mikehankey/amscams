cb935697eb4a971fb2e53e41bea1c4a6e3d83414
Perlerin Vincent <vperlerin@gmail.com>
2019-12-12 12:48:39 +0100

    M

--- pythonv2/LiveStreaming.py
+++ pythonv2/LiveStreaming.py
@@ -29,7 +29,7 @@ cmd = 'ffmpeg \
       [scaled]drawtext=:text=\'' + TEXT +'\':fontfile=\'/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf\':fontcolor=white@0.45:fontsize=14:x=20:y=20[texted]; \
       [texted]yadif[m];[m][2]overlay=25:25[out]" \
       -vcodec libx264 -pix_fmt yuv420p -preset '+QUAL+' -r '+FPS+' -g $(('+FPS+' * 2)) -b:v '+VBR+' \
-      -acodec libmp3lame -ar 11025 -ac 1 -crf:v 3 -b:a 712000 -bufsize 256k -maxrate ' + VBR +' -map "[out]" -map 0:a -c:a copy \
+      -c:a libfdk_aac -b:a 128k -ar 11025 -ac 1 -crf:v 3 -b:a 712000 -bufsize 256k -maxrate ' + VBR +' -map "[out]" -map 0:a -c:a copy \
       -f flv "'+YOUTUBE_URL+'"'
 
 
