543468f9686752c3273c044e472df018171cd1a0
Perlerin Vincent <vperlerin@gmail.com>
2019-12-12 10:50:41 +0100

    M

--- pythonv2/LiveStreaming.py
+++ pythonv2/LiveStreaming.py
@@ -28,6 +28,7 @@ cmd = 'ffmpeg \
       -filter_complex "[1:v]scale=1280x720[scaled];\
       [scaled]drawtext=:text=\'' + TEXT +'\':fontfile=\'/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf\':fontcolor=white@0.45:fontsize=14:x=20:y=20[texted]; \
       [texted]yadif[m];[m][2]overlay=25:25" \
+      -preset '+QUAL+' -r '+FPS+' -g $(('+FPS+'  * 2)) -b:v '+VBR+' \
       -acodec libmp3lame -ar 44100 -threads 6 -qscale 3 -b:a 712000 -bufsize 256k \
       -f flv "'+YOUTUBE_URL+'"'
 
