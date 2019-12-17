45cd795a77ae287954a989d7f198d5a27782f898
Perlerin Vincent <vperlerin@gmail.com>
2019-12-17 13:10:28 +0100

    M

--- pythonv2/lib/Archive_Listing.py
+++ pythonv2/lib/Archive_Listing.py
@@ -24,9 +24,9 @@ POSSIBLE_ANG_VELOCITIES = [1,2,3,4,5,6,7,8,9,10,11,12,13,15,16,17,18,19,20,21,22
  
 # Delete Multiple Detections at once
 def delete_multiple_archived_detection(detections):
-   print("detections")
-   print(detections)
-
+   print("delete_multiple_archived_detection") 
+   for det in detections:
+      print(det)
 
 
 # Function that read a json file (detection)
