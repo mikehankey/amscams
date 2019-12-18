67a505b50b4ad36ae0b7bafb7f358480d9a4c6da
Perlerin Vincent <vperlerin@gmail.com>
2019-12-18 14:42:19 +0100

    M

--- src/js/functions/reducepage/Manual_Synchronization.js
+++ src/js/functions/reducepage/Manual_Synchronization.js
@@ -1,14 +1,7 @@
 $(function() {
 
-   // Go to manual sync
-   $('#manual_synchronization').click(function(){
-
-
-      stack_file = form.getvalue('stack')
-      video_file = form.getvalue('video')
-      type_file  = form.getvalue('type')    # HD or SD
-      json_file  = form.getvalue('json')
-
+   // Go to manual sync step 1
+   $('#manual_synchronization').click(function(){ 
       window.location = './webUI.py?cmd=manual_sync&json_file=' + json_file +'&video_file=' + main_vid + '&stack_file=' + my_image + '&type_file=HD'
    })
 
