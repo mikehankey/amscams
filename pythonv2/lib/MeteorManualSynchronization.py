96122885ea37381e618a5594d9e7edd989d9b82e
Perlerin Vincent <vperlerin@gmail.com>
2019-12-18 14:46:53 +0100

    M

--- pythonv2/lib/MeteorManualSynchronization.py
+++ pythonv2/lib/MeteorManualSynchronization.py
@@ -10,32 +10,23 @@ MANUAL_SYNC_TEMPLATE_STEP1 = "/home/ams/amscams/pythonv2/templates/manual_reduct
 
 # First step of the manual synchronization
 def manual_synchronization(form):
-   
+
    # Debug
    cgitb.enable()
 
    stack_file = form.getvalue('stack')
    video_file = form.getvalue('video')
    type_file  = form.getvalue('type')    # HD or SD
-   json_file  = form.getvalue('json')
-
+   json_file  = form.getvalue('json') 
 
    # Build the page based on template  
    with open(MANUAL_SYNC_TEMPLATE_STEP1, 'r') as file:
       template = file.read()
-
-   # Here we have the possibility to "empty" the cache, ie regenerate the files (stacks) even if they already exists
-   # we just need to add "clear_cache=1" to the URL
-   if(form.getvalue("clear_cache") is not None):
-      clear_cache = True
-   else:
-      clear_cache = False
-
+ 
     # Video File
    if(video_file is None):
       print_error("<b>You need to add a video file in the URL.</b>")
-      
-    
+       
    analysed_name = get_analysed_name(json_file)
    
  
--- src/js/functions/reducepage/Manual_Synchronization.js
+++ src/js/functions/reducepage/Manual_Synchronization.js
@@ -2,7 +2,7 @@ $(function() {
 
    // Go to manual sync step 1
    $('#manual_synchronization').click(function(){ 
-      window.location = './webUI.py?cmd=manual_sync&json_file=' + json_file +'&video_file=' + main_vid + '&stack_file=' + my_image + '&type_file=HD'
+      window.location = './webUI.py?cmd=manual_sync&json=' + json_file +'&video=' + main_vid + '&stack=' + my_image + '&type=HD'
    })
 
 })
