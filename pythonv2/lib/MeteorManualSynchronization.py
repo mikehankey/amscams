1cd1074cf60d4ad48c5e337aca116f0633e81cf5
Perlerin Vincent <vperlerin@gmail.com>
2019-12-18 15:57:35 +0100

    M

--- pythonv2/lib/MeteorManualSynchronization.py
+++ pythonv2/lib/MeteorManualSynchronization.py
@@ -3,6 +3,7 @@ import os
 import sys
 import json
 
+from lib.FileIO import load_json_file
 from lib.MeteorReducePage import print_error
 from lib.Old_JSON_converter import get_analysed_name
 from lib.MeteorReduce_Tools import get_stacks, get_cache_path, does_cache_exist
@@ -35,15 +36,26 @@ def manual_synchronization_chooser(form):
    cache_hd_path  = does_cache_exist(analysed_name,'tmp_hd_cropped_sync')
    dest_sd_folder = get_cache_path(analysed_name,'tmp_sd_cropped_sync')
    cache_sd_path  = does_cache_exist(analysed_name,'tmp_sd_cropped_sync')
-   print("DEST HD FOLDER<br/>")
-   print(dest_hd_folder)
-   print("DEST SD FOLDER<br/>")
-   print(dest_sd_folder)
-    
-   print("CACHE HD PATH<br/>")
-   print(cache_hd_path)
-   print("CACHE SD PATH<br/>")
-   print(cache_sd_path)
+
+   # Parse the JSON file
+   mr = load_json_file(json_file)
+
+    if "frames" in mr:
+         for ind, frame in enumerate(mr['frames']):   
+
+               print(ind + " > ")
+               print(frame)
+               print("<br/>")
+
+               # Recreate the corresponding thumb
+               #original_HD_frame = get_HD_frame(analysed_name,val['fn'])   
+               #destination_cropped_frame = get_thumb(analysed_name,val['fn'])    
+
+               #if(len(original_HD_frame)!=0 and len(destination_cropped_frame)!=0): 
+               #   new_crop_thumb(original_HD_frame[0],int(val['x']),int(val['y']),destination_cropped_frame[0])
+               #else:
+               #   resp['error'].append("Impossible to update the frame " + str(int(val['fn'])))
+  
 
 
 # First step of the manual synchronization
