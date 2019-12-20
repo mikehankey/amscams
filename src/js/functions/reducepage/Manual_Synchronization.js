2bd5376a22900f153f30d57a36e464d35e585fa3
Perlerin Vincent <vperlerin@gmail.com>
2019-12-20 12:43:01 +0100

    M

--- src/js/functions/reducepage/Manual_Synchronization.js
+++ src/js/functions/reducepage/Manual_Synchronization.js
@@ -28,7 +28,7 @@ function manual_synchronization() {
    // Transparency
    $('#transparency').val(0).on('input', function () { 
          var val = parseInt($(this).val());
-         console.log(("TRANSPARENCY " + val); 
+         console.log("TRANSPARENCY " + val); 
          
    });  
 
