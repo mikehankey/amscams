25f19e3474761384e622a3a6c0b26fa03e18265a
Perlerin Vincent <vperlerin@gmail.com>
2019-12-23 14:10:55 +0100

    M.

--- pythonv2/lib/MeteorReduce_Ajax_Tools.py
+++ pythonv2/lib/MeteorReduce_Ajax_Tools.py
@@ -79,7 +79,10 @@ def get_frame(form):
    if('sync' in tmp_json):
       if('sd_ind' in tmp_json['sync'] and 'hd_ind' in tmp_json['sync']):
          frame_hd_sd_diff = int(tmp_json['sync']['hd_ind']) - int(tmp_json['sync']['sd_ind']) 
+         print('frame_hd_sd_diff ' + str(tmp_json['sync']['hd_ind']) +  ' - ' + str(tmp_json['sync']['sd_ind']))      
  
+
+
    # We should test if get_HD_frame's output is empty as the HD Frames
    # are all created by default on page load (recude2 page)
    # if they don't exist
