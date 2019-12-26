2200c72b22da50c4ed710c6740857636fbb58f69
Perlerin Vincent <vperlerin@gmail.com>
2019-12-26 15:27:33 +0100

    DT

--- src/js/functions/daterangepicker/daterangepicker_run.js
+++ src/js/functions/daterangepicker/daterangepicker_run.js
@@ -3,7 +3,9 @@ $(function() {
    // Setup Daterange picker
    $('input[name="daterange"]').daterangepicker({
      opens: 'right',
-     timePicker: true
+     timePicker: true,
+     startDate: moment().startOf('hour'),
+     endDate: moment().startOf('hour').add(24, 'hour'),
    }, function(start, end, label) {
       alert("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
    });
