91fd16c8c55b69a66dd0fc7b04f5fb086417828f
Perlerin Vincent <vperlerin@gmail.com>
2019-12-26 16:50:04 +0100

    DT

--- src/js/functions/daterangepicker/daterangepicker_run.js
+++ src/js/functions/daterangepicker/daterangepicker_run.js
@@ -6,13 +6,13 @@ $(function() {
      timePicker: true,
      //startDate: moment().startOf('day').add(-24, 'hour'),
      //endDate: moment().startOf('day'),
-     startDate: moment($('input[name=start_date]').val()).format('YYYY/MM/DD hh:mm'),
-     endDate: moment($('input[name=end_date]').val()).format('YYYY/MM/DD hh:mm'),
+     startDate: moment($('input[name=start_date]').val()).format('YYYY/MM/DD HH:mm'),
+     endDate: moment($('input[name=end_date]').val()).format('YYYY/MM/DD HH:mm'),
      locale: {
       format: 'YYYY/MM/DD hh:mm'
     }
    }, function(start, end, label) {
-      $('input[name=start_date]').val(start.format('YYYY/MM/DD hh:mm'));
-      $('input[name=end_date]').val(end.format('YYYY/MM/DD hh:mm'))
+      $('input[name=start_date]').val(start.format('YYYY/MM/DD HH:mm'));
+      $('input[name=end_date]').val(end.format('YYYY/MM/DD HH:mm'))
    });
  });
