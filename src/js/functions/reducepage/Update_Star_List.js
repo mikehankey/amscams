1934d1beb3e6d3e7edb8d1840d4976582ec430b6
Perlerin Vincent <vperlerin@gmail.com>
2019-12-27 16:16:51 +0100

    DT

--- src/js/functions/reducepage/Update_Star_List.js
+++ src/js/functions/reducepage/Update_Star_List.js
@@ -24,8 +24,8 @@ function update_stars_on_canvas_and_table(json_resp) {
       canvas.add(new fabric.Text( "Center Az: " + az.toFixed(4) + "° / El:" + el.toFixed(4) + "°" , {
          fontFamily: 'Arial',
          fontSize: 12,
-         left: 5,
-         top: 500,
+         right: 5,
+         top: 518,
          fill: 'rgba(255,255,255,.75)',
          selectable: false
      })); 
