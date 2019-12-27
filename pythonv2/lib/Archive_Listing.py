d3731aa2e3a8b166d6b89dbf42322ffad74c8a4d
Perlerin Vincent <vperlerin@gmail.com>
2019-12-27 15:08:10 +0100

    DT

--- pythonv2/lib/Archive_Listing.py
+++ pythonv2/lib/Archive_Listing.py
@@ -380,7 +380,7 @@ def test_criteria(criter,criteria,detection):
 
    # Sync
    if(criter=='sync'):
-      if(int(detection[sync])!==int(criteria[criter])):
+      if(int(detection[sync])!=int(criteria[criter])):
          return False
 
    return True
