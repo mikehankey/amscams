07281df5384298420bd87b141a16c528fdfd6c31
Perlerin Vincent <vperlerin@gmail.com>
2019-12-30 17:07:23 +0100

    DT

--- src/js/functions/gallery_or_list.js
+++ src/js/functions/gallery_or_list.js
@@ -4,15 +4,25 @@ function gallery_or_list() {
         $('.gallery').toggleClass('list');
         if($('.gallery').hasClass('list')) {
             $t.find('i').removeClass().addClass('icon-gallery');
-            Cookies.set('archive_view', "gal", { expires: 99999, path: '/' });
+            Cookies.set('archive_view', "list", { expires: 99999, path: '/' });
         } else {
             $t.find('i').removeClass().addClass('icon-list');
-            Cookies.set('archive_view', "list", { expires: 99999, path: '/' });
+            Cookies.set('archive_view', "gal", { expires: 99999, path: '/' });
         }
     })
 }
 
 
 $(function() {
-    gallery_or_list();
-})
+   
+
+   gallery_or_list();
+   
+   // List ?
+   if(Cookies.get('archive_view')=='list') {
+      $('#show_gal').click();
+   }
+
+});
+
+
