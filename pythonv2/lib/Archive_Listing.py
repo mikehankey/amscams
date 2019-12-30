930a02f624926439e10e3d9e56dca247dd5755aa
Perlerin Vincent <vperlerin@gmail.com>
2019-12-30 15:11:32 +0100

    DT

--- pythonv2/lib/Archive_Listing.py
+++ pythonv2/lib/Archive_Listing.py
@@ -481,6 +481,8 @@ def get_results_from_date_from_monthly_index(criteria,start_date,end_date,max_re
                   test = True
                   for criter in criteria:
 
+                     print("CRITER " + criter +"<br>")
+
                      if(criter in detection and detection[criter]!='unknown'):
                         test = test_criteria(criter,criteria,detection)
                         print("<hr/>CRITER " + criter )
