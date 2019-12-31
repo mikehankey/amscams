76a78d2560e8c47b64798624a6d664987d249b6e
Perlerin Vincent <vperlerin@gmail.com>
2019-12-31 10:55:04 +0100

    DT

--- src/js/functions/reducepage/ui/Canvas-Interactions.js
+++ src/js/functions/reducepage/ui/Canvas-Interactions.js
@@ -315,12 +315,7 @@ if ($('canvas#c').length!=0) {
 
          canvas.add(marker); 
 
-         console.log("POINTER AT ")
-         console.log(x_val + ', ' +  y_val )
-         console.log("HD RATIOS ")
-         console.log(wRatio + ', ' +  hRatio )
-         console.log("HD coord ")
-         console.log(wRatio*x_val + ', ' +  hRatio*y_val )
+          
 
          // Add the object info in rad_dec_object
          new_rad_dec_obj = {x_org: x_val, y_org: y_val, x_HD: wRatio*x_val, y_HD: hRatio*y_val};
