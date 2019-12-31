376b57c0ab9125ee8d634305c61d614f68ddde7f
Perlerin Vincent <vperlerin@gmail.com>
2019-12-31 14:15:17 +0100

    M

--- pythonv2/lib/Stats.py
+++ pythonv2/lib/Stats.py
@@ -1,10 +1,17 @@
 import datetime
 from lib.Archive_Listing import get_index 
+from lib.FileIO import load_json_file
 
 def get_stats_from_year_index(year):
    json_index =  get_index(year)
-   print("INDEX YEAR: ")
-   print(json_index)
+   json_data = load_json_file(json_index)
+
+   print("YEAR " + json_data['year'] + "<br/>")
+   print("STATION ID " + json_data['station_id'] + "<br/>")
+   for month in json_data['months']:
+      print(month)
+      print("<br/>")
+   
 
 # MAIN FUNCTION FOR THE STATS PAGE
 def stats_page(form): 
