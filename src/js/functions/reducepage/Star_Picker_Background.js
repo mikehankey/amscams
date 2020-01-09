7785f1be957947d9a89f7ece43f586e829fe12e1
Perlerin Vincent <vperlerin@gmail.com>
2020-01-09 10:21:58 +0100

    M

--- src/js/functions/reducepage/Star_Picker_Background.js
+++ src/js/functions/reducepage/Star_Picker_Background.js
@@ -3,12 +3,15 @@ function create_bg_picker(data) {
 
    var max_height = $(window).height() - 200;
 
-   var img_list = '<div class="box" style="position:absolute; width: 400px; top:0;  z-index:99999"> <h2 class="mb-0"><a data-toggle="collapse" href="#bg_box" role="button" class="d-block dropdown-toggle dt-title" aria-expanded="true">Background</a></h2><div id="bg_box" class="pt-2 collapse show"><div style"height:'+max_height+'px;overflow:auto" class="gallery gal-resize row text-center text-lg-left overflow-x:auto; ">';
+   var img_list = '<div class="box" style="position:absolute; width: 400px; top:0;  z-index:99999"> <h2 class="mb-0"><a data-toggle="collapse" href="#bg_box" role="button" class="d-block dropdown-toggle dt-title" aria-expanded="true">Background</a></h2><div id="bg_box" class="pt-2 collapse show"><div style="height:'+max_height+'px;overflow:auto" class="gallery gal-resize row text-center text-lg-left overflow-x:auto; ">';
+
+   // We add the HD stack at the beginning
+   img_list += '<div class="preview col-md-6 select-to mb-3"><a class="mtt has_soh select_bg select_stack"  title="HD Stack"><img alt="HD Stack" src="'+my_image+'"></a></div>';
 
    
    // Create list of image picker
    $.each(data,function(i,v) {
-      img_list += '<div class="preview col-md-6 select-to mb-3"><a class="mtt has_soh"  title="Select Background"><img alt="" src="'+v+'"></a></div>';
+      img_list += '<div class="preview col-md-6 select-to mb-3"><a class="mtt has_soh select_bg"  title="Select Background"><img alt="" src="'+v+'"></a></div>';
    });
 
    img_list += "</div></div></div>";
