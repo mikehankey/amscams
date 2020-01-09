4e872d7c76b885a0df4ec44a619eebf685071d3d
Perlerin Vincent <vperlerin@gmail.com>
2020-01-09 10:13:56 +0100

    M

--- src/js/functions/reducepage/Star_Picker_Background.js
+++ src/js/functions/reducepage/Star_Picker_Background.js
@@ -3,12 +3,12 @@ function create_bg_picker(data) {
 
    var max_height = $(window).height() - 200;
 
-   var img_list = '<div class="box" style="position:absolute; width: 400px; top:0; height:'+max_height+'px; z-index:99999"> <h2 class="mb-0"><a data-toggle="collapse" href="#bg_box" role="button" class="d-block dropdown-toggle dt-title" aria-expanded="true">Background</a></h2><div id="bg_box" class="pt-2 collapse show"><div class="d-flex mt-0 mb-0">';
+   var img_list = '<div class="box" style="overflow-x:auto; position:absolute; width: 400px; top:0; height:'+max_height+'px; z-index:99999"> <h2 class="mb-0"><a data-toggle="collapse" href="#bg_box" role="button" class="d-block dropdown-toggle dt-title" aria-expanded="true">Background</a></h2><div id="bg_box" class="pt-2 collapse show"><div class="gallery gal-resize row text-center text-lg-left ">';
 
    
    // Create list of image picker
    $.each(data,function(i,v) {
-      img_list += '<div class="flex-fill p-1"><a style="background:url('+v+')" class="img-link d-block" href="'+v+'" title="Select Background"><img alt="" src="'+v+'"></a></div>';
+      img_list += '<div class="preview col-md-6 select-to mb-3"><a style="background:url('+v+')" class="mtt has_soh" href="'+v+'" title="Select Background"><img alt="" src="'+v+'"></a></div>';
    });
 
    img_list += "</div></div></div>";
