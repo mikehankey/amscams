9958a582a86326cb3f04c2d6644b109436a19e77
Perlerin Vincent <vperlerin@gmail.com>
2020-01-09 09:50:45 +0100

    M

--- src/js/functions/reducepage/Star_Picker_Background.js
+++ src/js/functions/reducepage/Star_Picker_Background.js
@@ -1,3 +1,31 @@
+// Create BG Picker
+function create_bg_picker(data) {
+
+   var max_height = $(window).height() - 200;
+
+   var img_list = '<div class="box" style="position:absolute; width: 400px: top:0; heigh='+max_height+'px">\\
+   <h2 class="mb-0"><a data-toggle="collapse" href="#bg_box" role="button" class="d-block dropdown-toggle dt-title" aria-expanded="true">Background</a></h2>\\
+   <div id="bg_box" class="pt-2 collapse show">\\
+      <div class="d-flex mt-0 mb-0">\\';
+
+   // Create list of image picker
+   $.each(data,function(i,v) {
+      img_list += '<div class="flex-fill p-1">\\
+         <a style="background:url('+v+')" class="img-link d-block" href="'+v+'" title="Select Background">\\
+            <img alt="" src="'+v+'">\\
+         </a>\\
+      </div>';
+   });
+
+   img_list += "</div></div></div>";
+
+   $(img_list).appendTo($('.flex-fixed-r-canvas.h-100'));
+
+
+   
+}
+
+
 // Change the background of the canvas by a single frame (instead of stack)
 function change_canvas_bg() {
 
@@ -12,6 +40,7 @@ function change_canvas_bg() {
       success: function(data) {
          loading_done();
          data = JSON.parse(data); 
+         create_bg_picker(data);
       }
    })
 
