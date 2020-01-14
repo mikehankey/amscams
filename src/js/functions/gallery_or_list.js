4f1a07bbd0dc2f2ba4536dc1fdc18ff7efa47571
Perlerin Vincent <vperlerin@gmail.com>
2020-01-14 14:06:48 +0100

    AC

--- src/js/functions/gallery_or_list.js
+++ src/js/functions/gallery_or_list.js
@@ -1,16 +1,21 @@
 function gallery_or_list() {
     $('#show_gal').click(function() {
         var $t = $(this);
-        $('.gallery').toggleClass('list');
-        if($('.gallery').hasClass('list')) {
+
+         loading({'container':'moncul'})
+
+         $('.gallery').toggleClass('list');
+         if($('.gallery').hasClass('list')) {
             $t.find('i').removeClass().addClass('icon-gallery');
             // Add cookie for python
             Cookies.set('archive_view', "list", { expires: 99999, path: '/' });
-        } else {
+         } else {
             $t.find('i').removeClass().addClass('icon-list');
             // Add cookie for python
             Cookies.set('archive_view', "gal", { expires: 99999, path: '/' });
-        }
+         }
+
+         loading_done();
     })
 }
 
