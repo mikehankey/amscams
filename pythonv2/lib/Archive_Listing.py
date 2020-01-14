e0585337fce2d1eeb8eac8648c924beb84165bd3
Perlerin Vincent <vperlerin@gmail.com>
2020-01-14 15:27:05 +0100

    AC

--- pythonv2/lib/Archive_Listing.py
+++ pythonv2/lib/Archive_Listing.py
@@ -441,7 +441,7 @@ def get_results_from_date_from_monthly_index(criteria,start_date,end_date,max_re
    else:
       number_of_res_to_give_up = max_res_per_page*(cur_page-1)
 
-   print("NUMBER OF RES TO GIVE UP " + str(number_of_res_to_give_up))
+   #print("NUMBER OF RES TO GIVE UP " + str(number_of_res_to_give_up))
     
    # Get Station ID
    station_id = get_station_id()
