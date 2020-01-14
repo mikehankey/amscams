1079b40bfa58a902b56710217a8da14ca8876d89
Perlerin Vincent <vperlerin@gmail.com>
2020-01-14 13:49:54 +0100

    AC

--- src/js/functions/archive_listing/criteria.js
+++ src/js/functions/archive_listing/criteria.js
@@ -9,31 +9,31 @@ $(function() {
       if($('#mag').val()>=0) { 
          new_url = update_url_param(new_url ,'magnitude',$('#mag').val());
       } else {
-         new_url = delete_url_param('magnitude');
+         new_url = delete_url_param(new_url ,'magnitude');
       }
 
       if($('#res_er').val()>=0) { 
          new_url = update_url_param(new_url ,'res_er',$('#res_er').val());
       } else {
-         new_url = delete_url_param('res_er');
+         new_url = delete_url_param(new_url ,'res_er');
       }
 
       if($('#ang_v').val()>=0) { 
          new_url = update_url_param(window.location.href ,'ang_v',$('#ang_v').val());
       } else {
-         new_url = delete_url_param('ang_v');
+         new_url = delete_url_param(new_url ,'ang_v');
       }
 
       if($('#sync').val()>=0) { 
          new_url = update_url_param(window.location.href ,'sync',$('#sync').val());
       } else {
-         new_url = delete_url_param('sync');
+         new_url = delete_url_param(new_url ,'sync');
       }
 
       if($('#point_score').val()>=0) { 
          new_url = update_url_param(window.location.href ,'point_score',$('#point_score').val());
       } else {
-         new_url = delete_url_param('point_score');
+         new_url = delete_url_param(new_url ,'point_score');
       }
       
        // Back to page = 1 (so we dont have issues if the number of page is too mall)
