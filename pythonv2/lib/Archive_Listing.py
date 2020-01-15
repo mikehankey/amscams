c6b1b2b97867f2faea709c8bc4a72816695a5fed
Perlerin Vincent <vperlerin@gmail.com>
2020-01-15 16:39:02 +0100

    M

--- pythonv2/lib/Archive_Listing.py
+++ pythonv2/lib/Archive_Listing.py
@@ -429,7 +429,7 @@ def test_criteria(criter,criteria,detection):
          return False
 
    # Multi
-   if(criter=='multi' and criteria[criter]!=1):
+   if(criter=='multi' and criteria[criter]!=-1):
 
       print("<br>detection ")
       print(detection)
