e33710d36858dbef48c6f239c865e2365514ab31
Perlerin Vincent <vperlerin@gmail.com>
2020-01-16 14:30:25 +0100

    M

--- src/js/functions/reducepage/Reduce_ToolBar.js
+++ src/js/functions/reducepage/Reduce_ToolBar.js
@@ -1,4 +1,20 @@
 function active_tool_bar_menu(id) {
+   // Remove RA/Dec Mode
+      // We put back the stars
+      add_stars_info_on_canvas();
+
+      // Remove the radec info from canvas
+      remove_radec_info_from_canvas();
+
+      // We empty rad_dec_object
+      rad_dec_object = [];
+
+      // We delete the panel
+      $('#select_f_tools').remove();
+
+   // Remove the Star Background Picker
+      $("#bg_selector").remove();
+
    $('#canvas_toolbar .btn').removeClass('active');
    $('#canvas_toolbar .btn#'+ id).addClass('active');
 }
--- src/js/functions/reducepage/Star_Picker_Background.js
+++ src/js/functions/reducepage/Star_Picker_Background.js
@@ -25,7 +25,7 @@ function create_bg_picker(data) {
    $(document).unbind('mouseup').mouseup(function (e) { 
       var container = $("#bg_selector"); 
       if(!container.is(e.target) && container.has(e.target).length === 0) { 
-         container.hide(); 
+         container.remove(); 
       } 
   }); 
     
