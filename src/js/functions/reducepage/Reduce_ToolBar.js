0b4a9bd2d73425d561fdabd027ac2b77009ffb61
Perlerin Vincent <vperlerin@gmail.com>
2020-01-16 14:24:42 +0100

    M

--- src/js/functions/reducepage/Reduce_ToolBar.js
+++ src/js/functions/reducepage/Reduce_ToolBar.js
@@ -1,12 +1,23 @@
+function active_tool_bar_menu(id) {
+   $('#canvas_toolbar .btn').removeClass('active');
+   $('#canvas_toolbar .btn#'+ id).addClass('active');
+}
+
 $(function() {
    $('#star_picker_background').click(function() {
+      active_tool_bar_menu('star_picker_background');
       change_canvas_bg();
    });
 
    $('#radec_mode').click(function() {
+      active_tool_bar_menu('radec_mode');
       radec_action();
    });
 
-   
+   $('#star_mode').click(function() {
+      active_tool_bar_menu('star_mode'); 
+   });
+
+
       
 })
