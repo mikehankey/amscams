2cfe10792afc4c0bda930ef64885e72d53adf3d4
Perlerin Vincent <vperlerin@gmail.com>
2020-01-16 11:17:31 +0100

    M

--- src/js/functions/reducepage/Star_Picker_Background.js
+++ src/js/functions/reducepage/Star_Picker_Background.js
@@ -6,7 +6,7 @@ function create_bg_picker(data) {
 
    var max_height = $(window).height() - 200;
    
-   var img_list = '<div id="bg_selector" class="box" style="position:absolute; width: 400px; top:0;  z-index:99999"> <h2 class="mb-0"><a data-toggle="collapse" href="#bg_box" role="button" class="d-block dropdown-toggle dt-title" aria-expanded="true">Background</a></h2><div id="bg_box" class="pt-2 collapse show"><div style="height:'+max_height+'px;overflow:auto" class="gallery gal-resize row text-center text-lg-left overflow-x:auto; ">';
+   var img_list = '<div id="bg_selector" class="box" style="position:absolute; width: 400px; top:0; z-index:99999"> <h2 class="mb-0"><a data-toggle="collapse" href="#bg_box" role="button" class="d-block dropdown-toggle dt-title" aria-expanded="true">Select a Background</a></h2><div id="bg_box" class="pt-2 collapse show"><div style="height:'+max_height+'px;overflow:auto" class="gallery gal-resize row text-center text-lg-left overflow-x:auto; ">';
 
    // We add the HD stack at the beginning
    img_list += '<div class="preview col-md-6 select-to mb-3"><a  href="#"  class="mtt has_soh select_bg select_stack"  title="HD Stack"><img alt="HD Stack" src="'+my_image+'"></a></div>';
@@ -20,6 +20,14 @@ function create_bg_picker(data) {
    img_list += "</div></div></div>";
 
    $(img_list).prependTo($('.flex-fixed-r-canvas.h-100')); 
+
+   // Hide on click outside
+   $(document).unbind('mouseup').mouseup(function (e) { 
+      var container = $("#bg_selector"); 
+      if(!container.is(e.target) && container.has(e.target).length === 0) { 
+         container.hide(); 
+      } 
+  }); 
     
    // Click on image
    $('.select_bg').unbind('click').click(function() {
