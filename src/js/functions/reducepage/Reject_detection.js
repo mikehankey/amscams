f3b9e4363afd66b1fcee41172bb2d6f92432993c
Perlerin Vincent <vperlerin@gmail.com>
2020-01-20 17:34:01 +0100

    M

--- src/js/functions/reducepage/Reject_detection.js
+++ src/js/functions/reducepage/Reject_detection.js
@@ -1,7 +1,40 @@
-function reject_detection(json_file) {
-   
+function reject_detection_from_archive(detections) {
+   msg = "Are you sure you want to PERMANENTLY delete this detection?";
+   bootbox.confirm(msg, function(result){ 
+      if(result) {
+         // Deleting
+         
+
+         $.ajax({ 
+               type:"POST",
+               url:  "webUI.py?cmd=delete_archive_multiple_detection",
+               data: {detections: [detections]},
+               success: function(data) { 
+                  loading_done();
+                  window.location.reload();   
+               }, 
+               error: function() {
+                     alert('Impossible to reject. Please, reload the page and try again later.')
+                     loading_done();
+               }
+         });
+      }  
+   });
 
 }
+
+
+function reject_detection(json_file) { 
+   loading({'text': 'Moving the detection to Trash'});
+ 
+   var detections = [];  
+   var ids = [];
+   detections.push(json_file);
+   
+
+   reject_detection_from_archive(detections);
+}
+
 
 
 
