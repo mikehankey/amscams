86f2182085e7b39807bdd87db7881b2e545e0470
Perlerin Vincent <vperlerin@gmail.com>
2020-01-23 15:03:44 +0100

    M

--- pythonv2/lib/Make_Graphs.py
+++ pythonv2/lib/Make_Graphs.py
@@ -10,6 +10,9 @@ DEFAULT_PATH_TO_GRAPH = "/pycgi/graph.html?"
 def create_iframe_to_graph(data):
    link = DEFAULT_PATH_TO_GRAPH + '?w=7'  # w=7 for the next &
 
+   print("len(data['x1_vals'])" + str(len(data['x1_vals'])))
+   print("<br>len(data['y1_vals'])" + str(len(data['y1_vals'])))
+
    # Suprise: we need data to display
    if 'x1_vals' in data and 'y1_vals' in data :
       if len(data['x1_vals'])==0: 
