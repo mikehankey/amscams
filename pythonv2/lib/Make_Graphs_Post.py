9cc6d059f176c987255ee7ae11d6606d15020491
Perlerin Vincent <vperlerin@gmail.com>
2020-01-24 15:33:45 +0100

    M

--- pythonv2/lib/Make_Graphs_Post.py
+++ pythonv2/lib/Make_Graphs_Post.py
@@ -88,8 +88,8 @@ def make_light_curve(frames):
       return create_iframe_to_graph(
             {  'title':'Light Intensity',
                'title1': 'Intensity',
-               'x1_vals': str(lc_count),
-               'y1_vals': str(lc_cnt), 
+               'x1_vals':  lc_count,
+               'y1_vals':  lc_cnt, 
                'linetype1': 'lines+markers',
                'lineshape1': 'spline'
             })
