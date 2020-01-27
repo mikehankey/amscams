4ca16f2afcee42e0ab11f24aba9a0942485432e3
Perlerin Vincent <vperlerin@gmail.com>
2020-01-27 18:48:23 +0100

    M

--- pythonv2/lib/WebCalib.py
+++ pythonv2/lib/WebCalib.py
@@ -3111,7 +3111,7 @@ def reduce_meteor_new(json_conf,form):
 
          # We automatically fix the issue
          fix_hd_vid_real_inline(hd_video_file,video_file,meteor_json_file) 
-
+         print('HD FIXED')
          #errors += "<p>HD TRIM - <b><a href='" +  hd_trim + "'> " +  hd_trim + "</a></b> as defined in the JSON is missing. <br> Do you want to replace it with: <a href='" +  hd_video_file + "'><b> " +  hd_video_file + "</b></a>?<br><a href='/pycgi/webUI.py?cmd=fix_hd_vid&json_file="+meteor_json_file+"&hd_video_file="+hd_video_file+"&cur_video_file="+video_file+"' class='btn btn-primary mt-2'>FIX THIS</a></p>"
       else:
          errors += "<p>HD TRIM - <b><a href='" +  hd_trim + "'> " +  hd_trim + "</a></b> as defined in the JSON is missing.</p>"
