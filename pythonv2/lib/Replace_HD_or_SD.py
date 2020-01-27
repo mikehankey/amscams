03ff49456bc0293f9924821af325043c79e9eed5
Perlerin Vincent <vperlerin@gmail.com>
2020-01-27 13:45:24 +0100

    M

--- pythonv2/lib/Replace_HD_or_SD.py
+++ pythonv2/lib/Replace_HD_or_SD.py
@@ -100,44 +100,30 @@ def replace_HD(form):
    if(cfe(video_sd_full_path)==0):
       print("{'error':'SD file not found,'status':0}")
       sys.exit(0)
-
-
+ 
    # Are the SD & HD videos sync'd 
    json_data = load_json_file(json_file)
+   # We artificially sync the videos
+   json_data['sync'] = {hd_ind:1,sd_ind1}
+     
+  
+   # The video has been sync' 
+   # Resize SD and replace HD 
+   cmd = "ffmpeg -y -i " + video_sd_full_path + " -vf scale="+str(HD_W)+":"+str(HD_H)+" " + video_hd_full_path
+   os.system(cmd)
 
-   hd_ind = -1
-   sd_ind = -1
-   if('sync' in json_data):
-      if('hd_ind' in json_data['sync']):
-         hd_ind = json_data['sync']['hd_ind']
-      if('sd_ind' in json_data['sync']):
-         sd_ind = json_data['sync']['sd_ind']
-      
-   if(hd_ind != -1 and sd_ind != -1):
-      # The video has been sync' 
-      # Resize SD and replace HD 
-      cmd = "ffmpeg -y -i " + video_sd_full_path + " -vf scale="+str(HD_W)+":"+str(HD_H)+" " + video_hd_full_path
-      os.system(cmd)
-   
-      # We DONT update the JSON!!!
-      # if('info' not in json_data):
-      #    json_data['info'] = []
+   # We DONT update the JSON!!!
+   # if('info' not in json_data):
+   #    json_data['info'] = []
 
-      json_data['info']['HD_fix'] = 1
-      save_json_file(json_file,json_data)
+   json_data['info']['HD_fix'] = 1
+   save_json_file(json_file,json_data)
 
-      # We update the monthly & yearly index accordingly (not optimized but this function will be rarely called)
-      a = name_analyser(json_file)
-      write_month_index(int(a['month']),int(a['year']))
-      write_year_index(int(a['year']))
+   # We update the monthly & yearly index accordingly (not optimized but this function will be rarely called)
+   a = name_analyser(json_file)
+   write_month_index(int(a['month']),int(a['year']))
+   write_year_index(int(a['year']))
 
-      # We assume everything went fine
-      print(json.dumps("{'status':1}"))
-
-
-   else:
-      # We need to sync the video first.
-      print(json.dumps("{'status':0,'msg':'You need to synchronized the SD & HD videos first.}"))
+   # We assume everything went fine
+   print(json.dumps("{'status':1}"))
  
-   # We recreate the eventual media after we get rid of the HD video
-   #redirect_to("/pycgi/webUI.py?cmd=reduce2&video_file=" + json_file + "&clear_cache=1&c=" + str(random.randint(0,100000000)), "reduction")
