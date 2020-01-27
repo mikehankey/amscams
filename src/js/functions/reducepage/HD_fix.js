2deeb0108e3e075befbed03704d6b30793eb377b
Perlerin Vincent <vperlerin@gmail.com>
2020-01-27 13:32:49 +0100

    M

--- src/js/functions/reducepage/HD_fix.js
+++ src/js/functions/reducepage/HD_fix.js
@@ -37,11 +37,11 @@ function show_stacks_to_fix() {
       <div class="modal-body">\
        <div class="row">\
          <div class="col-6">\
-            <p>Below is the HD stack. Click the image if you want to<br><b>replace the HD video by the SD video</b>.</p>\
+            <p>Below is the SD stack. Click the image if you want to<br><b>replace the HD video by the SD video</b>.</p>\
             <img src="'+sd_stack+'" class="img-fluid selectab" onclick="HD_SD_fix(\'SD\')"/>\
          </div>\
          <div class="col-6">\
-            <p>Below is the SD stack. Click the image if you want to<br><b>replace the SD video by the HD video</b>.</p>\
+            <p>Below is the HD stack. Click the image if you want to<br><b>replace the SD video by the HD video</b>.</p>\
            <img src="'+hd_stack+'" class="img-fluid selectab" onclick="HD_SD_fix(\'HD\')"/>\
          </div>\
         </div>\
