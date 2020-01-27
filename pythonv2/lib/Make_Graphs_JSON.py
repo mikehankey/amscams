0714a5121321d035ba9b5fd4285093aca500697e
Perlerin Vincent <vperlerin@gmail.com>
2020-01-27 12:26:57 +0100

    M

--- pythonv2/lib/Make_Graphs_JSON.py
+++ pythonv2/lib/Make_Graphs_JSON.py
@@ -86,7 +86,8 @@ def create_iframe_to_graph(analysed_name,data,name,graph_config,clear_cache=Fals
       path_to_json = glob.glob(get_cache_path(analysed_name,"graphs")+name+'.json') 
    
 
-   return DEFAULT_IFRAME.replace('{JSONPATH}', path_to_json).replace('{GRAPH_CONFIG}',graph_config)
+   toReturn =  DEFAULT_IFRAME.replace('{JSONPATH}', path_to_json)
+   return toReturn.replace('{GRAPH_CONFIG}',graph_config)
  
 
 
