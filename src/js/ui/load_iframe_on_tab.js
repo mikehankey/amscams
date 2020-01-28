711361f58db80f31ec618af61bccb887edfd7c66
Perlerin Vincent <vperlerin@gmail.com>
2020-01-28 16:38:13 +0100

    M

--- src/js/ui/load_iframe_on_tab.js
+++ src/js/ui/load_iframe_on_tab.js
@@ -2,5 +2,6 @@
 $('a.load-frames-on-active').on('click', function(event){
    $($(this).attr('href')).find('iframe').each(function(){
       $(this).attr('src',$(this).attr('data-src'));
+      $(this).parent('.load_if').addClass('d');
    });
 });
