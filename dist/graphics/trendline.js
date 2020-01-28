4a240bc4599511337806ac2ed521c0609b062af6
Perlerin Vincent <vperlerin@gmail.com>
2020-01-28 15:10:38 +0100

    M

--- dist/graphics/trendline.js
+++ dist/graphics/trendline.js
@@ -29,8 +29,7 @@ var layout = {
          gridcolor: GRID_COLOR, 
          linecolor: H_LINE_COLOR,  
          title: { font: { size: 15, color: '#b1b1b1' } },
-         tickfont: { color: TICK_FONT_COLOR},
-         autorange: "reversed" 
+         tickfont: { color: TICK_FONT_COLOR} 
    },
    yaxis:{
          zerolinecolor: V_LINE_COLOR, 
