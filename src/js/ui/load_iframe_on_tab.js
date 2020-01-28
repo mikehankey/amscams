76f3e2e4ec1da7096cac40e76e9a52d45e67fe3c
Perlerin Vincent <vperlerin@gmail.com>
2020-01-28 15:43:32 +0100

    M

--- src/js/ui/load_iframe_on_tab.js
+++ src/js/ui/load_iframe_on_tab.js
@@ -1,6 +1,6 @@
-// Load the iframes on the tabd only when active
-$('a.load-frames-on-active').on('shown.bs.tab', function(event){
-   $($(this).href).find('iframe').each(function() {
-      $(this).attr('data-src','src');
+// Load iframe on shown tab only 
+$('a.load-frames-on-active').on('click', function(event){
+   $($(this).attr('href')).find('iframe').each(function(){
+      $(this).attr('src',$(this).attr('data-src'));
    });
- });
+});
