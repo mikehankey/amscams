d1034c236fe6c4fedcc2f42ddd08d4d1c48e8618
Perlerin Vincent <vperlerin@gmail.com>
2020-01-29 17:43:56 +0100

    TEST 3D

--- pythonv2/lib/3DLight_Curve.py
+++ pythonv2/lib/3DLight_Curve.py
@@ -1,4 +1,5 @@
 import cv2
+import sys
 import statistics 
 
 from lib.MeteorReduce_Tools import get_stacks
@@ -21,10 +22,14 @@ def get_json_for_3Dlight_curve(frames,analysed_name):
       if('y' in frame):
          all_y.append(frame['y'])   
 
-   min_pos_x = min(all_x)
-   max_pos_x = max(all_x)
-   min_pos_y = min(all_y)
-   max_pos_y = max(all_y)
+   try:
+      min_pos_x = min(all_x)
+      max_pos_x = max(all_x)
+      min_pos_y = min(all_y)
+      max_pos_y = max(all_y)
+   except:
+      print("FUCKING FUCK")
+      sys.exit(0)
 
    # Get the stack
    hd_stack = get_stacks(analysed_name,0,True)
@@ -37,12 +42,3 @@ def get_json_for_3Dlight_curve(frames,analysed_name):
             z_vals.append(statistics.mean(image[y,x]))  # Average of the 3 VALUES
 
  
-   if len(xvals)>0 and len(yvals)>0 and len(zvals)>0:
-      return create_iframe_to_graph({
-         'title':'3D Light Topography',
-         'x1_vals': str(xvals),
-         'y1_vals':str(yvals),
-         'z1_vals':str(zvals) 
-      })
-   else:
-      return ''
