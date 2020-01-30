fad2d6ddf2e187b349612047b8ab99be5d5a44c0
Perlerin Vincent <vperlerin@gmail.com>
2020-01-30 16:16:26 +0100

    M

--- src/js/functions/reducepage/Update_Reduction_Table.js
+++ src/js/functions/reducepage/Update_Reduction_Table.js
@@ -53,7 +53,7 @@ function update_reduction_on_canvas_and_table(json_resp) {
      
 
     for (var i = 0; i <= 255; i = i + step) {
-        all_colors.push('#'+rainbow.colourAt(i));
+        all_colors[i] = '#'+rainbow.colourAt(i); // .push();
     }
 
     console.log(all_colors);
