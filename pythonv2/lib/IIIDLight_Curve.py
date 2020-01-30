9898da344479ef21bb6f067b68f305fde7a56a19
Perlerin Vincent <vperlerin@gmail.com>
2020-01-30 09:06:16 +0100

    TEST 3D

--- pythonv2/lib/IIIDLight_Curve.py
+++ pythonv2/lib/IIIDLight_Curve.py
@@ -53,11 +53,5 @@ def get_json_for_3Dlight_curve(frames,analysed_name):
                x_vals.append(x)
                y_vals.append(y)
 
-   print(json.dumps(x_vals))
-
-   #x_vals = json.dumps(x_vals)
-   #y_vals = json.dumps(y_vals)
-   #z_vals = json.dumps(z_vals)
-   
-   #return {'x':x_vals,'y':y_vals,'z':z_vals}
+      return {'x':x_vals,'y':y_vals,'z':z_vals}
    return None
