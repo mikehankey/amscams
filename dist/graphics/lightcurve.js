2c8c3e934de889fb3b407345927108af621dc7fe
Perlerin Vincent <vperlerin@gmail.com>
2020-01-30 16:30:59 +0100

    M

--- dist/graphics/lightcurve.js
+++ dist/graphics/lightcurve.js
@@ -48,29 +48,3 @@ var layout = {
       } 
     }
 }; 
-
-// Create Colors
-var rainbow = new Rainbow();
-rainbow.setNumberRange(0, 255);
-
-var all_colors = [];
-var total = all_data.x1_vals.length; 
-var step = 1;
-
-if(total>=255) {   
-   // Default color for the end...
-   for (var i = 255; i <= total; i = i + step) {
-      all_colors[i] = '#0000ff';
-   }
-   for (var i = 0; i <= 255; i = i + step) {
-      all_colors[i] = 'rgba(0,0,255,1)';
-   }
-} else {
-   step = parseInt(255/total);  
-   for (var i = 0; i <= 255; i = i + step) {
-      all_colors.push('rgba('+hexToRgb(rainbow.colourAt(i))+'));
-   }
-}
-
-// We had the color scale for X
-trace1.marker.color  =  all_colors;  
