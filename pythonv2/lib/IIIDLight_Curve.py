0d1068c8d7286329973ab8e894d942400095514b
Perlerin Vincent <vperlerin@gmail.com>
2020-01-31 11:39:37 +0100

    M

--- pythonv2/lib/IIIDLight_Curve.py
+++ pythonv2/lib/IIIDLight_Curve.py
@@ -38,9 +38,10 @@ def get_json_for_3Dlight_curve(frames,analysed_name):
 
    
       # We get the pixel value for each x,y 
-      for y in range(min_pos_y,max_pos_y):
+      for x in range(min_pos_x,max_pos_x):
+     
          y_line = []
-         for x in range(min_pos_x,max_pos_x):
+         for y in range(min_pos_y,max_pos_y):
             y_line.append(int(statistics.mean(image[y,x])))
             #z_vals.append([int(x),int(y),int(statistics.mean(image[y,x]))])  # Average of the 3 VALUES
             #x_vals.append(int(x))
