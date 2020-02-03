be9b15824ef1c4464168c3a643b0847fcc11a381
Perlerin Vincent <vperlerin@gmail.com>
2020-02-03 17:45:50 +0100

    L

--- pythonv2/lib/MeteorReduce_Tools.py
+++ pythonv2/lib/MeteorReduce_Tools.py
@@ -52,9 +52,10 @@ def update_intensity(json_file, json_data, hd_frames, analysed_name):
    if(len(thumb0_file)==0):
       # WE NEED TO CREATE THE THUMB 0 with the VALUES x,y of the first frame
       # here x & y = the position of the meteor on the first available frame 
-      thumb0_file = generate_cropped_frame(analysed_name,json_data,hd_frames,"0",sync,x,y)
-
-
+      thumb0_file = generate_cropped_frame(analysed_name,json_data,hd_frames,"0",sync,json_data['frames'][0].x,json_data['frames'][0].y)
+      print("<br><br>GENERATED THUMB0")
+      print(thumb0_file)
+   
    
    return False
    thumb0 = cv2.imread(thumb0_file,0)
