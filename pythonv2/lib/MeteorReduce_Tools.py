eaa12db0ec019ca9c733d04455f91eecc447ac51
Perlerin Vincent <vperlerin@gmail.com>
2020-02-03 18:07:07 +0100

    L

--- pythonv2/lib/MeteorReduce_Tools.py
+++ pythonv2/lib/MeteorReduce_Tools.py
@@ -65,10 +65,11 @@ def update_intensity(json_file, json_data, hd_frames, analysed_name):
       fn = frame['fn']  
       cur_thumb = get_thumb(analysed_name,int(fn))
 
-      print("<br/>FRAMES FOR # " +  str(fn) + " FRAME<br>")
+      print("<br/>FRAMES FOR # " +  str(fn) + " FRAME<br><br>Cur Thumb:<br>")
       print(cur_thumb)
       print("<br>thumb0:(img read)<br>")
       print(thumb0)
+      sys.exit(0)
       
       # Substract cur frame thumb from thumb0
       cnt_sub = cv2.subtract(cur_thumb,thumb0)
