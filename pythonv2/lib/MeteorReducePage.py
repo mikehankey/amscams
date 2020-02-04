a50a9abcebd97d76864fccd9b1d264d52b147419
Perlerin Vincent <vperlerin@gmail.com>
2020-02-04 10:32:09 +0100

    L

--- pythonv2/lib/MeteorReducePage.py
+++ pythonv2/lib/MeteorReducePage.py
@@ -179,7 +179,7 @@ def reduce_meteor2(json_conf,form):
    update_intensity(json_full_path, meteor_json_file, HD_frames, tmp_analysed_name)
    print("INTENSITY UPDATED")
    print('<hr>')
-   os.system("cd /home/ams/amscams/pythonv2; /usr/bin/python3/flex-detect.py ep" + meteor_red_file)  
+   os.system("cd /home/ams/amscams/pythonv2; /usr/bin/python3/flex-detect.py ep" + json_full_path)  
    print('<hr>')
 
    # Get the thumbs (cropped HD frames) 
