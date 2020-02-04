8f574a82ae2f8e49b8305049a17eb9813b1b87f2
Perlerin Vincent <vperlerin@gmail.com>
2020-02-04 15:30:31 +0100

    M

--- pythonv2/lib/Get_Cam_ids.py
+++ pythonv2/lib/Get_Cam_ids.py
@@ -31,7 +31,9 @@ def get_mask(cam):
         data = json.load(jsonFile)
         for cam in data['cameras']:
            if("id" in cam):
-              print("IN GET MASK - TEST " + cam['id'] +   "  vs " + cam)
+              print("IN GET MASK - TEST ")
+              print(cam['id'])
+              print(cam)
               if(cam['id']==cam): 
                toReturn.append(cam['hd']['masks']) 
     return toReturn   
