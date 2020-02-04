ec4674a23f94e7befc8b7aa582359c2e98d36770
Perlerin Vincent <vperlerin@gmail.com>
2020-02-04 14:56:42 +0100

    L

--- pythonv2/lib/MeteorReduce_Tools.py
+++ pythonv2/lib/MeteorReduce_Tools.py
@@ -28,6 +28,7 @@ def update_intensity(conf_file, json_file, json_data, hd_video_file, sd_video_fi
    # Get Video frames 
    hd_frames = load_video_frames(hd_video_file, conf_file, limit=0, mask=1, color=1)
    
+   print("UPDATE INTENSITY HD F0<br>")
    print(hd_frames[0])
    sys.exit(0)
 
