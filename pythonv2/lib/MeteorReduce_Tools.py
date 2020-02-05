4e0309a98bfea82d7b4fd0a2cee8f3a9f7563ad2
Perlerin Vincent <vperlerin@gmail.com>
2020-02-05 15:57:30 +0100

    M

--- pythonv2/lib/MeteorReduce_Tools.py
+++ pythonv2/lib/MeteorReduce_Tools.py
@@ -217,8 +217,11 @@ def update_eval_points(json_file):
  
    x_dir_mod,y_dir_mod = meteor_dir(frames[0]['x'], frames[0]['y'], frames[-1]['x'], frames[-1]['y'])
    dom,z,med_dist,med_seg,mxd,myd = line_info(frames) 
+   
+   # Why 3 Times? No idea, it's Mike's code
    ps_old, new_frames = calc_score(frames)
    ps_new, new_frames = calc_score(new_frames) 
+   ps_new, new_frames = calc_score(new_frames)
 
    jd['report']['point_score'] = ps_new 
    jd['frames'] = new_frames 
