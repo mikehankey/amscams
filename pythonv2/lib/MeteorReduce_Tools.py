690ca716501affce41c10ba589ff4aac4d4028f1
Perlerin Vincent <vperlerin@gmail.com>
2020-02-06 10:23:23 +0100

    M

--- pythonv2/lib/MeteorReduce_Tools.py
+++ pythonv2/lib/MeteorReduce_Tools.py
@@ -253,37 +253,38 @@ def update_intensity(json_file, json_data, hd_video_file):
       json_frames = json_data['frames'] 
       if(len(json_frames)>0):
          cx1,cy1,cx2,cy2 = bound_cnt(json_frames[0]['x'],json_frames[0]['y'],hd_frames[0].shape[1],hd_frames[0].shape[0], 20)
+         # Frame 0 == Bg
+         bg_cnt = hd_frames[0][cy1:cy2,cx1:cx2] 
+         new_frames = []
+         
+         for frame in json_frames:   
+            fn = frame['fn'] + sync
+            cx1,cy1,cx2,cy2 = bound_cnt(frame['x'],frame['y'],hd_frames[0].shape[1],hd_frames[0].shape[0], 20)
+            try:
+               cnt = hd_frames[fn][cy1:cy2,cx1:cx2] 
+               bg_cnt = hd_frames[0][cy1:cy2,cx1:cx2] 
+               cnt_sub = cv2.subtract(cnt,bg_cnt)
+               cnt_int = np.sum(cnt) - np.sum(bg_cnt)
+               ff_sub = cv2.subtract(hd_frames[fn],hd_frames[0])
+               ff_int = np.sum(ff_sub) 
+               if cnt_int > 18446744073709:
+                  cnt_int = 0
+               if ff_int > 18446744073709:
+                  ff_int = 0
+
+               frame['intensity'] = int(cnt_int)
+               frame['intensity_ff'] = int(ff_int) 
+               new_frames.append(frame)
+            except:
+               # DONT DO ANYTHING 
+               print('')
+
+         json_data['frames'] = new_frames 
+         save_json_file(json_file,json_data) 
    else:
       return False
 
-   # Frame 0 == Bg
-   bg_cnt = hd_frames[0][cy1:cy2,cx1:cx2] 
-   new_frames = []
-   
-   for frame in json_frames:   
-      fn = frame['fn'] + sync
-      cx1,cy1,cx2,cy2 = bound_cnt(frame['x'],frame['y'],hd_frames[0].shape[1],hd_frames[0].shape[0], 20)
-      try:
-         cnt = hd_frames[fn][cy1:cy2,cx1:cx2] 
-         bg_cnt = hd_frames[0][cy1:cy2,cx1:cx2] 
-         cnt_sub = cv2.subtract(cnt,bg_cnt)
-         cnt_int = np.sum(cnt) - np.sum(bg_cnt)
-         ff_sub = cv2.subtract(hd_frames[fn],hd_frames[0])
-         ff_int = np.sum(ff_sub) 
-         if cnt_int > 18446744073709:
-            cnt_int = 0
-         if ff_int > 18446744073709:
-            ff_int = 0
-
-         frame['intensity'] = int(cnt_int)
-         frame['intensity_ff'] = int(ff_int) 
-         new_frames.append(frame)
-      except:
-         # DONT DO ANYTHING 
-         print('')
-
-   json_data['frames'] = new_frames 
-   save_json_file(json_file,json_data) 
+  
 
 
 
