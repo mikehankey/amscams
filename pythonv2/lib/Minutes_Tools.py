2fd896f6986414dbc3f21282ef183cfabe0eadf1
Perlerin Vincent <vperlerin@gmail.com>
2020-02-13 15:26:30 +0100

    M

--- pythonv2/lib/Minutes_Tools.py
+++ pythonv2/lib/Minutes_Tools.py
@@ -78,13 +78,11 @@ def create_json_index_minute_day(day,month, year):
    cam_ids = get_the_cam_ids();
 
    stacks_per_minute = []
-
-   
-
+ 
    for i in range(0,24):
       stacks_per_minute.append(i)
       for cam_id in cam_ids:
-         stacks_per_minute[i] = {"cam_id": cam_id, "stacks": []}
+         stacks_per_minute[i] = {'h': i, 'cam': {"cam_id": cam_id, "stacks": []}
 
    print(stacks_per_minute)
    sys.exit(0)
