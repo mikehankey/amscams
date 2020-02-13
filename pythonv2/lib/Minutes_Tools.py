0a81b8a201830a4cbaae82c56b25f5de8ee811f1
Perlerin Vincent <vperlerin@gmail.com>
2020-02-13 19:33:24 +0100

    M

--- pythonv2/lib/Minutes_Tools.py
+++ pythonv2/lib/Minutes_Tools.py
@@ -85,6 +85,11 @@ def create_json_index_minute_day(day,month, year):
    cam_ids = get_the_cam_ids(); 
 
    all_minutes = {}
+   for camid in cam_ids:
+      all_minutes[camid] = []
+
+
+
    for minute_stack in sorted(glob.iglob(main_dir + '*' + os.sep + '*' + MINUTE_STACK_EXT + '*', recursive=True), reverse=True):	
 
       cur_stack_data = []
@@ -95,10 +100,7 @@ def create_json_index_minute_day(day,month, year):
       added= False
       for _min in all_minutes:
          if('cam' in _min and _min['cam']==analysed_minute['cam_id']):
-            try:
-               all_minutes[_min['cam']]
-            except:
-               all_minutes[_min['cam']] = []
+             
             all_minutes[_min['cam']].append({'t': analysed_minute['hour'] +':'+ analysed_minute['min'] +':'+ analysed_minute['sec'] +'.'+ analysed_minute['ms']})
             added = True
           
