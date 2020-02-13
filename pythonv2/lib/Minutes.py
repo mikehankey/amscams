867fc7de601554c85898af082fc24d5a7471f0de
Perlerin Vincent <vperlerin@gmail.com>
2020-02-13 10:28:34 +0100

    Browse Minutes

--- pythonv2/lib/Minutes.py
+++ pythonv2/lib/Minutes.py
@@ -2,7 +2,19 @@ import glob
 import re
 import cgitb
 
+from lib.Get_Cam_ids import get_the_cam_ids
+
 PAGE_TEMPLATE = "/home/ams/amscams/pythonv2/templates/browse_minutes.html"
+
+
+# Build the Cam multi-selector
+def get_cam_ids(selected_cam_ids) {
+   toReturn = ""
+   cam_ids = get_the_cam_ids()
+   for cam_id in cam_ids:
+      toReturn = "<option value='"+cam_id+"'>'"+cam_id+"'</option>"
+   return toReturn
+}
 
 
 def browse_minute(form):
@@ -15,6 +27,9 @@ def browse_minute(form):
    # Build the page based on template  
    with open(PAGE_TEMPLATE, 'r') as file:
       template = file.read()
+
+   # Build the cam ids dd
+   template.replace('{CAM_IDS}',get_cam_ids(cams_ids))
    
    # Display Template
    print(template)
