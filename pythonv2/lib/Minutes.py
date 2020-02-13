5965a0e9538b83960663d024abd3da16405c50ce
Perlerin Vincent <vperlerin@gmail.com>
2020-02-13 10:57:24 +0100

    Browse Minutes

--- pythonv2/lib/Minutes.py
+++ pythonv2/lib/Minutes.py
@@ -5,10 +5,10 @@ import cgitb
 from lib.Get_Cam_ids import get_the_cam_ids
 
 PAGE_TEMPLATE = "/home/ams/amscams/pythonv2/templates/browse_minutes.html"
+PERIODS = ['minute','hour','day']
 
-
-# Build the Cam multi-selector
-def get_cam_ids(selected_cam_ids):
+# Build the Cam multi-selector & periods
+def get_select(selected_cam_ids):
    toReturn = ""
    cam_ids = get_the_cam_ids()
 
@@ -30,7 +30,7 @@ def browse_minute(form):
    # Debug
    cgitb.enable()
 
-   period   = form.getvalue('period')
+   selected_period   = form.getvalue('period')
    selected_cam_ids = form.getvalue('cams_ids')
 
    if(selected_cam_ids is not None):
@@ -38,13 +38,20 @@ def browse_minute(form):
    else:
       selected_cam_ids = []
 
+   if(selected_period is not None):
+      selected_period = [selected_period]
+   else:
+      selected_period = PERIODS[0] # See PERIODS 
 
    # Build the page based on template  
    with open(PAGE_TEMPLATE, 'r') as file:
       template = file.read()
 
    # Build the cam ids dd
-   template = template.replace('{CAM_IDS}',get_cam_ids(selected_cam_ids))
+   template = template.replace('{CAM_IDS}',get_select(selected_cam_ids))
+
+   # Build the period
+   template = template.replace('{PERIODS}',get_select(selected_period))
    
    # Display Template
    print(template)
