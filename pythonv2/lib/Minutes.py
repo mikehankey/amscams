e4a6d783c5a536b0704ece8437a267a56b9e9897
Perlerin Vincent <vperlerin@gmail.com>
2020-02-14 09:11:53 +0100

    M

--- pythonv2/lib/Minutes.py
+++ pythonv2/lib/Minutes.py
@@ -54,7 +54,7 @@ def get_minute_index_res(selected_start_date, selected_end_date,selected_period,
       #print(str(how_many_cams) + " CAMS SELECTED") 
       #sys.exit(0)
 
-      print(json_data)
+      print(json_data['cams'])
       sys.exit(0)
 
       for data in json_data:
