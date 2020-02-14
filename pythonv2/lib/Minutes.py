a3929f46731e03676cbd6e17d9919f561da3d2dc
Perlerin Vincent <vperlerin@gmail.com>
2020-02-14 09:43:03 +0100

    M

--- pythonv2/lib/Minutes.py
+++ pythonv2/lib/Minutes.py
@@ -35,8 +35,7 @@ def get_select(selected_cam_ids,_type):
 # Transform the result of a minute_index to get the full path to the stackeds
 # minute_file is actually the hour of the stack (ex: 11:16:26.000) > 2020_01_05_00_00_09_000_010038-stacked-tn.png	
 def get_min_details(cam_id,year,month,day,minute_file):
- 
-   return MINUTE_FOLDER +  os.sep + str(year) + os.sep + str(month+'_'+str(day) + os.sep +str(year)+'_'+str(month)+'_'+str(day)+minute_file.replace(':','_').replace('.','_') + '-' + MINUTE_STACK_EXT + '.png'
+   return MINUTE_FOLDER +  os.sep + str(year) + os.sep + str(month)+'_'+str(day) + os.sep +str(year)+'_'+str(month)+'_'+str(day)+minute_file.replace(':','_').replace('.','_') + '-' + MINUTE_STACK_EXT + '.png'
 
 # Get Results from the minutes indexes
 def get_minute_index_res(selected_start_date, selected_end_date,selected_period,selected_cam_ids):
