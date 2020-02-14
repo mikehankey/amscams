6e8cc6cf773b9439a8d5caa2300cca3ed98a2c78
Perlerin Vincent <vperlerin@gmail.com>
2020-02-14 08:25:26 +0100

    M

--- pythonv2/lib/WebUI.py
+++ pythonv2/lib/WebUI.py
@@ -2701,7 +2701,7 @@ def main_page(json_conf,form):
       day_dir = json_conf['site']['proc_dir'] + day + "/" 
 
       # Use to compare with end
-      day_cur_date = datetime.strptime(day_str,"%Y/%m/%d")
+      day_cur_date = datetime.strptime(day_str,"%Y_%m_%d")
      
       if "meteor" not in day_dir and "daytime" not in day_dir and "json" not in day_dir and "trash" not in day_dir:
          real_detections.append(day) 
