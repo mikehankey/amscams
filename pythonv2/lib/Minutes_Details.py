b0c0b06f84732b7a20120d66e49b80cc512f5753
Perlerin Vincent <vperlerin@gmail.com>
2020-02-17 11:01:37 +0100

    M

--- pythonv2/lib/Minutes_Details.py
+++ pythonv2/lib/Minutes_Details.py
@@ -1,7 +1,21 @@
 import cgitb
+ 
+from lib.Minutes_Tools import *
+
+PAGE_TEMPLATE = "/home/ams/amscams/pythonv2/templates/minute_details.html"
 
 def minute_details(form):
    # Debug
    cgitb.enable()
+   stack = form.getvalue('stack') 
 
-   stack = form.getvalue('stack')  
+   analysed_minute = minute_name_analyser(stack)
+   
+    # Build the page based on template  
+   with open(PAGE_TEMPLATE, 'r') as file:
+      template = file.read()
+
+
+   print(template)
+   print(analysed_minute)
+
