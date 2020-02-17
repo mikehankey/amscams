695bd4f8d780cd0372f92702eb1737a2383e7688
Perlerin Vincent <vperlerin@gmail.com>
2020-02-17 11:08:11 +0100

    M

--- pythonv2/lib/Minutes_Details.py
+++ pythonv2/lib/Minutes_Details.py
@@ -12,7 +12,7 @@ def minute_details(form):
 
    analysed_minute = minute_name_analyser(stack)
    string_date = analysed_minute['year']+'/'+analysed_minute['month']+'/'+analysed_minute['day']+' '+analysed_minute['hour']+':'+analysed_minute['min']+':'+analysed_minute['sec']
-   date = datetime.strptime(string_date,"%Y/%m/%d %H:%M:%s") 
+   date = datetime.strptime(string_date,"%Y/%m/%d %H:%M:%S") 
    
     # Build the page based on template  
    with open(PAGE_TEMPLATE, 'r') as file:
