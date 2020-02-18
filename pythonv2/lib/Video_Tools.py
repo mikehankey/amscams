96907a2469906e87826af24f9baa9ee9786eeebb
Perlerin Vincent <vperlerin@gmail.com>
2020-02-18 10:35:25 +0100

    M

--- pythonv2/lib/Video_Tools.py
+++ pythonv2/lib/Video_Tools.py
@@ -17,7 +17,7 @@ from pathlib import Path
 # Fix a MP4 just copying it 
 def fixmp4(path_to_mp4):
    path = Path(path_to_mp4).parent.absolute()
-   name = path_to_mp4.replace((str)path,'')
+   name = os.path.basename(path_to_mp4)
 
    print("PATH " + path)
    print("NAME " + name)
