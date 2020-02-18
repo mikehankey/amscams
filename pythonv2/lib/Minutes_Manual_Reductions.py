95d13e81d0b55972af82f39fcb1205a249816a3a
Perlerin Vincent <vperlerin@gmail.com>
2020-02-18 13:33:55 +0100

    M

--- pythonv2/lib/Minutes_Manual_Reductions.py
+++ pythonv2/lib/Minutes_Manual_Reductions.py
@@ -43,13 +43,12 @@ def automatic_detect(form):
    # SD video => /mnt/ams2/SD/proc2/2020_02_17/2020_02_17_11_12_20_000_010039.mp4
    # HD video => /mnt/ams2/HD/2020_02_17_11_12_XX_XXX_010039.mp4
    analysed_minute = minute_name_analyser(stack.replace(HD_TMP_STACK_EXT, MINUTE_STACK_EXT+'.png'))
-   print(stack.replace(HD_TMP_STACK_EXT,MINUTE_STACK_EXT+'.png'))
-   print("<br>")
-   print(analysed_minute)
-   sys.exit(0)
 
+   
    # Search for HD
    HD_path = MINUTE_HD_VID_FOLDER + os.sep + analysed_minute['full'].replace( MINUTE_STACK_EXT+'.png','.mp4')
+   print("HD PATH<br> ")
+   print(HD_path)
    HD_found = False
 
    # Search same path:
