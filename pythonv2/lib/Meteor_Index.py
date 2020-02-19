42733f8e5c7214ad2cedbd66e6422a63ce0fd9c1
Perlerin Vincent <vperlerin@gmail.com>
2020-02-19 10:56:32 +0100

    M

--- pythonv2/lib/Meteor_Index.py
+++ pythonv2/lib/Meteor_Index.py
@@ -23,7 +23,7 @@ def get_meteor_date_cam(ms,sec,_min,hour,day,month,year,cam_id):
    print(main_dir  + pseud_name)
    print("<br>")
 
-   if(cfe(main_dir,1)):
+   if(cfe(main_dir,1)==1):
       # We glob the folder to get all detection for this day
       all_jsons = glob.glob(main_dir + pseud_name)
       print(all_jsons)
