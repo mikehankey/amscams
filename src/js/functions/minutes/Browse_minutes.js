a4a32f4acf6b3ef7f6a7328416a62bca197402e5
Perlerin Vincent <vperlerin@gmail.com>
2020-02-19 14:54:59 +0100

    M

--- src/js/functions/minutes/Browse_minutes.js
+++ src/js/functions/minutes/Browse_minutes.js
@@ -3,6 +3,8 @@ $(function() {
       // Replace in cur URL and reload
       var cur_params = getQueryParameters(); 
       var param_to_update = $(this).attr('data-url-param');
+      var v = $(this).val();
+      cur_params[param_to_update]= v;
       setQueryParameters(cur_params[param_to_update]);
    })
 })
