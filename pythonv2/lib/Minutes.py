cc7dc9ec10ac2df6b783bc6a0056af4cb4ec8ef8
Perlerin Vincent <vperlerin@gmail.com>
2020-02-19 10:42:07 +0100

    M

--- pythonv2/lib/Minutes.py
+++ pythonv2/lib/Minutes.py
@@ -106,8 +106,11 @@ def create_minute_html_res(res,cam_ids,year,month,day):
 
             # Test if we have at least one meteor detection for this date & cam_id
             #get_meteor_date_cam(ms,sec,min,hour,day,month,year,cam_id)
-            print(t)
+            
+            tmp_analysed = minute_name_analyser(t)
+            print(tmp_analysed)
             print("<br>")
+
             toReturn += "<div style='padding: 0 1rem 1rem 0;'><a href='webUI.py?cmd=minute_details&stack="+t+"'><img src='"+t+"' style='width: 100%; max-width: 350px;height: 169px;' data-rel='"+cam_res+"' class='img-fluid cam_"+str(cam_id)+"'/></a><span style='font-size:.75rem'>"+cam_res+"</span></div>"
          else:
             toReturn += "<div style='padding: 0 1rem 1rem 0;width: 100%;height: 169px; background-color: transparent;max-width: calc(250px + 1rem);'></div>"
