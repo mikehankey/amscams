341f34a6f675fa7208abb8310ae62b7ebbe27697
Perlerin Vincent <vperlerin@gmail.com>
2020-02-19 13:40:35 +0100

    M

--- pythonv2/lib/Minutes.py
+++ pythonv2/lib/Minutes.py
@@ -145,7 +145,8 @@ def create_minute_html_res(res,cam_ids,year,month,day, meteor_only):
             we_have_res=0  
 
       cur_index+=1
-      toReturn += "XXXXX</div>"
+      if(cam_res is False):
+         toReturn += "</div>"
 
    return toReturn
 
