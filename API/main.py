e146884fee6f5c320446055e11aceac756ad797b
Perlerin Vincent <vperlerin@gmail.com>
2020-02-19 17:14:16 +0100

    M

--- API/main.py
+++ API/main.py
@@ -2,6 +2,9 @@
 
 import os
 import cgi 
+import json
+
+JSON_CONFIG = '/home/ams/amscams/conf/as6.json'
 
 print('Content-Type: text/html; charset=utf-8')
 
@@ -11,13 +14,28 @@ def api_controller(form):
    if(api_function=='login'):
       login(form)
 
+
+# LOGIN
 def login(form):
 
    user = form.getvalue('user')
    password = form.getvalue('pwd')
 
-   print("RIGHT")
+   if(user is not None and password is not None):
+      json_file = open('JSON_CONFIG')
+      json_str = json_file.read()
+      json_data = json.loads(json_str)
+       
+      try:
+         if(json_data['site']['ams_id']==user and json_data['site']['pwd']==pwd):
+               result['passed'] = 1
+         else:
+               result['passed'] = 0
+      except Exception:
+         result['passed'] = 0
+    
 
 
+# MAIN
 form = cgi.FieldStorage()
 api_controller(form)
