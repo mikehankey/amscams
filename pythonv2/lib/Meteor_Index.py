5a76167928c09408d692f317161085c4c6827993
Perlerin Vincent <vperlerin@gmail.com>
2020-02-19 10:54:56 +0100

    M

--- pythonv2/lib/Meteor_Index.py
+++ pythonv2/lib/Meteor_Index.py
@@ -15,7 +15,7 @@ METEOR_FOLDER = "/mnt/ams2/meteors"
 def get_meteor_date_cam(ms,sec,_min,hour,day,month,year,cam_id):
 
    # Dir to glob
-   main_dir = METEOR_FOLDER + os.sep + str(year) + "_" + str(month).zfill(2) + '_' + str(day).zfill(2)  
+   main_dir = METEOR_FOLDER + os.sep + str(year) + "_" + str(month).zfill(2) + '_' + str(day).zfill(2)  + os.sep
    pseud_name = str(year) + "_" + str(month).zfill(2) + '_' + str(day).zfill(2) + '_' + str(hour).zfill(2) + '_' + str(_min).zfill(2) + '_' + str(sec).zfill(2) + '_' + str(ms).zfill(3) + '_' + str(cam_id) + "*" + ".json"
    cam_ids = get_the_cam_ids(); 
  
@@ -25,7 +25,7 @@ def get_meteor_date_cam(ms,sec,_min,hour,day,month,year,cam_id):
 
    if(cfe(main_dir)):
       # We glob the folder to get all detection for this day
-      all_jsons = glob.glob(main_dir + os.sep + pseud_name)
+      all_jsons = glob.glob(main_dir  + pseud_name)
       print(all_jsons)
       sys.exit(0)
       for json in all_jsons:
