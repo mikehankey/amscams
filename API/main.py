a5b749a8af896dd2f1523a920448ca6f10c82b7b
Perlerin Vincent <vperlerin@gmail.com>
2020-02-19 16:20:11 +0100

    M

--- API/main.py
+++ API/main.py
@@ -1,8 +1,18 @@
 import os
 
+def controller(form):
+   api_function = form.getvalue('function')
+
+   if(api_function=='login'):
+      login(form)
+
 def login(form):
 
    user = form.getvalue('user')
    password = form.getvalue('pwd')
 
    print("RIGHT")
+ 
+controller(json_conf)
+ 
+
