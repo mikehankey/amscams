63f77e5df44f75b05f6b78f66d5236f130590e33
Perlerin Vincent <vperlerin@gmail.com>
2020-02-19 14:02:23 +0100

    M

--- pythonv2/lib/Minutes.py
+++ pythonv2/lib/Minutes.py
@@ -124,8 +124,8 @@ def create_minute_html_res_meteor(res,cam_ids,year,month,day):
 
    for cam_id in cam_ids:
       toReturn  += "<h2>Cam# "+cam_id + "</h2>"
-      for meteor in res_by_cam[cam_id]:
-         toReturn  += "<div class='minute meteor'><a class='d-block' href='webUI.py?cmd=minute_details&stack="+meteor+"'><img src='"+meteor+"' data-rel='"+cam_res+"' class='img-fluid'/></a><span style='font-size:.75rem'>"+meteor+"</span></div>"
+      for m in res_by_cam[cam_id]:
+         toReturn  += "<div class='minute meteor'><a class='d-block' href='webUI.py?cmd=minute_details&stack="+m+"'><img src='"+m+"'  class='img-fluid'/></a><span style='font-size:.75rem'>"+m+"</span></div>"
 
    return toReturn
 
