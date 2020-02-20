703180833f62d246d8419de261078ce89309aa56
Perlerin Vincent <vperlerin@gmail.com>
2020-02-20 11:35:08 +0100

    M

--- pythonv2/API/API_Tools.py
+++ pythonv2/API/API_Tools.py
@@ -1,6 +1,12 @@
 import json
 import sys
 
+
+def it_is_json():
+   print("Content-type: application/json\n\n")
+
+
 def send_error_message(msg):
-  print(json.dumps({'error':msg}))
-  sys.exit(0)
+   it_is_json()
+   print(json.dumps({'error':msg}))
+   sys.exit(0)
