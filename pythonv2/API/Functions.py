36707f2427a0ad9ca1de454f733afbd65261b448
Perlerin Vincent <vperlerin@gmail.com>
2020-02-20 10:36:45 +0100

    M

--- pythonv2/API/Functions.py
+++ pythonv2/API/Functions.py
@@ -65,7 +65,7 @@ def create_token():
    expiration = datetime.datetime.now() + datetime.timedelta(hours=1)
    
    # Create Token
-   tok = expiration.strftime("%d%b%Y%H%M%S_4llsk")  + ''.join(random.choice('AllonsEnfantsDeLaPatrie!!') for _ in range(18))
+   tok = expiration.strftime("%d%b%Y%H%M%S_4llsk")  + ''.join(random.choice('AbcDeFghIJklmNOpqRstUVWxYZ;?._!') for _ in range(18))
    
    return expiration.strftime("%a, %d-%b-%Y %H:%M:%S GMT"),tok
 
