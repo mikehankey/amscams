41f741315ab4ad0af9c161fee3bd4a1a61ffb629
Perlerin Vincent <vperlerin@gmail.com>
2020-02-20 18:28:24 +0100

    M

--- pythonv2/API/API_Tools.py
+++ pythonv2/API/API_Tools.py
@@ -2,10 +2,9 @@ import json
 import sys
 import cgitb 
 
-API_URL = 'https://sleaziest-somali-2255.dataplicity.io/pycgi/webUI.py?cmd=API'     
+ 
 
-def send_json(json_msg):
-   print("Content-type:application/json\r\n\r\n")
+def send_json(json_msg): 
    print(json_msg) 
 
 
