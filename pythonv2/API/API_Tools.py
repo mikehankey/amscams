fca4401375ccb94ad9cee27cb448272b95a9ddc3
Perlerin Vincent <vperlerin@gmail.com>
2020-02-20 18:18:59 +0100

    M

--- pythonv2/API/API_Tools.py
+++ pythonv2/API/API_Tools.py
@@ -5,13 +5,12 @@ import requests
 
 API_URL = 'https://sleaziest-somali-2255.dataplicity.io/pycgi/webUI.py?cmd=API'     
 
-def send_json(json_msg) {
+def send_json(json_msg:
    url = API_URL   
    parameters = json_msg
    headers = {'content-type': 'application/json'} 
    response = requests.post(url, data = json_msg),headers=headers)
-   print(response)
-}
+   print(response) 
 
 
 def send_error_message(msg): 
