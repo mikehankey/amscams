e8b8240b5ca9ffe118d86da9fefab8242bcf9377
Perlerin Vincent <vperlerin@gmail.com>
2020-02-20 18:20:06 +0100

    M

--- pythonv2/API/API_Tools.py
+++ pythonv2/API/API_Tools.py
@@ -9,7 +9,7 @@ def send_json(json_msg):
    url = API_URL   
    parameters = json_msg
    headers = {'content-type': 'application/json'} 
-   response = requests.post(url, data = json_msg),headers=headers)
+   response = requests.post(url, data = json_msg,headers=headers)
    print(response) 
 
 
