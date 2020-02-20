1a32007b51a29982f8ddb5cfa43e56768e6c7c20
Perlerin Vincent <vperlerin@gmail.com>
2020-02-20 18:17:45 +0100

    M

--- pythonv2/API/API_Tools.py
+++ pythonv2/API/API_Tools.py
@@ -1,9 +1,20 @@
 import json
 import sys
 import cgitb
- 
+import requests
+
+
+
+def send_json(json_msg) {
+   url = 'https://sleaziest-somali-2255.dataplicity.io/pycgi/webUI.py?cmd=API'        
+   parameters = json_msg
+   headers = {'content-type': 'application/json'} 
+   response = requests.post(url, data = json_msg),headers=headers)
+   print(response)
+}
+
 
 def send_error_message(msg): 
    cgitb.enable()
-   print(json.dumps({'error':msg}))
+   send_json(json.dumps({'error':msg}))
    sys.exit(0)
