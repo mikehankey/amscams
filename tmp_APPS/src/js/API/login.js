1d35d638d4079ba7fe8821c5b1d4664acf2e5cb1
Perlerin Vincent <vperlerin@gmail.com>
2020-02-24 16:30:22 +0100

    M3

--- tmp_APPS/src/js/API/login.js
+++ tmp_APPS/src/js/API/login.js
@@ -45,38 +45,39 @@ function loggedin() {
 
 // Create Login Modal
 function setup_login() {
+
+   // Add Login Modal
+   $('<div id="login_modal" class="modal fade" tabindex="-1" role="dialog"><div class="modal-dialog modal-dialog-centered" style="max-width:300px" role="document">\
+   <div class="modal-content">\
+   <div class="modal-header">\
+     <h5 class="modal-title">Login to '+STATION+'</h5>\
+     <button type="button" class="close" data-dismiss="modal" aria-label="Close">\
+       <span aria-hidden="true">&times;</span>\
+     </button>\
+   </div>\
+   <div class="modal-body pb-4" >\
+      <div class="d-flex justify-content-center form_container">\
+         <form>\
+            <input type="hidden" name="st" value="'+STATION+'"/>\
+            <div class="input-group mb-3">\
+               <input type="text" name="username" class="form-control input_user" value="" placeholder="username">\
+            </div>\
+            <div class="input-group mb-2">\
+               <input type="password" name="password" class="form-control input_pass" value="" placeholder="password">\
+            </div>\
+            <div class="d-flex justify-content-center mt-3 login_container">\
+               <button type="button" name="button" id="subm_login" class="btn btn-primary" style="width: 100%;">Login</button>\
+            </div>\
+         </form>\
+      </div>\
+   </div></div></div></div>').appendTo('body');
+
+
+
    // Login
    $('#login').unbind('click').click(function(e){
-      e.stopImmediatePropagation();
-      $('#login_modal').modal('hide')
-      $('#login_modal,.modal-backdrop').remove();
-      
-
-      $('<div id="login_modal" class="modal fade" tabindex="-1" role="dialog"><div class="modal-dialog modal-dialog-centered" style="max-width:300px" role="document">\
-      <div class="modal-content">\
-      <div class="modal-header">\
-        <h5 class="modal-title">Login to '+STATION+'</h5>\
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">\
-          <span aria-hidden="true">&times;</span>\
-        </button>\
-      </div>\
-      <div class="modal-body pb-4" >\
-         <div class="d-flex justify-content-center form_container">\
-            <form>\
-               <input type="hidden" name="st" value="'+STATION+'"/>\
-               <div class="input-group mb-3">\
-                  <input type="text" name="username" class="form-control input_user" value="" placeholder="username">\
-               </div>\
-               <div class="input-group mb-2">\
-                  <input type="password" name="password" class="form-control input_pass" value="" placeholder="password">\
-               </div>\
-               <div class="d-flex justify-content-center mt-3 login_container">\
-                  <button type="button" name="button" id="subm_login" class="btn btn-primary" style="width: 100%;">Login</button>\
-               </div>\
-            </form>\
-         </div>\
-      </div></div></div></div>').appendTo('body');
-
+      e.stopImmediatePropagation(); 
+ 
       $('#login_modal').modal('show');
 
       $('#subm_login').click(function() {
@@ -97,9 +98,7 @@ function setup_login() {
                      });
                      logout();
                   } else {
-                     $('#login_modal').modal('hide')
-                     $('#login_modal,.modal-backdrop').remove();
-                     $('body').removeClass('modal-open');
+                     $('#login_modal').modal('hide');
                      LOGGEDIN = true;
                      TOK = data.token;
                      EXPIRE = data.expire; 
@@ -107,7 +106,7 @@ function setup_login() {
                   } 
                }, 
                error:function() { 
-                  $('#login_modal').remove();
+                  $('#login_modal').modal('hide');
                   bootbox.alert({
                      message: "Impossible to reach the API. Please, try again later.",
                      className: 'rubberBand animated error',
