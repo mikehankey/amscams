fdd16c56d16652cc3b81bacb5e0c5256351fd19d
Perlerin Vincent <vperlerin@gmail.com>
2020-02-24 11:08:14 +0100

    Front End API

---
+++ tmp_APPS/src/js/API/shared.js
@@ -0,0 +1,14 @@
+var STATION = "AMS7";
+var API_URL = "https://sleaziest-somali-2255.dataplicity.io/pycgi/webUI.py?cmd=API";
+var LOGGEDIN = false; // Just UI - the rest is managed on both side with a token
+var TOK;
+var USR;
+
+function setup_action() {
+   setup_login();
+}
+ 
+
+$(function() {
+   setup_action();
+})
