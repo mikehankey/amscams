68e215b9f05b5517a045275da98a741124a0b3f1
Perlerin Vincent <vperlerin@gmail.com>
2020-02-25 18:07:57 +0100

    M

--- tmp_APPS/src/js/API/login.js
+++ tmp_APPS/src/js/API/login.js
@@ -12,12 +12,17 @@ function add_login_stuff() {
    if($('.lgi').length==0) {
       // Add buttons
       $('.prevproc').each(function() {
-         $('<div class="btn-toolbar lgi">\
-            <div class="d-flex justify-content-around">\
-               <a class="conf col btn btn-success btn-sm mr-2" title="Confirm Detection">Confirm</a>\
-               <a class="del col btn btn-danger btn-sm" title="Delete Detection"><i class="icon-delete"></i></a>\
-            </div>\
-         </div>').appendTo($(this))
+
+         if(!$(this).hasClass('arc')) {
+               $('<div class="btn-toolbar lgi">\
+               <div class="d-flex justify-content-around">\
+                  <a class="conf col btn btn-success btn-sm mr-2" title="Confirm Detection">Confirm</a>\
+                  <a class="del col btn btn-danger btn-sm" title="Delete Detection"><i class="icon-delete"></i></a>\
+               </div>\
+            </div>').appendTo($(this))
+         }
+
+
       });
    }
    
