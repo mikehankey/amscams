dd6a6f7a7928a62843fa0c6408d947431c660368
Perlerin Vincent <vperlerin@gmail.com>
2020-02-27 11:55:21 +0100

    M

--- tmp_APPS/src/js/ui/lio_filters.js
+++ tmp_APPS/src/js/ui/lio_filters.js
@@ -17,4 +17,23 @@ $(function() {
 
       $('.btn#'+id).addClass('active'); 
    })
+
+   $('#lio_sub_filters button').click(function(e) {
+      e.stopImmediatePropagation();
+
+      var id = $(this).attr('id');
+
+      $('#lio_sub_filters button').removeClass('active');
+      $('.prevproc').hide();
+
+      if(id=='lio_sub_btn_all') {
+         $('.prevproc').show();
+      } else if(id=='lio_sub_btn_single') {
+         $('.prevproc.pending').show();
+      } else if(id=='lio_sub_btn_multi') {
+         $('.prevproc.arc').show();
+      }
+
+      $('.btn#'+id).addClass('active'); 
+   })
 })
