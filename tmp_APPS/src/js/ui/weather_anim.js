234f29780ddf39c3bf626cde8b1b04b009e01cba
Perlerin Vincent <vperlerin@gmail.com>
2020-02-28 10:32:22 +0100

    M

--- tmp_APPS/src/js/ui/weather_anim.js
+++ tmp_APPS/src/js/ui/weather_anim.js
@@ -9,7 +9,7 @@ var weathplaying;
 var sens = "+"
  
 
-jQuery.fn.reverse = [].reverse;
+jQuery.fn.reverse = [].reverse; 
 
 // Modal With Player
 function addAnimWeatherModalTemplate($allstacks,cam_id) {
