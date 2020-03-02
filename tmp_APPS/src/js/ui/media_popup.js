05920dd6e4f8dcbb9041078ef3b256026e7cac98
Perlerin Vincent <vperlerin@gmail.com>
2020-03-02 15:36:33 +0100

    M

--- tmp_APPS/src/js/ui/media_popup.js
+++ tmp_APPS/src/js/ui/media_popup.js
@@ -5,8 +5,21 @@ $(function() {
         mainClass: 'mfp-fade',
         gallery:{
          enabled:true
-        }
+        },
+        callbacks: {
+         elementParse: function(item) {
+           // Function will fire for each target element
+           // "item.el" is a target DOM element (if present)
+           // "item.src" is a source that you may modify
+     
+           console.log(item); // Do whatever you want with "item" object
+         }
+       }
       });
+
+
+     
+      
 
       $('.img-link-gal').magnificPopup({
         type: 'image' ,
