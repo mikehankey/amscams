7176e0ace4b89fe620545c776c1ca4793c7d9569
Perlerin Vincent <vperlerin@gmail.com>
2020-03-03 14:57:19 +0100

    M

--- pythonv2/doDay.py
+++ pythonv2/doDay.py
@@ -370,7 +370,7 @@ def html_get_detects(day,tsid,event_files, events):
    
                single_html += "<div class='"+css_class+"'>" + elink +  "<img src='"+was_vh_dir + image_file+"' class='img-fluid'></a>"
                single_html += "<div class='d-flex mb-2'><div class='mr-auto'><span>"+'<b>Cam#' + analysed_name['cam_id'] + '</b> '+ analysed_name['hour']+':'+analysed_name['min']+':'+analysed_name['sec']+'.'+analysed_name['ms'] + "</div>"
-               single_html += "<div><a href='"+video_path+"' class='vid-link btn btn-secondary btn-sm'><span class='icon-youtube'></span></a></div></div></div>"
+               single_html += "<div>"+video_path+"</div></div></div>"
                ss_count += 1 
             else:
 
