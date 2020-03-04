9a802c0d81fc425867fc734eebca32508fa5ecba
Perlerin Vincent <vperlerin@gmail.com>
2020-03-04 11:11:35 +0100

    M

--- pythonv2/GetApiFiles.sh
+++ pythonv2/GetApiFiles.sh
@@ -3,8 +3,8 @@
 
 
 git pull
-cp -fr /home/ams/amscams/tmp_APPS/src /mnt/archive.allsky.tv/APPS
-cp -fr /home/ams/amscams/tmp_APPS/dist /mnt/archive.allsky.tv/APPS
+rsync --update /home/ams/amscams/tmp_APPS/src/* /mnt/archive.allsky.tv/APPS/src
+rsync --update /home/ams/amscams/tmp_APPS/dist/* /mnt/archive.allsky.tv/APPS/dist 
 
 rm /mnt/archive.allsky.tv/AMS7/METEOR/2019/12/24/2019_12_24/2019_12_24_08_17_10_000_010041-trim1298.html
 python3 /home/ams/amscams/pythonv2/publish.py event_station_report /AMS7/METEOR/2019/12/24/2019_12_24/2019_12_24_08_17_10_000_010041-trim1298.json
