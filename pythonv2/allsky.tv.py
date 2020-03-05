89d3985b884f6c31f55df4e359ec247e85129191
Perlerin Vincent <vperlerin@gmail.com>
2020-03-05 14:00:40 +0100

    M

--- pythonv2/allsky.tv.py
+++ pythonv2/allsky.tv.py
@@ -30,7 +30,76 @@ LIVE_DIR = "/mnt/archive.allsky.tv/LIVE/"
 json_conf = load_json_file("../conf/as6.json")
 station_id = json_conf['site']['ams_id'] 
 
-def update_live_html():
+
+def update_live_html_station():
+   template = get_template(LIVE_TEMPLATE)
+   now = datetime. now()
+   mday = now.strftime("%m_%d")
+   mon = now.strftime("%m")
+   dom = now.strftime("%Y_%m_%d")
+   year = now.strftime("%Y")
+
+   all_stations_file = "../conf/all_stations.json"
+   if cfe(all_stations_file) == 0:
+      build_all_stations()
+   all_stations = load_json_file(all_stations_file)
+   status = {}
+   all_station_data = []
+   for sd in all_stations:
+      station = sd['station']
+      status[station] = 0
+      data = {}
+      data['station'] = station
+      data['files'] = []
+      NOAA_DIR =  "/mnt/archive.allsky.tv/" + station + "/NOAA/ARCHIVE/" + year + "/" + mday + "/"
+      if cfe(NOAA_DIR, 1) == 0:
+
+         os.makedirs(NOAA_DIR)
+      day_index = NOAA_DIR + mday + "_index.json"
+      live_files = glob.glob(NOAA_DIR + "*.jpg")
+      for file in live_files: 
+         data['files'].append(file)
+         status[station] = 1
+      all_station_data.append(data)
+
+ 
+
+   for data in all_station_data:
+      station = data['station']
+      STATION_RPT_DIR =  "/mnt/archive.allsky.tv/" + station + "/REPORTS/" + year + "/" + mday + "/"
+      STATION_RPT_VDIR =  "/" + station + "/REPORTS/" + year + "/" + mday + "/"
+      files = sorted(data['files'], reverse=True)
+      data['files'] = files
+      print("ADDING STATION:", station,len(files))
+      if len(files) > 0:
+         fn = files[0].split("/")[-1]
+         file_index = files[0].replace(fn, "")
+         file_index = file_index.replace("/mnt/archive.allsky.tv", "")
+         st = os.stat(files[0])
+         size = st.st_size
+         if size != 0:
+            live_now +=  "<a href=" + STATION_RPT_VDIR + "index.html><img src=" + files[0].replace("/mnt/archive.allsky.tv", "") + "></a><BR>\n"
+
+
+   live_file = LIVE_DIR + "index.html"
+   for sd in status:
+      live_now += sd + " " + str(status[sd]) + "<BR>"
+
+   template = template.replace("{LIVE}", live_now)
+
+   # Cur Day
+   template = template.replace("{DAY}",dom.replace('_','/')) 
+
+   # nO cACHE
+   template = template.replace("{RAND}",str(random.randint(0, 99999999)))
+
+
+   fpo = open(live_file, "w")
+   fpo.write(template)
+   fpo.close()
+   print(live_file)
+
+def update_live_html_station_order():
    """ This function will only be runby a manager's node.
        The purpose is to update the HTML and json indexes for the live view
    """
@@ -61,8 +130,7 @@ def update_live_html():
          os.makedirs(NOAA_DIR)
       day_index = NOAA_DIR + mday + "_index.json"
       live_files = glob.glob(NOAA_DIR + "*.jpg")
-      for file in live_files:
-         #print("ADDING FILES FOR : ", station, file)
+      for file in live_files: 
          data['files'].append(file)
          # Here the key is only the digits of the station name
          # (so we can sort it easily later)
