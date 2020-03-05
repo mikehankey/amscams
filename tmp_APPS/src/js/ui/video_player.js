f670c3a27a8e50ca0c1d638e81bdac3463d2e7f6
Perlerin Vincent <vperlerin@gmail.com>
2020-03-05 10:32:19 +0100

    M

--- tmp_APPS/src/js/ui/video_player.js
+++ tmp_APPS/src/js/ui/video_player.js
@@ -1,8 +1,9 @@
 $(function() {
-   var $playpause, video;
+   var playpause, video;
    if($('#main_video_player').length!=0) {
-      $playpause = $('button[name=playersPlay]');
-      video = $('#main_video_player video')
+      playpause = document.getElementsByClassName("playpause");
+      video = document.getElementsByID("main_video_player");
+
       var controlFunction = function (e) {
          var playpause = this; 
      
@@ -16,6 +17,30 @@ $(function() {
              video.pause();
          }
      };
+
+      // For each play pause button
+      for (var i = 0; i < playpause.length; i++) {
+         playpause[i].addEventListener('click', controlFunction, false);
+      }
+
+      // Video methods
+      var videoPlayFunction = function () {
+         var playpause = this;
+         playpause.title = "pause";
+         playpause.innerHTML = "pause";
+      };
+      var videoPauseFunction = function () {
+         var playpause = this;
+         playpause.title = "play";
+         playpause.innerHTML = "play";
+      };
+
+      // For each video adds the event listeners
+      for (var i = 0; i < video.length; i++) {
+         video[i].addEventListener('play', videoPlayFunction, false);
+         video[i].addEventListener('pause', videoPauseFunction, false);
+         video[i].controls = false;
+      }
    }
 
 }) 
