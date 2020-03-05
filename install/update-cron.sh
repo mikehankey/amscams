fe8097bd94ae87d2767340cb33b6f65e62a5f0fc
Mike Hankey <mike.hankey@gmail.com>
2020-03-05 18:33:15 +0000

    upda

--- install/update-cron.sh
+++ install/update-cron.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-sudo cp crontab-ams.txt /var/spool/cron/crontabs/ams
-sudo cp crontab-root.txt /var/spool/cron/crontabs/root
+sudo cp crontab-ams /var/spool/cron/crontabs/ams
+sudo cp crontab-root /var/spool/cron/crontabs/root
 sudo chown ams /var/spool/cron/crontabs/ams
 sudo chgrp crontab /var/spool/cron/crontabs/ams
 sudo chmod 600 /var/spool/cron/crontabs/ams
