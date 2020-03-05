d335b42bd9c28584a32c6251984683c36e86e344
Perlerin Vincent <vperlerin@gmail.com>
2020-03-05 15:39:15 +0100

    M

--- tmp_APPS/src/js/functions/get_video_frames.js
+++ tmp_APPS/src/js/functions/get_video_frames.js
@@ -5,31 +5,31 @@ async function extractFramesFromVideo(videoUrl, fps=25) {
      // fully download it first (no buffering):
      let videoBlob = await fetch(videoUrl).then(r => r.blob());
      let videoObjectUrl = URL.createObjectURL(videoBlob);
-     let video = document.createElement("video");
+     let videoDD = document.createElement("video");
  
      let seekResolve;
-     video.addEventListener('seeked', async function() {
+     videoDD.addEventListener('seeked', async function() {
        if(seekResolve) seekResolve();
      });
  
-     video.addEventListener('loadeddata', async function() {
-       let canvas = document.createElement('canvas64');
-       let contextWW = canvas.getContext('2d');
-       let [w, h] = [video.videoWidth, video.videoHeight]
-       canvas.width =  w;
-       canvas.height = h;
+     videoDD.addEventListener('loadeddata', async function() {
+       let canvas64 = document.createElement('canvas');
+       let contextWW = canvas64.getContext('2d');
+       let [w, h] = [videoDD.videoWidth, videoDD.videoHeight]
+       canvas64.width =  w;
+       canvas64.height = h;
  
        let frames = [];
        let interval = 1 / fps;
        let currentTime = 0;
-       let duration = video.duration;
+       let duration = videoDD.duration;
  
        while(currentTime < duration) {
-         video.currentTime = currentTime;
+         videoDD.currentTime = currentTime;
          await new Promise(r => seekResolve=r);
  
-         contextWW.drawImage(video, 0, 0, w, h);
-         let base64ImageData = canvas.toDataURL();
+         contextWW.drawImage(videoDD, 0, 0, w, h);
+         let base64ImageData = canvas64.toDataURL();
          frames.push(base64ImageData);
  
          currentTime += interval;
