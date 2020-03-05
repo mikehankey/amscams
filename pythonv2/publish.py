93db55e78cbbcfb21804ac6a3f47ee64557f7d25
Perlerin Vincent <vperlerin@gmail.com>
2020-03-05 10:48:53 +0100

    M

--- pythonv2/publish.py
+++ pythonv2/publish.py
@@ -122,7 +122,7 @@ def make_event_station_report(json_file):
 		</ul>
     '''
 
-   template = template.replace('{VIDEO}','<div id="videoContainer"><video id="main_video_player" autoplay="" width="960" height="540" loop=""><source src="'+json_file.replace('.json','-HD.mp4')+'" type="video/mp4"></video>'+playBtns+'</div>')
+   template = template.replace('{VIDEO}','<figure id="videoContainer" data-fullscreen="false"><video id="main_video_player" autoplay="" width="960" height="540" loop=""><source src="'+json_file.replace('.json','-HD.mp4')+'" type="video/mp4"></video>'+playBtns+'</figure>')
 
    # NO-Cache
    template = template.replace("{RAND}",str(random.randint(0, 99999999)))
