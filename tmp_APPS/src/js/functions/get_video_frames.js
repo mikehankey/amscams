0f423ae49edc24b4983de7e786d741dee83f716d
Perlerin Vincent <vperlerin@gmail.com>
2020-03-09 16:41:32 +0100

    M

--- tmp_APPS/src/js/functions/get_video_frames.js
+++ tmp_APPS/src/js/functions/get_video_frames.js
@@ -1,57 +1,56 @@
-// Get all frames of a video as base64
 async function extractFramesFromVideo(videoUrl, fps=25) {
    return new Promise(async (resolve) => {
-      
+ 
      // fully download it first (no buffering):
      let videoBlob = await fetch(videoUrl).then(r => r.blob());
      let videoObjectUrl = URL.createObjectURL(videoBlob);
-     let videoDD = document.createElement("videoXEDZ");
+     let video = document.createElement("video");
  
      let seekResolve;
-     videoDD.addEventListener('seeked', async function() {
-        console.log("VIDEO SEEKED");
+     video.addEventListener('seeked', async function() {
        if(seekResolve) seekResolve();
      });
  
-     videoDD.addEventListener('loadeddata', async function() {
-       let canvas64 = document.createElement('canvasXEDZ');
-       let contextWW = canvas64.getContext('2d');
-       let [w, h] = [videoDD.videoWidth, videoDD.videoHeight]
-       canvas64.width =  w;
-       canvas64.height = h;
+     video.addEventListener('loadeddata', async function() {
+       let canvas = document.createElement('canvas');
+       let context = canvas.getContext('2d');
+       let [w, h] = [video.videoWidth, video.videoHeight]
+       canvas.width =  w;
+       canvas.height = h;
  
        let frames = [];
        let interval = 1 / fps;
        let currentTime = 0;
-       let duration = videoDD.duration;
+       let duration = video.duration;
  
        while(currentTime < duration) {
-         videoDD.currentTime = currentTime;
+         video.currentTime = currentTime;
          await new Promise(r => seekResolve=r);
  
-         contextWW.drawImage(videoDD, 0, 0, w, h);
-         let base64ImageData = canvas64.toDataURL();
+         context.drawImage(video, 0, 0, w, h);
+         let base64ImageData = canvas.toDataURL();
          frames.push(base64ImageData);
  
          currentTime += interval;
        }
        resolve(frames);
-});
+     });
  
      // set video src *after* listening to events in case it loads so fast
      // that the events occur before we were listening.
-     videoDD.src = videoObjectUrl; 
+     video.src = videoObjectUrl; 
  
    });
  }
-
- let frames
+ 
+ 
+ let croppedFrames
 
 
  async function asyncCall() { 
-   frames = await extractFramesFromVideo(cropped_video,1); 
+   croppedFrames = await extractFramesFromVideo(cropped_video,1); 
 
-   $.each(frames,function(i,v){ 
+   $.each(croppedFrames,function(i,v){ 
       // Add base64 thumbs to the table
       console.log(i);
       console.log(v);
@@ -61,8 +60,7 @@ async function extractFramesFromVideo(videoUrl, fps=25) {
  
  }
 
- $(function() {
-   console.log("OK");
+ $(function() { 
    if(typeof cropped_video !== 'undefined') {
       asyncCall()
    }
