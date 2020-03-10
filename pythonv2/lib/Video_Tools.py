e0625f97e3d79cd0febe744106a4a97b0435017a
Perlerin Vincent <vperlerin@gmail.com>
2020-03-10 10:39:58 +0100

    M

--- pythonv2/lib/Video_Tools.py
+++ pythonv2/lib/Video_Tools.py
@@ -247,9 +247,9 @@ def define_crop_video(json_file,video, size_margin=0):
          crop_video(video,w,h,x,y,output_file)
 
          # Create all the frames as images
-         all_frames = get_frames_from_cropped_video(output_file,json_file.replace(os.path.basename(json_file),''))
+         # all_frames = get_frames_from_cropped_video(output_file,json_file.replace(os.path.basename(json_file),''))
 
-         print(all_frames)
+         print(output_file)
    else:
       print(json_file +  ' not found or corrupted.')
  
