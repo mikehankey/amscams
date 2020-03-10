5c78129ddb787520c5000829a8a3d44cbce723cc
Perlerin Vincent <vperlerin@gmail.com>
2020-03-10 15:50:37 +0100

    M

--- pythonv2/doDay.py
+++ pythonv2/doDay.py
@@ -515,13 +515,16 @@ def do_all(day):
       # Create REPORT PAGE
       ff = os.sep + f.replace(ARCHIVE_RELATIVE_PATH,'').replace('/mnt/ams2/meteor_archive/','')
       print('***********CREATE STATION REPORT *************')
-      cmd = "python3 /home/ams/amscams/pythonv2/publish.py event_station_report " +  ARCHIVE_RELATIVE_PATH + ff
-      #os.system(cmd)
+      cmd = "python3 /home/ams/amscams/pythonv2/publish.py event_station_report " +  ARCHIVE_RELATIVE_PATH + ff.replace('//','/')
+      os.system(cmd)
       print(cmd)
-      print("=>JDON OK")
-      sys.exit(0)
       print('*****************************************************')
       # CREATE CROPPED VIDEO
+      
+      print("define_crop_video")
+      print(ff +","+ ff.replace('.json',"-HD.mp4"))
+         
+      sys.exit(0)
       define_crop_video(  ff , ff.replace('.json',"-HD.mp4"))
       print('********CROPPED VIDEO DONE ********************')
       # CREATE METEOR CENTERED VIDEO
