23b735bfaf767972a2c1a4d963522da83d7371f4
Perlerin Vincent <vperlerin@gmail.com>
2020-03-10 12:02:47 +0100

    M

--- pythonv2/doDay.py
+++ pythonv2/doDay.py
@@ -511,7 +511,7 @@ def do_all(day):
 
    # Make all the reports for the given day
    for f in arc_files:
-      cmd = "python3 /home/ams/amscams/pythonv2/publish.py event_station_report " + f 
+      cmd = "python3 /home/ams/amscams/pythonv2/publish.py event_station_report " + f.replace(ARCHIVE_RELATIVE_PATH,'')
       os.system(cmd)
 
 def check_time(day):
