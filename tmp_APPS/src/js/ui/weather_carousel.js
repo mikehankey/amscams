8f752d3f09f84a8b514d7a5aa315163afb8319fa
Perlerin Vincent <vperlerin@gmail.com>
2020-03-11 17:55:50 +0100

    M

--- tmp_APPS/src/js/ui/weather_carousel.js
+++ tmp_APPS/src/js/ui/weather_carousel.js
@@ -1,34 +1,11 @@
+// Carousel for NOAA
+$(function() {
+      $('#carouselWInd').addClass('carousel').carousel({'ride':'true','interval':'false'});
 
-
-   // Takes care of the lazy carousels
-   /*$(function() {
-   $("#carousel").carousel().on("slide.bs.carousel", function(ev) {
-       var lazy;
-       lazy = $(ev.relatedTarget).attr('data-src');
-       lazy.attr("src", lazy.data('src'));
-       lazy.removeAttr("data-src");
-     });
-     
-   });
-
-
-
-   $("#carousel").on("slide.bs.carousel", function(ev) {
-      var lazy;
-      console.log("SLIDING");
-      lazy = $(ev.relatedTarget).attr('data-src');
-      console.log(lazy);
-      lazy.attr("src", lazy.data('src'));
-      lazy.removeAttr("data-src");
-    }).carousel();*/
-
-
-
-    $('#carouselWInd').addClass('carousel').carousel({'ride':'false'});
-
-    $("#carouselWInd").on("slide.bs.carousel", function(ev) {
-      var $img = $(ev.relatedTarget).find('img')
-      var src  = $img.attr('data-src');
-      $img.attr("src", src);
-      $img.removeAttr("data-src");
-    });
+      $("#carouselWInd").on("slide.bs.carousel", function(ev) {
+         var $img = $(ev.relatedTarget).find('img')
+         var src  = $img.attr('data-src');
+         $img.attr("src", src);
+         $img.removeAttr("data-src");
+      });
+});
