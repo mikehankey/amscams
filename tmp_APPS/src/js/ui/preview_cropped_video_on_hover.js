c37ab96568fa9200f6e36a1171cd4fcd7fbcbb3f
Perlerin Vincent <vperlerin@gmail.com>
2020-03-11 13:33:50 +0100

    M

--- tmp_APPS/src/js/ui/preview_cropped_video_on_hover.js
+++ tmp_APPS/src/js/ui/preview_cropped_video_on_hover.js
@@ -6,7 +6,7 @@ $(function() {
       $('a.T[data-src]').each(function() {
          var $t = $(this);
          var video_path = $t.attr('data-src');
-         $t.mouseover(function()  { $('<video id="vvv" style="width:100%" autoplay loop><source src="'+video_path+'"></video>').appendTo($t); });
+         $t.mouseover(function()  { $('<video id="vvv" style="width:100%" autoplay loop><source src="'+video_path+'"></video>').prependTo($t); });
          $t.mouseout(function()  { $('#vvv').remove()});
       })
   }
