7cb55633fda88e744222aa719f85227c666bc2ef
Perlerin Vincent <vperlerin@gmail.com>
2020-03-11 11:59:39 +0100

    M

--- pythonv2/doDay.py
+++ pythonv2/doDay.py
@@ -519,7 +519,6 @@ def do_all(day):
       
       # Create REPORT PAGE
       ff = os.sep + f.replace(ARCHIVE_RELATIVE_PATH,'').replace('/mnt/ams2/meteor_archive/','')
-      ff = ARCHIVE_RELATIVE_PATH + ff.replace('//','/')
       ff = ff.replace('//','/')
       cmd = "python3 /home/ams/amscams/pythonv2/publish.py event_station_report " +  ff
       os.system(cmd)
