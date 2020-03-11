cbe2c66deb81029601c8e61abf07ea076b528550
Perlerin Vincent <vperlerin@gmail.com>
2020-03-11 10:24:27 +0100

    M

--- pythonv2/lib/Video_Tools_Fundamentals.py
+++ pythonv2/lib/Video_Tools_Fundamentals.py
@@ -89,8 +89,11 @@ def get_ROI_from_arc(arc_json_file):
    json_data = load_json_file(arc_json_file)
 
    if(json_data is not False):
+      print(json_data)
       if('frames' in json_data):
+         print("FRAMES OK")
          if(len(json_data['frames'])>0): 
+            print("FRAMES LEN OK")
             if('info' in json_data):
                if('hd_vid' in json_data['info']):
                   if(cfe(json_data['info']['hd_vid'])==1):
