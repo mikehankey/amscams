ba7a6e0e2f040a37269760bac287fb630e8e25ff
Perlerin Vincent <vperlerin@gmail.com>
2020-03-11 17:19:04 +0100

    M

--- tmp_APPS/src/js/ui/weather_carousel.js
+++ tmp_APPS/src/js/ui/weather_carousel.js
@@ -2,7 +2,7 @@ $(function() {
 
    // Takes care of the lazy carousels
    $("#carousel").carousel();
-     return $("#carousel.lazy").on("slide", function(ev) {
+     return $("#carousel.lazy").on("slide.bs.carousel", function(ev) {
        var lazy;
        lazy = $(ev.relatedTarget).find("img[data-src]");
        lazy.attr("src", lazy.data('src'));
