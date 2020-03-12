ada43339d841b9ba23f2f7c9c32303b141ab7975
Perlerin Vincent <vperlerin@gmail.com>
2020-03-12 11:52:25 +0100

    M

--- pythonv2/doDay.py
+++ pythonv2/doDay.py
@@ -423,13 +423,15 @@ def html_get_detects(day,tsid,event_files, events):
                   request = requests.get(full_path)
                   if request.status_code == 200:
                      video_path = "<a href='"+full_path+"' class='img-link btn btn-secondary btn-sm'><span class='icon-eye'></span></a>"
+               else:
+                  video_path = "<a href='"+video_path+"' class='img-link btn btn-secondary btn-sm'><span class='icon-eye'></span></a>"
 
                if(jreport_path!=''):
                   elink = "<a href=" + jreport_path + " class='T' data-src="+cropped_video_file+">"
 
                single_html += "<div class='"+css_class+"'>" + elink +  "<img src='"+was_vh_dir + image_file+"' class='img-fluid'></a>"
                single_html += "<div class='d-flex mb-2'><div class='mr-auto'><span>"+'<b>Cam#' + analysed_name['cam_id'] + '</b> '+ analysed_name['hour']+':'+analysed_name['min']+':'+analysed_name['sec']+'.'+analysed_name['ms'] + "</div>"
-               single_html += "<div class='position-relative'>WWWWWW"+video_path+"</div></div></div>"
+               single_html += "<div class='position-relative'>"+video_path+"</div></div></div>"
                ss_count += 1 
             else:
                 
