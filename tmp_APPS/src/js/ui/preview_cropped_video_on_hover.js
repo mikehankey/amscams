ce6b646eab8969a98a27153e0732d8159959466f
Perlerin Vincent <vperlerin@gmail.com>
2020-03-13 10:37:08 +0100

    M

--- tmp_APPS/src/js/ui/preview_cropped_video_on_hover.js
+++ tmp_APPS/src/js/ui/preview_cropped_video_on_hover.js
@@ -3,17 +3,29 @@ $(function() {
 
   // Show video preview on mousover on Gallery Daily Report
   if($('a.T[data-src]').length>0) {
-      $('a.T[data-src]').each(function() {
+
+
+      $('a.T[data-src]').mouseover(function() {
          var $t = $(this);
          var video_path = $t.attr('data-src');
-         if(video_path !== 'X') {
-            $t.hover(function() { $('#vvv').remove(); $('<video id="vvv" style="width:100%" autoplay loop><source src="'+video_path+'"></video>').prependTo($t); });
-         } else {
-            $('#vvv').remove(); 
-            $t.css('cursor','not-allowed')
-         } 
-      })
-  }
+
+         if(typeof video_path !== 'undefined' && video_path!=="X") {
+            if($t.find('video').length>0) {
+               $t.find('video').show();
+            } else {
+               $('<video style="width:100%" autoplay loop><source src="'+video_path+'"></video>').prependTo($t);
+            }
+         }
+
+      });
+
+      $('a.T[data-src]').mouseout(function() {
+         var $t = $(this);
+         $t.find('video').hide();
+      });
+
+
+ 
 
 })
  
