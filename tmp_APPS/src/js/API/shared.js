b4584e4f52ea6dc337fa4d49b9c32bfd0e0cd55f
Perlerin Vincent <vperlerin@gmail.com>
2020-03-13 14:39:29 +0100

    M

--- tmp_APPS/src/js/API/shared.js
+++ tmp_APPS/src/js/API/shared.js
@@ -8,7 +8,7 @@ function setup_action() {
 function already_done() {
    // We test if the page has already been updated within the hour
    if(readCookie(PAGE_MODIFIED)!=null && readCookie(PAGE_MODIFIED)==window.location.href && $('#dejavu').length==0) {
-      $('<div id="dejavu" class="alert alert-info"><span class="icon-notification"></span> <b>You have already made edits for this page within the last hour. Please allow for at least one hour for changes to take effect.</b></div>').prependTo('#main_container');
+      $('<div id="dejavu" class="alert alert-info m-4"><span class="icon-notification"></span> <b>You have already made edits for this page within the last hour. Please allow for at least one hour for changes to take effect.</b></div>').insertBefore('#main_container');
    
       // Disabled buttons
       $('.btn').attr('disabled','disabled');
