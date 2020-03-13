81078092046b3487dea5956d4dfd0c62a1b41285
Perlerin Vincent <vperlerin@gmail.com>
2020-03-13 13:50:39 +0100

    M

--- tmp_APPS/src/js/API/login.js
+++ tmp_APPS/src/js/API/login.js
@@ -42,6 +42,8 @@ function add_login_stuff() {
       // On Obs Page
       $(delButton).appendTo$($('#tools'));
       $(confButton).appendTo$($('#tools'));
+      // Show Tools
+      $('#tool_box').removeClass('d-none');
 
    }
    
