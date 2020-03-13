c2dfa6449ea7473a86847bed673cbeaf943c4b52
Perlerin Vincent <vperlerin@gmail.com>
2020-03-13 16:23:44 +0100

    M

--- tmp_APPS/src/js/functions/get_video_frames.js
+++ tmp_APPS/src/js/functions/get_video_frames.js
@@ -66,7 +66,7 @@ async function extractFramesFromVideo(videoUrl,firstframe, how_many_frames, fps=
    var i, frame_c = 0;
    for (i = first_frame; i <  (first_frame+how_many_frames); i++) {
        // Add base64 thumbs to the table  
-       $('#thb_'+i).find('img').attr('src',croppedFrames[frame_c]).css('border-color', $('#thb_'+i).attr('data-src')); 
+       $('#thb_'+i).find('img').attr('src',croppedFrames[frame_c]).css('border-color', $('#thb_'+i).attr('data-src')).css('max-width','180px'); 
        frame_c ++;
    }
 
