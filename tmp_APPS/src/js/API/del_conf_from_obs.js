47c7a97d81d82aba776ef710a52733fa2df66b76
Perlerin Vincent <vperlerin@gmail.com>
2020-03-16 15:23:55 +0100

    M

--- tmp_APPS/src/js/API/del_conf_from_obs.js
+++ tmp_APPS/src/js/API/del_conf_from_obs.js
@@ -1,3 +1,31 @@
+function setup_single_delete_buttons()  {
+ 
+  $('.delSingle').click(function() {
+
+      bootbox.confirm({
+         message: "You are about to delete this detection.<br/>Please, confirm.",
+         centerVertical: true,
+         buttons: {
+            cancel: {
+               label: 'No',
+               className: 'btn-danger'
+            },
+            confirm: {
+               label: 'Yes',
+               className: 'btn-success'
+            }
+         },
+         callback: function (result) {
+            if(result) {   send_API_task({'toDel':cropped_video},'','');
+            }
+         }
+      });
+   
+   });
+
+}
+
+
 // WARNING HERE WE SEND "CROPPED VIDEO" BECAUSE IT'S IN THE TEMPLATE
 // AND WE KNOW IT'S UNIQUE FOR THE DETECTION
 function setup_single_conf_buttons() {
