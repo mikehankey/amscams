d7843ed99a686c0ce10bd114a912b8740540436d
Perlerin Vincent <vperlerin@gmail.com>
2020-03-16 15:54:06 +0100

    M

--- tmp_APPS/src/js/ui/meteor_picker.js
+++ tmp_APPS/src/js/ui/meteor_picker.js
@@ -513,14 +513,17 @@ function get_help_pos(nextprev, org_id) {
  
 
 function get_neighbor_frames(cur_id) {
+
     // Get the thumbs & colors or -5 +5 frames
     // IN #nav_prev
     var all_thb = []; 
     cur_id = parseInt(cur_id)
+
     for(var i = cur_id-3; i < cur_id+4; i++) {
-        var $cur_tr = $('#fr_'+i);
+
+        var $cur_tr = $('#thb_'+i);
         var img =  $cur_tr.find('img').attr('src');
-        var color = $cur_tr.find('.st').css('background-color');
+        var color = $cur_tr.find('img').css('baorder-color');
         var id = i;
         vid = '';
 
@@ -546,108 +549,3 @@ function get_neighbor_frames(cur_id) {
 
 }
 
-
-
-function open_meteor_picker() {
-    var viewer_dim = viewer_DIM;
-    var all_frames_ids = [];
-
-    // Get all the known frame ids
-    $('#reduc-tab table tbody tr').each(function() {
-        var id = $(this).attr('id');
-        id = id.split('_');
-        all_frames_ids.push(parseInt(id[1]));
-    });
-
-    // Click on selector (button & thumb)
-    $('.select_meteor').click(function(e) {
-
-         e.stopImmediatePropagation();
-  
-        var $tr = $(this).closest('tr');
-        var rand;
-
-        // Get meteor id
-        var meteor_id = $tr.attr('id');
-        meteor_id = meteor_id.split('_')[1];
-  
-        // Get Image
-        var $img = $tr.find('img'); 
-
-        // Get Color
-        var color = $tr.find('.st').css('background-color');
-
-        var real_width, real_height;
-
-        // Get Neightbors
-        var neighbor = get_neighbor_frames(meteor_id); 
-
-        // Add template if necessary
-        addModalTemplate(meteor_id,neighbor);
-
-        // Prev Button
-        $('#met-sel-prev').unbind('click').click(function() {
-            meteor_select("prev",all_frames_ids);
-            return false;
-        });
-
-        // Next Button
-        $('#met-sel-next').unbind('click').click(function() {
-            meteor_select("next",all_frames_ids);
-            return false;
-        });
-
-        // Add image 
-        $('.meteor_chooser').css('background-image','url('+$img.attr('src')+')').css('border','2px solid ' + color);
-        
-        
-        // Add current ID
-        $('#sel_frame_id, .sel_frame_id').text(meteor_id);
-
-      
-        // Update image real dimensions 
-        var img = new Image();
-        var imgSrc = $img.attr('src');
-
-        $(img).on('load',function () {
-            real_width = img.width;
-            real_height = img.height;
-            $('input[name=thumb_w]').val(real_width);
-            $('input[name=thumb_h]').val(real_height); 
-            // garbage collect img
-            delete img;
-
-            // Redefine viewer depending on the thumb dimension
-            if(real_width !== real_height) {
-            
-                if(real_width > real_height) {
-                    $('.meteor_chooser').css('height',  real_height/real_width * viewer_dim);
-                } else {
-                    $('.meteor_chooser').css('width', real_width/real_height * viewer_dim);
-                }
-
-            } else {
-                $('.meteor_chooser').css({'height': viewer_dim + 'px', 'width':viewer_dim + 'px'});
-            }
-             
-            // Reset Cross position
-            $('#lh').css('top','50%');
-            $('#lv').css('left','50%');
-            
-            // Open Modal
-            $('#select_meteor_modal').modal('show');
-
-            // Reset
-            $(".meteor_chooser").removeClass('done'); 
-            setup_modal_actions(meteor_id, $tr.attr('data-org-x'),$tr.attr('data-org-y'));
-
-         
-        }).attr({ src: imgSrc }); 
-
-        return false;
-        
-
-    });
-}
-
- 
