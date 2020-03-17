bfef5485685ae3c497d2270c336abe784d17f4a7
Perlerin Vincent <vperlerin@gmail.com>
2020-03-17 16:16:43 +0100

    M

--- tmp_APPS/src/js/functions/meteor_picker.js
+++ tmp_APPS/src/js/functions/meteor_picker.js
@@ -18,7 +18,7 @@ function open_meteor_picker(all_frames_ids, meteor_id, color, img_path) {
          // Add image 
       var height = $('.select_meteor_holder').outerHeight() - $('#nav_prev').outerHeight() - 4;
       
-      $('.meteor_chooser').css({'background-image':'url('+img_path+')','height':height - 4}).css('border','2px solid red');
+      $('.meteor_chooser').css({'background-image':'url('+img_path+')','height':height - 4}).css('border','2px solid ' + color);
 
       // Setup 16/9 dim
       $('.meteor_chooser').css('width',parseInt($('.meteor_chooser').height()*16/9));
@@ -111,12 +111,7 @@ function meteor_select(dir,all_frames_ids) {
    var next_id;
    var cur_id = parseInt($('#sel_frame_id').text());
    var cur_index = all_frames_ids.indexOf(cur_id);
-
-   console.log("METEOR SELECT")
-   console.log("CUR ID " + cur_id)
-   console.log("CUR INDEX " + cur_index)   
-   console.log("ALL FRAMES ")
-   console.log(all_frames_ids)
+ 
 
    if(dir=="prev") {
        if(cur_index==0) {
@@ -130,11 +125,7 @@ function meteor_select(dir,all_frames_ids) {
        } else {
            next_id = cur_index + 1;
        }
-   } 
-   
-
-   console.log("NEXT ID ")
-   console.log(next_id)
+   }  
 
    // Open the next or previous one
    $('#reduc-tab table tbody tr#fr_' + all_frames_ids[next_id] + " a").click();
