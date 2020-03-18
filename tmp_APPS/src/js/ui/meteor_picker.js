bff05d30335ead6b45c47e09f7cc0da79b219999
Perlerin Vincent <vperlerin@gmail.com>
2020-03-18 11:45:08 +0100

    M

--- tmp_APPS/src/js/ui/meteor_picker.js
+++ tmp_APPS/src/js/ui/meteor_picker.js
@@ -186,43 +186,44 @@ function addPickerModalTemplate(meteor_id,neighbor) {
     var c;
     
     if($('#select_meteor_modal').length==0) {
-       c =' <div id="select_meteor_modal" class="modal fade" tabindex="-1" style="padding-right: 0!important; display: block; width: 100vw; height: 100vh;>\
-       <div class="modal-dialog  modal-lg modal-dialog-centered box" style="width: 100vw;max-width: 100%;margin: 0; padding: 0;">\
-         <div class="modal-content" style="height: 100vh;">\
-            <div class="modal-header p-0 pt-1" style="border:none!important">\
-               <h5 class="ml-1">Select Meteor Position</h5>\
-               <div class="alert alert-info mb-3 p-1 pr-1 pl-2">Select the <strong>POSITION</strong> on the meteor on each frame.</div>\
-               <div class="alert p-1 pl-1 pr-2"><span id="fr_cnt">0</span> Frames done</div>\
-            </div>\
-            <div class="d-flex flex-wrap">\
-                  <div class="d-flex justify-content-left" id="frame_select_mod"> \
-                     <div id="cropped_frame_select" class="d-flex justify-content-left">\
-                        <div">\
-                        </div> \
-                     </div>\ 
-               </div>\
-               <div id="cropped_frame_selector_hoder" class="mt-3 mb-2">\
-                     <div id="cropped_frame_selector" class="cur">\
-                        <div id="org_lh"></div>\
-                        <div id="org_lv"></div>\
-                        <div id="lh"></div>\
-                        <div id="lv"></div> \
-                        <div id="cirl" style="width:10px; height:10px; border-radius:50%; position: absolute; border: 1px solid red;"></div>\
-                     </div> \
-               </div>\
-               <div class="d-flex justify-content-between">\
-                  <button  class="btn btn-primary hidden" style="visibility: hidden;">Create All</button>\
-                  <div class="d-flex justify-content-center text-center">\
-                     <button id="skip_frame" class="btn btn-secondary ml-3">Skip</button>\
+      c ='  <div id="select_meteor_modal" class="modal fade" tabindex="-1" style="padding-right: 0!important; display: block; width: 100vw; height: 100vh;>\
+                  <div class="modal-dialog  modal-lg modal-dialog-centered box" style="width: 100vw;max-width: 100%;margin: 0; padding: 0;">\
+                     <div class="modal-content" style="height: 100vh;">\
+                        <div class="modal-header p-0 pt-1" style="border:none!important">\
+                           <h5 class="ml-1">Select Meteor Position</h5>\
+                           <div class="alert alert-info mb-3 p-1 pr-1 pl-2">Select the <strong>POSITION</strong> on the meteor on each frame.</div>\
+                           <div class="alert p-1 pl-1 pr-2"><span id="fr_cnt">0</span> Frames done</div>\
+                        </div>\
+                        <div class="d-flex flex-wrap">\
+                           <div class="d-flex justify-content-left" id="frame_select_mod">\
+                              <div id="cropped_frame_select" class="d-flex justify-content-left">\
+                                 <div>\
+                                 </div>\
+                              </div>\
+                           </div>\
+                        </div>\
+                        <div id="cropped_frame_selector_hoder" class="mt-3 mb-2">\
+                              <div id="cropped_frame_selector" class="cur">\
+                                 <div id="org_lh"></div>\
+                                 <div id="org_lv"></div>\
+                                 <div id="lh"></div>\
+                                 <div id="lv"></div> \
+                                 <div id="cirl" style="width:10px; height:10px; border-radius:50%; position: absolute; border: 1px solid red;"></div>\
+                              </div> \
+                        </div>\
+                        <div class="d-flex justify-content-between">\
+                           <button  class="btn btn-primary hidden" style="visibility: hidden;">Create All</button>\
+                           <div class="d-flex justify-content-center text-center">\
+                              <button id="skip_frame" class="btn btn-secondary ml-3">Skip</button>\
+                           </div>\
+                           <button id="create_all" class="btn btn-primary">Create All</button>\
+                        </div>\
+                        </div>\
+                     </div>\
+                  <div class="modal-footer bd-t mt-3 pt-2 pb-2 pr-2">\
+                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>\
                   </div>\
-                  <button id="create_all" class="btn btn-primary">Create All</button>\
-               </div>\
-            </div>\
-         </div>\
-         <div class="modal-footer bd-t mt-3 pt-2 pb-2 pr-2">\
-             <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>\
-         </div>\
-      </div>';
+               </div>';
       $(c).appendTo('body');
        /*
         c = '<div id="select_meteor_modal" class="modal fade multiple-select" tabindex="-1">\
