0afbaaf8b5e9ee5c713f0afe904dd30cbedd6119
Perlerin Vincent <vperlerin@gmail.com>
2020-03-18 14:25:20 +0100

    M

--- tmp_APPS/src/js/ui/meteor_picker.js
+++ tmp_APPS/src/js/ui/meteor_picker.js
@@ -97,7 +97,7 @@ function add_image_inside_meteor_select(img_path, color, all_frames_ids, meteor_
    $('#cropped_frame_selector').css('background-image','url('+img_path+')').css('border','2px solid ' + color);
 
    // Add image 
-   $('#cropped_frame_selector').css('height',$('#select_meteor_modal').outerHeight() - $('#select_meteor_modal .modal-header').outerHeight() - $("#thumb_browwser").outerHeight() - $('#below_cfs').outerHeight() - ('#select_meteor_modal .modal-footer').outerHeight())
+   $('#cropped_frame_selector').css('height',$('#select_meteor_modal').outerHeight() - $('#select_meteor_modal .modal-header').outerHeight() - $("#thumb_browwser").outerHeight() - $('#below_cfs').outerHeight() - $('#select_meteor_modal .modal-footer').outerHeight())
    $('#cropped_frame_selector').css('width', parseInt($('#cropped_frame_selector').outerHeight()*16/9));  
 
    $('#sel_frame_id, .sel_frame_id').text(meteor_id);   
