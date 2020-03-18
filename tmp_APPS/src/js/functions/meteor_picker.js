40b604b11cc380eb6b66928cd424fa4cd2d34a58
Perlerin Vincent <vperlerin@gmail.com>
2020-03-18 10:55:02 +0100

    M

--- tmp_APPS/src/js/functions/meteor_picker.js
+++ tmp_APPS/src/js/functions/meteor_picker.js
@@ -7,7 +7,7 @@ function add_image_inside_meteor_select(img_path, color, all_frames_ids,meteor_i
       $('.meteor_chooser').css({'background-image':'url('+img_path+')','height':height}).css('border','2px solid ' + color);
 
       // Setup 16/9 dim
-      $('.meteor_chooser').css('width', $('.meteor_chooser').height()*16/9); 
+      $('.meteor_chooser').css('width', $('.meteor_chooser').outerHeight()*16/9); 
       $('.meteor_chooser').css('height', $('.meteor_chooser').width()*9/16); 
       
       // Prev Button
