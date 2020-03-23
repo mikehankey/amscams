788c3fc4c2de3e0193cb2bbc6f4cac458d7bdba3
Perlerin Vincent <vperlerin@gmail.com>
2020-03-23 16:55:31 +0100

    M

--- tmp_APPS/src/js/ui/meteor_picker.js
+++ tmp_APPS/src/js/ui/meteor_picker.js
@@ -210,6 +210,11 @@ function add_image_inside_meteor_select(img_path, all_frames_ids, meteor_id) {
       var frames_before = [];
       for(var i = meteor_id; i >= meteor_id - 3 ; i--) {  
          if($('#fr_'+meteor_id).length>0 && i!=meteor_id) {
+
+            console.log("I ", i);
+            console.log("X ",parseInt($('#fr_'+i).attr('data-org-x')));
+            console.log("Y ",parseInt($('#fr_'+i).attr('data-org-y')));
+
             xy = convert_from_local(parseInt($('#fr_'+i).attr('data-org-x')),$('#fr_'+i).attr('data-org-y'));
 
             console.log("BEFORE");
