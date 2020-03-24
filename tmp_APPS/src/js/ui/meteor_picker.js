6e34a88179a445964c06e92f0e4405b734c2db3f
Perlerin Vincent <vperlerin@gmail.com>
2020-03-24 11:49:24 +0100

    M

--- tmp_APPS/src/js/ui/meteor_picker.js
+++ tmp_APPS/src/js/ui/meteor_picker.js
@@ -259,8 +259,8 @@ function add_image_inside_meteor_select(img_path, all_frames_ids, meteor_id) {
    xy = convert_to_local(parseInt($('#fr_'+meteor_id).attr('data-org-x')),parseInt($('#fr_'+meteor_id).attr('data-org-y'))); 
    addCircleRepair(xy[0]/factor,xy[1]/factor,meteor_id,'x'); 
 
-   add_debug("#" + meteor_id +  " => (local)" + xy[0]/factor +  "  , " + xy[1]/factor);
-   add_debug(" " + meteor_id +  " => (local)" + convert_from_local(xy[0],xy[1]));
+   add_debug("#" + meteor_id +  " => (local) " + xy[0]/factor +  "  , " + xy[1]/factor);
+   add_debug(" " + meteor_id +  " => (real ) " + convert_from_local(xy[0],xy[1]));
 
 
 
@@ -347,7 +347,7 @@ function add_image_inside_meteor_select(img_path, all_frames_ids, meteor_id) {
           $('#lv').css('left',relX-2); 
       }
 
-      add_mouse_pos(relX,relY)
+      add_mouse_pos(relX*factor,relY*factor);
 
   });
 
