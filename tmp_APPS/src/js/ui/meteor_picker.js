de610a8b1355dfcf667740d9317ba891eab29c2e
Perlerin Vincent <vperlerin@gmail.com>
2020-03-24 15:31:31 +0100

    M

--- tmp_APPS/src/js/ui/meteor_picker.js
+++ tmp_APPS/src/js/ui/meteor_picker.js
@@ -133,9 +133,9 @@ function addCircleRepair(_x,_y,fn,after_of_before) {
    if(after_of_before=='b') {
       $cir.css('border-color','rgba(255, 0, 0, .3)');
    } else if(after_of_before =='x') {
-      $cir.css('border-color','rgba(0, 0, 255, .3)');
-   } else if(after_of_before == 'na' || after_of_before == 'nb') {
       $cir.css('border-color','rgba(255, 229, 46, .3)');
+   } else if(after_of_before == 'na' || after_of_before == 'nb') {
+      $cir.css('border-color','rgba(255, 255, 255, .3)');
    } else {
       $cir.css('border-color','rgba(0, 255, 0, .3)');
    }
