4a2a9ef770960d5fa19aa1b4f3d2cc98e5e660d0
Perlerin Vincent <vperlerin@gmail.com>
2020-03-24 16:54:38 +0100

    M

--- tmp_APPS/src/js/functions/get_video_frames.js
+++ tmp_APPS/src/js/functions/get_video_frames.js
@@ -65,7 +65,7 @@ async function extractFramesFromVideo(videoUrl,firstframe, how_many_frames, fps=
 
 
    var i, frame_c = 0;
-   for (i = first_frame; i <  (first_frame+how_many_frames); i++) {
+   for (i = first_frame-1; i <=  (first_frame+how_many_frames); i++) {
        // Add base64 thumbs to the table  
        $('#thb_'+i).find('img').attr('src',croppedFrames[frame_c]).css('border-color', $('#thb_'+i).attr('data-src')).css('max-width','180px'); 
        frame_c ++;
@@ -90,10 +90,7 @@ async function extractFramesFromVideo(videoUrl,firstframe, how_many_frames, fps=
       // Frame by frame animation holding
       loading_button($("#play_anim_tv"));
       loading_button($(".fr_only"));
-
-      first_frame-=1;
-      how_many_frames-=1;
-
+  
       // What's the first frame we want to get?
       asyncCall(first_frame, how_many_frames)
    }
