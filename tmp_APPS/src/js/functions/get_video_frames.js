20688fa9b3dc76fe0beac02cac84b396d04b6dcd
Perlerin Vincent <vperlerin@gmail.com>
2020-03-25 11:27:59 +0100

    M

--- tmp_APPS/src/js/functions/get_video_frames.js
+++ tmp_APPS/src/js/functions/get_video_frames.js
@@ -65,13 +65,13 @@ async function extractFramesFromVideo(videoUrl,firstframe, how_many_frames, fps=
  async function asyncCall(first_frame, how_many_frames) {  
    croppedFrames = await extractFramesFromVideo(cropped_video,first_frame, how_many_frames); 
    
-   var i, frame_c = 0;
-
+   var i; //, frame_c = 0;
+   
    for (i = first_frame; i <=  (first_frame+how_many_frames); i++) {
        // Add base64 thumbs to the table  
        var index = i-1;
-       $('#thb_'+index).find('img').attr('src',croppedFrames[frame_c]).css('border-color', $('#thb_'+index).attr('data-src')).css('max-width','180px'); 
-       frame_c++;
+       $('#thb_'+index).find('img').attr('src',croppedFrames[index]).css('border-color', $('#thb_'+index).attr('data-src')).css('max-width','180px'); 
+       //frame_c++;
    }
 
    // We enable the frame by frame animation when it's loaded
