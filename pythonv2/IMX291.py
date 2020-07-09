819271e4e55f156e51b48b92b6d390ff6dc0c1b3
mikehankey <mike.hankey@gmail.com>
2020-07-09 07:10:49 -0400

    updates

---
+++ pythonv2/IMX291.py
@@ -0,0 +1,42 @@
+import sys
+from dvrip import DVRIPCam
+from time import sleep
+import json
+
+#FILL IN YOUR DETAILS HERE:
+CameraUserName = "admin"
+CameraPassword = ""
+CameraIP = '192.168.76.71'
+BitrateRequired = 7000
+#END OF USER DETAILS
+
+
+if len(sys.argv) > 1:
+    CameraIP = str(sys.argv[1])
+
+cam = DVRIPCam(CameraIP,CameraUserName,CameraPassword)
+
+if cam.login():
+    print ("Success! Connected to " + CameraIP)
+else:
+    print ("Failure. Could not connect to camera!")
+
+sleep(2)
+print ("Current encoding settings:")
+#enc_info = cam.get_info("Simplify.Encode")
+cam_info = cam.get_info("Camera.ParamEx")
+print (cam_info)
+cam_info[0]['BroadTrends']['AutoGain'] = 1
+cam.set_info("Camera.ParamEx", cam_info)
+print ("\r\n")
+cam.close()
+exit()
+sleep(2)
+enc_info[0]['MainFormat']['Video']['BitRate'] = BitrateRequired
+cam.set_info("Simplify.Encode", enc_info)
+print ("Sent new bitrate settings\r\n")
+sleep(5)
+print ("New encoding settings:")
+print(cam.get_info("Simplify.Encode"))
+print(cam.get_info("Simplify.Encode"))
+sleep(5)
