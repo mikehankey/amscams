818502b6fd7f0fb39d99f08f230fcd74572bbe11
mikehankey <mike.hankey@gmail.com>
2020-08-15 08:45:31 -0400

    pppp

--- pipeline/mln.sh
+++ pipeline/mln.sh
@@ -1,5 +1,11 @@
 
-./Process.py mln 2020_08_13
-./Process.py mln 2020_08_12
-./Process.py mln 2020_08_11
-./Process.py mln 2020_08_10
+#./Process.py mln 2020_08_13
+#./Process.py mln 2020_08_12
+#./Process.py mln 2020_08_11
+#./Process.py mln 2020_08_10
+
+
+./Process.py super_stack 2020_08_13
+./Process.py super_stack 2020_08_12
+./Process.py super_stack 2020_08_11
+./Process.py super_stack 2020_08_10
