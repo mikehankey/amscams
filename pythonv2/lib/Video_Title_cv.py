fe89610ce743a5cfc41fce63a410b37d50394fbc
Perlerin Vincent <vperlerin@gmail.com>
2020-08-17 16:43:02 +0200

    M

--- pythonv2/lib/Video_Title_cv.py
+++ pythonv2/lib/Video_Title_cv.py
@@ -321,4 +321,5 @@ def concat_videos_fade(video1,video2,output,_from,_to):
 # Resize a video
 def resize(video1,output,_width):
    cmd = 'ffmpeg -i ' + video1 + ' -filter:v scale='+str(_width)+':-1 -c:a copy ' + output
-   os.system(cmd)  
+   output = subprocess.check_output(cmd, shell=True).decode("utf-8")     
+    print(output)  
