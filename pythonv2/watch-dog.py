53b01b714fa17c9091fc7e41d482b8b30b1f341c
ams <mike.hankey@gmail.com>
2020-09-15 11:26:04 +0000

    updates

--- pythonv2/watch-dog.py
+++ pythonv2/watch-dog.py
@@ -10,6 +10,20 @@ import time
 from lib.FileIO import load_json_file
 import psutil
 
+def check_running(cam_num, type, json_conf):
+   cam_key = 'cam' + str(cam_num)
+   cam_ip = json_conf['cameras'][cam_key]['ip']
+
+   if type == "HD":
+      cmd = "ps -aux |grep \"ffmpeg\" | grep \"HD\" | grep " + cam_ip + " | grep -v grep | wc -l"
+   else:
+      cmd = "ps -aux |grep \"ffmpeg\" | grep \"SD\" | grep " + cam_ip + " | grep -v grep | wc -l"
+   output = subprocess.check_output(cmd, shell=True).decode("utf-8")
+   output = int(output.replace("\n", ""))
+   print("RUNNING:", output)
+   return(int(output))
+
+
 def clean_zombies():
    kill_zombies("sro-settings.py", 10)
    kill_zombies("examine-still.py", 60)
@@ -109,11 +123,13 @@ def check_stream(cam_num, stream_type):
    return(bad)
 errors = []
 config = load_json_file("../conf/as6.json")
+json_conf = config
 obs_name = config['site']['obs_name']
 #print (obs_name)
 
 clean_zombies()
-
+cams_with_err = {}
+cams_with_np = {}
 # Check disk space 
 derrs = []
 #derrs = check_disk_space()
@@ -127,6 +143,8 @@ for i in range (1,7):
    if res == 0: 
       errors.append("Cam " + str(i) + "did not respond to ping")
       ping_errors = 1
+      cams_with_err[str(i)] = "no ping"
+      cams_with_np[str(i)] = "no ping"
 
 # Check SD Streams
 stream_errors = 0
@@ -137,13 +155,26 @@ for i in range (1,7):
    if res == 0:
       errors.append("Cam " + str(cams_id) + "SD Stream not present. ")
       stream_errors = 1
+      cams_with_err[str(i)] = "no SD stream"
+
 # Check HD Streams
 for i in range (1,7):
    res = check_stream(str(cams_id), "HD")
    if res == 0:
       errors.append("Cam " + str(cams_id) + "HD Stream not present. ")
+      cams_with_err[str(i)] = "no HD stream"
       stream_errors = 1
 
+# check ffmpeg process
+for i in range (1,7):
+   hd_run = check_running(str(i), "HD", json_conf)
+   sd_run = check_running(str(i), "SD", json_conf)
+   if int(hd_run) == 0 or int(sd_run) == 0:
+      print("NO FFMPEG RUNNING FOR CAM:", str(i))
+      cams_with_err[str(i)] = "no FFMPEG Process Running " + str(i)
+      stream_errors = 1
+
+print("CAMS WITH ER:", cams_with_err)
 
 msg = "Error messages for " + obs_name + "<BR>\n"
 for error in derrs: 
@@ -155,9 +186,14 @@ for error in errors:
    print (error)
 
 if stream_errors == 1:
-   os.system("../python/ffmpeg_record.py stop 1")
-   time.sleep(3)
-   os.system("../python/ffmpeg_record.py start_all")
+   for bad_cam in cams_with_err:
+      print("BAD:", bad_cam, cams_with_err[bad_cam])
+      if bad_cam not in cams_with_np:
+         os.system("../python/ffmpeg_record.py stop " + bad_cam)
+         time.sleep(3)
+         os.system("../python/ffmpeg_record.py start_all")
+      else: 
+         print("This cam is down, no point in restarting.")
 
 if len(derrs) > 0 or len(errors) > 0:
    #sendmail('mike.hankey@gmail.com', 'mike.hankey@gmail.com', 'AS6 Alert', msg)
