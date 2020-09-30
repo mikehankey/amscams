66048c290c29e4b8fb8793ab30888d941ee3807a
Perlerin Vincent <vperlerin@gmail.com>
2020-09-30 10:21:27 +0200

    M

--- pythonv2/lib/Archive_Listing.py
+++ pythonv2/lib/Archive_Listing.py
@@ -821,7 +821,7 @@ def get_html_detections(res,all_days_details,clear_cache,version,video_prev):
       
       if(prev_date is None):
          res_html += '<div class="h2_holder d-flex justify-content-between mr-5 ml-5"><h2>'+cur_date.strftime("%Y/%m/%d")+" - %TOTAL%</h2></div>"
-         res_html += '<div class="gallery gal-resize row text-center text-lg-left '+version+' mb-5">'
+         res_html += '<div class="gallery gal-resize reg   text-center text-lg-left '+version+' mb-5">'
 
       elif(cur_date.month != prev_date.month or cur_date.day != prev_date.day or cur_date.year != prev_date.year):
          if(cur_count>1):
@@ -829,7 +829,7 @@ def get_html_detections(res,all_days_details,clear_cache,version,video_prev):
          else:
             res_html = res_html.replace('%TOTAL%',str(cur_count)+ ' detection only')
          res_html +=  '</div><div class="h2_holder d-flex justify-content-between "><h2>'+cur_date.strftime("%Y/%m/%d")+" - %TOTAL%</h2></div>"
-         res_html += '<div class="gallery gal-resize row text-center text-lg-left '+version+' mb-5">'
+         res_html += '<div class="gallery gal-resize reg text-center text-lg-left '+version+' mb-5">'
         
          cur_count = 0
 
