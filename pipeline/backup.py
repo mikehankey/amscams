ef7a7ac07c2df9182f8bf8f33cbefdbb71c5d761
ams <mike.hankey@gmail.com>
2020-10-19 15:19:08 +0000

    updates

--- pipeline/backup.py
+++ pipeline/backup.py
@@ -5,22 +5,24 @@ from lib.DEFAULTS import *
 
 from lib.PipeUtil import cfe
 
-BK_MNT_PT = "/mnt/backup/"
-BK_DEV = "/dev/sda1/"
-BK_UUID = ""
+# INDEFAULTS
+#BK_MNT_PT = "/mnt/backup/"
+#BK_DEV = "/dev/sda1/"
+#BK_UUID = ""
+#BK_DIR = ""
 
-if cfe(BK_MNT_PT + "/meteors/", 1) == 0:
+if cfe(BK_DIR + "/meteors/", 1) == 0:
    cmd = "sudo mount " + BK_DEV + " " + BK_MNT_PT
    print("DRIVE IS NOT MOUNTED!")
    print(cmd)
    exit()
 
-cmd = "/usr/bin/rsync -av /mnt/ams2/meteors " + BK_MNT_PT 
+cmd = "/usr/bin/rsync -av /mnt/ams2/meteors " + BK_DIR
 os.system(cmd)
-cmd = "/usr/bin/rsync -av /mnt/ams2/cal " + BK_MNT_PT 
+cmd = "/usr/bin/rsync -av /mnt/ams2/cal " + BK_DIR
 os.system(cmd)
-cmd = "/usr/bin/rsync -av /home/ams/amscams/conf " + BK_MNT_PT 
+cmd = "/usr/bin/rsync -av /home/ams/amscams/conf " + BK_DIR
 os.system(cmd)
-cmd = "/usr/bin/rsync -av /mnt/ams2/meteor_archive " + BK_MNT_PT 
+cmd = "/usr/bin/rsync -av /mnt/ams2/meteor_archive " + BK_DIR
 os.system(cmd)
 print(cmd)
