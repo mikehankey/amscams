7cabb539cda99f371e1343a628c2f8307de3b9f0
Your Name <you@example.com>
2020-12-02 15:22:49 +0000

    updates

--- pipeline/run-uwsgi.sh
+++ pipeline/run-uwsgi.sh
@@ -1,2 +1,2 @@
-uwsgi --http 192.168.1.4:5000 --wsgi-file flaskAdmin.py --callable app --processes 4 --threads 2 --stats 127.0.0.1:9191 --check-static /mnt/ams2/ --pidfile=/tmp/uwsgi.pid
+uwsgi --http 0.0.0.0:80 --wsgi-file flaskAdmin.py --callable app --processes 4 --threads 2 --stats 127.0.0.1:9191 --check-static /mnt/ams2/ --pidfile=/tmp/uwsgi.pid
 
