cbf34e7d2a82386adf5530106c2c4381952e53e0
Your Name <you@example.com>
2020-12-07 20:49:05 +0000

    updates

---
+++ install/purge-all-data.sh
@@ -0,0 +1,19 @@
+## ONLY RUN THIS DURING SETUP PHASE AS IT DELETES ALL DATA TREATING THINGS AS A NEW SYSTEM
+## DO NOT RUN IN PRODUCTION!
+#
+#
+#rm /mnt/ams2/SD/*.mp4
+#rm /mnt/ams2/HD/*.mp4
+#rm -rf /mnt/ams2/meteors/*
+#rm -rf /mnt/ams2/SD/proc2/daytime/*
+#rm -rf /mnt/ams2/SD/proc2/202*
+#rm -rf /mnt/ams2/meteor_archive/AMS104/*
+#rm -rf /mnt/ams2/CACHE/*
+#rm -rf /mnt/ams2/latest/*
+#
+#mkdir /mnt/ams2/meteor_archive/AMS104/CAL/
+#mkdir /mnt/ams2/meteor_archive/AMS104/DETECTS/
+#mkdir /mnt/ams2/meteor_archive/AMS104/METEOR/
+#mkdir /mnt/ams2/meteor_archive/AMS104/NOAA/
+#mkdir /mnt/ams2/meteor_archive/AMS104/STACKS/
+#mkdir /mnt/ams2/meteor_archive/AMS104/TL/
