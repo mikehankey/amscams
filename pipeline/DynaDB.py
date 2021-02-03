e4f574b090968787009f40cebac6fb05d621a7a5
AMS11 <mike.hankey@gmail.com>
2021-02-03 23:37:16 +0000

    updates

--- pipeline/DynaDB.py
+++ pipeline/DynaDB.py
@@ -607,5 +607,11 @@ if __name__ == "__main__":
       load_obs_month(dynamodb, station_id, sys.argv[2])
    if cmd == "ddd":
       do_dyna_day(dynamodb, sys.argv[2])
+   if cmd == "dddm":
+      wild = sys.argv[2]
+      files = glob.glob("/mnt/ams2/meteors/" + wild)
+      for file in sorted(files):
+         day = file.split("/")[-1]
+         do_dyna_day(dynamodb, day)
    if cmd == "umje":
       update_mj_events(dynamodb, sys.argv[2])
