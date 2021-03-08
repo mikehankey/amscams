002014e32a7dff096cc73b084a58aab010ccdab0
ams <mike.hankey@gmail.com>
2021-03-08 00:21:13 +0000

    upda

--- pipeline/lib/PipeWeather.py
+++ pipeline/lib/PipeWeather.py
@@ -1565,6 +1565,8 @@ def make_flat(cam,day,json_conf):
    mask_file = MASK_DIR + cam + "_mask.png"
    flat_file = MASK_DIR + cam + "_flat.png"
    latest_file = "/mnt/ams2/latest/" + cam + ".jpg"
+   cv2.imwrite(MASK_DIR + cam + "_mask.png", default)
+
    if cfe(mask_file) == 0:
       applied_file = mask_file.replace("mask", "applied")
       late = cv2.imread(latest_file)
