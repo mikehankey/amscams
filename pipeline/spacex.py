ddf88b92083f5116da906d17181b9cce1ef62770
ams <mike.hankey@gmail.com>
2021-03-14 14:48:27 +0000

    upda

--- pipeline/spacex.py
+++ pipeline/spacex.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
 import datetime
-from lib.PipeUtil import cfe, save_json_file
+from lib.PipeUtil import cfe, save_json_file, load_json_file
 import os
 import glob
 # script to archive footage from longer events like space-x rockets or satellites
 
-ams_id = "AMS15"
-# define cams that caught the event
-cams = ['010087', '010086']
+conf = load_json_file("spacex.json")
 
-event_start_time = '2021_03_14_10_09_01'
-event_end_time = '2021_03_14_10_11_01'
+title = conf['title']
+credits = conf['credits']
+ams_id = conf['ams_id']
+
+# define cams that caught the event
+cams = conf['cams']
+
+event_start_time = conf['event_start_time']
+event_end_time = conf['event_end_time']
 base_file = event_start_time[:-5]
 date = event_start_time[0:10]
 (year,month,day,hour,minute,second) = event_start_time.split("_" )
@@ -23,6 +28,8 @@ if cfe(out_dir,1) == 0:
 if cfe(cloud_dir,1) == 0:
    os.makedirs(cloud_dir)
 
+os.system("rm " + out_dir + "*.mp4")
+os.system("rm " + cloud_dir + "*.mp4")
 
 start_dt = datetime.datetime.strptime(event_start_time, "%Y_%m_%d_%H_%M_%S")
 end_dt = datetime.datetime.strptime(event_end_time, "%Y_%m_%d_%H_%M_%S")
@@ -30,20 +37,26 @@ end_dt = datetime.datetime.strptime(event_end_time, "%Y_%m_%d_%H_%M_%S")
 min_diff = int((end_dt - start_dt).total_seconds() / 60)
 all_sd = []
 all_hd = []
-for i in range(0, min_diff+1):
-   mn = int(minute) + i
-   wild = base_file + str(mn) + "*.mp4"
-   print("B:", wild)
-   sd_files = glob.glob(sd_dir + wild)
-   for ff in sd_files:
-      if "trim" not in ff:
-         all_sd.append(ff)
-   sd_files = glob.glob(hd_dir + wild)
-   for ff in sd_files:
-      if "trim" not in ff:
-         all_hd.append(ff)
 
-html = ""
+for cam in cams:
+   for i in range(0, min_diff+1):
+      mn = int(minute) + i
+      mns = "{:02d}".format(mn)
+      wild = base_file + str(mns) + "*" + cam + "*.mp4"
+      print("B:", wild)
+      sd_files = glob.glob(sd_dir + wild)
+      for ff in sd_files:
+         if "trim" not in ff:
+            all_sd.append(ff)
+      sd_files = glob.glob(hd_dir + wild)
+      for ff in sd_files:
+         if "trim" not in ff:
+            all_hd.append(ff)
+
+html = "<h1>" + title + "</h1>"
+html += "<p>Photo Credit: " + credits + "</p>"
+html += "<h2>SD Files</h2>"
+
 for vf in all_sd:
    fn = vf.split("/")[-1]
    out_file = ams_id + "_" + "SD_" + fn
@@ -56,6 +69,8 @@ for vf in all_sd:
       cmd = "cp " + vf + " " + cloud_dir + out_file
       print(cmd)
       os.system(cmd)
+
+html += "<h2>HD Files</h2>"
 
 for vf in all_hd:
    fn = vf.split("/")[-1]
