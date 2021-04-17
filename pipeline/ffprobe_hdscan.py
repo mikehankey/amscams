4aed6f3adec8b7f4ed3b6ad79b7c440ce2c4e020
Mike Hankey <mike.hankey@gmail.com>
2021-04-17 19:34:25 +0000

    upda

---
+++ pipeline/ffprobe_hdscan.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3 
+
+import glob
+import os
+
+files = sorted(glob.glob("/mnt/ams2/HD/2021_04_*.mp4"), reverse=True)
+c = 0
+for file in files:
+   if "trim" in file:
+      continue
+   cmd = "/usr/bin/ffprobe " + file + " 2>&1 |grep fps"
+   print(cmd)
+   os.system(cmd)
+   if c > 100:
+      exit()
+   c += 1
+
