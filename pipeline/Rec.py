59c5f248f75b0638164d076f020d4f7ab2119fff
Mike Hankey <mike.hankey@gmail.com>
2021-06-30 11:34:47 +0000

    upda

--- pipeline/Rec.py
+++ pipeline/Rec.py
@@ -13,10 +13,10 @@ if __name__ == "__main__":
       os.system("./Process.py purge_meteors")
       os.system("python3 ./Filter.py fm " + year + "_" + mon)
       R.fix_missing_cloud_files(year,mon)
-      R.reconcile_cloud_media(year, mon)
+      #R.reconcile_cloud_media(year, mon)
       R.update_cloud_index(year, mon)
       R.reconcile_media()
-      R.reconcile_cloud_media(year, mon)
+      #R.reconcile_cloud_media(year, mon)
       R.save_rec_data()
       R.fix_missing_cloud_files(year,mon)
 
