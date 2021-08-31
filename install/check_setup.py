98aece560543f52ec991a3a3dc815eb31e9d67e2
Mike Hankey <mike.hankey@gmail.com>
2021-08-31 23:51:15 +0000

    upda

---
+++ install/check_setup.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+import tkinter as tk
+import tkinter.messagebox as tkMessageBox
+root = tk.Tk()
+root.withdraw()
+tkMessageBox.showwarning('ALLSKY7 SETUP', 'We see this is the first time you have booted with ALLSKY7 software. We need to register your system and update your configuration.')
