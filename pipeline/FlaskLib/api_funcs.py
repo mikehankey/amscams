e309daa2683bc16339d772413791f6081a3692e8
Mike Hankey <mike.hankey@gmail.com>
2021-09-09 12:16:43 +0000

    upda

--- pipeline/FlaskLib/api_funcs.py
+++ pipeline/FlaskLib/api_funcs.py
@@ -219,7 +219,7 @@ def restore_meteor(jsid, data):
    trash_dir = "/mnt/ams2/trash/" + day + "/" 
    meteor_dir = "/mnt/ams2/meteors/" + day + "/" 
    mj = load_json_file("/mnt/ams2/trash/" + day + "/" + json_file)
-   mh['hc'] = 1
+   mj['hc'] = 1
    save_json_file("/mnt/ams2/trash/" + day + "/" + json_file)
    if "hd_trim" in mj:
       hd_root, hd_dir = fn_dir(mj['hd_trim'])
