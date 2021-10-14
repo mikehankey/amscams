ea0a9fb0095b531a8e39f5e99eabfea7de73c2ce
Mike Hankey <mike.hankey@gmail.com>
2021-10-14 05:55:04 -0400

    upda

--- pipeline/lib/PipeAutoCal.py
+++ pipeline/lib/PipeAutoCal.py
@@ -7344,6 +7344,12 @@ def reduce_fit(this_poly,field, cal_params, cal_params_file, fit_img, json_conf,
 
 def draw_star_image(img, cat_image_stars,cp=None) :
 
+   for star in cat_image_stars:
+      if len(star) == 9:
+         dcname,mag,ra,dec,new_cat_x,new_cat_y,six,siy,cat_dist = star
+      else:
+         dcname,mag,ra,dec,img_ra,img_dec,match_dist,org_x,org_y,img_az,img_el,new_cat_x,new_cat_y,six,siy,cat_dist,star_int = star
+      img[int(siy),int(six)] = [0,0,255]
 
 
    image = Image.fromarray(img)
@@ -7372,8 +7378,8 @@ def draw_star_image(img, cat_image_stars,cp=None) :
       print("DRAW:", dcname, match_dist)
 
       res_line = [(six,siy),(new_cat_x,new_cat_y)]
+      draw.rectangle((six-7, siy-7, six+7, siy+7), outline=white)
       draw.rectangle((new_cat_x-7, new_cat_y-7, new_cat_x + 7, new_cat_y + 7), outline=color)
-      draw.rectangle((six-6, siy-6, six+6, siy+6), outline=color)
       #draw.ellipse((six-5, siy-5, six+7, siy+7),  outline ="white")
       draw.line(res_line, fill=color, width = 0) 
       draw.text((new_cat_x, new_cat_y), str(dcname), font = font, fill="white")
