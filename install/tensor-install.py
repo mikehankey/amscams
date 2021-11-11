8bbd4e0c02e24932420a3ef990842b5460c2762a
Mike Hankey <mike.hankey@gmail.com>
2021-11-11 17:58:54 +0000

    upda

---
+++ install/tensor-install.py
@@ -0,0 +1,5 @@
+
+#wget https://archive.allskyt.tv/AMS9/tensorflow-2.4.4-cp36-cp36m-linux_x86_64.whl
+sudo pip3 install /home/ams/tensorflow-2.4.4-cp36-cp36m-linux_x86_64.whl
+sudo pip3 install tensorboard
+sudo pip3 install tb-nightly
