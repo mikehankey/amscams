201d785bd459ddcadbf124d2bd6dedb3e1d556ef
Mike Hankey <mike.hankey@gmail.com>
2021-11-22 17:37:33 -0500

    upda

--- pipeline/blendImage.py
+++ pipeline/blendImage.py
@@ -8,6 +8,8 @@ img2f = sys.argv[2]
 
 img1 = cv2.imread(img1f)
 img2 = cv2.imread(img2f)
+img1 = cv2.resize(img1, (1920,1080))
+img2 = cv2.resize(img2, (1920,1080))
 
 blend_image = cv2.addWeighted(img1, .5, img2, .5,0)
 blendf = img1f.replace(".jpg", "-blend.jpg")
