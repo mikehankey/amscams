290877dc3d58711a9c03cb3cea49f99165a9667c
Mike Hankey <mike.hankey@gmail.com>
2022-01-03 09:14:18 -0500

    update

--- pipeline/lib/PipeProcess.py
+++ pipeline/lib/PipeProcess.py
@@ -133,7 +133,6 @@ def run_jobs(json_conf):
    if running > 0:
       os.system("kill -9 $(ps aux | grep 'DynaDB' | awk '{print $2}')")
 
-   check_sync_cal_ai_db(json_conf)
    #print("EXIT")
    #exit()
 
@@ -172,6 +171,7 @@ def run_jobs(json_conf):
 
       gitpull(json_conf)
 
+   check_sync_cal_ai_db(json_conf)
    print("checking cloud dir") 
    amsid = json_conf['site']['ams_id']
    # check to make sure the cloud drive is setup and cal sync'd
