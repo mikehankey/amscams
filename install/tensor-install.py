7e72daff0ea7f3c745058f77e9a8e0e57b1e32ee
Mike Hankey <mike.hankey@gmail.com>
2022-01-04 15:05:24 -0500

    upda

--- install/tensor-install.py
+++ install/tensor-install.py
@@ -5,7 +5,7 @@ import sys
 if os.path.exists("tensorflow-2.4.4-cp36-cp36m-linux_x86_64.whl") is False:
    cmd = "wget https://archive.allsky.tv/AMS9/tensorflow-2.4.4-cp36-cp36m-linux_x86_64.whl"
    os.system(cmd)
-if os.path.exists("/usr/bin/python3.6") is False:
+if os.path.exists("/usr/bin/python3.6") is False and os.path.exists("/usr/local/bin/python3.6") is False:
    print("NO PYTHON3.6 INSTALL IT.")
    cmd = "sudo add-apt-repository -y ppa:deadsnakes/ppa"
    os.system(cmd)
