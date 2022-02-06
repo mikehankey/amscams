f5d96180f217076d80be2a389fd664508442ec9f
Mike Hankey <mike.hankey@gmail.com>
2022-02-06 14:35:16 +0000

    upda

--- pipeline/AIDay.py
+++ pipeline/AIDay.py
@@ -1,6 +1,6 @@
 from Classes.AIDB import AllSkyDB
-import datetime as dt
 from datetime import datetime 
+import datetime as dt
 import sys
 import os
 
@@ -9,29 +9,28 @@ yest = today - dt.timedelta(days=1)
 yest = yest.strftime("%Y_%m_%d")
 today = datetime.now().strftime("%Y_%m_%d")
 
-print("Init DB Starting")
+print("\rInit DB Starting",end="")
 AIDB = AllSkyDB()
-print("Init DB Complete")
-
-date = sys.argv[1]
+if len(sys.argv) > 1:
+   date = sys.argv[1]
+else:
+   date = today
+print("\rDate:" + date, end="")
 meteor_dir = "/mnt/ams2/meteors/"
-if date == "ALL":
+if date == "ALL" or date == "all":
    mdirs = os.listdir(meteor_dir)
    for md in sorted(mdirs,reverse=True):
       if os.path.isdir(meteor_dir + md) is True:
-         print("DO THE DAY!")
          date = md
          AIDB.load_all_meteors(date)
          AIDB.reconcile_db(date)
    AIDB.check_update_status(date)
 
 else:
-   print("Load")
    AIDB.load_all_meteors(date)
-
    AIDB.verify_media_day(date)
    AIDB.reconcile_db(date)
 
-   # print("Update")
-   # print("Reconcile")
+   AIDB.reducer(date)
    AIDB.check_update_status(date)
+   #print("\rDONE DAY:" + date + "                                   ",end="" )
