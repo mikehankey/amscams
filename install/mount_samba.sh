1ff332d7e34d9bd58f252c4d87b70c9396a3cbad
Mike Hankey <mike.hankey@gmail.com>
2022-02-19 05:45:55 -0500

    upda

--- install/mount_samba.sh
+++ install/mount_samba.sh
@@ -1,4 +1,5 @@
 # use this command to mount the allsky data drive to another PC with samba. 
+#xx
 # NOTE! Samba server must be installed and configured on the AllSky PC before this will work.  (it is not setup by default)
 # 
 sudo mount -t drvfs '\\192.168.1.4\Public' /mnt/ams2 -o noperm,user=ams,pass=xrp23q,uid=1001,gid=0,file_mode=0777,dir_mode=0777
