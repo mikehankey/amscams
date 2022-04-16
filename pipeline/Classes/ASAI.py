77d80de4ea268af719fb4565fd47d3eefe30eb53
Mike Hankey <mike.hankey@gmail.com>
2022-04-16 00:30:52 +0000

    upda

--- pipeline/Classes/ASAI.py
+++ pipeline/Classes/ASAI.py
@@ -1044,11 +1044,15 @@ class AllSkyAI():
       self.multi_class_labels = pickle.loads(open("models/" + mo_lib + ".labels", "rb").read())
 
 
-      self.model_weather_condition =load_model("models/weather_condition.h5")
-      self.model_weather_condition.compile(loss='categorical_crossentropy',
-         optimizer='rmsprop',
-         metrics=['accuracy'])
-      self.weather_condition_classes = pickle.loads(open("models/weather_condition.labels", "rb").read())
+      if os.path.exits("models/weather_condition.h5") is True:
+         self.model_weather_condition =load_model("models/weather_condition.h5")
+         self.model_weather_condition.compile(loss='categorical_crossentropy',
+            optimizer='rmsprop',
+            metrics=['accuracy'])
+         self.weather_condition_classes = pickle.loads(open("models/weather_condition.labels", "rb").read())
+      else:
+         self.model_weather_condition = None
+
 
 
 
