65d8687010c2daea0b8dbc0a64f501a1eed20e7e
Mike Hankey <mike.hankey@gmail.com>
2022-04-19 16:07:08 +0000

    upda

--- pipeline/Classes/AIDB.py
+++ pipeline/Classes/AIDB.py
@@ -156,14 +156,14 @@ class AllSkyDB():
       rows = self.cur.fetchall()
       total_meteor_obs = rows[0][0]
 
-      # get total number of METEORS in the system marked as meteor_yn = 1
+      # get total number of METEORS in the system marked as meteor_yn_conf >= 50 
       sql = "SELECT count(*) as ccc from meteors WHERE meteor_yn = 1"
       self.cur.execute(sql)
       rows = self.cur.fetchall()
       total_meteor_obs_yes = rows[0][0]
 
-      # get total number of METEORS in the system marked as meteor_yn = 0
-      sql = "SELECT count(*) as ccc from meteors WHERE meteor_yn = 0"
+      # get total number of METEORS in the system marked as meteor_yn_conf <= 50  
+      sql = "SELECT count(*) as ccc from meteors WHERE meteor_yn_conf <= 50"
       self.cur.execute(sql)
       rows = self.cur.fetchall()
       total_meteor_obs_no = rows[0][0]
@@ -206,7 +206,7 @@ class AllSkyDB():
       ai_meteor_learning_samples = rows[0][0]
 
       # get total NON METEOR samples BY DAY 
-      sql = "SELECT count(*) as ccc from ml_samples WHERE meteor_yn = 0"
+      sql = "SELECT count(*) as ccc from ml_samples WHERE meteor_yn_conf <= 50"
       self.cur.execute(sql)
       rows = self.cur.fetchall()
       ai_non_meteor_learning_samples = rows[0][0]
