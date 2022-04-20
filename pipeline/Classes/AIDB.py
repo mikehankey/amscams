0079dce723056ef82e11b8c5450e2628ea3672cf
Mike Hankey <mike.hankey@gmail.com>
2022-04-20 15:24:28 +0000

    upda

--- pipeline/Classes/AIDB.py
+++ pipeline/Classes/AIDB.py
@@ -53,6 +53,34 @@ class AllSkyDB():
       self.check_update_status()
       #self.reconcile_db()
 
+   def create_jobs_table(self):
+
+ 
+      # Connecting to sqlite
+      # connection object
+      connection_obj = sqlite3.connect('geek.db')
+
+      # cursor object
+      cursor_obj = connection_obj.cursor()
+ 
+      # Drop the GEEK table if already exists.
+      cursor_obj.execute("DROP TABLE IF EXISTS GEEK")
+ 
+      # Creating table
+      table = """ CREATE TABLE GEEK (
+            Email VARCHAR(255) NOT NULL,
+            First_Name CHAR(25) NOT NULL,
+            Last_Name CHAR(25),
+            Score INT
+      ); """
+ 
+      cursor_obj.execute(table)
+ 
+      print("Table is Ready")
+ 
+      # Close the connection
+      connection_obj.close()
+
    def report_day(self,date):
       sql = "SELECT root_fn, hd_vid, meteor_yn, meteor_yn_conf,fireball_yn_conf,mc_class, roi, ai_resp from meteors where sd_vid like ?"
       ivals = [date + "%"]
