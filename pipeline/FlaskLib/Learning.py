d1585760a852d902e2c86aaff8046aac62583ea0
Mike Hankey <mike.hankey@gmail.com>
2022-04-26 14:05:10 +0000

    upda

--- pipeline/FlaskLib/Learning.py
+++ pipeline/FlaskLib/Learning.py
@@ -2017,8 +2017,8 @@ def ai_rejects(station_id, options, json_conf):
                    mc_class_conf >  meteor_yn_conf
                AND mc_class_conf > fireball_yn_conf)
                AND mc_class_conf >= 98
-               AND meteor_yn <= 60
-               AND fireball_yn <= 60
+               AND meteor_yn_conf <= 60
+               AND fireball_yn_conf <= 60
                AND mc_class not like 'meteor%'
                AND mc_class not like 'orion%'
                AND root_fn not like '2019%'
