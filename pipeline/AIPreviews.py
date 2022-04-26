e295a14605f30141c18191b070b1eee2315e6390
Mike Hankey <mike.hankey@gmail.com>
2022-04-26 20:54:10 +0000

    upda

--- pipeline/AIPreviews.py
+++ pipeline/AIPreviews.py
@@ -1,7 +1,14 @@
-
+import cv2
 import os
 
 def preview_rois():
+   export_all_mets_dir = "/mnt/ams2/AI/DATASETS/NETWORK_PREV/ALL_METS_I64/"
+   export_all_non_mets_dir = "/mnt/ams2/AI/DATASETS/NETWORK_PREV/ALL_NON_METS_I64/"
+   export_all_unsure_dir = "/mnt/ams2/AI/DATASETS/NETWORK_PREV/ALL_UNSURE_I64/"
+   all_dirs = [export_all_mets_dir, export_all_non_mets_dir, export_all_unsure_dir]
+   for ad in all_dirs:
+      if os.path.exists(ad) is False:
+         os.makedirs(ad)
    cmd = "find /mnt/ams2/AI/DATASETS/NETWORK_PREV/ | grep METEOR | grep -v NON > /mnt/ams2/AI/DATASETS/NETWORK_PREV/meteors.txt "
    print(cmd)
    os.system(cmd)
@@ -14,11 +21,20 @@ def preview_rois():
 
    fp = open("/mnt/ams2/AI/DATASETS/NETWORK_PREV/meteors.txt")
    out = ""
-   for line in fp:
+   for line in fp: 
+      line = line.replace("\n", "")
       if "jpg" in line:
          img_url = line.replace("/mnt/ams2", "")
          img_url = img_url.replace("\n", "")
          out += "<img src=" + img_url + ">"
+         fn = img_url.split("/")[-1]
+         i64_file = export_all_mets_dir + fn
+         if os.path.exists(i64_file) is False:
+            img = cv2.imread(line)
+            img = cv2.resize(img, (64,64))
+            cv2.imwrite(i64_file, img)
+            print("WROTE ", i64_file)
+
    ofp = open("/mnt/ams2/AI/DATASETS/NETWORK_PREV/meteors.html", "w")
    ofp.write(out)
    ofp.close()
@@ -28,9 +44,21 @@ def preview_rois():
    out = ""
    for line in fp:
       if "jpg" in line:
+         line = line.replace("\n", "")
          img_url = line.replace("/mnt/ams2", "")
          img_url = img_url.replace("\n", "")
          out += "<img src=" + img_url + ">"
+         fn = img_url.split("/")[-1]
+         i64_file = export_all_non_mets_dir + fn
+         if os.path.exists(i64_file) is False:
+            print("LINE", line)
+            img = cv2.imread(line)
+            if img is not None:
+               img = cv2.resize(img, (64,64))
+               cv2.imwrite(i64_file, img)
+               print("WROTE ", i64_file)
+            else:
+               print("BAD IMG:", line)
    ofp = open("/mnt/ams2/AI/DATASETS/NETWORK_PREV/non_meteors.html", "w")
    ofp.write(out)
    ofp.close()
@@ -40,9 +68,17 @@ def preview_rois():
    out = ""
    for line in fp:
       if "jpg" in line:
+         line = line.replace("\n", "")
          img_url = line.replace("/mnt/ams2", "")
          img_url = img_url.replace("\n", "")
          out += "<img src=" + img_url + ">"
+         fn = img_url.split("/")[-1]
+         i64_file = export_all_unsure_dir + fn
+         if os.path.exists(i64_file) is False:
+            img = cv2.imread(line)
+            img = cv2.resize(img, (64,64))
+            cv2.imwrite(i64_file, img)
+            print("WROTE ", i64_file)
    ofp = open("/mnt/ams2/AI/DATASETS/NETWORK_PREV/unsure.html", "w")
    ofp.write(out)
    ofp.close()
