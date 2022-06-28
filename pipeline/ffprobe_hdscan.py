582ed3d861d1db42554485be937c4adb707db644
Mike Hankey <mike.hankey@gmail.com>
2022-06-28 20:22:23 +0000

    upd

--- pipeline/ffprobe_hdscan.py
+++ pipeline/ffprobe_hdscan.py
@@ -3,12 +3,33 @@
 import glob
 import os
 from lib.FFFuncs import ffprobe
+import sys
 
-files = sorted(glob.glob("/mnt/ams2/SD/proc2/2022_06_28/*.mp4"), reverse=True)
+cam_id = sys.argv[1]
+files = sorted(glob.glob("/mnt/ams2/SD/proc2/2022_06_28/*" + cam_id + "*.mp4"), reverse=True)
 c = 0
-for file in files[20:]:
+data = {}
+for file in sorted(files[20:]):
+
    if "trim" in file:
       continue
+   fn = file.split("/")[-1]
+   hour = fn[0:13]
+   if hour not in data:
+      data[hour] = {}
+      data[hour]['good'] = []
+      data[hour]['bad'] = []
 
    resp = ffprobe(file)
-   print(resp, file)
+   fps = resp[3] / 60
+   if fps >= 20:
+      data[hour]['good'].append(fn)
+      print("GOOD", fps, fn, resp)
+   else:   
+      data[hour]['bad'].append(fn)
+      print("BAD", fps, fn, resp)
+
+for hour in data:
+   total = len(data[hour]['good']) + len(data[hour]['bad'])
+   print(hour, len(data[hour]['good']), len(data[hour]['bad']), total)
+
