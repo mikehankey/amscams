b6bd2769370c710d7875fc60b7004860065ccde5
Mike Hankey <mike.hankey@gmail.com>
2022-08-07 07:32:30 -0400

    upd

--- pipeline/recal.py
+++ pipeline/recal.py
@@ -899,7 +899,7 @@ def cal_status_report(cam_id, con, cur, json_conf):
       mcp = None
 
    if mcp is None:
-      print("Can't update until the MCP is made!")
+      print("Can't update until the MCP is made!", cam_id)
       return()
 
    # get all paired stars by file 
