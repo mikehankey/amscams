b55ff121203fdd7018210b5684fe62cbab1ccc94
Mike Hankey <mike.hankey@gmail.com>
2022-08-18 08:24:58 -0400

    upda

---
+++ install/copy-models.py
@@ -0,0 +1,17 @@
+
+if os.path.exists("../pipeline/models") is False:
+   os.makedirs("../pipeline/models")
+
+cmd = "sudo chown -R ams:ams /home/ams/amscams/pipeline/models"
+os.system(cmd)
+
+if os.path.exists("/home/ams/amscams/pipeline/models/ASAI-v2.7z") is False:
+   cmd = "cp /mnt/archive.allsky.tv/AMS1/ML/ASAI-v2.7z ../pipeline/models/"
+   os.system(cmd)
+   cmd = "cd /home/ams/amscams/pipeline/models; 7z e /home/ams/amscams/pipeline/models/ASAI-v2.7z"
+   os.system(cmd)
+   cmd = "chown -R ams:ams /mnt/ams/amscams/models"
+   os.system(cmd)
+
+cmd = "pip3 install requests"
+os.system(cmd)
