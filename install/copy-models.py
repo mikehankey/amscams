85b036c9c3ffb042411ede1854ac38ded1e88c05
Mike Hankey <mike.hankey@gmail.com>
2022-08-18 15:16:27 -0400

    upda

--- install/copy-models.py
+++ install/copy-models.py
@@ -1,5 +1,8 @@
 import os
 
+cmd = "sudo chown -R ams:ams /home/ams/amscams/pipeline/models"
+os.system(cmd)
+
 if os.path.exists("../pipeline/models") is False:
    os.makedirs("../pipeline/models")
 
@@ -7,9 +10,10 @@ cmd = "sudo chown -R ams:ams /home/ams/amscams/pipeline/models"
 os.system(cmd)
 
 if os.path.exists("/home/ams/amscams/pipeline/models/ASAI-v2.7z") is False:
-   cmd = "sudo cp /mnt/archive.allsky.tv/AMS1/ML/ASAI-v2.7z ../pipeline/models/"
-   os.system(cmd)
    cmd = "sudo chown -R ams:ams /home/ams/amscams/pipeline/models"
+   os.system(cmd)
+
+   cmd = "cp /mnt/archive.allsky.tv/AMS1/ML/ASAI-v2.7z ../pipeline/models/"
    os.system(cmd)
    cmd = "cd /home/ams/amscams/pipeline/models; 7z e /home/ams/amscams/pipeline/models/ASAI-v2.7z"
    os.system(cmd)
