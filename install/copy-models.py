3a3d65f171a3ca902f88c0285375c08f50159a1c
Mike Hankey <mike.hankey@gmail.com>
2022-08-18 15:14:16 -0400

    up

--- install/copy-models.py
+++ install/copy-models.py
@@ -7,11 +7,11 @@ cmd = "sudo chown -R ams:ams /home/ams/amscams/pipeline/models"
 os.system(cmd)
 
 if os.path.exists("/home/ams/amscams/pipeline/models/ASAI-v2.7z") is False:
-   cmd = "cp /mnt/archive.allsky.tv/AMS1/ML/ASAI-v2.7z ../pipeline/models/"
+   cmd = "sudo cp /mnt/archive.allsky.tv/AMS1/ML/ASAI-v2.7z ../pipeline/models/"
+   os.system(cmd)
+   cmd = "sudo chown -R ams:ams /home/ams/amscams/pipeline/models"
    os.system(cmd)
    cmd = "cd /home/ams/amscams/pipeline/models; 7z e /home/ams/amscams/pipeline/models/ASAI-v2.7z"
-   os.system(cmd)
-   cmd = "chown -R ams:ams /home/ams/amscams/pipeline/models"
    os.system(cmd)
 
 cmd = "python3.6 -m pip install requests"
