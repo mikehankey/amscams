03ad979948a673f5e9a37e3fa9e4d8b5c11c4698
Mike Hankey <mike.hankey@gmail.com>
2022-08-25 13:55:50 -0400

    upda

---
+++ install/install-pip.sh
@@ -0,0 +1,8 @@
+#PIP3
+wget https://bootstrap.pypa.io/get-pip.py
+sudo python3 get-pip.py
+sudo mv /usr/local/bin/pip /usr/local/bin/pip3
+
+#PIP2
+wget https://bootstrap.pypa.io/pip/3.6/get-pip.py -O get-pipv2.py
+sudo python get-pipv2.py
