26ff8a74ad43d7b8c2e1a9f7baf2272b557c02ee
Mike Hankey <mike.hankey@gmail.com>
2022-08-30 14:18:59 +0000

    ua

--- pipeline/recal.py
+++ pipeline/recal.py
@@ -3396,7 +3396,7 @@ def apply_calib (cal_file, calfiles_data, json_conf, mcp, last_cal_params=None, 
          cal_params = default_cp
          if cal_params is None:
             print("NO CAL PARAMS!?", cam_id, cal_file)
-            return()
+            return(cal_params)
          cal_params= update_center_radec(cal_file,cal_params,json_conf )
 
       cal_image_file = cal_file.replace("-calparams.json", ".png")
