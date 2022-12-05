8889f1c37c4f4c72d2528a037ec94e4fd5beb16f
Mike Hankey <mike.hankey@gmail.com>
2022-12-05 10:59:01 -0500

    upda

--- pipeline/recal.py
+++ pipeline/recal.py
@@ -1927,7 +1927,8 @@ def start_calib(cal_fn, json_conf, calfiles_data, mcp=None):
       mask = cv2.imread(mask_file)
       mask = cv2.resize(mask, (1920,1080))
    else:
-      mask = np.zeros((1080,1920),dtype=np.uint8)
+      size = len(clean_cal_img.shape)
+      mask = np.zeros((1080,1920,size),dtype=np.uint8)
 
    print(clean_cal_img.shape, mask.shape)
    clean_cal_img = cv2.subtract(clean_cal_img, mask)
