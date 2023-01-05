9e1a9cfd84e80af74121420ef6cdfb2ed1cf1f38
Mike Hankey <mike.hankey@gmail.com>
2023-01-05 04:55:29 -0500

    upda

--- pipeline/lib/PipeAutoCal.py
+++ pipeline/lib/PipeAutoCal.py
@@ -8645,7 +8645,6 @@ def pair_stars(cal_params, cal_params_file, json_conf, cal_img=None, show = 0):
          #cv2.circle(temp_img,(int(new_x),int(new_y)), 7, (128,128,255), 1)
          used_key = str(ra) + "-" + str(dec)
 
-         print("MATCH DIST:", name,  match_dist)
          if match_dist >= 30 or used_key in used:
             bad = 1
             if used_key in used:
@@ -8654,7 +8653,6 @@ def pair_stars(cal_params, cal_params_file, json_conf, cal_img=None, show = 0):
                dd = "too far"
             #plt.plot(xs, ys)
             #plt.show()
-            print("*** NO PAIR", dd, match_dist)
          else:
             my_close_stars.append((name,mag,ra,dec,img_ra,img_dec,match_dist,new_x,new_y,img_az,img_el,new_cat_x,new_cat_y,six,siy,cat_dist,bp))
             total_match_dist = total_match_dist + match_dist
@@ -8662,11 +8660,8 @@ def pair_stars(cal_params, cal_params_file, json_conf, cal_img=None, show = 0):
             total_matches = total_matches + 1
             used[used_key] = 1
             found = 1
-            print("PAIR FOUND", match_dist)
       if found == 0:
          if len(close_stars) >= 1:
-            
-            print("*** NO MATCH STARS", dd, match_dist)
             no_match.append(close_stars[0])
       cc += 1
 
