69a958fcaf2a7f4f6ca762c96d456f500e844232
Mike Hankey <mike.hankey@gmail.com>
2023-03-21 12:00:10 -0400

    upda

--- install/python36-install.sh
+++ install/python36-install.sh
@@ -2,10 +2,12 @@ sudo apt install build-essential checkinstall
 sudo apt install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
 
 # Retrieve and unzip package
-wget https://www.python.org/ftp/python/3.6.0/Python-3.6.0.tar.xz
-tar xvf Python-3.6.0.tar.xz
+
+wget https://www.python.org/ftp/python/3.6.9/Python-3.6.9.tgz
+tar xvf Python-3.6.9.tgz
 
 # Make
-cd Python-3.6.0/
+cd Python-3.6.9/
 ./configure
+make -j 4
 sudo make altinstall
