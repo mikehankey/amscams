20d6a0e86f9b6f8dfb0846ec962134ede2e06f02
Mike Hankey <mike.hankey@gmail.com>
2023-05-18 07:31:02 -0400

    upda

--- install/ai-install.sh
+++ install/ai-install.sh
@@ -5,8 +5,11 @@ sudo python3.6 -m pip install redis
 sudo python3.6 -m pip install boto3
 
 mkdir /home/ams/amscams/pipeline/models
-cp /mnt/archive.allsky.tv/AMS1/ML/meteor_prev_yn.h5 /home/ams/amscams/pipeline/models
-cp /mnt/archive.allsky.tv/AMS1/ML/i64.7z /home/ams/amscams/pipeline/models
+#cp /mnt/archive.allsky.tv/AMS1/ML/meteor_prev_yn.h5 /home/ams/amscams/pipeline/models
+#cp /mnt/archive.allsky.tv/AMS1/ML/i64.7z /home/ams/amscams/pipeline/models
+cp /mnt/archive.allsky.tv/AMS1/ML/ASAI-v2.7z /home/ams/amscams/pipeline/models
+cp /mnt/archive.allsky.tv/AMS1/ML/star_yn.h5 /home/ams/amscams/pipeline/models
+
 cd /home/ams/amscams/pipeline/models; 7z e i64.7z
 
 
