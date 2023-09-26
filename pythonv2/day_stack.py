05ed584aa3dd3c567f13cb819515811af95417bb
mikehankey <mike.hankey@gmail.com>
2023-09-26 08:06:46 -0400

    upda

--- pythonv2/day_stack.py
+++ pythonv2/day_stack.py
@@ -226,7 +226,7 @@ elif len(sys.argv) > 1:
       last_stack = None
       for ff in sorted(files):
          if len(files) > 1000:
-            interval = 50
+            interval = 300 
          elif 100 <= len(files) <= 1000:
             interval = 25
          elif len(files) < 100:
