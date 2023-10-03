e636699f00dc29ed5dbee9919501cc96839df6b2
mikehankey <mike.hankey@gmail.com>
2023-10-03 17:17:49 -0400

    up

---
+++ pipeline/purge_masks.py
@@ -0,0 +1,21 @@
+# purge all meteor dirs that match mask rejects
+import os
+
+mdir = "/mnt/ams2/meteors/"
+dirs = os.listdir(mdir)
+for d in dirs:
+    if os.path.isdir(mdir + d) is True:
+        day = d.split("/")[-1]
+        if "20" == day[:2]:
+            cmd = "./Process.py reject_masks " + day
+            os.system(cmd)
+
+            # FAST SYNC
+            cmd = "python3 ./Meteor.py 10 " + day
+            os.system(cmd)
+            save_json_file("dyn.log", dyn_log)
+
+            # sync up dyna deletes with local deletes
+            cmd = "python3 Rec.py del_aws_day " + day
+            print(cmd)
+            os.system(cmd)
