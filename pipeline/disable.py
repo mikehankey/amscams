02fa334f5758b95f66580ca51c255d993c3656f2
Mike Hankey <mike.hankey@gmail.com>
2023-11-26 13:33:26 -0500

    up

--- pipeline/disable.py
+++ pipeline/disable.py
@@ -6,24 +6,24 @@ print("DIS:", sys.argv)
 go = sys.argv[1]
 
 if int(go) == 1:
-    cmd = "/usr/bin/python3 ca.py ca -c cam7 -p 7676"
+    cmd = "/usr/bin/python3 ca.py ca -c cam7 -p 7777"
     os.system(cmd)
     print(cmd)
-    cmd = "/usr/bin/python3 ca.py ca -c cam6 -p 7676"
+    cmd = "/usr/bin/python3 ca.py ca -c cam6 -p 7777"
     print(cmd)
     os.system(cmd)
-    cmd = "/usr/bin/python3 ca.py ca -c cam5 -p 7676"
+    cmd = "/usr/bin/python3 ca.py ca -c cam5 -p 7777"
     os.system(cmd)
     print(cmd)
-    cmd = "/usr/bin/python3 ca.py ca -c cam4 -p 7676"
+    cmd = "/usr/bin/python3 ca.py ca -c cam4 -p 7777"
     os.system(cmd)
     print(cmd)
-    cmd = "/usr/bin/python3 ca.py ca -c cam3 -p 7676"
+    cmd = "/usr/bin/python3 ca.py ca -c cam3 -p 7777"
     os.system(cmd)
     print(cmd)
-    cmd = "/usr/bin/python3 ca.py ca -c cam2 -p 7676"
+    cmd = "/usr/bin/python3 ca.py ca -c cam2 -p 7777"
     os.system(cmd)
     print(cmd)
-    cmd = "/usr/bin/python3 ca.py ca -c cam1 -p 7676"
+    cmd = "/usr/bin/python3 ca.py ca -c cam1 -p 7777"
     os.system(cmd)
     print(cmd)
