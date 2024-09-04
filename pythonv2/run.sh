14c5a234293dae7a7f62aad7799632c45d8ced2f
Mike Hankey <mike.hankey@gmail.com>
2024-09-04 14:36:36 -0400

    up

--- pythonv2/run.sh
+++ pythonv2/run.sh
@@ -1,9 +1,9 @@
-./IMX307.py test 192.168.76.71 15
-./IMX307.py test 192.168.76.72 15
-./IMX307.py test 192.168.76.73 15
-./IMX307.py test 192.168.76.74 15
-./IMX307.py test 192.168.76.75 15
-./IMX307.py test 192.168.76.76 15
-./IMX307.py test 192.168.76.77 15
+./IMX307.py test 192.168.76.71 25
+./IMX307.py test 192.168.76.72 25
+./IMX307.py test 192.168.76.73 25
+./IMX307.py test 192.168.76.74 25
+./IMX307.py test 192.168.76.75 25
+./IMX307.py test 192.168.76.76 25
+./IMX307.py test 192.168.76.77 25
 cd ../python; ./AS7-latest.py ; cd ../pythonv2;
 
