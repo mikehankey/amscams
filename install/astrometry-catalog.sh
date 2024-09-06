cd310bd0633f98cc7c8357090c13c3ba2f156e65
Mike Hankey <mike.hankey@gmail.com>
2024-09-06 13:46:26 -0400

    update

---
+++ install/astrometry-catalog.sh
@@ -0,0 +1,9 @@
+wget http://broiler.astrometry.net/~dstn/4100/index-4116.fits
+mv index-4116.fits /usr/share/astrometry/
+wget http://broiler.astrometry.net/~dstn/4100/index-4117.fits
+mv index-4117.fits /usr/share/astrometry/
+wget http://broiler.astrometry.net/~dstn/4100/index-4118.fits
+mv index-4118.fits /usr/share/astrometry/
+wget http://broiler.astrometry.net/~dstn/4100/index-4119.fits
+mv index-4119.fits /usr/share/astrometry/ 
+
